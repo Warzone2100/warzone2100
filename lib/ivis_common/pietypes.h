9d1d34d2a9aae049985d979640daa952a8f66ce3
Dennis Schridde <devurandom@gna.org>
2006-09-23 21:25:56 +0000

    Convert PIE types to SDL types.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@754 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/ivis_common/pietypes.h
+++ lib/ivis_common/pietypes.h
@@ -19,53 +19,44 @@
  */
 /***************************************************************************/
 
-#define PI 	  					3.141592654
-
-/***************************************************************************/
-/*
- *	Global Macros
- */
-/***************************************************************************/
+#define PI 3.141592654
 
 /***************************************************************************/
 /*
  *	Global Type Definitions
  */
 /***************************************************************************/
-typedef signed char int8;
-typedef signed short int16;
-typedef int int32;
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
+typedef Sint8 int8;
+typedef Sint16 int16;
+typedef Sint32 int32;
+typedef Uint8 uint8;
+typedef Uint16 uint16;
+typedef Uint32 uint32;
 
 //*************************************************************************
 //
 // Simple derived types
 //
 //*************************************************************************
-typedef struct {int left, top, right, bottom;} iClip;
-typedef uint8 iBitmap;
-typedef struct {uint8 r, g, b;} iColour;
-typedef int iBool;
-typedef struct {int32 x, y;} iPoint;
-typedef struct {unsigned int width, height; iBitmap *bmp;} iSprite;
+typedef struct { Sint32 left, top, right, bottom; } iClip;
+typedef Uint8 iBitmap;
+typedef struct { Uint8 r, g, b; } iColour;
+typedef BOOL iBool;
+typedef struct { Sint32 x, y; } iPoint;
+typedef struct { Uint32 width, height; iBitmap *bmp; } iSprite;
 typedef iColour iPalette[256];
-typedef struct {uint8 r, g, b, p;} iRGB8;
-typedef struct {uint16 r, g, b, p;} iRGB16;
-typedef struct {uint32 r, g, b, p;} iRGB32;
-typedef struct {int8 x, y;} iPoint8;
-typedef struct {int16 x, y;} iPoint16;
-typedef struct {int32 x, y;} iPoint32;
-
-
-	typedef struct {int32 x, y, z;} iVector;
-	typedef struct {double x, y, z;} iVectorf;
-	typedef struct {int xshift, width, height; iBitmap *bmp;
+typedef struct { Uint8 r, g, b, p; } iRGB8;
+typedef struct { Uint16 r, g, b, p; } iRGB16;
+typedef struct { Uint32 r, g, b, p; } iRGB32;
+typedef struct { Sint8 x, y; } iPoint8;
+typedef struct { Sint16 x, y; } iPoint16;
+typedef struct { Sint32 x, y; } iPoint32;
+typedef struct { Sint32 x, y, z; } iVector;
+typedef struct { double x, y, z; } iVectorf;
+typedef struct { Sint32 xshift, width, height; iBitmap *bmp;
 			iColour *pPal; iBool bColourKeyed; } iTexture;
-	typedef struct {int32 x, y, z, u, v; uint8 g;} iVertex;
-
-typedef struct {FRACT x,y,z;} PIEVECTORF;
-typedef struct {iVector p, r;} iView;
+typedef struct { Sint32 x, y, z, u, v; uint8 g; } iVertex;
+typedef struct { float x,y,z; } PIEVECTORF;
+typedef struct { iVector p, r; } iView;
 
 #endif // _pieTypes_h
