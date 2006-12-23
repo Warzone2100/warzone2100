62f1bcf118eb7890342cbfa2ab5fc01567101b73
Dennis Schridde <devurandom@gna.org>
2006-12-23 16:22:43 +0000

    - WIN32 Makefile.raw buildsystem improvements by Giel van Schijndel - Updated ChangeLog - Fixed "make install" for Automake < 1.10
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.0@907 4a71c877-e1ca-e34f-864e-861f7616d084

--- makerules/exe.mk
+++ makerules/exe.mk
@@ -1,6 +1,6 @@
 include $(MAKERULES)/common.mk
 
-DEPS=$(patsubst %.c,%.o, $(SRC:%.rc=%.o)) $(LIBS:%=../lib/lib%.a)
+DEPS=$(patsubst %.c,%.o, $(SRC:%.rc=%.o)) $(LIBS)
 
 all: $(EXE)$(EXEEXT)
 $(EXE)$(EXEEXT): $(DEPS) ; $(CC) $(CFLAGS) -o $(EXE)$(EXEEXT) $(DEPS) $(LDFLAGS)
