28fe84976ee66dace55fdbaf225567b40632a5ed
Gerard Krol <gerard_@gna.org>
2007-04-09 10:56:26 +0000

    Removed unused "cluster" functions from the sound code. I suppose clusters are groups of close units.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1399 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/sound/aud.h
+++ lib/sound/aud.h
@@ -41,13 +41,6 @@ void	audio_GetObjectPos( void *psObj, SDWORD *piX, SDWORD *piY,
 void	audio_GetStaticPos( SDWORD iWorldX, SDWORD iWorldY,
 								SDWORD *piX, SDWORD *piY, SDWORD *piZ );
 BOOL	audio_ObjectDead( void * psObj );
-#if 0
-BOOL	audio_GetClusterCentre( void *psGroup, SDWORD *piX, SDWORD *piY,
-								SDWORD *piZ );
-BOOL	audio_GetNewClusterObject( void **psClusterObj, SDWORD iClusterID );
-BOOL	audio_ClusterEmpty( void * psGroup );
-SDWORD	audio_GetClusterIDFromObj( void *psClusterObj );
-#endif
 void	audio_Get2DPlayerPos( SDWORD *piX, SDWORD *piY, SDWORD *piZ );
 void	audio_Get3DPlayerPos( SDWORD *piX, SDWORD *piY, SDWORD *piZ );
 void	audio_Get2DPlayerRotAboutVerticalAxis( SDWORD *piA );
