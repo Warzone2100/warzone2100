697ccff2b6174ff1bc37f5521b818640cbff5947
Dennis Schridde <devurandom@gna.org>
2007-04-10 17:57:51 +0000

    Require gettext 0.15 here, too.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1426 4a71c877-e1ca-e34f-864e-861f7616d084

--- autogen.sh
+++ autogen.sh
@@ -82,8 +82,8 @@ version_check ()
   fi
 }
 
-version_check 1 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 14 || DIE=1
-version_check 1 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 14 || DIE=1
+version_check 1 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 15 || DIE=1
+version_check 1 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 15 || DIE=1
 version_check 1 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 56 || DIE=1
 version_check 1 "automake" "ftp://ftp.gnu.org/pub/gnu/automake/" 1 8 || DIE=1
 version_check 1 "bison" "ftp://ftp.gnu.org/pub/gnu/bison/" 1 31 || DIE=1
