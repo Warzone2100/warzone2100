2b00b2e4c1f65b5e90c920097516a547546f2def
Giel van Schijndel <giel@wz2100.net>
2007-07-24 14:54:31 +0000

    * Add svn:keywords
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2175 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/editworld/src/macros.h
+++ tools/editworld/src/macros.h
@@ -16,6 +16,10 @@
 	You should have received a copy of the GNU General Public License
 	along with Warzone 2100; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+	$Revision$
+	$Id$
+	$HeadURL$
 */
 
 #ifndef _INCLUDED_MACROS_
