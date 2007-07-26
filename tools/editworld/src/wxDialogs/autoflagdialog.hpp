c355c4dcdf4549ffb2c40432980c8c46de792197
Giel van Schijndel <giel@wz2100.net>
2007-07-26 14:38:22 +0000

    * Add directory wxDialogs; I'll use this as a temporary storage for dialogs which have been ported from MFC to wxWidgets  * Add first converted dialog: AutoFlagDialog; to directory wxDialogs
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2206 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/editworld/src/wxDialogs/autoflagdialog.hpp
@@ -0,0 +1,113 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 1999-2004  Eidos Interactive
+	Copyright (C) 2005-2007  Warzone Resurrection Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+	$Revision$
+	$Id$
+	$HeadURL$
+*/
+
+#ifndef __INCLUDE_AUTOFLAGDIALOG_HPP__
+#define __INCLUDE_AUTOFLAGDIALOG_HPP__
+
+#include <wx/wxprec.h>
+
+//(*Headers(AutoFlagDialog)
+#include <wx/button.h>
+#include <wx/checkbox.h>
+#include <wx/dialog.h>
+#include <wx/radiobut.h>
+#include <wx/sizer.h>
+//*)
+
+/////////////////////////////////////////////////////////////////////////////
+// AutoFlagDialog dialog
+
+class AutoFlagDialog : public wxDialog
+{
+	public:
+		// Construction
+		AutoFlagDialog(wxWindow* parent,
+		               bool RandRotate = false,
+		               bool RandXFlip = false,
+		               bool RandYFlip = false,
+		               bool XFlip = false,
+		               bool YFlip = false,
+		               unsigned int Rotate = 0,
+		               bool IncRotate = false,
+		               bool ToggleXFlip = false,
+		               bool ToggleYFlip = false);
+
+		virtual ~AutoFlagDialog();
+
+		bool RandRotate() const;
+		bool RandXFlip() const;
+		bool RandYFlip() const;
+		bool XFlip() const;
+		bool YFlip() const;
+		unsigned int Rotate() const;
+		bool IncRotate() const;
+		bool ToggleXFlip() const;
+		bool ToggleYFlip() const;
+
+	public:
+		//(*Identifiers(AutoFlagDialog)
+		//*)
+
+	private:
+		//(*Handlers(AutoFlagDialog)
+		void OnOK(wxCommandEvent& event);
+		void OnCancel(wxCommandEvent& event);
+		void OnDegree(wxCommandEvent& event);
+		void OnXFlip(wxCommandEvent& event);
+		void OnYFlip(wxCommandEvent& event);
+		void OnRandRotate(wxCommandEvent& event);
+		void OnRandXFlip(wxCommandEvent& event);
+		void OnRandYFlip(wxCommandEvent& event);
+		//*)
+
+		//(*Declarations(AutoFlagDialog)
+		wxRadioButton*  Degree0_RadioButton;
+		wxRadioButton*  Degree90_RadioButton;
+		wxRadioButton*  Degree180_RadioButton;
+		wxRadioButton*  Degree270_RadioButton;
+		wxCheckBox*  XFlip_CheckBox;
+		wxCheckBox*  YFlip_CheckBox;
+		wxCheckBox*  RandRotate_CheckBox;
+		wxCheckBox*  RandXFlip_CheckBox;
+		wxCheckBox*  RandYFlip_CheckBox;
+		wxButton*  OK_Button;
+		wxButton*  Cancel_Button;
+		//*)
+
+	private:
+		DECLARE_EVENT_TABLE()
+
+	private:
+		bool _RandRotate;
+		bool _RandXFlip;
+		bool _RandYFlip;
+		bool _XFlip;
+		bool _YFlip;
+		unsigned int _Rotate;
+		bool _IncRotate;
+		bool _ToggleXFlip;
+		bool _ToggleYFlip;
+};
+
+#endif // __INCLUDE_AUTOFLAGDIALOG_HPP__
