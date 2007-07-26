80c4492003cb09dbbbc5dfbdc165770807770880
Giel van Schijndel <giel@wz2100.net>
2007-07-26 11:17:16 +0000

    * Clean out class declaration of CEdgeBrush  * Turn protected section of CEdgeBrush into a private section  * Mark all the functions that have their definition in the header as inline
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2204 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/editworld/src/brush.h
+++ tools/editworld/src/brush.h
@@ -62,67 +62,90 @@ struct EdgeTableEntry {
 	DWORD TFlags;
 };
 
-class CEdgeBrush : public CChnkIO {
+class CEdgeBrush : public CChnkIO
+{
+	public:
+		CEdgeBrush(CHeightMap* HeightMap,DWORD TextureWidth,DWORD TextureHeight,DWORD NumImages);
+		~CEdgeBrush();
 
-protected:
-	CHeightMap* m_HeightMap;
-	EdgeTableEntry m_EdgeTable[16];
-	int m_EdgeFlags[16];
-	Brush m_EdgeBrush[13];
-	BrushMapEntry m_BrushMap[29];
-	DWORD m_TextureWidth;
-	DWORD m_TextureHeight;
-	BOOL m_IsWater;
+		inline void SetTextureSize(DWORD TextureWidth,DWORD TextureHeight)
+		{
+			m_TextureWidth = TextureWidth;
+			m_TextureHeight = TextureHeight;
+		}
 
-	static DWORD m_NumImages;
-	static DWORD m_NumInstances;
-	static BOOL m_BrushSize;
+		void SetBrushTexture(DWORD TableIndex,int Tid);
+		void SetBrushTextureFromMap(void);
+		int GetBrushTexture(DWORD TableIndex);
+		BOOL Paint(DWORD XCoord,DWORD YCoord,BOOL SetHeight);
+		void DrawEdgeBrushIcon(CDIBDraw *DIBDraw,DDImage **Images,int x, int y);
+		void DrawEdgeBrush(CDIBDraw *DIBDraw,DDImage **Images,int ScrollX, int ScrollY);
+		void RotateTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
+		void XFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
+		void YFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
+		void TriFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
+		void SetBrushMap(int Tid,DWORD TFlags,int XCoord,int YCoord,int ScrollX,int ScrollY);
 
-	DWORD m_ID;
+		inline DWORD GetNumImages()
+		{
+			return m_NumImages;
+		}
 
-	DWORD ConvertTileIDToCode(DWORD Tid,DWORD TFlags);
-	void ConvertTileCodeToID(DWORD Code,DWORD &Tid,DWORD &TFlags);
+		inline void SetNumImages(DWORD NumImages)
+		{
+			m_NumImages = NumImages;
+		}
 
-public:
-	int m_HeightMode;
-	int m_Height;
-	int m_RandomRange;
+		void DrawButtons(CDC *dc);
+		int DoButtons(int XCoord,int YCoord);
+		int GetButtonState(int Index);
+		void SetButtonState(int Index,int State);
+		void SetBrushDirect(int Index,int HeightMode,int Height,int RandomRange,int Tid,int Flags);
 
-	CEdgeBrush(CHeightMap* HeightMap,DWORD TextureWidth,DWORD TextureHeight,DWORD NumImages);
-	~CEdgeBrush();
-	void SetTextureSize(DWORD TextureWidth,DWORD TextureHeight) { m_TextureWidth = TextureWidth;
-																m_TextureHeight = TextureHeight; }
-	void SetBrushTexture(DWORD TableIndex,int Tid);
-	void SetBrushTextureFromMap(void);
-	int GetBrushTexture(DWORD TableIndex);
-	BOOL Paint(DWORD XCoord,DWORD YCoord,BOOL SetHeight);
-	void DrawEdgeBrushIcon(CDIBDraw *DIBDraw,DDImage **Images,int x, int y);
-	void DrawEdgeBrush(CDIBDraw *DIBDraw,DDImage **Images,int ScrollX, int ScrollY);
-	void RotateTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
-	void XFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
-	void YFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
-	void TriFlipTile(int XCoord,int YCoord,int ScrollX,int ScrollY);
-	void SetBrushMap(int Tid,DWORD TFlags,int XCoord,int YCoord,int ScrollX,int ScrollY);
-	DWORD GetNumImages(void) { return m_NumImages; }
-	void SetNumImages(DWORD NumImages) { m_NumImages = NumImages; }
-	void DrawButtons(CDC *dc);
-	int DoButtons(int XCoord,int YCoord);
-	int GetButtonState(int Index);
-	void SetButtonState(int Index,int State);
-	void SetBrushDirect(int Index,int HeightMode,int Height,int RandomRange,int Tid,int Flags);
-	void SetLargeBrush(BOOL LargeBrush) { m_BrushSize = LargeBrush; }
-	BOOL GetLargeBrush(void) { return m_BrushSize; }
-	BOOL Write(FILE* Stream);
-	BOOL ReadV1(FILE* Stream);
-	BOOL ReadV2(FILE* Stream);
+		inline void SetLargeBrush(BOOL LargeBrush)
+		{
+			m_BrushSize = LargeBrush;
+		}
 
-	void FillMap(DWORD Selected,DWORD TextureID,DWORD Type,DWORD Flags);
-	Pixel PixelRead(int x,int y);
-	BOOL IsBrushEdge(Pixel ct);
-	BOOL PixelCompare(int x,int y,Pixel ov);
-	void PixelWrite(int x,int y,Pixel nv,DWORD Type,DWORD Flags);
-	void SeedFill(int x, int y, FRect *win, Pixel nv,DWORD Type,DWORD Flags);
+		inline BOOL GetLargeBrush()
+		{
+			return m_BrushSize;
+		}
 
+		BOOL Write(FILE* Stream);
+		BOOL ReadV1(FILE* Stream);
+		BOOL ReadV2(FILE* Stream);
+
+		void FillMap(DWORD Selected,DWORD TextureID,DWORD Type,DWORD Flags);
+		Pixel PixelRead(int x,int y);
+		BOOL IsBrushEdge(Pixel ct);
+		BOOL PixelCompare(int x,int y,Pixel ov);
+		void PixelWrite(int x,int y,Pixel nv,DWORD Type,DWORD Flags);
+		void SeedFill(int x, int y, FRect *win, Pixel nv,DWORD Type,DWORD Flags);
+
+	public:
+		int m_HeightMode;
+		int m_Height;
+		int m_RandomRange;
+
+	private:
+		CHeightMap* m_HeightMap;
+		EdgeTableEntry m_EdgeTable[16];
+		int m_EdgeFlags[16];
+		Brush m_EdgeBrush[13];
+		BrushMapEntry m_BrushMap[29];
+		DWORD m_TextureWidth;
+		DWORD m_TextureHeight;
+		BOOL m_IsWater;
+
+		static DWORD m_NumImages;
+		static DWORD m_NumInstances;
+		static BOOL m_BrushSize;
+
+		DWORD m_ID;
+
+		DWORD ConvertTileIDToCode(DWORD Tid,DWORD TFlags);
+		void ConvertTileCodeToID(DWORD Code,DWORD &Tid,DWORD &TFlags);
 };
 
 #endif
