d8b7837c3b5fb75117cfd2ec965a805f5345a346
Giel van Schijndel <giel@wz2100.net>
2007-07-26 22:28:55 +0000

    * Add another ported dialog: SaveSegmentDialog; to directory wxDialogs
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2214 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/editworld/src/wxDialogs/savesegmentdialog.cpp
@@ -0,0 +1,124 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 1999-2004  Eidos Interactive
+	Copyright (C) 2005-2007  Warzone Resurrection Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+	$Revision$
+	$Id$
+	$HeadURL$
+*/
+
+#include "savesegmentdialog.hpp"
+
+//(*InternalHeaders(SaveSegmentDialog)
+#include <wx/bitmap.h>
+#include <wx/font.h>
+#include <wx/fontenum.h>
+#include <wx/fontmap.h>
+#include <wx/image.h>
+#include <wx/intl.h>
+#include <wx/settings.h>
+#include <wx/xrc/xmlres.h>
+//*)
+
+#include "numtextval.hpp"
+
+//(*IdInit(SaveSegmentDialog)
+//*)
+
+BEGIN_EVENT_TABLE(SaveSegmentDialog,wxDialog)
+	//(*EventTable(SaveSegmentDialog)
+	//*)
+END_EVENT_TABLE()
+
+/////////////////////////////////////////////////////////////////////////////
+// SaveSegmentDialog dialog
+
+
+SaveSegmentDialog::SaveSegmentDialog(wxWindow* parent,
+                                     unsigned int StartX_,
+                                     unsigned int StartY_,
+                                     unsigned int Width_,
+                                     unsigned int Height_) :
+	_StartX(StartX_),
+	_StartY(StartY_),
+	_Width(Width_),
+	_Height(Height_)
+{
+	//(*Initialize(SaveSegmentDialog)
+	wxXmlResource::Get()->LoadObject(this,parent,_T("SaveSegmentDialog"),_T("wxDialog"));
+	StartX_TextCtrl = (wxTextCtrl*)FindWindow(XRCID("ID_TEXT_STARTX"));
+	StartY_TextCtrl = (wxTextCtrl*)FindWindow(XRCID("ID_TEXT_STARTY"));
+	Width_TextCtrl = (wxTextCtrl*)FindWindow(XRCID("ID_TEXT_WIDTH"));
+	Height_TextCtrl = (wxTextCtrl*)FindWindow(XRCID("ID_TEXT_HEIGHT"));
+	//*)
+
+	// Make sure all characters entered into these text controls
+	// are numeric and connected to our private member number variables
+	StartX_TextCtrl->SetValidator(wxNumericTextValidator(&_StartX));
+	StartY_TextCtrl->SetValidator(wxNumericTextValidator(&_StartY));
+	Width_TextCtrl->SetValidator(wxNumericTextValidator(&_Width));
+	Height_TextCtrl->SetValidator(wxNumericTextValidator(&_Height));
+
+	// Use default handlers for the OK and Cancel Button
+	Connect(XRCID("ID_OK"), wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&wxDialog::OnOK);
+	Connect(XRCID("ID_CANCEL"), wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&wxDialog::OnCancel);
+}
+
+SaveSegmentDialog::~SaveSegmentDialog()
+{
+}
+
+void SaveSegmentDialog::StartX(unsigned long StartX_)
+{
+	_StartX = StartX_;
+}
+
+void SaveSegmentDialog::StartY(unsigned long StartY_)
+{
+	_StartY = StartY_;
+}
+
+void SaveSegmentDialog::Width(unsigned long Width_)
+{
+	_Width = Width_;
+}
+
+void SaveSegmentDialog::Height(unsigned long Height_)
+{
+	_Height = Height_;
+}
+
+unsigned long SaveSegmentDialog::StartX() const
+{
+	return _StartX;
+}
+
+unsigned long SaveSegmentDialog::StartY() const
+{
+	return _StartY;
+}
+
+unsigned long SaveSegmentDialog::Width() const
+{
+	return _Width;
+}
+
+unsigned long SaveSegmentDialog::Height() const
+{
+	return _Height;
+}
