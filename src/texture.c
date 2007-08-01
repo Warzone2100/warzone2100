bb99887986e5622d27f4e660b7cbd9b0bc89c713
Per Inge Mathisen <per@wz2100.net>
2007-08-01 13:27:40 +0000

    Use GL_NEAREST_MIPMAP_LINEAR for mipmap filter to reduce texture seam artifacts.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2304 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/texture.c
+++ src/texture.c
@@ -76,7 +76,7 @@ static int newPage(const char *name, int level, int width, int height, int count
 	glTexImage2D(GL_TEXTURE_2D, level, wz_texture_compression, width, height, 0,
 	             GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
