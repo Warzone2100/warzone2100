7792f1b21122b358b18f27c5a519084f949e5034
Dennis Schridde <devurandom@gna.org>
2007-10-09 11:10:47 +0000

    - Be more explicit about what is missing during configure and only ask the user to install something if it is not obvious what is missing. - Check for pkg-config earlier, pkg-config 0.9 should be sufficient according to configure. - Add pkg-config m4 file to be able to check for pkg-config where it is not installed. - Fix typo in ChangeLog, add recent buildsystem changes.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2541 4a71c877-e1ca-e34f-864e-861f7616d084

--- autogen.sh
+++ autogen.sh
@@ -82,7 +82,7 @@ version_check ()
   fi
 }
 
-version_check 1 "pkg-config" "http://pkgconfig.freedesktop.org/" 0 15 || DIE=1
+version_check 1 "pkg-config" "http://pkgconfig.freedesktop.org/" 0 9 || DIE=1
 version_check 1 "xgettext" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 15 || DIE=1
 version_check 1 "msgfmt" "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 15 || DIE=1
 version_check 1 "autoconf" "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 56 || DIE=1
