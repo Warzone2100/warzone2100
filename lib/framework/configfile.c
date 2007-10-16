8151788f142c37254c46904fd1a561b7bbfc2b7a
Dennis Schridde <devurandom@gna.org>
2007-10-16 20:57:55 +0000

    Giel wants this to be commented...
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2555 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/configfile.c
+++ lib/framework/configfile.c
@@ -176,7 +176,7 @@ static BOOL registry_load( const char *filename )
 	char buffer[MAXLINESIZE];
 	char *bptr = NULL, *bufstart = NULL;
 	char key[32];
-	int l;
+	int l; // sscanf expects an int to recieve %n, not an unsigned int
 	UDWORD filesize;
 
 	debug(LOG_WZ, "Loading the registry from %s", filename);
