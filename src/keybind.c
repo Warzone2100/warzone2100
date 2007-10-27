Warzone2100/warzone2100

commit 6b4cba170023da2389d5fcaffc41df1eec58c85c

* Rename pLevelName to aLevelName (hungarian notation is a pain. Especially when it's wrong!)  * Get rid of several magic numbers  * Some const correctness  * Replace a huge amount of strcpy(), strcat(), strncpy() and strncat() calls with a strlcpy() and strlcat() calls respectively
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2635 4a71c877-e1ca-e34f-864e-861f7616d084

src/keybind.c
@@ -1736,8 +1736,8 @@ void kf_SendTextMessage(void)
 	if(bAllowOtherKeyPresses)									// just starting.
 	{
 		bAllowOtherKeyPresses = FALSE;
-		strcpy(sTextToSend,"");
-		strcpy(sCurrentConsoleText,"");							//for beacons
+		strlcpy(sTextToSend, "", sizeof(sTextToSend));
+		strlcpy(sCurrentConsoleText, "", sizeof(sTextToSend));							//for beacons
 		inputClearBuffer();
 	}
 
@@ -1752,7 +1752,7 @@ void kf_SendTextMessage(void)
 			bAllowOtherKeyPresses = TRUE;
 		 //	flushConsoleMessages();
 
-			strcpy(sCurrentConsoleText,"");		//reset beacon msg, since console is empty now
+			strlcpy(sCurrentConsoleText, "", sizeof(sCurrentConsoleText));		//reset beacon msg, since console is empty now
 
 			// don't send empty lines to other players
 			if(!strcmp(sTextToSend, ""))
@@ -1770,7 +1770,7 @@ void kf_SendTextMessage(void)
 			//console callback message
 			//--------------------------
 			ConsolePlayer = selectedPlayer;
-			strcpy(ConsoleMsg,sTextToSend);
+			strlcpy(ConsoleMsg, sTextToSend, sizeof(ConsoleMsg));
 			eventFireCallbackTrigger((TRIGGER_TYPE)CALL_CONSOLE);
 
 
@@ -1783,9 +1783,9 @@ void kf_SendTextMessage(void)
 				//show the message we sent on our local console as well (even in skirmish, to see console commands)
 				//sprintf(tmp,"%d",selectedPlayer);
 
-				sprintf(tmp,"%s",getPlayerName(selectedPlayer));
-				strcat(tmp," : ");											// seperator
-				strcat(tmp,sTextToSend);											// add message
+				strlcpy(tmp, getPlayerName(selectedPlayer), sizeof(tmp));
+				strlcat(tmp, " : ", sizeof(tmp));        // seperator
+				strlcat(tmp, sTextToSend, sizeof(tmp));  // add message
 				addConsoleMessage(tmp,DEFAULT_JUSTIFY);
 
 				//in skirmish send directly to AIs, for command and chat procesing
@@ -1809,20 +1809,23 @@ void kf_SendTextMessage(void)
 			if(sTextToSend[0] != '\0')							// cant delete nothing!
 			{
 				sTextToSend[strlen(sTextToSend)-1]= '\0';
-				strcpy(sCurrentConsoleText,sTextToSend);		//beacons
+				strlcpy(sCurrentConsoleText, sTextToSend, sizeof(sCurrentConsoleText));		//beacons
 			}
 		}
 		else if(ch == INPBUF_ESC)								//abort.
 		{
 			bAllowOtherKeyPresses = TRUE;
-			strcpy(sCurrentConsoleText,"");
+			strlcpy(sCurrentConsoleText, "", sizeof(sCurrentConsoleText));
 		 //	flushConsoleMessages();
 			return;
 		}
 		else							 						// display
 		{
-			sprintf(sTextToSend,"%s%c",sTextToSend,inputGetCharKey());
-			strcpy(sCurrentConsoleText,sTextToSend);
+			snprintf(sTextToSend, sizeof(sTextToSend), "%s%c", sTextToSend, inputGetCharKey());
+			// Guarantee to nul-terminate
+			sTextToSend[sizeof(sTextToSend) - 1] = '\0';
+
+			strlcpy(sCurrentConsoleText, sTextToSend, sizeof(sCurrentConsoleText));
 		}
 
 		ch = (char)inputGetKey();
@@ -1833,11 +1836,11 @@ void kf_SendTextMessage(void)
 	{
 		if(keyDown(KEY_LCTRL))
 		{
-			strcpy(ingame.phrases[0],sTextToSend );
+			strlcpy(ingame.phrases[0], sTextToSend, sizeof(ingame.phrases[0]));
 		}
 		else
 		{
-			strcpy(sTextToSend,ingame.phrases[0]);
+			strlcpy(sTextToSend, ingame.phrases[0], sizeof(sTextToSend));
 			bAllowOtherKeyPresses = TRUE;
 		 //	flushConsoleMessages();
 			sendTextMessage(sTextToSend,FALSE);
@@ -1848,11 +1851,11 @@ void kf_SendTextMessage(void)
 	{
 		if(keyDown(KEY_LCTRL))
 		{
-			strcpy(ingame.phrases[1],sTextToSend );
+			strlcpy(ingame.phrases[1], sTextToSend, sizeof(ingame.phrases[1]));
 		}
 		else
 		{
-			strcpy(sTextToSend,ingame.phrases[1]);
+			strlcpy(sTextToSend, ingame.phrases[1], sizeof(sTextToSend));
 			bAllowOtherKeyPresses = TRUE;
 		//	flushConsoleMessages();
 			sendTextMessage(sTextToSend,FALSE);
@@ -1863,11 +1866,11 @@ void kf_SendTextMessage(void)
 	{
 		if(keyDown(KEY_LCTRL))
 		{
-			strcpy(ingame.phrases[2],sTextToSend );
+			strlcpy(ingame.phrases[2], sTextToSend, sizeof(ingame.phrases[2]));
 		}
 		else
 		{
-			strcpy(sTextToSend,ingame.phrases[2]);
+			strlcpy(sTextToSend, ingame.phrases[2], sizeof(sTextToSend));
 			bAllowOtherKeyPresses = TRUE;
 		//	flushConsoleMessages();
 			sendTextMessage(sTextToSend,FALSE);
@@ -1878,11 +1881,11 @@ void kf_SendTextMessage(void)
 	{
 		if(keyDown(KEY_LCTRL))
 		{
-			strcpy(ingame.phrases[3],sTextToSend );
+			strlcpy(ingame.phrases[3], sTextToSend, sizeof(ingame.phrases[3]));
 		}
 		else
 		{
-			strcpy(sTextToSend,ingame.phrases[3]);
+			strlcpy(sTextToSend, ingame.phrases[3], sizeof(sTextToSend));
 			bAllowOtherKeyPresses = TRUE;
 		//	flushConsoleMessages();
 			sendTextMessage(sTextToSend,FALSE);
@@ -1893,11 +1896,11 @@ void kf_SendTextMessage(void)
 	{
 		if(keyDown(KEY_LCTRL))
 		{
-			strcpy(ingame.phrases[4],sTextToSend );
+			strlcpy(ingame.phrases[4], sTextToSend, sizeof(ingame.phrases[4]));
 		}
 		else
 		{
-			strcpy(sTextToSend,ingame.phrases[4]);
+			strlcpy(sTextToSend, ingame.phrases[4], sizeof(sTextToSend));
 			bAllowOtherKeyPresses = TRUE;
 		 //	flushConsoleMessages();
 			sendTextMessage(sTextToSend,FALSE);
@@ -2612,16 +2615,16 @@ void	kf_AddHelpBlip( void )
 	//if chat message is empty, just send player name
 	//if(!strcmp(sCurrentConsoleText, ""))
 	//{
-		sprintf(tempStr,"%s",getPlayerName(selectedPlayer));		//temporary solution
+		strlcpy(tempStr, getPlayerName(selectedPlayer), sizeof(tempStr));		//temporary solution
 	//}
 	//else
 	//{
-	//	strcpy(tempStr,sCurrentConsoleText);
+	//	strlcpy(tempStr, sCurrentConsoleText, sizeof(tempStr));
 	//}
 
 
 	/* add beacon for the sender */
-	strcpy(beaconMsg[selectedPlayer], tempStr);
+	strlcpy(beaconMsg[selectedPlayer], tempStr, sizeof(beaconMsg[selectedPlayer]));
 	sendBeaconToPlayer(worldX, worldY, selectedPlayer, selectedPlayer, beaconMsg[selectedPlayer]);
 
 	/* send beacon to other players */
@@ -2629,7 +2632,7 @@ void	kf_AddHelpBlip( void )
 	{
 		if(openchannels[i] && (i != selectedPlayer))
 		{
-			strcpy(beaconMsg[i], tempStr);
+			strlcpy(beaconMsg[i], tempStr, sizeof(beaconMsg[i]));
 			sendBeaconToPlayer(worldX, worldY, i, selectedPlayer, beaconMsg[i]);
 		}
 	}

lib/framework/debug.c
@@ -114,10 +114,8 @@ void debug_callback_win32debug( void ** data, const char * outputBuffer )
 	strncpy(tmpStr, outputBuffer, sizeof(tmpStr));
 	if (!strchr(tmpStr, '\n'))
 	{
-		strncat(tmpStr, "\n", sizeof(tmpStr));
+		strncat(tmpStr, "\n", sizeof(tmpStr) - strlen(tmpStr) - 1);
 	}
-	// Guarantee to nul-terminate
-	tmpStr[sizeof(tmpStr) - 1] = '\0';
 
 	OutputDebugStringA( tmpStr );
 }
@@ -276,8 +274,10 @@ void _debug( code_part part, const char *str, ... )
 	static unsigned int prev = 0;     /* total on last update */
 
 	va_start(ap, str);
-	vsnprintf( useInputBuffer1 ? inputBuffer[1] : inputBuffer[0], MAX_LEN_LOG_LINE, str, ap );
+	vsnprintf(inputBuffer[useInputBuffer1 ? 1 : 0], MAX_LEN_LOG_LINE, str, ap);
 	va_end(ap);
+	// Guarantee to nul-terminate
+	inputBuffer[useInputBuffer1 ? 1 : 0][MAX_LEN_LOG_LINE - 1] = '\0';
 
 	if ( strncmp( inputBuffer[0], inputBuffer[1], MAX_LEN_LOG_LINE - 1 ) == 0 ) {
 		// Received again the same line

lib/framework/exceptionhandler.c
@@ -47,9 +47,7 @@ static LONG WINAPI windowsExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
 	}
 
 	// Append the filename
-	strncat(miniDumpPath, "warzone2100.mdmp", sizeof(miniDumpPath));
-	// Guarantee to nul-terminate
-	miniDumpPath[sizeof(miniDumpPath) - 1] = '\0';
+	strncat(miniDumpPath, "warzone2100.mdmp", sizeof(miniDumpPath) - strlen(miniDumpPath) - 1);
 
 	/*
 	Alternative:

lib/framework/frameresource.c
@@ -489,9 +489,7 @@ BOOL resLoadFile(const char *pType, const char *pFile)
 	// Guarantee to nul-terminate
 	aFileName[sizeof(aFileName) - 1] = '\0';
 
-	strncat(aFileName, pFile, sizeof(aFileName));
-	// Guarantee to nul-terminate
-	aFileName[sizeof(aFileName) - 1] = '\0';
+	strncat(aFileName, pFile, sizeof(aFileName) - strlen(aFileName) - 1);
 
 	makeLocaleFile(aFileName);  // check for translated file
 

lib/framework/resource_parser.y
@@ -32,10 +32,10 @@ int res_lex (void);
 // directory printfs
 #define DEBUG_GROUP0
 
+#include "lib/framework/frame.h"
 #include <string.h>
 #include <stdlib.h>
 
-#include "lib/framework/frame.h"
 #include "lib/framework/frameresource.h"
 #include "lib/framework/resly.h"
 
@@ -88,13 +88,12 @@ dir_line:			DIRECTORY QTEXT_T		{
 												$2[0] == '/')
 											{
 												// the new dir is rooted
-												strcpy(aCurrResDir, $2);
+												strlcpy(aCurrResDir, $2, sizeof(aCurrResDir));
 											}
 											else
 											{
-												strcpy(aCurrResDir, aResDir);
-												strcpy(aCurrResDir + strlen(aResDir),
-													   $2);
+												strlcpy(aCurrResDir, aResDir, sizeof(aCurrResDir));
+												strlcat(aCurrResDir, $2, sizeof(aCurrResDir));
 											}
 											if (strlen($2) > 0)
 											{

lib/framework/tagfile.c
@@ -49,7 +49,8 @@ static char errbuf[ERR_BUF_SIZE];
 #define TF_ERROR(...) \
 do { \
 	tag_error = true; \
-	snprintf(errbuf, ERR_BUF_SIZE, __VA_ARGS__); \
+	snprintf(errbuf, sizeof(errbuf), __VA_ARGS__); \
+	errbuf[sizeof(errbuf) - 1] = '\0'; /* Guarantee to nul-terminate */ \
 	assert(false); \
 } while(0)
 
@@ -69,12 +70,12 @@ static void print_nested_groups(struct define *group)
 	if (parent != NULL)
 	{
 		snprintf(groupdesc, PRNG_LEN, "\nNested inside element %d", (int)parent->element);
-		strncat(errbuf, groupdesc, ERR_BUF_SIZE);
+		strncat(errbuf, groupdesc, sizeof(errbuf) - strlen(errbuf) - 1);
 		print_nested_groups(parent);
 	}
 	if (parent == NULL)
 	{
-		strncat(errbuf, "\n", ERR_BUF_SIZE);
+		strncat(errbuf, "\n", sizeof(errbuf) - strlen(errbuf) - 1);
 	}
 }
 

lib/ivis_common/imdload.c
@@ -737,9 +737,7 @@ iIMDShape *iV_ProcessIMD( const char **ppFileData, const char *FileDataEnd )
 			debug(LOG_ERROR, "iV_ProcessIMD %s: only png textures supported", pFileName);
 			return NULL;
 		}
-		strncat(texfile, ".png", sizeof(texfile));
-		// Guarantee to nul-terminate
-		texfile[sizeof(texfile) - 1] = '\0';
+		strncat(texfile, ".png", sizeof(texfile) - strlen(texfile) - 1);
 
 		pie_MakeTexPageName(texfile);
 

lib/ivis_opengl/textdraw.c
@@ -78,8 +78,7 @@ static inline void iV_printFontList()
 		char prBuffer[1024];
 		snprintf(prBuffer, sizeof(prBuffer), "Font #%d : %s ", font, (const char*)glcGetFontc(font, GLC_FAMILY));
 		prBuffer[sizeof(prBuffer) - 1] = 0;
-		strncat(prBuffer, glcGetFontFace(font), sizeof(prBuffer));
-		prBuffer[sizeof(prBuffer) - 1] = 0;
+		strncat(prBuffer, glcGetFontFace(font), sizeof(prBuffer) - strlen(prBuffer) - 1);
 		debug(LOG_NEVER, prBuffer);
 	}
 }
@@ -546,9 +545,7 @@ UDWORD iV_DrawFormattedText(const char* String, UDWORD x, UDWORD y, UDWORD Width
 			FWord[i] = 0;
 
 			// And add it to the output string.
-			strncat(FString, FWord, sizeof(FString));
-			// Guarantee to nul-terminate
-			FString[sizeof(FString) - 1] = '\0';
+			strncat(FString, FWord, sizeof(FString) - strlen(FString) - 1);
 		}
 
 

commit 86328025d2b524ad5d59626e6cdeb558feb7e842

* Fix a bug introduced in r2635 where we would only be able to enter one character as chat message    * This was the result from the fact that snprintf apparently deals differently than sprintf with output buffers that appear as part of the input as well
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2636 4a71c877-e1ca-e34f-864e-861f7616d084

src/keybind.c
@@ -1821,9 +1821,9 @@ void kf_SendTextMessage(void)
 		}
 		else							 						// display
 		{
-			snprintf(sTextToSend, sizeof(sTextToSend), "%s%c", sTextToSend, inputGetCharKey());
-			// Guarantee to nul-terminate
-			sTextToSend[sizeof(sTextToSend) - 1] = '\0';
+			const char input_char[2] = { inputGetCharKey(), '\0' };
+
+			strlcat(sTextToSend, input_char, sizeof(sTextToSend));
 
 			strlcpy(sCurrentConsoleText, sTextToSend, sizeof(sCurrentConsoleText));
 		}