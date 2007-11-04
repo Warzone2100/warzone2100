Warzone2100/warzone2100

commit 5bcda1934549351e6a7f54258eafdde5637431a9

Backport: Fix crash where rearming VTOL would try to set a dead target.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.0@2652 4a71c877-e1ca-e34f-864e-861f7616d084

src/order.c
@@ -1169,18 +1169,13 @@ if(!bMultiPlayer || myResponsibility(psDroid->player))
 		ASSERT( FALSE, "orderUpdateUnit: unknown order" );
 	}
 
-
 	// catch any vtol that is rearming but has finished his order
-	if ((psDroid->order == DORDER_NONE) &&
-		vtolRearming(psDroid))
+	if (psDroid->order == DORDER_NONE && vtolRearming(psDroid)
+	    && (psDroid->psActionTarget[0] == NULL || !psDroid->psActionTarget[0]->died))
 	{
-//		DBPRINTF(("VTOL %d: reseting to rearm order\n", psDroid->id));
 		psDroid->order = DORDER_REARM;
 		psDroid->psTarget = psDroid->psActionTarget;
 	}
-
-//	ASSERT( psDroid->x != 0 && psDroid->y != 0,
-//		"orderUpdateUnit: unit at (0,0)" );
 }
 
 

commit b6bc728c556823dd650468691faf62bb59d451c2

* Fix a compile error introduced in r2652 (2.0 branch)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.0@2718 4a71c877-e1ca-e34f-864e-861f7616d084

src/order.c
@@ -1171,7 +1171,7 @@ if(!bMultiPlayer || myResponsibility(psDroid->player))
 
 	// catch any vtol that is rearming but has finished his order
 	if (psDroid->order == DORDER_NONE && vtolRearming(psDroid)
-	    && (psDroid->psActionTarget[0] == NULL || !psDroid->psActionTarget[0]->died))
+	 && (psDroid->psActionTarget == NULL || !psDroid->psActionTarget->died))
 	{
 		psDroid->order = DORDER_REARM;
 		psDroid->psTarget = psDroid->psActionTarget;