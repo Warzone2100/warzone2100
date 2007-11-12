8566e972d575e658370998dfc3c114e0ccd9d52a
Giel van Schijndel <giel@wz2100.net>
2007-11-12 19:51:24 +0000

    * Add some SQL statements (SQLite likes them enough) to create tables in the form of the "original" data set  * Set mime-type of SQL query files to text/x-sql since it is more than just plain text
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@2811 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ data/stats-sql/tables/WeaponSubClass.sql
@@ -0,0 +1,3 @@
+CREATE TABLE WeaponSubClass (id integer primary key autoincrement,  -- Unique ID of the weapon subclass
+                             name text                              -- name of the weapon subclass
+);
