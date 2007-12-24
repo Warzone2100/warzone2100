811f62370c4312224ada4ed177b90f09b22a2896
Giel van Schijndel <giel@wz2100.net>
2007-12-24 00:40:28 +0000

    * Add another ported dialog: AboutDialog; to directory wxDialogs
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3148 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/editworld/src/wxDialogs/aboutdialog.hpp
@@ -0,0 +1,44 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2007  Giel van Schijndel
+	Copyright (C) 2007  Warzone Resurrection Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+	$Revision$
+	$Id$
+	$HeadURL$
+*/
+
+#ifndef __INCLUDED_ABOUTDIALOG_HPP__
+#define __INCLUDED_ABOUTDIALOG_HPP__
+
+#include <wx/wxprec.h>
+
+//(*Headers(AboutDialog)
+#include <wx/button.h>
+#include <wx/dialog.h>
+#include <wx/sizer.h>
+#include <wx/statbmp.h>
+#include <wx/stattext.h>
+//*)
+
+class AboutDialog: public wxDialog
+{
+	public:
+		AboutDialog(wxWindow* parent = NULL);
+};
+
+#endif // __INCLUDED_ABOUTDIALOG_HPP__
