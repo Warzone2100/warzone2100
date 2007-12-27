5c2ac3b1f2d8558212b302be165b4890a9b8811f
Giel van Schijndel <giel@wz2100.net>
2007-12-27 01:08:26 +0000

    * Add a "main.cpp" file to contain the main application start code
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3190 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/editworld/src/wxDialogs/main.cpp
@@ -0,0 +1,58 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2007  Giel van Schijndel
+	Copyright (C) 2007  Warzone Resurrection Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+	$Revision$
+	$Id$
+	$HeadURL$
+*/
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+# include <wx/app.h>
+# include <wx/xrc/xmlres.h>
+# include <wx/fs_arc.h>
+# include <wx/image.h>
+# include <wx/imagpng.h>
+#endif
+
+#include "wx/hyperlink.h"
+
+class EditWorldApp : public wxApp
+{
+    public:
+        virtual bool OnInit()
+        {
+            wxFileSystem::AddHandler(new wxArchiveFSHandler);
+            wxImage::AddHandler(new wxPNGHandler);
+            wxXmlResource::Get()->InitAllHandlers();
+            wxXmlResource::Get()->AddHandler(new wxHyperLink::XmlHandler);
+
+            // Load the resource file containing all the windows & dialogs
+            wxXmlResource::Get()->Load(_T("windows.xrs"));
+
+            bool wxsOK = true;
+
+            /// @todo construct our main window/frame here and show it
+
+            return wxsOK;
+        }
+};
+
+IMPLEMENT_APP(wxApp);
