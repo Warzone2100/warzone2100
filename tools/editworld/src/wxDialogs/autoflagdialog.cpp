45125b9fc0c1c38b227c1d7d8234e20730d78eca
Giel van Schijndel <giel@wz2100.net>
2007-12-27 15:46:33 +0000

    * Add all wxWidgets dialogs to the Code::Blocks project file and let wxSmith manage them
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3197 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/editworld/src/wxDialogs/autoflagdialog.cpp
+++ tools/editworld/src/wxDialogs/autoflagdialog.cpp
@@ -26,12 +26,14 @@
 
 //(*InternalHeaders(AutoFlagDialog)
 #include <wx/bitmap.h>
+#include <wx/button.h>
 #include <wx/font.h>
 #include <wx/fontenum.h>
 #include <wx/fontmap.h>
 #include <wx/image.h>
 #include <wx/intl.h>
 #include <wx/settings.h>
+#include <wx/string.h>
 #include <wx/xrc/xmlres.h>
 //*)
 
--- tools/editworld/src/wxDialogs/savesegmentdialog.cpp
+++ tools/editworld/src/wxDialogs/savesegmentdialog.cpp
@@ -26,12 +26,14 @@
 
 //(*InternalHeaders(SaveSegmentDialog)
 #include <wx/bitmap.h>
+#include <wx/button.h>
 #include <wx/font.h>
 #include <wx/fontenum.h>
 #include <wx/fontmap.h>
 #include <wx/image.h>
 #include <wx/intl.h>
 #include <wx/settings.h>
+#include <wx/string.h>
 #include <wx/xrc/xmlres.h>
 //*)
 
