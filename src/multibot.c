Warzone2100/warzone2100

commit ed2ac88c21fccb7634694dd2f2a0c306cbf70ef1

* Move the send/recv-DroidInfo functions over to the new net primitives API (patch #905 by myself)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3238 4a71c877-e1ca-e34f-864e-861f7616d084

src/multibot.c
@@ -674,93 +674,91 @@ BOOL recvGroupOrder(NETMSG *pMsg)
 	return TRUE;
 }
 
-
 // ////////////////////////////////////////////////////////////////////////////
 // Droid update information
-BOOL SendDroidInfo(DROID *psDroid, DROID_ORDER order, uint32_t x, uint32_t y, BASE_OBJECT *psObj)
+BOOL SendDroidInfo(const DROID* psDroid, DROID_ORDER order, uint32_t x, uint32_t y, const BASE_OBJECT* psObj)
 {
-	NETMSG m;
-	NET_ORDER_SUBTYPE subType = psObj ? NET_ORDER_SUBTYPE_OBJECT :
-			( (x == 0 && y == 0) ? NET_ORDER_SUBTYPE_SPECIAL : NET_ORDER_SUBTYPE_POSITION );
-
 	if (!myResponsibility(psDroid->player))
 	{
 		return TRUE;
 	}
 
-	NetAdd(m,0,psDroid->id);
-	NetAdd(m,4,order);
-
-	switch (subType)
+	NETbeginEncode(NET_DROIDINFO, NET_ALL_PLAYERS);
 	{
-		// If they are being ordered to `goto' an object
-		case NET_ORDER_SUBTYPE_OBJECT:
-			NetAdd(m,8, psObj->id);
-			NetAdd(m,12,psObj->type);
-			break;
-		// If the droids are being ordered to `goto' a specific position or have special orders
-		case NET_ORDER_SUBTYPE_POSITION:
-		case NET_ORDER_SUBTYPE_SPECIAL:
-			NetAdd(m,8,x);
-			NetAdd(m,12,y);
-			break;
+		uint32_t droidId = psDroid->id;
+		BOOL subType = (psObj) ? TRUE : FALSE;
+
+		// Send the droid's ID
+		NETuint32_t(&droidId);
+
+		// Send the droid's order
+		NETenum(&order);
+		NETbool(&subType);
+
+		if (subType)
+		{
+			uint32_t destId = psObj->id;
+			uint32_t destType = psObj->type;
+
+			NETuint32_t(&destId);
+			NETenum(&destType);
+		}
+		else
+		{
+			NETuint32_t(&x);
+			NETuint32_t(&y);
+		}
 	}
-
-	m.body[16] = subType;
-
-	m.size= (4*(sizeof(UDWORD))) + 1;
-	m.type = NET_DROIDINFO;
-	return NETbcast(&m,FALSE);
+	return NETend();
 }
-
-
-
 
 // ////////////////////////////////////////////////////////////////////////////
 // receive droid information form other players.
-BOOL recvDroidInfo(NETMSG *pMsg)
+BOOL recvDroidInfo()
 {
-	uint32_t x = 0, y = 0, id = 0, destid = 0;
-	DROID_ORDER order;
-	NET_ORDER_SUBTYPE subType = pMsg->body[16];
-	DROID *psDroid = NULL;
-	OBJECT_TYPE desttype = OBJ_DROID;
-
-	NetGet(pMsg,0,id); //droid's id
-	NetGet(pMsg,4,order); //droid's order
-
-	// If we could not find the droid, request it
-	if(!IdToDroid(id, ANYPLAYER, &psDroid))
+	NETbeginDecode();
 	{
-		sendRequestDroid(id);
-		return (FALSE);
-	}
+		uint32_t    droidId;
+		DROID*      psDroid;
+		DROID_ORDER order;
+		BOOL        subType;
 
-	// Now process the actual order
-	switch (subType)
-	{
-		// If they are being ordered to `goto' an object
-		case NET_ORDER_SUBTYPE_OBJECT:
-			NetGet(pMsg,8,destid);
-			NetGet(pMsg,12,desttype);
-			ProcessDroidOrder(psDroid, order, 0, 0, desttype, destid);
-			break;
-		// If the droids are being ordered to `goto' a specific position
-		case NET_ORDER_SUBTYPE_POSITION:
-			NetGet(pMsg,8,x);
-			NetGet(pMsg,12,y);
+		// Get the droid
+		NETuint32_t(&droidId);
+
+		if (!IdToDroid(droidId, ANYPLAYER, &psDroid))
+		{
+			sendRequestDroid(droidId);
+			return FALSE;
+		}
+
+		// Get the droid's order
+		NETenum(&order);
+		NETbool(&subType);
+
+		if (subType)
+		{
+			uint32_t destId, destType;
+
+			NETuint32_t(&destId);
+			NETenum(&destType);
+
+			ProcessDroidOrder(psDroid, order, 0, 0, destType, destId);
+		}
+		else
+		{
+			uint32_t x, y;
+
+			NETuint32_t(&x);
+			NETuint32_t(&y);
+
 			ProcessDroidOrder(psDroid, order, x, y, 0, 0);
-			break;
-		case NET_ORDER_SUBTYPE_SPECIAL:
-			turnOffMultiMsg(TRUE);
-			orderDroid(psDroid, order);
-			turnOffMultiMsg(FALSE);
-			break;
+		}
 	}
+	NETend();
 
 	return TRUE;
 }
-
 
 // ////////////////////////////////////////////////////////////////////////////
 // process droid order

src/multiplay.c
@@ -637,7 +637,7 @@ BOOL recvMessage(void)
 				recvDroid();
 				break;
 			case NET_DROIDINFO:					//droid update info
-				recvDroidInfo(&msg);
+				recvDroidInfo();
 				break;
 			case NET_DROIDDEST:					// droid destroy
 				recvDestroyDroid();

src/multiplay.h
@@ -244,7 +244,7 @@ extern BOOL sendLasSat			(UBYTE player, STRUCTURE *psStruct, BASE_OBJECT *psObj)
 extern BOOL SendDroid			(const DROID_TEMPLATE* pTemplate, uint32_t x, uint32_t y, uint8_t player, uint32_t id);
 extern BOOL SendDestroyDroid	(const DROID* psDroid);
 extern BOOL SendDemolishFinished(STRUCTURE *psS,DROID *psD);
-extern BOOL SendDroidInfo		(DROID *psDroid, DROID_ORDER order, UDWORD x, UDWORD y, BASE_OBJECT *psObj);
+extern BOOL SendDroidInfo		(const DROID* psDroid, DROID_ORDER order, uint32_t x, uint32_t y, const BASE_OBJECT* psObj);
 extern BOOL SendDroidMove		(DROID *psDroid, UDWORD x, UDWORD y,BOOL bFormation);
 extern BOOL SendGroupOrderSelected	(UBYTE player, UDWORD x, UDWORD y, BASE_OBJECT *psObj);
 extern BOOL SendCmdGroup		(DROID_GROUP *psGroup, UWORD x, UWORD y, BASE_OBJECT *psObj);

src/multirecv.h
@@ -27,7 +27,7 @@
  */
 
 extern BOOL recvDroid				(void);
-extern BOOL recvDroidInfo			(NETMSG *pMsg);
+extern BOOL recvDroidInfo			(void);
 extern BOOL recvDestroyDroid		(void);
 extern BOOL recvGroupOrder			(NETMSG *pMsg);
 extern BOOL recvDroidMove			(NETMSG *pMsg);

commit 2f50ee2495e9aa3500f8bd6fa6bbb93f5321e3cd

* Fix a bug introduced in r3238 as a result of an incomplete conversion of the send/recv-DroidInfo functions   * Make sure to now also handle "special" droid orders (whatever a "special" order may be...)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3253 4a71c877-e1ca-e34f-864e-861f7616d084

src/multibot.c
@@ -820,7 +820,19 @@ BOOL recvDroidInfo()
 			NETuint32_t(&x);
 			NETuint32_t(&y);
 
-			ProcessDroidOrder(psDroid, order, x, y, 0, 0);
+			// If both the X _and_ Y coordinate are zero we've been given a
+			// "special" order.
+			if (x == 0 && y == 0)
+			{
+				turnOffMultiMsg(TRUE);
+				orderDroid(psDroid, order);
+				turnOffMultiMsg(FALSE);
+			}
+			// Otherwise it is just a normal "goto location" order
+			else
+			{
+				ProcessDroidOrder(psDroid, order, x, y, 0, 0);
+			}
 		}
 	}
 	NETend();