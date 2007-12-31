dfdf738deba7ff48d3cd2541f18bd437c56bbf38
Freddie Witherden <evilguru@gna.org>
2007-12-31 23:02:22 +0000

    Remove some commented-out PSX specific raycasting code.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3287 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/raycast.c
+++ src/raycast.c
@@ -136,26 +136,6 @@ BOOL rayInitialise(void)
 }
 
 
-// playstation
-//void rayCast(UDWORD x, UDWORD y, UDWORD ray, UDWORD length, RAY_CALLBACK callback)
-//{
-//	static UDWORD Tx;
-//	static UDWORD Ty;
-//	static UDWORD Tray;
-//	static UDWORD Tlength;
-//	static RAY_CALLBACK Tcallback;
-//
-//	Tx = x;
-//	Ty = y;
-//	Tray = ray;
-//	Tlength = length;
-//	Tcallback = callback;
-//	// Stack in the DCache.
-//	SetSpDCache();
-//	rayC(Tx, Ty, Tray, Tlength, Tcallback);
-//	SetSpNormal();
-//}
-
 /* cast a ray from x,y (world coords) at angle ray (0-360)
  * The ray angle starts at zero along the positive y axis and
  * increases towards -ve X.
