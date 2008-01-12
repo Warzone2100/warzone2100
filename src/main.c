Warzone2100/warzone2100

commit f2468393595b2636054cc630c7d0f082ee908e88

patch #928 ported map send/rev to new netAPI by Buginator with style fixes by me
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3431 4a71c877-e1ca-e34f-864e-861f7616d084

src/main.c
@@ -860,6 +860,8 @@ int main(int argc, char *argv[])
 	/*** Initialize directory structure ***/
 	make_dir(ScreenDumpPath, "screendumps", NULL);
 	make_dir(SaveGamePath, "savegame", NULL);
+	make_dir(SaveGamePath, "maps", NULL);		//MUST have this to prevent crashes when getting map
+	make_dir(SaveGamePath, "music", NULL);
 	make_dir(MultiPlayersPath, "multiplay", NULL);
 	make_dir(MultiPlayersPath, "multiplay", "players");
 	make_dir(MultiForcesPath, "multiplay", "forces");

lib/netplay/netplay.c
@@ -1038,17 +1038,18 @@ BOOL NETsetupTCPIP(void ** addr, const char * machine)
 
 // ////////////////////////////////////////////////////////////////////////
 // File Transfer programs.
-// uses guaranteed messages to send files between clients.
 // send file. it returns % of file sent. when 100 it's complete. call until it returns 100.
-UBYTE NETsendFile(BOOL newFile, const char *fileName, UDWORD player)
+#define MAX_FILE_TRANSFER_PACKET 256
+UBYTE NETsendFile(BOOL newFile, char *fileName, UDWORD player)
 {
-	static PHYSFS_sint64	fileSize,currPos;
+	static int32_t  	fileSize,currPos;
 	static PHYSFS_file	*pFileHandle;
-	PHYSFS_sint64		bytesRead;
-	UBYTE		inBuff[2048];
-	NETMSG		msg;
+	int32_t  		bytesRead;
+	char			inBuff[MAX_FILE_TRANSFER_PACKET];
+	uint8_t			sendto = 0;
 
-	if(newFile)		//force it true for now -Q
+	memset(inBuff, 0x0, sizeof(inBuff));
+	if (newFile)
 	{
 		// open the file.
 		pFileHandle = PHYSFS_openRead(fileName);			// check file exists
@@ -1059,37 +1060,39 @@ UBYTE NETsendFile(BOOL newFile, const char *fileName, UDWORD player)
 		}
 		// get the file's size.
 		fileSize = 0;
-		currPos =0;
+		currPos = 0;
 		do
 		{
-			bytesRead = PHYSFS_read( pFileHandle, &inBuff, 1, sizeof(inBuff) );
+			bytesRead = PHYSFS_read(pFileHandle, inBuff, 1, MAX_FILE_TRANSFER_PACKET);
 			fileSize += bytesRead;
 		} while(bytesRead != 0);
 
-		PHYSFS_seek(pFileHandle, 0 );
+		PHYSFS_seek(pFileHandle, 0);
 	}
 	// read some bytes.
-	bytesRead = PHYSFS_read(pFileHandle, &inBuff, sizeof(inBuff), 1 );
+	bytesRead = PHYSFS_read(pFileHandle, inBuff,1, MAX_FILE_TRANSFER_PACKET);
 
-	// form a message
-	NetAdd(msg,0,fileSize);		// total bytes in this file.
-	NetAdd(msg,4,bytesRead);	// bytes in this packet
-	NetAdd(msg,8,currPos);		// start byte
-	msg.body[12]=strlen(fileName);
-	msg.size	= 13;
-	NetAddSt(msg,msg.size,fileName);
-	msg.size	+= strlen(fileName);
-	memcpy(&(msg.body[msg.size]),&inBuff,bytesRead);
-	msg.size	+= bytesRead;
-	msg.type	=  FILEMSG;
-	if(player==0)
-	{
-		NETbcast(&msg,TRUE);		// send it.
+	if (player == 0)
+	{	// FIXME: why would you send (map) file to everyone ??
+		// even if they already have it? multiplay.c 1529 & 1550 are both
+		// NETsendFile(TRUE,mapStr,0); & NETsendFile(FALSE,game.map,0);
+		// so we ALWAYS send it, it seems?
+		NETbeginEncode(FILEMSG, NET_ALL_PLAYERS);	// send it.
 	}
 	else
 	{
-		NETsend(&msg,player,TRUE);
+		sendto = (uint8_t) player;
+		NETbeginEncode(FILEMSG,sendto);
 	}
+
+	// form a message
+	NETint32_t(&fileSize);		// total bytes in this file.
+	NETint32_t(&bytesRead);	// bytes in this packet
+	NETint32_t(&currPos);		// start byte
+
+	NETstring(fileName, 256);	//256 = max filename size
+	NETbin(inBuff, bytesRead);
+	NETend();
 
 	currPos += bytesRead;		// update position!
 	if(currPos == fileSize)
@@ -1097,45 +1100,49 @@ UBYTE NETsendFile(BOOL newFile, const char *fileName, UDWORD player)
 		PHYSFS_close(pFileHandle);
 	}
 
-	return (currPos*100)/fileSize;
+	return (currPos * 100) / fileSize;
 }
 
 
 // recv file. it returns % of the file so far recvd.
-UBYTE NETrecvFile(NETMSG *pMsg)
+UBYTE NETrecvFile(void)
 {
-	UDWORD			pos, fileSize, currPos, bytesRead;
-	char			fileName[256];
-	unsigned int		len;
+	int32_t		fileSize, currPos, bytesRead;
+	char		fileName[256];
+	char		outBuff[MAX_FILE_TRANSFER_PACKET];
 	static PHYSFS_file	*pFileHandle;
 
+	memset(fileName, 0x0, sizeof(fileName));
+	memset(outBuff, 0x0, sizeof(outBuff));
+
 	//read incoming bytes.
-	NetGet(pMsg,0,fileSize);
-	NetGet(pMsg,4,bytesRead);
-	NetGet(pMsg,8,currPos);
+	NETbeginDecode();
+	NETint32_t(&fileSize);		// total bytes in this file.
+	NETint32_t(&bytesRead);		// bytes in this packet
+	NETint32_t(&currPos);		// start byte
 
 	// read filename
-	len = (unsigned int)(pMsg->body[12]);
-	memcpy(fileName,&(pMsg->body[13]),len);
-	fileName[len] = '\0';	// terminate string.
-	pos = 13+len;
+	NETstring(fileName, 256);	// Ugh. 256 = max array size
 	debug(LOG_NET, "NETrecvFile: Creating new file %s", fileName);
 
-	if(currPos == 0)	// first packet!
+	if (currPos == 0)	// first packet!
 	{
 		pFileHandle = PHYSFS_openWrite(fileName);	// create a new file.
 	}
 
-	//write packet to the file.
-	PHYSFS_write( pFileHandle, &(pMsg->body[pos]), bytesRead, 1 );
+	NETbin(outBuff, bytesRead);
+	NETend();
 
-	if(currPos+bytesRead == fileSize)	// last packet
+	//write packet to the file.
+	PHYSFS_write(pFileHandle, outBuff, bytesRead, 1);
+
+	if (currPos+bytesRead == fileSize)	// last packet
 	{
 		PHYSFS_close(pFileHandle);
 	}
 
-	//return the percent count.
-	return ((currPos+bytesRead)*100) / fileSize;
+	//return the percentage count
+	return ((currPos + bytesRead) * 100) / fileSize;
 }
 
 void NETregisterServer(int state)

lib/netplay/netplay.h
@@ -123,8 +123,8 @@ extern BOOL   NETsend(NETMSG *msg, UDWORD player, BOOL guarantee);// send to pla
 extern BOOL   NETbcast(NETMSG *msg,BOOL guarantee);		// broadcast to everyone, possibly guaranteed
 extern BOOL   NETrecv(NETMSG *msg);				// recv a message if possible
 
-extern UBYTE   NETsendFile(BOOL newFile, const char *fileName, UDWORD player);	// send file chunk.
-extern UBYTE   NETrecvFile(NETMSG *pMsg);			// recv file chunk
+extern UBYTE   NETsendFile(BOOL newFile, char *fileName, UDWORD player);	// send file chunk.
+extern UBYTE   NETrecvFile(void);			// recv file chunk
 
 extern BOOL NETclose	(void);					// close current game
 extern BOOL NETshutdown(void);				// leave the game in play.

lib/netplay/nettypes.c
@@ -331,7 +331,43 @@ BOOL NETstring(char *str, uint16_t maxlen)
 
 	return TRUE;
 }
+BOOL NETbin(char *str, uint16_t maxlen)
+{
+	/*
+	 * Strings sent over the network are prefixed with their length, sent as an
+	 * unsigned 16-bit integer.
+	 */
 
+	// Work out the length of the string if we are encoding
+	uint16_t len = (NETgetPacketDir() == PACKET_ENCODE) ? maxlen : 0;
+	char *store;
+
+	// Add/fetch the length from the packet
+	NETuint16_t(&len);
+
+	// Map store to the message buffer
+	store = (char *) &NetMsg.body[NetMsg.size];
+
+	// Make sure there is enough data/space left in the packet
+	if (len + NetMsg.size > MaxMsgSize || !NetMsg.status)
+	{
+		return NetMsg.status = FALSE;
+	}
+
+	if (NETgetPacketDir() == PACKET_ENCODE)
+	{
+		memcpy(store, str, len);
+	}
+	else if (NETgetPacketDir() == PACKET_DECODE)
+	{
+		memcpy(str, store, len);
+	}
+
+	// Increment the size of the message
+	NetMsg.size += sizeof(len) + len;
+
+	return TRUE;
+}
 BOOL NETVector3uw(Vector3uw* vp)
 {
 	return (NETuint16_t(&vp->x)

lib/netplay/nettypes.h
@@ -48,6 +48,7 @@ BOOL NETfloat(float* fp);
 BOOL NETbool(BOOL *bp);
 BOOL NETnull(void);
 BOOL NETstring(char *str, uint16_t maxlen);
+BOOL NETbin(char *str, uint16_t maxlen);
 
 #define NETenum(enumPtr) \
 { \

src/multiint.c
@@ -2016,10 +2016,10 @@ void frontendMultiMessages(void)
 		switch(msg.type)
 		{
 		case NET_REQUESTMAP:
-			recvMapFileRequested(&msg);
+			recvMapFileRequested();
 			break;
 		case FILEMSG:
-			recvMapFileData(&msg);
+			recvMapFileData();
 			break;
 
 

src/multiplay.c
@@ -1511,7 +1511,7 @@ BOOL recvDestroyFeature()
 
 // ////////////////////////////////////////////////////////////////////////////
 // Network File packet processor.
-BOOL recvMapFileRequested(NETMSG *pMsg)
+BOOL recvMapFileRequested()
 {
 	char mapStr[256],mapName[256],fixedname[256];
 
@@ -1528,8 +1528,7 @@ BOOL recvMapFileRequested(NETMSG *pMsg)
 		memset(mapName,0,256);
 		memset(fixedname,0,256);
 		bSendingMap = TRUE;
-		addConsoleMessage("SENDING MAP!",DEFAULT_JUSTIFY);
-		addConsoleMessage("FIX FOR LINUX!",DEFAULT_JUSTIFY);
+		addConsoleMessage("Map was requested: SENDING MAP!",DEFAULT_JUSTIFY);
 
 		strlcpy(mapName, game.map, sizeof(mapName));
 		// chop off the -T1
@@ -1578,11 +1577,11 @@ UBYTE sendMap(void)
 }
 
 // another player is broadcasting a map, recv a chunk
-BOOL recvMapFileData(NETMSG *pMsg)
+BOOL recvMapFileData()
 {
 	UBYTE done;
 
-	done =  NETrecvFile(pMsg);
+	done =  NETrecvFile();
 	if(done == 100)
 	{
 		addConsoleMessage("MAP DOWNLOADED!",DEFAULT_JUSTIFY);
@@ -1591,7 +1590,9 @@ BOOL recvMapFileData(NETMSG *pMsg)
 		// clear out the old level list.
 		levShutDown();
 		levInitialise();
-		if (!buildMapList()) {
+		rebuildSearchPath(mod_multiplay, TRUE);	// MUST rebuild search path for the new maps we just got!
+		if (!buildMapList())
+		{
 			return FALSE;
 		}
 	}

src/multirecv.h
@@ -62,8 +62,8 @@ extern BOOL recvScoreSubmission		(void);
 extern BOOL recvHappyVtol			(void);
 extern BOOL recvResearchStatus		(void);
 extern BOOL recvLasSat				(void);
-extern BOOL	recvMapFileData				(NETMSG *pMsg);
-extern BOOL	recvMapFileRequested		(NETMSG *pMsg);
+extern BOOL	recvMapFileData				(void);
+extern BOOL	recvMapFileRequested		(void);
 
 
 extern BOOL recvTextMessageAI		(void);		//AI multiplayer message

commit 437e22e8170ed6633c8dba216c99ff496588d43e

Fix savegame path problem introduced in r3431 and patch #928 which made us save savegames in the music directory(!).
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3439 4a71c877-e1ca-e34f-864e-861f7616d084

src/main.c
@@ -860,8 +860,8 @@ int main(int argc, char *argv[])
 	/*** Initialize directory structure ***/
 	make_dir(ScreenDumpPath, "screendumps", NULL);
 	make_dir(SaveGamePath, "savegame", NULL);
-	make_dir(SaveGamePath, "maps", NULL);		//MUST have this to prevent crashes when getting map
-	make_dir(SaveGamePath, "music", NULL);
+	PHYSFS_mkdir("maps");		// MUST have this to prevent crashes when getting map
+	PHYSFS_mkdir("music");
 	make_dir(MultiPlayersPath, "multiplay", NULL);
 	make_dir(MultiPlayersPath, "multiplay", "players");
 	make_dir(MultiForcesPath, "multiplay", "forces");