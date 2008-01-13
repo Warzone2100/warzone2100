b557a843cae23fcd2bb0ae14e92c1577441bb37e
Dennis Schridde <devurandom@gna.org>
2008-01-13 23:39:45 +0000

    - Make dump_pie_file work on parameters instead of globals - Use C89 tolower() for string conversion - Fix unsignedness warnings - Some variable renames - Move parse_args and associated globals into !WZ_3DS2PIE_GUI
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3460 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/3ds2pie/src/3ds2pie.c
+++ tools/3ds2pie/src/3ds2pie.c
@@ -4,13 +4,13 @@
  * All rights reserved.
  *
  * This program is  free  software;  you can redistribute it and/or modify it
- * under the terms of the  GNU Lesser General Public License  as published by 
- * the  Free Software Foundation;  either version 2.1 of the License,  or (at 
+ * under the terms of the  GNU Lesser General Public License  as published by
+ * the  Free Software Foundation;  either version 2.1 of the License,  or (at
  * your option) any later version.
  *
  * This  program  is  distributed in  the  hope that it will  be useful,  but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or  FITNESS FOR A  PARTICULAR PURPOSE.  See the  GNU Lesser General Public  
+ * or  FITNESS FOR A  PARTICULAR PURPOSE.  See the  GNU Lesser General Public
  * License for more details.
  *
  * You should  have received  a copy of the GNU Lesser General Public License
@@ -32,66 +32,18 @@
 
 // gcc -o 3ds2pie 3ds2pie.c -Wall -g -O0 `pkg-config --cflags --libs lib3ds` -Wshadow
 
-static char *filename = "";
-static char *output = "";
-static char *page = "";
-static bool switchYZ = true;
-static bool reversewinding = false;
-static bool inverseUV = false;
-static int baseTexFlags = 200;
-
-static void parse_args(int argc, char **argv)
-{
-	int i = 1;
-
-	while (argc >= 3 + i && argv[i][0] == '-')
-	{
-		if (argv[i][1] == 'y')
-		{
-			switchYZ = false; // exporting program used Y-axis as "up", like we do, don't switch
-		}
-		if (argv[i][1] == 'i')
-		{
-			inverseUV = true;
-		}
-		if (argv[i][1] == 't')
-		{
-			baseTexFlags = 2200;
-		}
-		if (argv[i][1] == 'r')
-		{
-			reversewinding = true;
-		}
-		i++;
-	}
-	if (argc < 3 + i)
-	{
-		fprintf(stderr, "Syntax: 3ds2m [-y|-r|-t] input_filename output_filename page_number\n");
-		fprintf(stderr, "  -y  Exporter uses Y-axis as \"up\", so do not switch Y and Z axis.\n");
-		fprintf(stderr, "  -r  Reverse winding of all polygons.\n");
-		fprintf(stderr, "  -t  Use two sided polygons (slower; unused in WRP).\n");
-		fprintf(stderr, "  -i  Invert the vertical texture coordinates (for 3DS MAX etc.).\n");
-		exit(1);
-	}
-	filename = argv[i++];
-	output = argv[i++];
-	page = argv[i++];
-}
-
 // Make a string lower case
-void resToLower(char *pStr)
+static void resToLower(char *pStr)
 {
 	while (*pStr != 0)
 	{
-		if (isupper(*pStr))
-		{
-			*pStr = (char)(*pStr - (char)('A' - 'a'));
-		}
+		*pStr = tolower(*pStr);
 		pStr += 1;
 	}
 }
 
-static void dump_pie_file(Lib3dsFile *f, FILE *o)
+
+void dump_pie_file(Lib3dsFile *f, FILE *o, const char *page, bool swapYZ, bool invertUV, bool reverseWinding, unsigned int baseTexFlags)
 {
 	Lib3dsMesh *m;
 	Lib3dsMaterial *material;
@@ -116,7 +68,7 @@ static void dump_pie_file(Lib3dsFile *f, FILE *o)
 	fprintf(o, "LEVELS %d\n", f->frames);
 	for (meshIdx = 0, m = f->meshes; m; m = m->next, meshIdx++)
 	{
-		int i;
+		unsigned int i;
 
 		if (meshIdx > 0)
 		{
@@ -132,7 +84,7 @@ static void dump_pie_file(Lib3dsFile *f, FILE *o)
 
 			lib3ds_vector_copy(pos, m->pointL[i].pos);
 
-			if (switchYZ)
+			if (swapYZ)
 			{
 				fprintf(o, "\t%d %d %d\n", (int)pos[0], (int)pos[2], (int)pos[1]);
 			}
@@ -148,7 +100,7 @@ static void dump_pie_file(Lib3dsFile *f, FILE *o)
 			Lib3dsFace *face = &m->faceL[i];
 			int texel[3][2];
 
-			if (!inverseUV)
+			if (!invertUV)
 			{
 				texel[0][0] = m->texelL[face->points[0]][0] * 256.0f;
 				texel[0][1] = m->texelL[face->points[0]][1] * 256.0f;
@@ -167,7 +119,7 @@ static void dump_pie_file(Lib3dsFile *f, FILE *o)
 				texel[2][1] = (1.0f - m->texelL[face->points[2]][1]) * 256.0f;
 			}
 
-			if (reversewinding)
+			if (reverseWinding)
 			{
 				fprintf(o, "\t%d 3 %d %d %d", baseTexFlags, (int)face->points[2], (int)face->points[1], (int)face->points[0]);
 				fprintf(o, " %d %d %d %d %d %d\n", texel[2][0], texel[2][1], texel[1][0], texel[1][1], texel[0][0], texel[0][1]);
@@ -183,25 +135,71 @@ static void dump_pie_file(Lib3dsFile *f, FILE *o)
 
 
 #if !defined(WZ_3DS2PIE_GUI)
+static char *input_file = "";
+static char *output_file = "";
+static char *page = "";
+static bool swapYZ = true;
+static bool reverseWinding = false;
+static bool invertUV = false;
+static unsigned int baseTexFlags = 200;
+
+static void parse_args(int argc, char **argv)
+{
+	unsigned int i = 1;
+
+	for (i = 1; argc >= 3 + i && argv[i][0] == '-'; i++)
+	{
+		if (argv[i][1] == 'y')
+		{
+			swapYZ = false; // exporting program used Y-axis as "up", like we do, don't switch
+		}
+		if (argv[i][1] == 'i')
+		{
+			invertUV = true;
+		}
+		if (argv[i][1] == 't')
+		{
+			baseTexFlags = 2200;
+		}
+		if (argv[i][1] == 'r')
+		{
+			reverseWinding = true;
+		}
+	}
+	if (argc < 3 + i)
+	{
+		fprintf(stderr, "Syntax: 3ds2m [-y|-r|-t] input_filename output_filename page_number\n");
+		fprintf(stderr, "  -y  Exporter uses Y-axis as \"up\", so do not switch Y and Z axis.\n");
+		fprintf(stderr, "  -r  Reverse winding of all polygons.\n");
+		fprintf(stderr, "  -t  Use two sided polygons (slower; unused in WRP).\n");
+		fprintf(stderr, "  -i  Invert the vertical texture coordinates (for 3DS MAX etc.).\n");
+		exit(1);
+	}
+	input_file = argv[i++];
+	output_file = argv[i++];
+	page = argv[i++];
+}
+
+
 int main(int argc, char **argv)
 {
 	Lib3dsFile *f = NULL;
-	FILE *o;
+	FILE *o = NULL;
 
 	parse_args(argc, argv);
-	f = lib3ds_file_load(filename);
+	f = lib3ds_file_load(input_file);
 	if (!f)
 	{
-		fprintf(stderr, "***ERROR***\nLoading file %s failed\n", filename);
+		fprintf(stderr, "***ERROR***\nLoading file %s failed\n", input_file);
 		exit(1);
 	}
-	o = fopen(output, "w+");
+	o = fopen(output_file, "w+");
 	if (!o)
 	{
-		fprintf(stderr, "***ERROR***\nCan't open %s for writing\n", output);
+		fprintf(stderr, "***ERROR***\nCan't open %s for writing\n", output_file);
 		exit(1);
 	}
-	dump_pie_file(f, o);
+	dump_pie_file(f, o, page, swapYZ, invertUV, reverseWinding, baseTexFlags);
 	fclose(o);
 
 	lib3ds_file_free(f);
