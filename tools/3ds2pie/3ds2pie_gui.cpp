356ed35cbbf72b0c23c50ff410b0cf5a6089868c
Dennis Schridde <devurandom@gna.org>
2008-01-14 01:19:55 +0000

    Add Qt4 GUI. It uses 3ds2pie.c:dump_pie_file() and only has very basic error checking right now. - To get the (currently only german) translation, run "lrelease 3ds2pie_gui.pro". - When translating with (Qt Linguist), make sure to use the preview function to check whether your strings fit into the UI.   If they don't, try to use a shorter translation, otherwise adapt the UI.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3464 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/3ds2pie/3ds2pie_gui.cpp
@@ -0,0 +1,108 @@
+/*
+	Copyright (C) 2008 by Dennis Schridde
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this program. If not, see
+	<http://www.gnu.org/licenses/>.
+*/
+#include "3ds2pie_gui.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+
+// For dump_pie_file
+#include <lib3ds/file.h>
+
+
+extern "C" void dump_pie_file(Lib3dsFile *f, FILE *o, const char *page, bool swapYZ, bool inverseUV, bool reverseWinding, int baseTexFlags);
+
+
+Gui3ds2pie::Gui3ds2pie( QWidget *parent )
+		: QDialog(parent, Qt::WindowTitleHint), Ui_Gui3ds2pie()
+{
+	setupUi(this);
+
+	connect(inputFile_browse, SIGNAL(clicked()), this, SLOT(browseInputFile()));
+	connect(outputFile_browse, SIGNAL(clicked()), this, SLOT(browseOutputFile()));
+}
+
+
+Gui3ds2pie::~Gui3ds2pie()
+{
+}
+
+
+void Gui3ds2pie::browseInputFile()
+{
+	QString path = QFileDialog::getOpenFileName(this, tr("Choose input file"), QString::null, QString::null);
+	inputFile_edit->setText(path);
+}
+
+
+void Gui3ds2pie::browseOutputFile()
+{
+	QString path = QFileDialog::getSaveFileName(this, tr("Choose output file"), QString::null, QString::null);
+	outputFile_edit->setText(path);
+}
+
+
+void Gui3ds2pie::accept()
+{
+	QString inputFile = inputFile_edit->text();
+	QString outputFile = outputFile_edit->text();
+	QString texpageNr = texpageNr_edit->text();
+	unsigned int baseTexFlags = (twoSidedPolys->isChecked() ? 2200 : 200 );
+
+	if (inputFile.isEmpty())
+	{
+		QMessageBox::critical(this, tr("Error"), tr("Please specify an input file"));
+		return;
+	}
+	if (outputFile.isEmpty())
+	{
+		QMessageBox::critical(this, tr("Error"), tr("Please specify an output file"));
+		return;
+	}
+	if (texpageNr.isEmpty())
+	{
+		QMessageBox::critical(this, tr("Error"), tr("Please specify a texture page number"));
+		return;
+	}
+
+	Lib3dsFile *f = lib3ds_file_load(inputFile.toAscii().data());
+	if (!f)
+	{
+		QMessageBox::critical(this, tr("Error"), tr("Loading file %1 failed").arg(inputFile));
+		QDialog::reject();
+		return;
+	}
+
+	FILE *o = fopen(outputFile.toAscii().data(), "w+");
+	if (!o)
+	{
+		lib3ds_file_free(f);
+
+		QMessageBox::critical(this, tr("Error"), tr("Can't open %1 for writing").arg(outputFile));
+		QDialog::reject();
+		return;
+	}
+
+	dump_pie_file(f, o, texpageNr.toAscii().data(), swapYZ->isChecked(), invertUV->isChecked(), reverseWinding->isChecked(), baseTexFlags);
+
+	fclose(o);
+	lib3ds_file_free(f);
+
+	QDialog::accept();
+}
+
+
---
+++ tools/3ds2pie/3ds2pie_gui_de.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<context>
+    <name>Gui3ds2pie</name>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="47"/>
+        <source>Choose input file</source>
+        <translation>Wählen Sie eine Eingabedatei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="54"/>
+        <source>Choose output file</source>
+        <translation>Wählen Sie eine Ausgabedatei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="95"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="68"/>
+        <source>Please specify an input file</source>
+        <translation>Bitte geben Sie eine Eingabedatei an</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="73"/>
+        <source>Please specify an output file</source>
+        <translation>Bitte geben Sie eine Ausgabedatei an</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="78"/>
+        <source>Please specify a texture page number</source>
+        <translation>Bitte geben Sie eine Texture Page Nummer an</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="85"/>
+        <source>Loading file %1 failed</source>
+        <translation>Laden von Datei %1 schlug fehl</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.cpp" line="95"/>
+        <source>Can&apos;t open %1 for writing</source>
+        <translation>%1 kann nicht zum Schreiben geöffnet werden</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="13"/>
+        <source>3ds2pie</source>
+        <translation>3ds2pie</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="54"/>
+        <source>Browse</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="67"/>
+        <source>Swap Y/Z</source>
+        <translation>Vertausche Y/Z</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="80"/>
+        <source>Invert U/V</source>
+        <translation>Invertiere U/V</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="93"/>
+        <source>Reverse polygon winding</source>
+        <translation>Invertiere Polygon Wickelung</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="106"/>
+        <source>Two sided polygons</source>
+        <translation>Zweiseitige Polygone</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="149"/>
+        <source>Input file</source>
+        <translation>Eingabedatei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="162"/>
+        <source>Output file</source>
+        <translation>Ausgabedatei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="175"/>
+        <source>Texture page Nr.</source>
+        <translation>Texture Page Nr.</translation>
+    </message>
+</context>
+</TS>
