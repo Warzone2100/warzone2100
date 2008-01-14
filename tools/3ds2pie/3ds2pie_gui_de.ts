d22bf8f49cfe1207b297ad66fb7942526dfa39d9
Dennis Schridde <devurandom@gna.org>
2008-01-14 22:28:13 +0000

    Add tooltips
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3476 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/3ds2pie/3ds2pie_gui_de.ts
+++ tools/3ds2pie/3ds2pie_gui_de.ts
@@ -1,44 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
 <context>
     <name>Gui3ds2pie</name>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="47"/>
+        <location filename="3ds2pie_gui.cpp" line="51"/>
         <source>Choose input file</source>
         <translation>Wählen Sie eine Eingabedatei</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="54"/>
+        <location filename="3ds2pie_gui.cpp" line="58"/>
         <source>Choose output file</source>
         <translation>Wählen Sie eine Ausgabedatei</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="95"/>
+        <location filename="3ds2pie_gui.cpp" line="119"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="68"/>
+        <location filename="3ds2pie_gui.cpp" line="97"/>
         <source>Please specify an input file</source>
         <translation>Bitte geben Sie eine Eingabedatei an</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="73"/>
+        <location filename="3ds2pie_gui.cpp" line="102"/>
         <source>Please specify an output file</source>
         <translation>Bitte geben Sie eine Ausgabedatei an</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="78"/>
-        <source>Please specify a texture page number</source>
-        <translation>Bitte geben Sie eine Texture Page Nummer an</translation>
-    </message>
-    <message>
-        <location filename="3ds2pie_gui.cpp" line="85"/>
+        <location filename="3ds2pie_gui.cpp" line="109"/>
         <source>Loading file %1 failed</source>
         <translation>Laden von Datei %1 schlug fehl</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.cpp" line="95"/>
+        <location filename="3ds2pie_gui.cpp" line="119"/>
         <source>Can&apos;t open %1 for writing</source>
         <translation>%1 kann nicht zum Schreiben geöffnet werden</translation>
     </message>
@@ -53,39 +49,87 @@
         <translation>Durchsuchen</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="67"/>
+        <location filename="3ds2pie_gui.ui" line="71"/>
         <source>Swap Y/Z</source>
         <translation>Vertausche Y/Z</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="80"/>
+        <location filename="3ds2pie_gui.ui" line="91"/>
         <source>Invert U/V</source>
         <translation>Invertiere U/V</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="93"/>
+        <location filename="3ds2pie_gui.ui" line="108"/>
         <source>Reverse polygon winding</source>
         <translation>Invertiere Polygon Wickelung</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="106"/>
+        <location filename="3ds2pie_gui.ui" line="125"/>
         <source>Two sided polygons</source>
         <translation>Zweiseitige Polygone</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="149"/>
+        <location filename="3ds2pie_gui.ui" line="164"/>
         <source>Input file</source>
         <translation>Eingabedatei</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="162"/>
+        <location filename="3ds2pie_gui.ui" line="177"/>
         <source>Output file</source>
         <translation>Ausgabedatei</translation>
     </message>
     <message>
-        <location filename="3ds2pie_gui.ui" line="175"/>
-        <source>Texture page Nr.</source>
-        <translation>Texture Page Nr.</translation>
+        <location filename="3ds2pie_gui.ui" line="67"/>
+        <source>Assume that &apos;Z&apos; denotes &quot;up&quot; direction
+(Should be enabled for 3DS Max created files)</source>
+        <translation>Annehmen, dass &apos;Z&apos; in &quot;hoch&quot; Richtung zeigt
+(Sollte for von 3DS Max erzeugte Dateien angeschaltet sein)</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="190"/>
+        <source>Texture page</source>
+        <translation>Texture Page</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="206"/>
+        <source>page-</source>
+        <translation>page-</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="87"/>
+        <source>Invert the vertical point of origin for UV coordinates
+(Should be enabled for 3DS Max created files)</source>
+        <translation>Invertiere den vertikalen Ursprungspunkt für U/V coordinaten
+(Sollte for von 3DS Max erzeugte Dateien angeschaltet sein)</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="104"/>
+        <source>Reverse the winding of all polygon faces
+(Should be enabled for 3DS Max created files)</source>
+        <translation>Vertausche Wickelung aller Polygon Seiten
+(Sollte for von 3DS Max erzeugte Dateien angeschaltet sein)</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="121"/>
+        <source>Use both sides of Polygons
+(Not used in Warzone 2100)</source>
+        <translation>Benutze beide Seiten von Polygonen
+(Wird in Warzone 2100 nicht benutzt)</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="138"/>
+        <source>Path to the 3DS file</source>
+        <translation>Pfad zur 3DS Datei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="151"/>
+        <source>Path to the PIE file</source>
+        <translation>Pfad zur PIE Datei</translation>
+    </message>
+    <message>
+        <location filename="3ds2pie_gui.ui" line="203"/>
+        <source>Texture page to use</source>
+        <translation>Zu benutzende Texture Page</translation>
     </message>
 </context>
 </TS>
