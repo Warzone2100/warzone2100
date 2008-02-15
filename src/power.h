f98f19d38f510963770c86957d3df1f82407f348
Per Inge Mathisen <per@wz2100.net>
2008-02-15 21:59:53 +0000

    Doxygenize power.h
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3793 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/power.h
+++ src/power.h
@@ -26,95 +26,97 @@
 #ifndef _power_h
 #define _power_h
 
-#define OILDRUM_POWER		100			// free power on collection of oildrum.
+/** Free power on collection of oildrum. */
+#define OILDRUM_POWER		100
 
 #define	POWER_PER_CYCLE		5
 
-#define REPAIR_POWER_FACTOR 1/5          //% used to determine the power cost of repairing a droid
-                                         //definately DON'T WANT the brackets round 1/2 - it will equate to zero!
-#define POWER_FACTOR        100          //used to multiply all repair calculations by to avaoid rounding errors
+/** Used to determine the power cost of repairing a droid.
+    Definately DON'T WANT the brackets round 1/2 - it will equate to zero! */
+#define REPAIR_POWER_FACTOR 1/5
+
+/** Used to multiply all repair calculations by to avaoid rounding errors. */
+#define POWER_FACTOR        100
 
 typedef struct _player_power
 {
-	UDWORD		currentPower;		    /* the current amount of power avaialble 
-									    to the player*/
-	UDWORD		extractedPower;		    /* the power extracted but not converted */
-	struct _base_object *psLastPowered;	/*the last object that received power 
-									    before it ran out*/
+	UDWORD		currentPower;		/**< The current amount of power avaialble to the player. */
+	UDWORD		extractedPower;		/**< The power extracted but not converted. */
+	BASE_OBJECT	*psLastPowered;		/**< The last object that received power before it ran out. */
 } PLAYER_POWER;
 
-/*allocate the space for the playerPower*/
+/** Allocate the space for the playerPower. */
 extern BOOL allocPlayerPower(void);
 
-/*clear the playerPower */
+/** Clear the playerPower. */
 extern void clearPlayerPower(void);
 
-/*reset the power levels when a power_gen or resource_extractor is destroyed */
+/** Reset the power levels when a power_gen or resource_extractor is destroyed. */
 extern BOOL resetPlayerPower(UDWORD player, STRUCTURE *psStruct);
 
-/*Free the space used for playerPower */
+/** Free the space used for playerPower. */
 extern void releasePlayerPower(void);
 
-/*check the available power*/
+/** Check the available power. */
 extern BOOL checkPower(UDWORD player, UDWORD quantity, BOOL playAudio);
 
-/*subtract the power required */
+/** Subtract the power required. */
 extern BOOL usePower(UDWORD player, UDWORD quantity);
 
-//return the power when a structure/droid is deliberately destroyed
+/** Return the power when a structure/droid is deliberately destroyed. */
 extern void addPower(UDWORD player, UDWORD quantity);
 
-/* Each Resource Extractor yields EXTRACT_POINTS per second until there are none
-   left in the resource. */
+/** Each Resource Extractor yields EXTRACT_POINTS per second until there are none left in the resource. */
 extern UDWORD updateExtractedPower(STRUCTURE	*psBuilding);
 
-/* Update current power based on what was extracted during the last cycle and 
-   what Power Generators exist */
+/** Update current power based on what was extracted during the last cycle and what Power Generators exist. */
 extern void updatePlayerPower(UDWORD player);
 
-// used in multiplayer to force power levels.
+/** Used in multiplayer to force power levels. */
 extern void setPower(UDWORD player, UDWORD avail); 
 
 /** Get the amount of power current held by the given player. */
 extern UDWORD getPower(UDWORD player);
 
-/*resets the power levels for all players when power is turned back on*/
+/** Resets the power levels for all players when power is turned back on. */
 void powerCalc(BOOL on);
 
-/*sets the initial value for the power*/
+/** Sets the initial value for the power. */
 void setPlayerPower(UDWORD power, UDWORD player);
 
-/*Temp function to give all players some power when a new game has been loaded*/
+/** Temp function to give all players some power when a new game has been loaded. */
 void newGameInitPower(void);
 
-//informs the power array that a Object has been destroyed
+/** Informs the power array that a Object has been destroyed. */
 extern void powerDestroyObject(BASE_OBJECT *psObject);
 
-/*accrue the power in the facilities that require it*/
+/** Accrue the power in the facilities that require it. */
 extern BOOL accruePower(BASE_OBJECT *psObject);
 
-/*inform the players power struct that the last Object to receive power has changed*/
+/** Inform the players power struct that the last Object to receive power has changed. */
 extern void updateLastPowered(BASE_OBJECT *psObject,UBYTE player);
 
-/*	Returns the next res. Ext. in the list from the one passed in. returns 1st one
+/**	Returns the next res. Ext. in the list from the one passed in. returns 1st one
 	in list if passed in is NULL and NULL if there's none?
 */
 extern STRUCTURE *getRExtractor(STRUCTURE *psStruct);
 
-/*checks if the object to be powered next - returns TRUE if power*/
+/** Checks if the object to be powered next - returns TRUE if power. */
 extern BOOL getLastPowered(BASE_OBJECT *psStructure);
 
-/*defines which structure types draw power - returns TRUE if use power*/
+/** Defines which structure types draw power - returns TRUE if use power. */
 extern BOOL structUsesPower(STRUCTURE *psStruct);
-/*defines which droid types draw power - returns TRUE if use power*/
+
+/** Defines which droid types draw power - returns TRUE if use power. */
 extern BOOL droidUsesPower(DROID *psDroid);
 
-//this is a check cos there is a problem with the power but not sure where!!
+/** This is a check cos there is a problem with the power but not sure where!! */
 extern void powerCheck(BOOL bBeforePowerUsed, UBYTE player);
 
+/** @todo Wrap access to this global and make it static for easier sanity checking! */
 extern PLAYER_POWER		*asPower[MAX_PLAYERS];
 
-//flag used to check for power calculations to be done or not
+/** Flag used to check for power calculations to be done or not. */
 extern	BOOL			powerCalculated;
 
 #endif //power.h
