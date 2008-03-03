c747bbe231b695de2023d4d4d67022845ea84b87
Roman C <troman@gna.org>
2008-03-03 22:02:24 +0000

    A better fix to bug #11084, last one was buggy.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3909 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/scriptobj.h
+++ src/scriptobj.h
@@ -64,7 +64,7 @@ enum _objids
 };
 
 // marks a nullpointer for the save-game loading routine
-#define	UNALLOCATED_OBJECT	0
+#define	UNALLOCATED_OBJECT	(-1)
 
 // id's for group variables
 enum _groupids
