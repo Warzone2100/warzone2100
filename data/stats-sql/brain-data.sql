f55d9ac01319933a2e07cf69d319b143385c8e6d
Giel van Schijndel <giel@wz2100.net>
2008-03-08 16:09:20 +0000

    Convert the 'brain.txt' stats to the SQLite database
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@3956 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ data/stats-sql/brain-data.sql
@@ -0,0 +1,22 @@
+BEGIN TRANSACTION;
+
+-- Table structure for table `brain`
+
+CREATE TABLE `brain` (
+  id                    INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name                  TEXT    NOT NULL, -- Text id name (short language independant name)
+  techlevel             TEXT    NOT NULL, -- Technology level of this component
+  buildPower            NUMERIC NOT NULL, -- Power required to build this component
+  buildPoints           NUMERIC NOT NULL, -- Time required to build this component
+  weight                NUMERIC NOT NULL, -- Component's weight (mass?)
+  hitpoints             NUMERIC NOT NULL, -- Component's hitpoints - SEEMS TO BE UNUSED
+  systempoints          NUMERIC NOT NULL, -- Space the component takes in the droid - SEEMS TO BE UNUSED
+  weapon                INTEGER,          -- A reference to `weapons`.`id`, refers to the weapon stats associated with this brain (can be NULL for none) - for Command Droids
+  program_capcity       INTEGER NOT NULL  -- Program's capacity
+);
+
+-- Data for table `brain`
+INSERT INTO `brain` SELECT NULL, 'ZNULLBRAIN', 'Level All', 0, 0, 0, 0, 0, `weapons`.`id`, 0 FROM `weapons` WHERE `weapons`.`name`='ZNULLWEAPON';
+INSERT INTO `brain` SELECT NULL, 'CommandBrain01' ,'Level All', 1, 1, 1, 1, 1, `weapons`.`id`, 0 FROM `weapons` WHERE `weapons`.`name`='CommandTurret1';
+
+COMMIT TRANSACTION;
