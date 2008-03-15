e32e928c38baa146a5229b1c7deb5ca1165ca6b7
Dennis Schridde <devurandom@gna.org>
2008-03-15 11:40:35 +0000

    Remove missing ftol() and add some docs
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4055 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/frame.h
+++ lib/framework/frame.h
@@ -59,6 +59,7 @@
 #include "printf_ext.h"
 
 #include "trig.h"
+
 #include <physfs.h>
 
 extern UDWORD selectedPlayer;
@@ -117,14 +118,16 @@ extern UDWORD frameGetFrameNumber(void);
 /** Return average framerate of the last seconds. */
 extern UDWORD frameGetAverageRate(void);
 
+/*! Open a file for reading */
+extern PHYSFS_file* openLoadFile(const char* fileName, bool hard_fail);
+
+/*! Open a file for writing */
+extern PHYSFS_file* openSaveFile(const char* fileName);
 
 /** Load the file with name pointed to by pFileName into a memory buffer. */
 extern BOOL loadFile(const char *pFileName,		// The filename
               char **ppFileData,	// A buffer containing the file contents
               UDWORD *pFileSize);	// The size of this buffer
-
-extern PHYSFS_file* openLoadFile(const char* fileName, bool hard_fail);
-extern PHYSFS_file* openSaveFile(const char* fileName);
 
 /** Save the data in the buffer into the given file */
 extern BOOL saveFile(const char *pFileName, const char *pFileData, UDWORD fileSize);
@@ -135,8 +138,6 @@ BOOL loadFileToBuffer(const char *pFileName, char *pFileBuffer, UDWORD bufferSiz
 /** Load a file from disk, but returns quietly if no file found. */
 BOOL loadFileToBufferNoError(const char *pFileName, char *pFileBuffer, UDWORD bufferSize,
                              UDWORD *pSize);
-
-extern SDWORD ftol(float f);
 
 UDWORD HashString( const char *String );
 UDWORD HashStringIgnoreCase( const char *String );
