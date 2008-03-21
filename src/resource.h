12f8bbbb31d2159ddd0c30bf82a38d4f89256b05
Giel van Schijndel <giel@wz2100.net>
2008-03-21 21:37:15 +0000

    Clean out unused trash from resource.h
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4212 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/resource.h
+++ src/resource.h
@@ -21,19 +21,8 @@
 #ifndef __INCLUDED_SRC_RESOURCE_H__
 #define __INCLUDED_SRC_RESOURCE_H__
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by Deliv.rc
-//
 #define IDC_DEST                        101
-#define IDC_SIGHT                       102
-#define IDC_TARGET                      103
-#define IDC_LARROW                      104
-#define IDC_RARROW                      105
-#define IDC_DARROW                      106
-#define IDC_UARROW                      107
 #define IDC_DEFAULT                     108
-#define IDC_EDGEOFMAP                   109
 #define IDC_ATTACH                      110
 #define IDC_ATTACK                      111
 #define IDC_BOMB                        112
@@ -44,23 +33,10 @@
 #define IDC_GUARD                       117
 #define IDC_JAM                         118
 #define IDC_LOCKON                      119
-#define IDC_MENU                        120
 #define IDC_MOVE                        121
 #define IDC_NOTPOSSIBLE                 122
 #define IDC_PICKUP                      123
 #define IDC_SEEKREPAIR                  124
 #define IDC_SELECT                      125
-#define IDI_ICON1                       126
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        128
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
 
 #endif // __INCLUDED_SRC_RESOURCE_H__
