Warzone2100/warzone2100

commit e9c167118d3cc8f2bd10697769f8a4063c5b7aa6

Fix beacon code for real (r4408 used sizeof for the source variable, while it should be the destiniation)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4413 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiplay.c
@@ -1906,7 +1906,7 @@ static BOOL recvBeacon()
 	debug(LOG_WZ, "Received beacon for player: %d, from: %d",receiver, sender);
 
 	strlcat(msg, NetPlay.players[sender].name, sizeof(msg));    // name
-	strlcpy(beaconReceiveMsg[sender], msg, sizeof(msg));
+	strlcpy(beaconReceiveMsg[sender], msg, sizeof(beaconReceiveMsg[player]));
 
 	return addHelpBlip(locX, locY, receiver, sender, beaconReceiveMsg[sender]);
 }

commit 2fb4cf960bb574ac856209d76c872f86eb3ef5cd

Fix compile error introduced in r4413 (oops)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4415 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiplay.c
@@ -1906,7 +1906,7 @@ static BOOL recvBeacon()
 	debug(LOG_WZ, "Received beacon for player: %d, from: %d",receiver, sender);
 
 	strlcat(msg, NetPlay.players[sender].name, sizeof(msg));    // name
-	strlcpy(beaconReceiveMsg[sender], msg, sizeof(beaconReceiveMsg[player]));
+	strlcpy(beaconReceiveMsg[sender], msg, sizeof(beaconReceiveMsg[sender]));
 
 	return addHelpBlip(locX, locY, receiver, sender, beaconReceiveMsg[sender]);
 }