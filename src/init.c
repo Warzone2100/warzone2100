aa8f321cc73cc23f1c5b23bd92cab11ca51e5e6a
Giel van Schijndel <giel@wz2100.net>
2008-04-14 16:29:21 +0000

    Backport r4602:4604 from trunk to the 2.1 branch:  * Pass NULL to addSubdirs and removeSubdirs, not false (as the target type is a char* pointer)  * Add the ability for auto-loading of mods    Automatically load all mods found in:   * "mods/global/autoload" in both campaign as well as multiplayer   * "mods/campaign/autoload" in campaign only   * "mods/multiplay/autoload" in multiplayer only
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.1@4605 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/init.c
+++ src/init.c
@@ -255,10 +255,13 @@ BOOL rebuildSearchPath( searchPathMode mode, BOOL force )
 					debug( LOG_WZ, "rebuildSearchPath: Removing [%s] from search path", curSearchPath->path );
 #endif // DEBUG
 					// Remove maps and mods
-					removeSubdirs( curSearchPath->path, "maps", FALSE );
+					removeSubdirs( curSearchPath->path, "maps", NULL );
 					removeSubdirs( curSearchPath->path, "mods/global", global_mods );
+					removeSubdirs( curSearchPath->path, "mods/global/autoload", NULL );
 					removeSubdirs( curSearchPath->path, "mods/campaign", campaign_mods );
+					removeSubdirs( curSearchPath->path, "mods/campaign/autoload", NULL );
 					removeSubdirs( curSearchPath->path, "mods/multiplay", multiplay_mods );
+					removeSubdirs( curSearchPath->path, "mods/multiplay/autoload", NULL );
 
 					// Remove multiplay patches
 					strlcpy(tmpstr, curSearchPath->path, sizeof(tmpstr));
@@ -291,7 +294,9 @@ BOOL rebuildSearchPath( searchPathMode mode, BOOL force )
 					PHYSFS_addToSearchPath( curSearchPath->path, PHYSFS_APPEND );
 
 					addSubdirs( curSearchPath->path, "mods/global", PHYSFS_APPEND, global_mods );
+					addSubdirs( curSearchPath->path, "mods/global/autoload", PHYSFS_APPEND, NULL );
 					addSubdirs( curSearchPath->path, "mods/campaign", PHYSFS_APPEND, campaign_mods );
+					addSubdirs( curSearchPath->path, "mods/campaign/autoload", PHYSFS_APPEND, NULL );
 					PHYSFS_removeFromSearchPath( curSearchPath->path );
 
 					// Add plain dir
@@ -315,9 +320,11 @@ BOOL rebuildSearchPath( searchPathMode mode, BOOL force )
 #endif // DEBUG
 					// Add maps and global and multiplay mods
 					PHYSFS_addToSearchPath( curSearchPath->path, PHYSFS_APPEND );
-					addSubdirs( curSearchPath->path, "maps", PHYSFS_APPEND, FALSE );
+					addSubdirs( curSearchPath->path, "maps", PHYSFS_APPEND, NULL );
 					addSubdirs( curSearchPath->path, "mods/global", PHYSFS_APPEND, global_mods );
+					addSubdirs( curSearchPath->path, "mods/global/autoload", PHYSFS_APPEND, NULL );
 					addSubdirs( curSearchPath->path, "mods/multiplay", PHYSFS_APPEND, multiplay_mods );
+					addSubdirs( curSearchPath->path, "mods/multiplay/autoload", PHYSFS_APPEND, NULL );
 					PHYSFS_removeFromSearchPath( curSearchPath->path );
 
 					// Add multiplay patches
