56e269d2c139d22b643d1b994bb496c21c7d6d12
Giel van Schijndel <giel@wz2100.net>
2008-04-26 23:38:28 +0000

    Add a lexer for Pygments (a generic syntax highlighter in Python)
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4807 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ tools/pygments-lexers/warzone/__init__.py
@@ -0,0 +1,26 @@
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class WRFLexer(RegexLexer):
+    name = 'WRF'
+    aliases = ['wrf']
+    filenames = ['*.wrf']
+
+    tokens = {
+        'root': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r'\bdirectory\b', Keyword),
+            (r'\bfile\b', Keyword, 'file_line'),
+            (r'"[^"]*"', String),
+            (r'[ \t\n\x0d\x0a]+', Whitespace),
+        ],
+        'comment': [
+            (r'[^*]+', Comment.Multiline),
+            (r'\*/', Comment.Multiline, '#pop'),
+            (r'\*[^/]', Comment.Multiline),
+        ],
+        'file_line': [
+            (r'[ \t\n\x0d\x0a]+', Whitespace),
+            (r'[^ \t\n\x0d\x0a]+', Literal, '#pop'),
+        ]
+    }
