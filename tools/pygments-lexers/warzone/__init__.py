d83e9ff74945e94fa3f96a88ac9ea1e4eeae4157
Giel van Schijndel <giel@wz2100.net>
2008-04-28 00:41:59 +0000

    * Add yet another Pygments lexer: LevelLexer which lexes .lev files  * Mark the .lev files as text/x-wzlev
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4833 4a71c877-e1ca-e34f-864e-861f7616d084

--- tools/pygments-lexers/warzone/__init__.py
+++ tools/pygments-lexers/warzone/__init__.py
@@ -49,3 +49,34 @@ class STRRESLexer(RegexLexer):
             (r'\*[^/]', Comment.Multiline),
         ],
     }
+
+class LevelLexer(RegexLexer):
+    name = 'WZLevel'
+    aliases = ['wzlevel']
+    filenames = ['*.lev']
+    mimetypes = ['text/x-wzlev']
+
+    tokens = {
+        'root': [
+            (r'/\*', Comment.Multiline, 'comment'),
+            (r'//.*?$', Comment.Singleline),
+            (r'\b(level|campaign|camstart|camchange|dataset|expand|expand_limbo|between|miss_keep|miss_keep_limbo|miss_clear)\b', Keyword, 'dataset_line'),
+            (r'\b(players|type)\b', Keyword, 'integer_line'),
+            (r'\b(data|game)\b', Keyword),
+            (r'"[^"]*"', String),
+            (r'[ \t\n\x0d\x0a]+', Text),
+        ],
+        'comment': [
+            (r'[^*]+', Comment.Multiline),
+            (r'\*/', Comment.Multiline, '#pop'),
+            (r'\*[^/]', Comment.Multiline),
+        ],
+        'dataset_line': [
+            (r'[ \t\n\x0d\x0a]+', Text),
+            (r'\b[_a-zA-Z][-0-9_a-zA-Z]+\b', Literal, '#pop'),
+        ],
+        'integer_line': [
+            (r'[ \t\n\x0d\x0a]+', Text),
+            (r'-?[0-9]+', Number, '#pop'),
+        ]
+    }
