a5a74770e89ae3d26cfeed74f5dec5c9299e5dcf
Giel van Schijndel <giel@wz2100.net>
2008-04-28 20:46:33 +0000

    Allow proper linking of the exceptionhandler with the raw Makefile system
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4845 4a71c877-e1ca-e34f-864e-861f7616d084

--- makerules/exe.mk
+++ makerules/exe.mk
@@ -3,5 +3,5 @@ include $(MAKERULES)/common.mk
 DEPS=$(patsubst %.c,%.o, $(SRC:%.rc=%.o)) $(LIBS)
 
 all: $(EXE)$(EXEEXT)
-$(EXE)$(EXEEXT): $(DEPS) ; $(CC) $(CFLAGS) -o $(EXE)$(EXEEXT) $(DEPS) $(LDFLAGS)
+$(EXE)$(EXEEXT): $(DEPS) ; $(CXX) $(CFLAGS) -o $(EXE)$(EXEEXT) $(DEPS) $(LDFLAGS)
 clean: ; $(RMF) $(EXE)$(EXEEXT) *.o
--- makerules/lib.mk
+++ makerules/lib.mk
@@ -1,5 +1,5 @@
 include $(MAKERULES)/common.mk
 
 all: ../lib$(LIB).a
-../lib$(LIB).a: $(SRC:.c=.o) ; $(AR) rcv ../lib$(LIB).a $(SRC:.c=.o)
+../lib$(LIB).a: $(SRC:.c=.o) ; $(AR) rcv ../lib$(LIB).a $(patsubst %.c,%.o, $(SRC:%.cpp=%.o))
 clean: ; $(RMF) ..$(DIRSEP)lib$(LIB).a *.o
