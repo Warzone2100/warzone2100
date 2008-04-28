0f7138168566a2c8641623d86668046f43d303a3
Giel van Schijndel <giel@wz2100.net>
2008-04-28 20:58:13 +0000

    Lets use some proper make rules!  * Use $@ and $< to refer to the targets and dependencies of make rules  * Don't refer to make rule dependencies and targets by copying them!  * Putting make rules on multiple lines isn't bad, in fact it increases readability!  * Mark phony make rules (i.e. rules who's actions don't produce files) as `.PHONY`
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@4846 4a71c877-e1ca-e34f-864e-861f7616d084

--- makerules/exe.mk
+++ makerules/exe.mk
@@ -3,5 +3,11 @@ include $(MAKERULES)/common.mk
 DEPS=$(patsubst %.c,%.o, $(SRC:%.rc=%.o)) $(LIBS)
 
 all: $(EXE)$(EXEEXT)
-$(EXE)$(EXEEXT): $(DEPS) ; $(CXX) $(CFLAGS) -o $(EXE)$(EXEEXT) $(DEPS) $(LDFLAGS)
-clean: ; $(RMF) $(EXE)$(EXEEXT) *.o
+
+$(EXE)$(EXEEXT): $(DEPS)
+	$(CXX) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+clean:
+	$(RMF) $(EXE)$(EXEEXT) *.o
+
+.PHONY: all clean
--- makerules/lib.mk
+++ makerules/lib.mk
@@ -1,5 +1,11 @@
 include $(MAKERULES)/common.mk
 
 all: ../lib$(LIB).a
-../lib$(LIB).a: $(SRC:.c=.o) ; $(AR) rcv ../lib$(LIB).a $(patsubst %.c,%.o, $(SRC:%.cpp=%.o))
-clean: ; $(RMF) ..$(DIRSEP)lib$(LIB).a *.o
+
+../lib$(LIB).a: $(patsubst %.c,%.o, $(SRC:%.cpp=%.o))
+	$(AR) rcv $@ $^
+
+clean:
+	$(RMF) ..$(DIRSEP)lib$(LIB).a *.o
+
+.PHONY: all clean
