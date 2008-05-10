73be5e4b4db1d02c752539919cfb3b883381c73d
Giel van Schijndel <giel@wz2100.net>
2008-05-10 21:13:14 +0000

    Add BUILT_SOURCES and CLEANFILES to the raw Makefile system as well (similar to the vars with the same name in automake)
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5028 4a71c877-e1ca-e34f-864e-861f7616d084

--- makerules/lib.mk
+++ makerules/lib.mk
@@ -1,11 +1,11 @@
 include $(MAKERULES)/common.mk
 
-all: ../lib$(LIB).a
+all: $(BUILT_SOURCES) ../lib$(LIB).a
 
 ../lib$(LIB).a: $(patsubst %.c,%.o, $(SRC:%.cpp=%.o))
 	$(AR) rcv $@ $^
 
 clean:
-	$(RMF) ..$(DIRSEP)lib$(LIB).a *.o
+	$(RMF) ..$(DIRSEP)lib$(LIB).a *.o $(CLEANFILES)
 
 .PHONY: all clean
