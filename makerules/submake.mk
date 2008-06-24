876b00154a919ba8911f7ad33b66452eba2f03af
Dennis Schridde <devurandom@gna.org>
2008-06-24 23:19:09 +0000

    Forgot one uppercase makefile.raw...
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5324 4a71c877-e1ca-e34f-864e-861f7616d084

--- makerules/submake.mk
+++ makerules/submake.mk
@@ -4,7 +4,7 @@ all clean: $(SUBDIRS)
 
 $(SUBDIRS):
 	-$(MKDIR_P) $(builddir)/$@
-	$(MAKE) -f $(srcdir)/$@/Makefile.raw -C $(builddir)/$@ $(MAKECMDGOALS)
+	$(MAKE) -f $(srcdir)/$@/makefile.raw -C $(builddir)/$@ $(MAKECMDGOALS)
 
 clean: $(SUBDIRS)
 	$(RMDIR) $(SUBDIRS)
