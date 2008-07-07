Warzone2100/warzone2100

commit 9c501e46416f9a84a75dada9cec2e1d08cad7fe3

vtolHappy: move the body-health check outside of the weapon check
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5406 4a71c877-e1ca-e34f-864e-861f7616d084

src/droid.c
@@ -4575,6 +4575,13 @@ BOOL vtolHappy(const DROID* psDroid)
 	CHECK_DROID(psDroid);
 
 	ASSERT(isVtolDroid(psDroid), "not a VTOL droid");
+
+	if (psDroid->body <= psDroid->originalBody)
+	{
+		// VTOLs with less health than their original aren't happy
+		return false;
+	}
+
 	ASSERT( psDroid->droidType == DROID_WEAPON, "vtolHappy: not a weapon droid" );
 
 	//check full complement of ammo
@@ -4606,9 +4613,6 @@ BOOL vtolHappy(const DROID* psDroid)
 			 && !(rearmedWeaps & curWeap))
 				return false;
 		}
-
-		// Check to see if we're fully repaired
-		return (psDroid->body == psDroid->originalBody);
 	}
 
 	ASSERT(!"Do we ever get here?", "If a crash/abortion occurs here, please attach a backtrace (the crash dump file) to https://gna.org/bugs/?11865 as it will affect the bugfix for that bug.");

commit 336656a0a8f0c6db867949f79c0c477808c8dcfe

Fix a bug introduced in r5406:  * VTOLs didn't ever become "happy" again (i.e. where marked as fully armed and healed)
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5407 4a71c877-e1ca-e34f-864e-861f7616d084

src/droid.c
@@ -4576,7 +4576,7 @@ BOOL vtolHappy(const DROID* psDroid)
 
 	ASSERT(isVtolDroid(psDroid), "not a VTOL droid");
 
-	if (psDroid->body <= psDroid->originalBody)
+	if (psDroid->body < psDroid->originalBody)
 	{
 		// VTOLs with less health than their original aren't happy
 		return false;
@@ -4613,6 +4613,8 @@ BOOL vtolHappy(const DROID* psDroid)
 			 && !(rearmedWeaps & curWeap))
 				return false;
 		}
+
+		return true;
 	}
 
 	ASSERT(!"Do we ever get here?", "If a crash/abortion occurs here, please attach a backtrace (the crash dump file) to https://gna.org/bugs/?11865 as it will affect the bugfix for that bug.");