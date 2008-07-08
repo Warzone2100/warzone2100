f77542e2f8e96818a8fa3e36d78b840b1a2d43da
Dennis Schridde <devurandom@gna.org>
2008-07-08 17:46:45 +0000

    Move config dir on Windows back to My Documents
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5427 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/main.c
+++ src/main.c
@@ -268,7 +268,7 @@ static void getPlatformUserDir(char * const tmpstr, size_t const size)
 {
 #if defined(WZ_OS_WIN)
 	ASSERT(size >= MAX_PATH, "size (%u) is smaller than the required minimum of MAX_PATH (%u)", size, (size_t)MAX_PATH);
-	if ( SUCCEEDED( SHGetFolderPathA( NULL, CSIDL_LOCAL_APPDATA|CSIDL_FLAG_CREATE, NULL, SHGFP_TYPE_CURRENT, tmpstr ) ) )
+	if ( SUCCEEDED( SHGetFolderPathA( NULL, CSIDL_PERSONAL|CSIDL_FLAG_CREATE, NULL, SHGFP_TYPE_CURRENT, tmpstr ) ) )
 		strlcat(tmpstr, PHYSFS_getDirSeparator(), size);
 	else
 #elif defined(WZ_OS_MAC)
