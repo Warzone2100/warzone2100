7d5723455c712bb0c3b2353590f768771c3e0698
Freddie Witherden <evilguru@gna.org>
2008-07-09 16:51:17 +0000

    Add keycode.h to the repository (which I forgot in r5445).
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5449 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ lib/betawidget/keycode.h
@@ -0,0 +1,36 @@
+#ifndef KEYCODE_H_
+#define KEYCODE_H_
+
+/*
+ * Forward declarations
+ */
+typedef enum _eventKeycode eventKeycode;
+
+enum _eventKeycode
+{
+	// `Control' keys
+	EVT_KEYCODE_BACKSPACE,
+	EVT_KEYCODE_TAB,
+	EVT_KEYCODE_RETURN,
+	EVT_KEYCODE_ESCAPE,
+	EVT_KEYCODE_ENTER,
+	
+	// Arrow keys
+	EVT_KEYCODE_LEFT,
+	EVT_KEYCODE_RIGHT,
+	EVT_KEYCODE_UP,
+	EVT_KEYCODE_DOWN,
+	
+	// Home/end pad
+	EVT_KEYCODE_INSERT,
+	EVT_KEYCODE_DELETE,
+	EVT_KEYCODE_HOME,
+	EVT_KEYCODE_END,
+	EVT_KEYCODE_PAGEUP,
+	EVT_KEYCODE_PAGEDOWN,
+	
+	// Unknown
+	EVT_KEYCODE_UNKNOWN
+};
+
+#endif /*KEYCODE_H_*/
