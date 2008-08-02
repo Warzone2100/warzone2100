496d5d0ad8bd1b17a0be8a43c7071a1b9a661632
Per Inge Mathisen <per@wz2100.net>
2008-08-02 16:14:59 +0000

    Remove unused code
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5742 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/displaydef.h
+++ src/displaydef.h
@@ -25,26 +25,17 @@
 #define __INCLUDED_DISPLAYDEF_H__
 
 #include "lib/ivis_common/imd.h"
-		// ffs am
 #include "lib/ivis_common/pieclip.h"
 
 #define	BOUNDARY_X			(16)
 #define BOUNDARY_Y			(16)
 //#define BOUNDARY_X		(DISP_WIDTH/20)	   // proportional to resolution - Alex M
 //#define	BOUNDARY_Y		(DISP_WIDTH/16)
-//#define	BOUNDARY_X		(24)
-//#define	BOUNDARY_Y		(24)
 
 typedef struct _screen_disp_data
 {
-	//UDWORD		dummy;
 	iIMDShape	*imd;
-//	BOOL		drawnThisFrame;		// for sorting - have we drawn the imd already?
 	UDWORD		frameNumber;		// last frame it was drawn
-//	UDWORD		animFrame;			// anim Frame
-//	SDWORD		bucketDepth;
-//	BOOL		onScreen;
-//	UDWORD		numTiles;
 	UDWORD		screenX,screenY;
 	UDWORD		screenR;
 } SCREEN_DISP_DATA;
