aabf08d6fa51d0cd8afb96534b3fab9c814ca895
Giel van Schijndel <giel@wz2100.net>
2008-09-01 19:10:03 +0000

    Change the UTF-8 encoder and decoder to only accept valid Unicode codepoints, not just every representable 32 bit unsigned integer
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5914 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/utf.c
+++ lib/framework/utf.c
@@ -101,71 +101,12 @@ static utf_32_char decode_utf8_char(const char * const utf8_char, const char** n
 		decoded |= (*((*next_char)++) & 0x3f) << 6;
 		decoded |= (*((*next_char)++) & 0x3f) << 0;
 	}
-	// first octect: 111110xx: 26 bit
-	else if ((*utf8_char & 0xfc) == 0xf8)
-	{
-		// 5 byte long encoding
-		ASSERT_NON_START_OCTET(utf8_char[1]);
-		ASSERT_NON_START_OCTET(utf8_char[2]);
-		ASSERT_NON_START_OCTET(utf8_char[3]);
-		ASSERT_NON_START_OCTET(utf8_char[4]);
-
-		decoded  = (*((*next_char)++) & 0x03) << 24;
-		decoded |= (*((*next_char)++) & 0x3f) << 18;
-		decoded |= (*((*next_char)++) & 0x3f) << 12;
-		decoded |= (*((*next_char)++) & 0x3f) << 6;
-		decoded |= (*((*next_char)++) & 0x3f) << 0;
-	}
-	// first octect: 1111110x: 31 bit
-	else if ((*utf8_char & 0xfe) == 0xfc)
-	{
-		// 6 byte long encoding
-		ASSERT_NON_START_OCTET(utf8_char[1]);
-		ASSERT_NON_START_OCTET(utf8_char[2]);
-		ASSERT_NON_START_OCTET(utf8_char[3]);
-		ASSERT_NON_START_OCTET(utf8_char[4]);
-		ASSERT_NON_START_OCTET(utf8_char[5]);
-
-		decoded  = (*((*next_char)++) & 0x01) << 30;
-		decoded |= (*((*next_char)++) & 0x3f) << 24;
-		decoded |= (*((*next_char)++) & 0x3f) << 18;
-		decoded |= (*((*next_char)++) & 0x3f) << 12;
-		decoded |= (*((*next_char)++) & 0x3f) << 6;
-		decoded |= (*((*next_char)++) & 0x3f) << 0;
-	}
-	// first octect: 11111110: 36 bit (we'll only use 32bit though)
-	else if ((*utf8_char & 0xff) == 0xfe)
-	{
-		// 7 byte long encoding
-		ASSERT_NON_START_OCTET(utf8_char[1]);
-		ASSERT_NON_START_OCTET(utf8_char[2]);
-		ASSERT_NON_START_OCTET(utf8_char[3]);
-		ASSERT_NON_START_OCTET(utf8_char[4]);
-		ASSERT_NON_START_OCTET(utf8_char[5]);
-		ASSERT_NON_START_OCTET(utf8_char[6]);
-
-		// original: decoded  = (*((*next_char)++) & 0x00) << 36;
-		// The first octect contains no data bits
-		decoded = 0; ++(*next_char);
-
-		// original: decoded |= (*((*next_char)++) & 0x3f) << 30;
-		// Use only the 2 least significant bits of this byte
-		// to make sure we use 32bit at maximum
-		decoded |= (*((*next_char)++) & 0x03) << 30;
-
-		decoded |= (*((*next_char)++) & 0x3f) << 24;
-		decoded |= (*((*next_char)++) & 0x3f) << 18;
-		decoded |= (*((*next_char)++) & 0x3f) << 12;
-		decoded |= (*((*next_char)++) & 0x3f) << 6;
-		decoded |= (*((*next_char)++) & 0x3f) << 0;
-	}
-	// first octet: 11111111: 41 bit or more
 	else
 	{
-		// apparently this character uses more than 36 bit
+		// apparently this character uses more than 21 bit
 		// this decoder is not developed to cope with those
 		// characters so error out
-		ASSERT(!"out-of-range UTF-8 character", "utf8_character_count: this UTF-8 character is too large (> 36bits) for this UTF-8 decoder");
+		ASSERT(!"out-of-range UTF-8 character", "this UTF-8 character is too large (> 21bits) for this UTF-8 decoder and too large to be a valid Unicode codepoint");
 	}
 
 	return decoded;
@@ -239,7 +180,8 @@ char* utf8_char_at_offset(const char* utf8_string, size_t index)
 /** Encodes a single Unicode character to a UTF-8 encoded string.
  *
  *  \param unicode_char A UTF-32 encoded Unicode codepoint that will be encoded
- *                      into UTF-8.
+ *                      into UTF-8. This should be a valid Unicode codepoint
+ *                      (i.e. ranging from 0x0 to 0x10FFFF inclusive).
  *  \param out_char     Points to the position in a buffer where the UTF-8
  *                      encoded character can be stored.
  *
@@ -276,7 +218,10 @@ static char* encode_utf8_char(const utf_32_char unicode_char, char * const out_c
 		*(next_char++) = 0x80 | (unicode_char & 0x3f);
 	}
 	// 21 bits
-	else if (unicode_char < 0x00200000)
+	/* This encoder can deal with < 0x00200000, but Unicode only ranges
+	 * from 0x0 to 0x10FFFF. Thus we don't accept anything else.
+	 */
+	else if (unicode_char < 0x00110000)
 	{
 		// 0xf0 provides the counting bits: 11110
 		// then append the 3 most significant bits
@@ -286,43 +231,12 @@ static char* encode_utf8_char(const utf_32_char unicode_char, char * const out_c
 		*(next_char++) = 0x80 | ((unicode_char >> 6) & 0x3f);
 		*(next_char++) = 0x80 | (unicode_char & 0x3f);
 	}
-	// 26 bits
-	else if (unicode_char < 0x04000000)
-	{
-		// 0xf8 provides the counting bits: 111110
-		// then append the 2 most significant bits
-		*(next_char++) = 0xf8 | (unicode_char >> 24 );
-		// Put the next 24 bits in four bytes of their own
-		*(next_char++) = 0x80 | ((unicode_char >> 18) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 12) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 6) & 0x3f);
-		*(next_char++) = 0x80 | (unicode_char & 0x3f);
-	}
-	// 31 bits
-	else if (unicode_char < 0x80000000)
-	{
-		// 0xfc provides the counting bits: 1111110
-		// then append the 1 most significant bit
-		*(next_char++) = 0xfc | (unicode_char >> 30);
-		// Put the next 30 bits in five bytes of their own
-		*(next_char++) = 0x80 | ((unicode_char >> 24) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 18) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 12) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 6) & 0x3f);
-		*(next_char++) = 0x80 | (unicode_char & 0x3f);
-	}
-	// 36 bits
 	else
 	{
-		// 0xfe provides the counting bits: 11111110
-		*(next_char++) = 0xfe;
-		// Put the next 36 bits in six bytes of their own
-		*(next_char++) = 0x80 | ((unicode_char >> 30) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 24) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 18) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 12) & 0x3f);
-		*(next_char++) = 0x80 | ((unicode_char >> 6) & 0x3f);
-		*(next_char++) = 0x80 | (unicode_char & 0x3f);
+		/* Apparently this character lies outside the 0x0 - 0x10FFFF
+		 * Unicode range, so don't accept it.
+		 */
+		ASSERT(!"out-of-range Unicode codepoint", "This Unicode codepoint is too large (%u > 0x10FFFF) to be a valid Unicode codepoint", (unsigned int)unicode_char);
 	}
 
 	return next_char;
@@ -430,7 +344,8 @@ static utf_32_char decode_utf16_char(const utf_16_char * const utf16_char, const
 /** Encodes a single Unicode character to a UTF-16 encoded string.
  *
  *  \param unicode_char A UTF-32 encoded Unicode codepoint that will be encoded
- *                      into UTF-16.
+ *                      into UTF-16. This should be a valid Unicode codepoint
+ *                      (i.e. ranging from 0x0 to 0x10FFFF inclusive).
  *  \param out_char     Points to the position in a buffer where the UTF-16
  *                      encoded character can be stored.
  *
@@ -463,7 +378,7 @@ static utf_16_char* encode_utf16_char(const utf_32_char unicode_char, utf_16_cha
 		 * Unicode range, and UTF-16 cannot cope with that, so error
 		 * out.
 		 */
-		ASSERT(!"out-of-range Unicode codepoint", "This Unicode codepoint too large (%u > 0x10FFFF) for the UTF-16 encoding", (unsigned int)unicode_char);
+		ASSERT(!"out-of-range Unicode codepoint", "This Unicode codepoint is too large (%u > 0x10FFFF) to be a valid Unicode codepoint", (unsigned int)unicode_char);
 	}
 
 	return next_char;
