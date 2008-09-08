5bdf883268345ac1265d8ec60689d6ac8bf3d54a
Freddie Witherden <evilguru@gna.org>
2008-09-08 17:45:51 +0000

    Add GPLv2 or above licensing cruft to the SDL betawidget test application; remove the erroneous call to SDL_Quit which was added in r5964.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5970 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/betawidget/tools/sdl-testapp.c
+++ lib/betawidget/tools/sdl-testapp.c
@@ -1,3 +1,24 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2008  Freddie Witherden
+	Copyright (C) 2008  Giel van Schijndel
+	Copyright (C) 2008  Warzone Resurrection Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -129,7 +150,6 @@ int main(int argc, char *argv[])
 
 	lua_close(L);
 	widgetSDLQuit();
-	SDL_Quit();
 
 	return EXIT_SUCCESS;
 }
