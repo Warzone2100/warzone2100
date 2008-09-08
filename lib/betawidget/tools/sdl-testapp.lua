966e1bfedca91e6b1db70a34037e0c962f76daaa
Giel van Schijndel <giel@wz2100.net>
2008-09-08 00:06:04 +0000

    Add an example of how to use a closure in Lua in order to bind user data to an event handler
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@5966 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/betawidget/tools/sdl-testapp.lua
+++ lib/betawidget/tools/sdl-testapp.lua
@@ -2,10 +2,16 @@ wnd = betawidget.window("myWindow", 400, 400)
 wnd:reposition(400, 50)
 wnd:show()
 wnd:addEventHandler(betawidget.EVT_MOUSE_CLICK,
-  function (self, evt, handlerId)
-      print("Clicked")
+-- Closure example: The variable `clicked' is bound to the closure of the
+-- anonymous function which we return here.
+  (function ()
+    local clicked = 0
+    return function (self, evt, handlerId)
+      clicked = clicked + 1
+      print(string.format("Clicked %d times", clicked))
       self:reposition(0, 0)
-  end
+    end
+  end)()
 )
 wnd:addEventHandler(betawidget.EVT_KEY_DOWN,
   function (self, evt, handlerId)
