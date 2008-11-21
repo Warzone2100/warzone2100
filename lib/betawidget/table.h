4d163dd309e52d720929ec2ff7ee7d58884b150a
Freddie Witherden <evilguru@gna.org>
2008-11-21 18:10:56 +0000

    Provide a complete, mostly working, table widget implementation. Unit tests are still required.
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@6357 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/betawidget/table.h
+++ lib/betawidget/table.h
@@ -34,7 +34,6 @@ struct _tableVtbl
 	struct _widgetVtbl widgetVtbl;
 	
 	// No additional virtual methods
-	
 };
 
 struct _table
@@ -50,7 +49,9 @@ struct _table
 	tableVtbl *vtbl;
 	
 	/**
-	 * Child layout properties
+	 * Child layout properties, this must be kept in sync with widget->children
+	 * array at all times. Any add/remove operation on one needs to be
+	 * accompanied by a corresponding operation on the other
 	 */
 	vector *childPositions;
 	
@@ -63,6 +64,26 @@ struct _table
 	 * The number of columns in the table
 	 */
 	int numColumns;
+	
+	/**
+	 * Default vertical alignment of new cells
+	 */
+	vAlign vAlignment;
+	
+	/**
+	 * Default horizontal alignment of new cells
+	 */
+	hAlign hAlignment;
+	
+	/**
+	 * Vertical cell (row) padding
+	 */
+	int rowPadding;
+	
+	/**
+	 * Horizontal cell (column) padding
+	 */
+	int columnPadding;
 };
 
 /*
@@ -82,6 +103,7 @@ extern const classInfo tableClassInfo;
 void tableInit(table *self, const char *id);
 void tableDestroyImpl(widget *self);
 bool tableAddChildDummyImpl(widget *self, widget *child);
+void tableRemoveChildImpl(widget *self, widget *child);
 void tableDoDrawImpl(widget *self);
 bool tableDoLayoutImpl(widget *self);
 size tableGetMinSizeImpl(widget *self);
@@ -100,12 +122,25 @@ size tableGetMaxSizeImpl(widget *self);
 table *tableCreate(const char *id);
 
 /**
+ * Sets the default alignment scheme for newly added child widgets. This is the
+ * horizontal/vertical alignment used if none is specified.
+ *
+ * It is important to note that changing these defaults will only affect
+ * subsequent calls to tableAddChild* and will have no effect on existing cells.
+ *
+ * @param self  The table to set the default alignment of.
+ * @param h The new horizontal alignment scheme to use.
+ * @param v The new vertical alignment scheme to use.
+ */
+void tableSetDefaultAlign(table *self, hAlign h, vAlign v);
+
+/**
  * Returns the number of distinct rows in the table.
  *
  * @param self  The table to return the number of rows of.
  * @return The number of rows in the table.
  */
-int tableGetRowCount(table *self);
+int tableGetRowCount(const table *self);
 
 /**
  * Returns the number of distinct columns in the table.
@@ -113,7 +148,7 @@ int tableGetRowCount(table *self);
  * @param self  The table to return the number of columns of.
  * @return The number of columns in the table.
  */
-int tableGetColumnCount(table *self);
+int tableGetColumnCount(const table *self);
 
 /**
  * Returns the contents of the table at (row,column). Should the cell be empty,
@@ -130,26 +165,61 @@ int tableGetColumnCount(table *self);
  * @param column    The column of the table cell.
  * @return The widget at (row,column) should one exist; otherwise NULL.
  */
-widget *tableGetCell(table *self, int row, int column);
+widget *tableGetCell(const table *self, int row, int column);
 
 /**
  * Adds the widget, child, to the table, self, at (row,column). This method is
  * simply a convenience method which wraps around the more functional
- * tableAddSpannedChild, with rowspan = colspan = 1. As a result, all
- * constraints which apply to tableAddSpannedChild also apply here.
+ * tableAddChildWithSpanAlign. The cells alignment is that of the tables default
+ * and the row-span and column span are both 1.
  *
  * @param self  The table to add the widget to.
  * @param child The child widget to add.
  * @param row   The row in the table to add the child to.
  * @param column    The column in the table add the child to.
  * @return true if child was successfully added, false otherwise.
- * @see tableAddSpannedChild
+ * @see tableAddChildWithSpan
  */
 bool tableAddChild(table *self, widget *child, int row, int column);
 
 /**
- * Adds the widget, child, to the table set, at (row,column) with child spanning
+ * Adds the widget, child, to the table, at (row,column) with child spanning
  * rowspan rows and colspan columns.
+ *
+ * @param self  The table to add the widget to.
+ * @param child The child widget to add.
+ * @param row   The row in the table to add the child to.
+ * @param rowspan   The number of rows spanned by the child.
+ * @param column    The column in the table to add the child to.
+ * @param colpsan   The number of columns spanned by the child.
+ * @return true if the child was successfully added, false otherwise.
+ * @see tableAddChildWithSpanAlign
+ */
+bool tableAddChildWithSpan(table *self, widget *child,
+                           int row, int rowspan,
+                           int column, int colspan);
+
+/**
+ * Adds the widget, child, to the table, self, at (row,column) with the child
+ * having a vertical alignment, v and a horizontal alignment, h.
+ *
+ * @param self  The table to add the widget to.
+ * @param child The child widget to add.
+ * @param row   The row in the table to add the child to.
+ * @param v	Vertical alignment scheme to use.
+ * @param column    The column in the table to add the child to.
+ * @param h Horizontal alignment scheme to use.
+ * @return true if the child was successfully added, false otherwise.
+ * @see tableAddChildWithSpanAlign
+ */
+bool tableAddChildWithAlign(table *self, widget *child,
+                            int row, vAlign v,
+                            int column, hAlign h);
+
+/**
+ * Adds the widget, child, to the table, self, at (row,column) with the child
+ * spanning rowspan rows and colspan columns having a vertical alignment, v and
+ * a horizontal alignment, h.
  *
  * This method is subject to the following constraints:
  *  - 0 < row <= tableGetRowCount(self) + 1;
@@ -161,11 +231,14 @@ bool tableAddChild(table *self, widget *child, int row, int column);
  * @param child The child widget to add.
  * @param row   The row in the table to add the child to.
  * @param rowspan   The number of rows spanned by the child.
+ * @param v Vertical alignment scheme to use.
  * @param column    The column in the table to add the child to.
- * @param colpsan   The number of columns spanned by the child.
- * @return true if the child was successfully added, false otherwise.
+ * @param colspan   The number of columns spanned by the child.
+ * @param v Horizontal alignment of the child.
+* @return true if the child was successfully added, false otherwise.
  */
-bool tableAddSpannedChild(table *self, widget *child, int row, int rowspan,
-                          int column, int colspan);
+bool tableAddChildWithSpanAlign(table *self, widget *child,
+                                int row, int rowspan, vAlign v,
+                                int column, int colspan, hAlign h);
 
 #endif /*TABLE_H_*/
