83d33dc6c5641d0b20bc5f61545c42e117a2e7b1
Giel van Schijndel <giel@wz2100.net>
2009-01-11 21:34:54 +0000

    Update changelogs and bump version numbers in prepration of releasing 2.1.1
    This addresses #202
    
    
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.1@6548 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/config-macosx.h
+++ lib/framework/config-macosx.h
@@ -264,13 +264,13 @@
 #define PACKAGE_NAME "Warzone 2100"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Warzone 2100 2.1"
+#define PACKAGE_STRING "Warzone 2100 2.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "warzone2100"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.1"
+#define PACKAGE_VERSION "2.1.1"
 
 /* Define if <inttypes.h> exists and defines unusable PRI* macros. */
 /* #undef PRI_MACROS_BROKEN */
