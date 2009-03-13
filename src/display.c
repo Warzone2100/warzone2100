a7674f82f2811830920a16906bcaf3e9e3b6c408
Guangcong Luo <zarel@wz2100.net>
2009-03-13 03:32:37 +0000

    Fix bug #309 - display.c errors can't compile
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@6843 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/display.c
+++ src/display.c
@@ -2416,7 +2416,7 @@ static void dealWithRMB( void )
 	BASE_OBJECT			*psClickedOn;
 	DROID				*psDroid;
 	STRUCTURE			*psStructure;
-	BOOL				bDemolish = FALSE;
+	BOOL				bDemolish = false;
 
 	if (driveModeActive() || mouseOverRadar ||
 	    InGameOpUp == true || widgGetFromID(psWScreen,INTINGAMEOP))
@@ -2553,7 +2553,7 @@ static void dealWithRMB( void )
 						if (psDroid->selected &&
 						    chooseOrderObj(psDroid, psClickedOn) == DORDER_DEMOLISH)
 						{
-							bDemolish = TRUE;
+							bDemolish = true;
 							break;
 						}
 					}
