49e1e2026ea7093664c956f533888a3343b908b6
Per Inge Mathisen <per@wz2100.net>
2009-03-31 17:44:21 +0000

    Prepare tagging of upcoming branch
    git-svn-id: svn+ssh://svn.gna.org/svn/warzone/branches/2.2@6935 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/config-macosx.h
+++ lib/framework/config-macosx.h
@@ -264,13 +264,13 @@
 #define PACKAGE_NAME "Warzone 2100"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Warzone 2100 TRUNK"
+#define PACKAGE_STRING "Warzone 2100 2.2 beta 1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "warzone2100"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "TRUNK"
+#define PACKAGE_VERSION "2.2_beta1"
 
 /* Define if <inttypes.h> exists and defines unusable PRI* macros. */
 /* #undef PRI_MACROS_BROKEN */
