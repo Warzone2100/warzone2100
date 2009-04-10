Warzone2100/warzone2100

commit 4388cc3ecc3d2901c6581fe17d5b77bcc90b71e2

Commit patch #366: Show "Connection error" in case of connection error
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@6998 4a71c877-e1ca-e34f-864e-861f7616d084

lib/netplay/netplay.c
@@ -1733,6 +1733,13 @@ BOOL NEThaltJoining(void)
 	return true;
 }
 
+NETERR_TYPES connError = NETERR_NOERR;
+
+NETERR_TYPES getConnError(void)
+{
+	return connError;
+}
+
 // ////////////////////////////////////////////////////////////////////////
 // find games on open connection
 BOOL NETfindGame(void)
@@ -1743,6 +1750,9 @@ BOOL NETfindGame(void)
 	unsigned int port = (hostname == masterserver_name) ? masterserver_port : gameserver_port;
 	int result = 0;
 	debug(LOG_NET, "Looking for games...");
+	
+	if (connError > NETERR_CONN) return false;
+	connError = NETERR_CONN;
 
 	NetPlay.games[0].desc.dwSize = 0;
 	NetPlay.games[0].desc.dwCurrentPlayers = 0;
@@ -1752,6 +1762,7 @@ BOOL NETfindGame(void)
 	{
 		NetPlay.dpidPlayer		= HOST_DPID;
 		NetPlay.bHost			= true;
+		connError = NETERR_NOERR;
 		return true;
 	}
 	// We first check to see if we were given a IP/hostname from the command line
@@ -1847,6 +1858,7 @@ BOOL NETfindGame(void)
 		++gamecount;
 	} while (gamecount < gamesavailable);
 
+	connError = NETERR_NOERR;
 	return true;
 }
 

src/multiint.c
@@ -606,7 +606,7 @@ static void addGames(void)
 	// in case they refresh, and a game becomes available.
 	widgDelete(psWScreen,FRONTEND_NOGAMESAVAILABLE); 
 	// only have to do this if we have any games available.
-	if (gcount)
+	if (!getConnError() && gcount)
 	{
 		for(i=0;i<MaxGames;i++)							// draw games
 		{
@@ -646,7 +646,16 @@ static void addGames(void)
 	{
 	// display that no games are available in lobby.
 	// This is a 'button', not text so it can be hilighted/centered.
-		const char *txt = _("NO GAMES ARE AVAILABLE");
+		const char *txt;
+		switch (getConnError())
+		{
+		case NETERR_NOERR:
+			txt = _("No games are available");
+			break;
+		default:
+			txt = _("Connection Error");
+			break;
+		}
 		W_BUTINIT sButInit;
 
 		// delete old widget if necessary

lib/netplay/netplay.h
@@ -170,6 +170,15 @@ extern NETPLAY				NetPlay;
 
 extern NETMSG NetMsg;
 
+// Connection errors
+
+typedef enum {
+	NETERR_NOERR,
+	NETERR_CONN
+} NETERR_TYPES;
+
+extern NETERR_TYPES getConnError(void);
+
 // ////////////////////////////////////////////////////////////////////////
 // functions available to you.
 extern int   NETinit(BOOL bFirstCall);				// init

commit 04263fb3b0481548bad7f85f176ac0e821028f81

Fix incorrect connection error display introduced in r6998
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@6999 4a71c877-e1ca-e34f-864e-861f7616d084

lib/netplay/netplay.c
@@ -1752,7 +1752,7 @@ BOOL NETfindGame(void)
 	debug(LOG_NET, "Looking for games...");
 	
 	if (connError > NETERR_CONN) return false;
-	connError = NETERR_CONN;
+	connError = NETERR_NOERR;
 
 	NetPlay.games[0].desc.dwSize = 0;
 	NetPlay.games[0].desc.dwCurrentPlayers = 0;
@@ -1762,7 +1762,6 @@ BOOL NETfindGame(void)
 	{
 		NetPlay.dpidPlayer		= HOST_DPID;
 		NetPlay.bHost			= true;
-		connError = NETERR_NOERR;
 		return true;
 	}
 	// We first check to see if we were given a IP/hostname from the command line
@@ -1772,6 +1771,7 @@ BOOL NETfindGame(void)
 		{
 			debug(LOG_ERROR, "Error connecting to client via hostname provided (%s)",iptoconnect);
 			debug(LOG_ERROR, "Cannot resolve hostname :%s",SDLNet_GetError());
+			connError = NETERR_CONN;
 			return false;
 		}
 		else
@@ -1784,6 +1784,7 @@ BOOL NETfindGame(void)
 	else if (SDLNet_ResolveHost(&ip, hostname, port) == -1)
 	{
 		debug(LOG_ERROR, "Cannot resolve hostname \"%s\": %s", hostname, SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 
@@ -1799,6 +1800,7 @@ BOOL NETfindGame(void)
 	if (tcp_socket == NULL)
 	{
 		debug(LOG_ERROR, "Cannot connect to \"%s:%d\": %s", hostname, port, SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 	debug(LOG_NET, "New tcp_socket = %p", tcp_socket);
@@ -1807,6 +1809,7 @@ BOOL NETfindGame(void)
 	if (socket_set == NULL)
 	{
 		debug(LOG_ERROR, "Cannot create socket set: %s", SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 	debug(LOG_NET, "Created socket_set %p", socket_set);
@@ -1831,6 +1834,7 @@ BOOL NETfindGame(void)
 			tcp_socket = NULL; // unsure how to handle invalid sockets?
 		}
 		// when we fail to receive a game count, bail out
+		connError = NETERR_CONN;
 		return false;
 	}
 

src/multiint.c
@@ -652,8 +652,11 @@ static void addGames(void)
 		case NETERR_NOERR:
 			txt = _("No games are available");
 			break;
-		default:
+		case NETERR_CONN:
 			txt = _("Connection Error");
+			break;
+		default:
+			txt = _("Unknown Error");
 			break;
 		}
 		W_BUTINIT sButInit;