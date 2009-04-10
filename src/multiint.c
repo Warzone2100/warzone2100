Warzone2100/warzone2100

commit fd4251c2aaecd5ad27e9c7c24dea8b9d4f6000c4

Backport r6998-r6999: Commit patch #366: Show "Connection error" in case of connection error.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7001 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiint.c
@@ -654,7 +654,17 @@ static void addGames(void)
 	{
 	// display that no games are available in lobby.
 	// This is a 'button', not text so it can be hilighted/centered.
-		const char *txt = _("NO GAMES ARE AVAILABLE");
+		const char *txt;
+		switch (getConnError())
+		{
+		case NETERR_NOERR:
+			txt = _("No games are available");
+			break;
+		case NETERR_CONN:
+		default:
+			txt = _("Connection Error");
+			break;
+		}
 		W_BUTINIT sButInit;
 
 		// delete old widget if necessary

lib/netplay/netplay.c
@@ -1869,6 +1869,15 @@ BOOL NEThaltJoining(void)
 }
 
 // ////////////////////////////////////////////////////////////////////////
+// connection error reading
+NETERR_TYPES connError = NETERR_NOERR;
+
+NETERR_TYPES getConnError(void)
+{
+	return connError;
+}
+
+// ////////////////////////////////////////////////////////////////////////
 // find games on open connection
 BOOL NETfindGame(void)
 {
@@ -1878,6 +1887,9 @@ BOOL NETfindGame(void)
 	unsigned int port = (hostname == masterserver_name) ? masterserver_port : gameserver_port;
 	int result = 0;
 	debug(LOG_NET, "Looking for games...");
+
+	if (connError > NETERR_CONN) return false;
+	connError = NETERR_NOERR;
 
 	NetPlay.games[0].desc.dwSize = 0;
 	NetPlay.games[0].desc.dwCurrentPlayers = 0;
@@ -1896,6 +1908,7 @@ BOOL NETfindGame(void)
 		{
 			debug(LOG_ERROR, "Error connecting to client via hostname provided (%s)",iptoconnect);
 			debug(LOG_ERROR, "Cannot resolve hostname :%s",SDLNet_GetError());
+			connError = NETERR_CONN;
 			return false;
 		}
 		else
@@ -1908,6 +1921,7 @@ BOOL NETfindGame(void)
 	else if (SDLNet_ResolveHost(&ip, hostname, port) == -1)
 	{
 		debug(LOG_ERROR, "Cannot resolve hostname \"%s\": %s", hostname, SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 
@@ -1923,6 +1937,7 @@ BOOL NETfindGame(void)
 	if (tcp_socket == NULL)
 	{
 		debug(LOG_ERROR, "Cannot connect to \"%s:%d\": %s", hostname, port, SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 	debug(LOG_NET, "New tcp_socket = %p", tcp_socket);
@@ -1931,6 +1946,7 @@ BOOL NETfindGame(void)
 	if (socket_set == NULL)
 	{
 		debug(LOG_ERROR, "Cannot create socket set: %s", SDLNet_GetError());
+		connError = NETERR_CONN;
 		return false;
 	}
 	debug(LOG_NET, "Created socket_set %p", socket_set);
@@ -1955,6 +1971,7 @@ BOOL NETfindGame(void)
 			tcp_socket = NULL; // unsure how to handle invalid sockets?
 		}
 		// when we fail to receive a game count, bail out
+		connError = NETERR_CONN;
 		return false;
 	}
 

lib/netplay/netplay.h
@@ -173,6 +173,15 @@ extern NETPLAY				NetPlay;
 
 extern NETMSG NetMsg;
 
+// Connection errors
+
+typedef enum {
+	NETERR_NOERR,
+	NETERR_CONN
+} NETERR_TYPES;
+
+extern NETERR_TYPES getConnError(void);
+
 // ////////////////////////////////////////////////////////////////////////
 // functions available to you.
 extern int   NETinit(BOOL bFirstCall);				// init

commit 41171b4e348b82bd3b8ccefe5e1da69f5504d458

Fix a C99 issue that prevented compiles on MSVC. Broken in r7001
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7008 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiint.c
@@ -655,6 +655,8 @@ static void addGames(void)
 	// display that no games are available in lobby.
 	// This is a 'button', not text so it can be hilighted/centered.
 		const char *txt;
+		W_BUTINIT sButInit;
+
 		switch (getConnError())
 		{
 		case NETERR_NOERR:
@@ -665,7 +667,6 @@ static void addGames(void)
 			txt = _("Connection Error");
 			break;
 		}
-		W_BUTINIT sButInit;
 
 		// delete old widget if necessary
 		widgDelete(psWScreen,FRONTEND_NOGAMESAVAILABLE);