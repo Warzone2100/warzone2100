f7ce2805e50972a0dabc274386f23f8e6014117f
Buginator <buginator@gna.org>
2009-04-11 05:03:31 +0000

    Change config directory for the trunk.
    Yes, everything is now set to default values.
    Do *NOT* copy over your old "config" file and "keymap.map" file from your old config directory!
    You can copy these directories though:
    savegame  (holds your savegames)
    maps      (the ONLY place non default maps should be)
    multiplay  (player stats)
    music      (...)
    
    See what a working svn host can do? ;)
    
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7019 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/main.c
+++ src/main.c
@@ -87,13 +87,13 @@
 
 
 #if defined(WZ_OS_WIN)
-# define WZ_WRITEDIR "Warzone 2100 2.1"
+# define WZ_WRITEDIR "Warzone 2100 2.2"
 #elif defined(WZ_OS_MAC)
 # include <CoreServices/CoreServices.h>
 # include <unistd.h>
-# define WZ_WRITEDIR "Warzone 2100 2.1"
+# define WZ_WRITEDIR "Warzone 2100 2.2"
 #else
-# define WZ_WRITEDIR ".warzone2100-2.1"
+# define WZ_WRITEDIR ".warzone2100-2.2"
 #endif
 
 char datadir[PATH_MAX] = "\0"; // Global that src/clparse.c:ParseCommandLine can write to, so it can override the default datadir on runtime. Needs to be \0 on startup for ParseCommandLine to work!
