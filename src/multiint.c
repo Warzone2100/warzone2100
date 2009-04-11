Warzone2100/warzone2100

commit 4388cc3ecc3d2901c6581fe17d5b77bcc90b71e2

Commit patch #366: Show "Connection error" in case of connection error
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@6998 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiint.c
@@ -606,7 +606,7 @@ static void addGames(void)
 	// in case they refresh, and a game becomes available.
 	widgDelete(psWScreen,FRONTEND_NOGAMESAVAILABLE); 
 	// only have to do this if we have any games available.
-	if (gcount)
+	if (!getConnError() && gcount)
 	{
 		for(i=0;i<MaxGames;i++)							// draw games
 		{
@@ -646,7 +646,16 @@ static void addGames(void)
 	{
 	// display that no games are available in lobby.
 	// This is a 'button', not text so it can be hilighted/centered.
-		const char *txt = _("NO GAMES ARE AVAILABLE");
+		const char *txt;
+		switch (getConnError())
+		{
+		case NETERR_NOERR:
+			txt = _("No games are available");
+			break;
+		default:
+			txt = _("Connection Error");
+			break;
+		}
 		W_BUTINIT sButInit;
 
 		// delete old widget if necessary

lib/netplay/netplay.c
@@ -1733,6 +1733,13 @@ BOOL NEThaltJoining(void)
 	return true;
 }
 
+NETERR_TYPES connError = NETERR_NOERR;
+
+NETERR_TYPES getConnError(void)
+{
+	return connError;
+}
+
 // ////////////////////////////////////////////////////////////////////////
 // find games on open connection
 BOOL NETfindGame(void)
@@ -1743,6 +1750,9 @@ BOOL NETfindGame(void)
 	unsigned int port = (hostname == masterserver_name) ? masterserver_port : gameserver_port;
 	int result = 0;
 	debug(LOG_NET, "Looking for games...");
+	
+	if (connError > NETERR_CONN) return false;
+	connError = NETERR_CONN;
 
 	NetPlay.games[0].desc.dwSize = 0;
 	NetPlay.games[0].desc.dwCurrentPlayers = 0;
@@ -1752,6 +1762,7 @@ BOOL NETfindGame(void)
 	{
 		NetPlay.dpidPlayer		= HOST_DPID;
 		NetPlay.bHost			= true;
+		connError = NETERR_NOERR;
 		return true;
 	}
 	// We first check to see if we were given a IP/hostname from the command line
@@ -1847,6 +1858,7 @@ BOOL NETfindGame(void)
 		++gamecount;
 	} while (gamecount < gamesavailable);
 
+	connError = NETERR_NOERR;
 	return true;
 }
 

lib/netplay/netplay.h
@@ -170,6 +170,15 @@ extern NETPLAY				NetPlay;
 
 extern NETMSG NetMsg;
 
+// Connection errors
+
+typedef enum {
+	NETERR_NOERR,
+	NETERR_CONN
+} NETERR_TYPES;
+
+extern NETERR_TYPES getConnError(void);
+
 // ////////////////////////////////////////////////////////////////////////
 // functions available to you.
 extern int   NETinit(BOOL bFirstCall);				// init

commit 89cf9009aa6c71b18b4c044c1bc8391ba7c99679

Fix a C99 issue that prevented compiles on MSVC. Broken in r6998
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7015 4a71c877-e1ca-e34f-864e-861f7616d084

src/multiint.c
@@ -649,6 +649,8 @@ static void addGames(void)
 	// display that no games are available in lobby.
 	// This is a 'button', not text so it can be hilighted/centered.
 		const char *txt;
+		W_BUTINIT sButInit;
+
 		switch (getConnError())
 		{
 		case NETERR_NOERR:
@@ -661,7 +663,6 @@ static void addGames(void)
 			txt = _("Unknown Error");
 			break;
 		}
-		W_BUTINIT sButInit;
 
 		// delete old widget if necessary
 		widgDelete(psWScreen,FRONTEND_NOGAMESAVAILABLE);