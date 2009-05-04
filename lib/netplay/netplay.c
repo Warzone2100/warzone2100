Warzone2100/warzone2100

commit 7bacf93ab786a44c63e587d0304c13852d71459d

Lobby protocol; change the way the "addg" command works:  * Make the "addg" command return a status code (ranging 200-299 on success) and message (MOTD on succes)  * Don't use the "motd" command anymore to check the lobby status, instead use the returned status code for that purpose.  * Allow MOTD strings of arbitrary length (by sending along the length)
This modification ensures that ''proper'' connectivity checking is being performed on the game server (and reported to the user accurately).

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7323 4a71c877-e1ca-e34f-864e-861f7616d084

lib/netplay/netplay.c
@@ -625,6 +625,71 @@ static int checkSockets(const SocketSet* set, unsigned int timeout)
 	return ret;
 }
 
+/**
+ * Similar to read(2) with the exception that this function won't be
+ * interrupted by signals (EINTR) and will only return when <em>exactly</em>
+ * @c size bytes have been received. I.e. this function blocks until all data
+ * has been received or a timeout occurred.
+ *
+ * @param timeout When non-zero this function times out after @c timeout
+ *                milliseconds. When zero this function blocks until success or
+ *                an error occurs.
+ *
+ * @c return @c size when succesful, less than @c size but at least zero (0)
+ * when the other end disconnected or a timeout occurred. Or @c SOCKET_ERROR if
+ * an error occurred.
+ */
+static ssize_t readAll(Socket* sock, void* buf, size_t size, unsigned int timeout)
+{
+	Socket* sockAr[] = { sock };
+	const SocketSet set = { ARRAY_SIZE(sockAr), sockAr };
+
+	size_t received = 0;
+
+	while (received < size)
+	{
+		ssize_t ret;
+
+		// If a timeout is set, wait for that amount of time for data to arrive (or abort)
+		if (timeout)
+		{
+			ret = checkSockets(&set, timeout);
+			if (ret < set.len
+			 || !sock->ready)
+			{
+				if (ret == 0)
+					setSockErr(ETIMEDOUT);
+				return SOCKET_ERROR;
+			}
+		}
+
+		ret = recv(sock->fd, &((char*)buf)[received], size - received, 0);
+		sock->ready = false;
+		if (ret == 0)
+		{
+			debug(LOG_NET, "Socket disconnected.");
+			return received;
+		}
+
+		if (ret == SOCKET_ERROR)
+		{
+			switch (getSockErr())
+			{
+				case EINTR:
+				case EAGAIN:
+					continue;
+
+				default:
+					return SOCKET_ERROR;
+			}
+		}
+
+		received += ret;
+	}
+
+	return received;
+}
+
 static void socketClose(Socket* sock)
 {
 	if (sock->fd != INVALID_SOCKET)
@@ -1607,7 +1672,7 @@ int NETinit(BOOL bFirstCall)
 		NetPlay.GamePassworded = false;
 		NetPlay.ShowedMOTD = false;
 		NetPlay.gamePassword[0] = '\0';
-		NetPlay.MOTDbuffer[0] = '\0';
+		NetPlay.MOTD = strdup("");
 		sstrcpy(NetPlay.gamePassword,"Enter Password First");
 		NETstartLogging();
 	}
@@ -2465,12 +2530,61 @@ UBYTE NETrecvFile(void)
 	return ((currPos + bytesRead) * 100) / fileSize;
 }
 
+static ssize_t readLobbyResponse(Socket* sock, unsigned int timeout)
+{
+	uint32_t lobbyStatusCode;
+	uint32_t MOTDLength;
+	uint32_t buffer[2];
+	ssize_t result, received = 0;
+
+	// Get status and message length
+	result = readAll(sock, &buffer, sizeof(buffer), timeout);
+	if (result != sizeof(buffer))
+		goto error;
+	received += result;
+	lobbyStatusCode = ntohl(buffer[0]);
+	MOTDLength = ntohl(buffer[1]);
+
+	// Get status message
+	free(NetPlay.MOTD);
+	NetPlay.MOTD = malloc(MOTDLength + 1);
+	result = readAll(sock, NetPlay.MOTD, MOTDLength, timeout);
+	if (result != MOTDLength)
+		goto error;
+	received += result;
+	// NUL terminate string
+	NetPlay.MOTD[MOTDLength] = '\0';
+
+	if (lobbyStatusCode / 100 != 2) // Check whether status code is 2xx (success)
+	{
+		debug(LOG_ERROR, "Lobby error (%u): %s", (unsigned int)lobbyStatusCode, NetPlay.MOTD);
+		return SOCKET_ERROR;
+	}
+
+	debug(LOG_NET, "Lobby success (%u): %s", (unsigned int)lobbyStatusCode, NetPlay.MOTD);
+	return received;
+
+error:
+	if (result == SOCKET_ERROR)
+	{
+		free(NetPlay.MOTD);
+		asprintf(&NetPlay.MOTD, "Error while communicating with the lobby server: %s", strSockError(getSockErr()));
+		debug(LOG_ERROR, "%s", NetPlay.MOTD);
+	}
+	else
+	{
+		free(NetPlay.MOTD);
+		asprintf(&NetPlay.MOTD, "Disconnected from lobby server. Failed to register game.");
+		debug(LOG_ERROR, "%s", NetPlay.MOTD);
+	}
+
+	return SOCKET_ERROR;
+}
+
 static void NETregisterServer(int state)
 {
 	static Socket* rs_socket = NULL;
 	static int registered = 0;
-	static SocketSet* masterset;
-	int result = 0;
 
 	if (server_not_there)
 	{
@@ -2485,10 +2599,12 @@ static void NETregisterServer(int state)
 			{
 				struct addrinfo* cur;
 				struct addrinfo* const hosts = resolveHost(masterserver_name, masterserver_port);
+
 				if (hosts == NULL)
 				{
 					debug(LOG_ERROR, "Cannot resolve masterserver \"%s\": %s", masterserver_name, strSockError(getSockErr()));
-					ssprintf(NetPlay.MOTDbuffer, _("Could not resolve masterserver name (%s)!"), masterserver_name);
+					free(NetPlay.MOTD);
+					asprintf(&NetPlay.MOTD, _("Could not resolve masterserver name (%s)!"), masterserver_name);
 					server_not_there = true;
 					return;
 				}
@@ -2504,60 +2620,31 @@ static void NETregisterServer(int state)
 				if(rs_socket == NULL)
 				{
 					debug(LOG_ERROR, "Cannot connect to masterserver \"%s:%d\": %s", masterserver_name, masterserver_port, strSockError(getSockErr()));
-					ssprintf(NetPlay.MOTDbuffer, _("Could not communicate with lobby server! Is TCP port %u open?"), masterserver_port);
+					free(NetPlay.MOTD);
+					asprintf(&NetPlay.MOTD, _("Could not communicate with lobby server! Is TCP port %u open?"), masterserver_port);
 					server_not_there = true;
 					return;
 				}
-				// master server socket set to be friendly
-				if (!masterset)
-				{
-					masterset = allocSocketSet(1);
-					if (!masterset)
-					{
-						debug(LOG_ERROR, "Couldn't create socket set for master server because: %s", strSockError(getSockErr()));
-					}
-					if (addSocket(masterset, rs_socket))
-					{
-						debug(LOG_NET,"TCP_AddSocket using socket set %p, socket %p", masterset, rs_socket);
-					}
-				}
-				// get the MOTD from the server
-				writeAll(rs_socket, "motd", sizeof("motd"));
-				if (checkSockets(masterset, 1000) == SOCKET_ERROR)
-				{
-					debug(LOG_NET, "Warning, MOTD CheckSockets Failed. Error %s", strSockError(getSockErr()));
-				}
-				if (rs_socket->ready)	// true on activity
-				{
-					result = readNoInt(rs_socket, NetPlay.MOTDbuffer, sizeof(NetPlay.MOTDbuffer) - 1);
-					if (result <= 0)
-					{
-						debug(LOG_NET, "Warning, MOTD TCP_Recv failed. result = %d, error %s", result,  strSockError(getSockErr()));
-						sstrcpy(NetPlay.MOTDbuffer, "MOTD communication error with lobby server, TCP_Recv failed!");
-					}
-					// Make sure to NUL terminate the MOTD string
-					NetPlay.MOTDbuffer[result] = '\0';
-					debug(LOG_NET, "MOTD is [%s]", NetPlay.MOTDbuffer);
-				}
-				else
-				{
-					debug(LOG_NET, "Warning, MOTD Socket Not ready.  Is the motd.txt file empty?  Error %s", strSockError(getSockErr()));
-					sstrcpy(NetPlay.MOTDbuffer, "MOTD communication error with lobby server, socket not ready?");
-				}
 
+				// Register our game with the lobby server
 				writeAll(rs_socket, "addg", sizeof("addg"));
-				// and now send what the server wants
 				NETsendGAMESTRUCT(rs_socket, &game);
+
+				// Get the return code
+				if (readLobbyResponse(rs_socket, 1000) == SOCKET_ERROR)
+				{
+					// The socket has been invalidated, so get rid of it. (using it now may cause SIGPIPE).
+					socketClose(rs_socket);
+					rs_socket = NULL;
+					return;
+				}
 			}
 			break;
 
 			case 0:
 				// we don't need this anymore, so clean up
-				delSocket(masterset,rs_socket);
-				free(masterset);
-				masterset = NULL;
 				socketClose(rs_socket);
-				rs_socket=NULL;
+				rs_socket = NULL;
 			break;
 		}
 		registered=state;

lib/netplay/netplay.h
@@ -203,7 +203,7 @@ typedef struct {
 	char gamePassword[StringSize];		//
 	bool GamePassworded;				// if we have a password or not.
 	bool ShowedMOTD;					// only want to show this once
-	char MOTDbuffer[255];				// buffer for MOTD
+	char* MOTD;
 } NETPLAY;
 
 /// This is the hardcoded dpid (player ID) value for the hosting player.

src/multijoin.c
@@ -337,5 +337,5 @@ void ShowMOTD(void)
 {
 	// when HOST joins the game, show server MOTD message first
 	addConsoleMessage(_("System message:"), DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
-	addConsoleMessage(NetPlay.MOTDbuffer, DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
+	addConsoleMessage(NetPlay.MOTD, DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
 }

tools/masterserver/wzmasterserver.py
@@ -131,22 +131,30 @@ requestlock = Lock()
 protocol = Protocol()
 
 class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHandler):
+	SUCCESS_OK = 200
+
+	CLIENT_ERROR_NOT_ACCEPTABLE = 406
+
+	def sendStatusMessage(self, status, message):
+		logging.debug("(%s) Sending response status (%d) and message: %s" % (self.gameHost, status, message))
+		self.wfile.write(struct.pack('!II%ds' % len(message), status, len(message), message))
+
 	def __init__(self, request, client_address, server):
 		self.request = request
 		self.client_address = client_address
 		self.server = server
 
 		# Client's address
-		gameHost = self.client_address[0]
+		self.gameHost = self.client_address[0]
 
 		try:
 			self.setup()
 			try:
 				self.handle()
 			except struct.error, e:
-				logging.warning("(%s) Data decoding error: %s" % (gameHost, traceback.format_exc()))
+				logging.warning("(%s) Data decoding error: %s" % (self.gameHost, traceback.format_exc()))
 			except:
-				logging.error("(%s) Unhandled exception: %s" % (gameHost, traceback.format_exc()))
+				logging.error("(%s) Unhandled exception: %s" % (self.gameHost, traceback.format_exc()))
 				raise
 			finally:
 				self.finish()
@@ -168,10 +176,6 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 				gamedb.checkGames()
 				requests = 0
 
-		# client address
-		gameHost = self.client_address[0]
-
-
 		while True:
 			# Read the incoming command.
 			netCommand = self.rfile.read(4)
@@ -185,7 +189,8 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 			# Process the incoming command. #
 			#################################
 
-			logging.debug("(%s) Command: [%s] " % (gameHost, netCommand))
+			logging.debug("(%s) Command: [%s] " % (self.gameHost, netCommand))
+			# TODO: When releasing a new 2.2 version remove the "motd" command
 			# Give the MOTD
 			if netCommand == 'motd':
 				self.wfile.write(MOTDstring[0:255])
@@ -194,7 +199,7 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 			# Add a game.
 			elif netCommand == 'addg':
 				# The host is valid
-				logging.debug("(%s) Adding gameserver..." % gameHost)
+				logging.debug("(%s) Adding gameserver..." % self.gameHost)
 
 				# create a game object
 				self.g = Game()
@@ -206,17 +211,19 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 				while True:
 					newGameData = protocol.decodeSingle(self.rfile, self.g)
 					if not newGameData:
-						logging.debug("(%s) Removing aborted game" % gameHost)
+						logging.debug("(%s) Removing aborted game" % self.gameHost)
 						return
 
-					logging.debug("(%s) Updated game: %s" % (gameHost, self.g))
+					logging.debug("(%s) Updated game: %s" % (self.gameHost, self.g))
 					#set gamehost
-					self.g.host = gameHost
+					self.g.host = self.gameHost
 
 					if not protocol.check(self.g):
-						logging.debug("(%s) Removing unreachable game" % gameHost)
+						logging.debug("(%s) Removing unreachable game" % self.gameHost)
+						self.sendStatusMessage(self.CLIENT_ERROR_NOT_ACCEPTABLE, 'Game unreachable, failed to open a connection to: [%s]:%d' % (self.g.host, protocol.lobbyPort))
 						return
 
+					self.sendStatusMessage(self.SUCCESS_OK, MOTDstring)
 					gamedb.listGames()
 				return
 			# Get a game list.
@@ -224,7 +231,7 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 				# Lock the gamelist to prevent new games while output.
 				with gamedb.lock:
 					games = list(gamedb.getGames())
-					logging.debug("(%s) Gameserver list: %i game(s)" % (gameHost, len(games)))
+					logging.debug("(%s) Gameserver list: %i game(s)" % (self.gameHost, len(games)))
 
 					# Transmit the games.
 					for game in games:
@@ -234,7 +241,7 @@ class RequestHandler(SocketServer.ThreadingMixIn, SocketServer.StreamRequestHand
 
 			# If something unknown appears.
 			else:
-				raise Exception("(%s) Received an unknown command: %s" % (gameHost, netCommand))
+				raise Exception("(%s) Received an unknown command: %s" % (self.gameHost, netCommand))
 
 	def finish(self):
 		if self.g:
@@ -270,7 +277,7 @@ if __name__ == '__main__':
 	in_file = open("motd.txt", "r")
 	MOTDstring = in_file.read()
 	in_file.close()
-	logging.info("The MOTD is (%s)" % MOTDstring[0:255])
+	logging.info("The MOTD is (%s)" % MOTDstring)
 
 
 	gamedb = GameDB()

commit c639258772d42f0a17ba906e31fa6de9e076d6dd

Merge r7271, r7323 and r7324 from trunk into the 2.2 branch:
r7271:
  Lets not use an intermediate buffer for the MOTD before adding it to the console (as a message)

r7323:
  Lobby protocol; change the way the "addg" command works:
   * Make the "addg" command return a status code (ranging 200-299 on success) and message (MOTD on succes)
   * Don't use the "motd" command anymore to check the lobby status, instead use the returned status code for that purpose.
   * Allow MOTD strings of arbitrary length (by sending along the length)

  This modification ensures that ''proper'' connectivity checking is being performed on the game server (and reported to the user accurately).

r7324:
  Fix error introduced in r7323: gameHost is a member of "self" now, not a local variable

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7325 4a71c877-e1ca-e34f-864e-861f7616d084

lib/netplay/netplay.c
@@ -170,6 +170,70 @@ static int NETCODE_VERSION_MINOR = 2;	// unused for now
 static int NUMBER_OF_MODS = 0;			// unused for now
 static int NETCODE_HASH = 0;			// unused for now
 
+/**
+ * Similar to read(2) with the exception that this function won't be
+ * interrupted by signals (EINTR) and will only return when <em>exactly</em>
+ * @c size bytes have been received. I.e. this function blocks until all data
+ * has been received or a timeout occurred.
+ *
+ * @param timeout When non-zero this function times out after @c timeout
+ *                milliseconds. When zero this function blocks until success or
+ *                an error occurs.
+ *
+ * @c return @c size when succesful, less than @c size but at least zero (0)
+ * when the other end disconnected or a timeout occurred. Or @c -1 if an error
+ * occurred.
+ */
+static int readAll(TCPsocket sock, void* buf, size_t size, unsigned int timeout)
+{
+	int received = 0;
+
+	SDLNet_SocketSet set = SDLNet_AllocSocketSet(1);
+	if (set == NULL)
+	{
+		debug(LOG_ERROR, "Failed to allocate socket set");
+		return -1;
+	}
+
+	SDLNet_TCP_AddSocket(set, sock);
+
+	while (received < size)
+	{
+		int ret;
+
+		// If a timeout is set, wait for that amount of time for data to arrive (or abort)
+		if (timeout)
+		{
+			if (SDLNet_CheckSockets(set, timeout) < 1
+			 || !SDLNet_SocketReady(sock))
+			{
+				debug(LOG_NET, "Timed out");
+				received = -1
+				goto error:
+			}
+		}
+
+		ret = SDLNet_TCP_Recv(sock, &((char*)buf)[received], size - received);
+		if (ret == 0)
+		{
+			debug(LOG_NET, "Socket disconnected.");
+			goto error:
+		}
+
+		if (ret == -1)
+		{
+			received = -1;
+			goto error;
+		}
+
+		received += ret;
+	}
+
+error:
+	SDLNet_TCP_DelSocket(set, sock);
+	SDLNet_FreeSocketSet(set);
+	return received;
+}
 void sendVersionCheck( void )
 {
 	uint32_t playerdpid = NetPlay.dpidPlayer;
@@ -919,7 +983,7 @@ int NETinit(BOOL bFirstCall)
 		NetPlay.GamePassworded = false;
 		NetPlay.ShowedMOTD = false;
 		NetPlay.gamePassword[0] = '\0';
-		NetPlay.MOTDbuffer[0] = '\0';
+		NetPlay.MOTD = strdup("");
 		sstrcpy(NetPlay.gamePassword,"Enter Password First");
 		NETstartLogging();
 	}
@@ -1761,6 +1825,57 @@ UBYTE NETrecvFile(void)
 	return ((currPos + bytesRead) * 100) / fileSize;
 }
 
+static int readLobbyResponse(TCPsocket sock, unsigned int timeout)
+{
+	uint32_t lobbyStatusCode;
+	uint32_t MOTDLength;
+	uint32_t buffer[2];
+	int result, received = 0;
+
+	// Get status and message length
+	result = readAll(sock, &buffer, sizeof(buffer), timeout);
+	if (result != sizeof(buffer))
+		goto error;
+	received += result;
+	lobbyStatusCode = SDL_SwapBE32(buffer[0]);
+	MOTDLength = SDL_SwapBE32(buffer[1]);
+
+	// Get status message
+	free(NetPlay.MOTD);
+	NetPlay.MOTD = malloc(MOTDLength + 1);
+	result = readAll(sock, NetPlay.MOTD, MOTDLength, timeout);
+	if (result != MOTDLength)
+		goto error;
+	received += result;
+	// NUL terminate string
+	NetPlay.MOTD[MOTDLength] = '\0';
+
+	if (lobbyStatusCode / 100 != 2) // Check whether status code is 2xx (success)
+	{
+		debug(LOG_ERROR, "Lobby error (%u): %s", (unsigned int)lobbyStatusCode, NetPlay.MOTD);
+		return -1;
+	}
+
+	debug(LOG_NET, "Lobby success (%u): %s", (unsigned int)lobbyStatusCode, NetPlay.MOTD);
+	return received;
+
+error:
+	if (result == -1)
+	{
+		free(NetPlay.MOTD);
+		asprintf(&NetPlay.MOTD, "Error while communicating with the lobby server: %s", SDLNet_GetError());
+		debug(LOG_ERROR, "%s", NetPlay.MOTD);
+	}
+	else
+	{
+		free(NetPlay.MOTD);
+		asprintf(&NetPlay.MOTD, "Disconnected from lobby server. Failed to register game.");
+		debug(LOG_ERROR, "%s", NetPlay.MOTD);
+	}
+
+	return -1;
+}
+
 static void NETregisterServer(int state)
 {
 	static TCPsocket rs_socket = NULL;
@@ -1782,7 +1897,8 @@ static void NETregisterServer(int state)
 				if(SDLNet_ResolveHost(&ip, masterserver_name, masterserver_port) == -1)
 				{
 					debug(LOG_ERROR, "Cannot resolve masterserver \"%s\": %s", masterserver_name, SDLNet_GetError());
-					ssprintf(NetPlay.MOTDbuffer, _("Could not resolve masterserver name (%s)!"), masterserver_name);
+					free(NetPlay.MOTD);
+					asprintf(&NetPlay.MOTD, _("Could not resolve masterserver name (%s)!"), masterserver_name);
 					server_not_there = true;
 					return;
 				}
@@ -1791,7 +1907,8 @@ static void NETregisterServer(int state)
 				if(rs_socket == NULL)
 				{
 					debug(LOG_ERROR, "Cannot connect to masterserver \"%s:%d\": %s", masterserver_name, masterserver_port, SDLNet_GetError());
-					sstrcpy(NetPlay.MOTDbuffer, _("Could not communicate with lobby server!  Is TCP port 9990 open?"));
+					free(NetPlay.MOTD);
+					NetPlay.MOTD = strdup(_("Could not communicate with lobby server!  Is TCP port 9990 open?"));
 					server_not_there = true;
 					return;
 				}
@@ -1809,34 +1926,19 @@ static void NETregisterServer(int state)
 						debug(LOG_NET,"TCP_AddSocket using socket set %p, socket %p", masterset, rs_socket);
 					}
 				}
-				// get the MOTD from the server
-				SDLNet_TCP_Send(rs_socket, (void*)"motd", sizeof("motd"));
-				result = SDLNet_CheckSockets(masterset, 1000);
-				if (result ==-1)
-				{
-					debug(LOG_NET, "Warning, MOTD CheckSockets Failed. Error %s", SDLNet_GetError());
-				}
-				if (SDLNet_SocketReady(rs_socket))	// true on activity
-				{
-					result = SDLNet_TCP_Recv(rs_socket, NetPlay.MOTDbuffer, sizeof(NetPlay.MOTDbuffer) - 1);
-					if (result <= 0)
-					{
-						debug(LOG_NET, "Warning, MOTD TCP_Recv failed. result = %d, error %s", result,  SDLNet_GetError());
-						sstrcpy(NetPlay.MOTDbuffer, "MOTD communication error with lobby server, TCP_Recv failed!");
-					}
-					// Make sure to NUL terminate the MOTD string
-					NetPlay.MOTDbuffer[result] = '\0';
-					debug(LOG_NET, "MOTD is [%s]", NetPlay.MOTDbuffer);
-				}
-				else
-				{
-					debug(LOG_NET, "Warning, MOTD Socket Not ready.  Is the motd.txt file empty?  Error %s", SDLNet_GetError());
-					sstrcpy(NetPlay.MOTDbuffer, "MOTD communication error with lobby server, socket not ready?");
-				}
 				
+				// Register our game with the lobby server
 				SDLNet_TCP_Send(rs_socket, (void*)"addg", sizeof("addg"));
-				// and now send what the server wants
 				NETsendGAMESTRUCT(rs_socket, &game);
+
+				// Get the return code
+				if (readLobbyResponse(rs_socket, 1000) == -1)
+				{
+					// The socket has been invalidated, so get rid of it. (using it now may cause SIGPIPE).
+					SDLNet_TCP_Close(rs_socket);
+					rs_socket = NULL;
+					return;
+				}
 			}
 			break;
 
@@ -1850,7 +1952,7 @@ static void NETregisterServer(int state)
 				SDLNet_FreeSocketSet(masterset);
 				masterset = NULL;
 				SDLNet_TCP_Close(rs_socket);
-				rs_socket=NULL;
+				rs_socket = NULL;
 			break;
 		}
 		registered=state;

lib/netplay/netplay.h
@@ -202,7 +202,7 @@ typedef struct {
 	char gamePassword[StringSize];		//
 	bool GamePassworded;				// if we have a password or not.
 	bool ShowedMOTD;					// only want to show this once
-	char MOTDbuffer[255];				// buffer for MOTD
+	char* MOTD;
 } NETPLAY;
 
 /// This is the hardcoded dpid (player ID) value for the hosting player.

src/multijoin.c
@@ -330,12 +330,9 @@ void setupNewPlayer(UDWORD dpid, UDWORD player)
 // unfortunatly, we don't get the message until after the setup is done.
 void ShowMOTD(void)
 {
-	char buf[255];
 	// when HOST joins the game, show server MOTD message first
-	ssprintf(buf, _("System message:"));
-	addConsoleMessage(buf, DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
-	ssprintf(buf, "%s", NetPlay.MOTDbuffer);
-	addConsoleMessage(buf, DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
+	addConsoleMessage(_("System message:"), DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
+	addConsoleMessage(NetPlay.MOTD, DEFAULT_JUSTIFY, NOTIFY_MESSAGE);
 }
 
 // ////////////////////////////////////////////////////////////////////////////