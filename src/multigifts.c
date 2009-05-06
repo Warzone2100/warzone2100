Warzone2100/warzone2100

commit c08a223531fdd610791da433677e7846dd676c69

Fix an array out of bounds error (off by one) that could occur when picking up an artifact during a multiplayer game.
From: Gerard Krol <gerard_@gna.org>

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@6969 4a71c877-e1ca-e34f-864e-861f7616d084

src/multigifts.c
@@ -738,7 +738,7 @@ void giftArtifact(UDWORD owner, UDWORD x, UDWORD y)
 	{
 		pO	 = asPlayerResList[owner];
 
-		for (topic = numResearch; topic > 0; topic--)
+		for (topic = numResearch - 1; topic >= 0; topic--)
 		{
 			if (IsResearchCompleted(&pO[topic])
 			 && !IsResearchPossible(&pR[topic]))

commit 36e42f53c9f946435d1e616543825cbe4d29cc09

Let's not underflow a unsigned variable. Broken in r6969
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7334 4a71c877-e1ca-e34f-864e-861f7616d084

src/multigifts.c
@@ -738,7 +738,7 @@ void giftArtifact(UDWORD owner, UDWORD x, UDWORD y)
 	{
 		pO	 = asPlayerResList[owner];
 
-		for (topic = numResearch - 1; topic >= 0; topic--)
+		for (topic = numResearch - 1; topic > 0; topic--)
 		{
 			if (IsResearchCompleted(&pO[topic])
 			 && !IsResearchPossible(&pR[topic]))