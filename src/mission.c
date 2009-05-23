616e04be82255f115401f96b812ee4c9853f2923
Buginator <buginator@gna.org>
2009-05-23 20:01:33 +0000

    Only use abort() for fatal conditions: abort() cleanup patch.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7528 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/mission.c
+++ src/mission.c
@@ -478,7 +478,7 @@ BOOL startMission(LEVEL_TYPE missionType, char *pGame)
 		{
 			//error!
 			debug( LOG_ERROR, "Unknown Mission Type" );
-			abort();
+
 			loaded = false;
 		}
 	}
@@ -486,7 +486,7 @@ BOOL startMission(LEVEL_TYPE missionType, char *pGame)
 	if (!loaded)
 	{
 		debug( LOG_ERROR, "Unable to load mission file" );
-		abort();
+
 		return false;
 	}
 
