Warzone2100/warzone2100

commit 6a8abcb158cfbacdf54023b655c1bb899bb92efb

Simplify some dreadful code by adding the TILE_HAS_WALL(pTile) macro, and reintroducing the pTile->psObject reference that was optimized away to save memory on the PSX.
git-svn-id: svn+ssh://svn.gna.org/svn/warzone/trunk@1734 4a71c877-e1ca-e34f-864e-861f7616d084

src/structure.c
@@ -167,9 +167,6 @@ static	STRUCTURE_STATS	*g_psStatDestroyStruct = NULL;
 // the structure that was last hit
 STRUCTURE	*psLastStructHit;
 
-//flag for oil derrick anim
-//static	UBYTE	powerGenExists[MAX_PLAYERS];
-
 //flag for drawing radar
 static		UBYTE	hqExists[MAX_PLAYERS];
 //flag for drawing all sat uplink sees
@@ -1701,15 +1698,12 @@ void buildFlatten(STRUCTURE_STATS *pStructureType, UDWORD atx, UDWORD aty,UDWORD
 /*Builds an instance of a Structure - the x/y passed in are in world coords. */
 STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, UDWORD player, BOOL FromSave)
 {
-	STRUCTURE*	psBuilding;
-	FEATURE		*psFeature, *psFNext;
 	UDWORD		mapX, mapY, mapH;
 	UDWORD		width, breadth, weapon, capacity, bodyDiff = 0;
 	SDWORD		wallType = 0, preScrollMinX = 0, preScrollMinY = 0, preScrollMaxX = 0, preScrollMaxY = 0;
-	MAPTILE		*psTile;
-	BOOL		bUpgraded;
 	UDWORD		min,max;
 	int			i;
+	STRUCTURE	*psBuilding = NULL;
 
 	if (IsStatExpansionModule(pStructureType)==FALSE)
 	{
@@ -1792,8 +1786,7 @@ STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, U
 			}
 		}
 
-		if (pStructureType->type == REF_DEFENSE &&
-			mapTile(x >> TILE_SHIFT, y >> TILE_SHIFT)->tileInfoBits & BITS_WALL)
+		if (pStructureType->type == REF_DEFENSE && TILE_HAS_WALL(mapTile(x >> TILE_SHIFT, y >> TILE_SHIFT)))
 		{
 			// building a gun tower over a wall, replace it
 			psBuilding = getTileStructure(x >> TILE_SHIFT, y >> TILE_SHIFT);
@@ -1825,12 +1818,28 @@ STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, U
 		//set up the imd to use for the display
 		psBuilding->sDisplay.imd = pStructureType->pIMD;
 
+		//if resource extractor - need to remove oil feature and prox Msg
+		if (pStructureType->type == REF_RESOURCE_EXTRACTOR)
+		{
+			FEATURE *psFeature = getTileFeature(map_coord(x), map_coord(y));
+
+			if (psFeature->psStats->subType == FEAT_OIL_RESOURCE)
+			{
+				// remove it from the map
+				turnOffMultiMsg(TRUE); // dont send this one!
+				removeFeature(psFeature);
+				turnOffMultiMsg(FALSE);
+			} else {
+				ASSERT(FALSE, "buildStructure: Tried to build derrick but feature is not oil at (%u, %u)!", x, y);
+			}
+		}
+
 		mapH = buildFoundation(pStructureType, x, y);
 		for (width = 0; width < pStructureType->baseWidth; width++)
 		{
 			for (breadth = 0; breadth < pStructureType->baseBreadth; breadth++)
 			{
-				psTile = mapTile(mapX+width,mapY+breadth);
+				MAPTILE *psTile = mapTile(mapX+width,mapY+breadth);
 
 				// don't really think this should be done here, but dont know otherwise.alexl
 				if(pStructureType->type == REF_WALLCORNER || pStructureType->type == REF_WALL)
@@ -1853,20 +1862,12 @@ STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, U
 					       mapX + width, mapY + breadth);
 				}
 
-				SET_TILE_STRUCTURE(psTile);
+				psTile->psObject = (BASE_OBJECT*)psBuilding;
+
 				// if it's a tall structure then flag it in the map.
 				if(psBuilding->sDisplay.imd->ymax > TALLOBJECT_YMAX) 
 				{
 					SET_TILE_TALLSTRUCTURE(psTile);
-				}
-				if ((pStructureType->type == REF_WALL) ||
-					(pStructureType->type == REF_WALLCORNER))
-				{
-					psTile->tileInfoBits |= BITS_WALL;
-				}
-				if (pStructureType->height == 1)
-				{
-					SET_TILE_SMALLSTRUCTURE(psTile);
 				}
 			}
 		}
@@ -2063,46 +2064,18 @@ STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, U
 		gridAddObject((BASE_OBJECT *)psBuilding);
 
 		clustNewStruct(psBuilding);
-
-		//if resource extractor - need to remove oil feature and prox Msg
-		if (pStructureType->type == REF_RESOURCE_EXTRACTOR)
-		{
-			//find the resource at this point
-			for (psFeature = apsFeatureLists[0]; psFeature != NULL; psFeature = psFNext)
-			{
-				psFNext = psFeature->psNext;
-				if (psFeature->psStats->subType == FEAT_OIL_RESOURCE)
-				{
-					if ((psFeature->x == psBuilding->x) && (psFeature->y == psBuilding->y))
-					{
-						//remove it from the map
-						turnOffMultiMsg(TRUE); // dont send this one!
-						removeFeature(psFeature);
-						turnOffMultiMsg(FALSE);
-
-						//set the map to hold the resource extractor again
-						SET_TILE_STRUCTURE(mapTile(psFeature->x >> TILE_SHIFT,
-							psFeature->y >> TILE_SHIFT));
-						// if it's a tall structure then flag it in the map.
-						if(psBuilding->sDisplay.imd->ymax > TALLOBJECT_YMAX) {
-
-							SET_TILE_TALLSTRUCTURE(mapTile(psFeature->x >> TILE_SHIFT,
-								psFeature->y >> TILE_SHIFT));
-						}
-					}
-				}
-			}
-		}
 	}
 	else //its an upgrade
 	{
-		bUpgraded = FALSE;
+		BOOL bUpgraded = FALSE;
+
+		psBuilding = getTileStructure(map_coord(x), map_coord(y));
+
 		//don't create the Structure use existing one
-		psBuilding = getTileStructure(x>>TILE_SHIFT, y>>TILE_SHIFT);
+
 		if (psBuilding == NULL)
 		{
-			debug( LOG_ERROR, "No owning structure for this module - %s", getStructName(pStructureType) );
-			abort();
+			ASSERT(FALSE, "No owning structure for this module - %s", getStructName(pStructureType));
 			return FALSE;
 		}
 		if (pStructureType->type == REF_FACTORY_MODULE)
@@ -2287,6 +2260,7 @@ STRUCTURE* buildStructure(STRUCTURE_STATS* pStructureType, UDWORD x, UDWORD y, U
 
 	/* why is this necessary - it makes tiles under the structure visible */
 	setUnderTilesVis((BASE_OBJECT*)psBuilding,player);
+
 	return psBuilding;
 }
 
@@ -4306,7 +4280,6 @@ BOOL validLocation(BASE_STATS *psStats, UDWORD x, UDWORD y, UDWORD player,
 	UDWORD				min, max;
 	HIGHLIGHT			site;
 	FLAG_POSITION		*psCurrFlag;
-	MAPTILE				*psTile;
 
 	psBuilding = (STRUCTURE_STATS *)psStats;
 
@@ -4487,6 +4460,8 @@ BOOL validLocation(BASE_STATS *psStats, UDWORD x, UDWORD y, UDWORD player,
 	if (psStats->ref >= REF_STRUCTURE_START &&
 		psStats->ref < (REF_STRUCTURE_START + REF_RANGE))
 	{
+		MAPTILE	*psTile;
+
 		switch(psBuilding->type)
 		{
 			case REF_HQ:
@@ -4666,9 +4641,9 @@ BOOL validLocation(BASE_STATS *psStats, UDWORD x, UDWORD y, UDWORD player,
 							psTile = mapTile(i,j);
 							if (TILE_OCCUPIED(psTile))
 							{
-								if ((psBuilding->type == REF_DEFENSE ||
-									psBuilding->type == REF_WALL) &&
-									(psTile->tileInfoBits & BITS_WALL))
+								if (TILE_HAS_WALL(psTile)
+								    && (psBuilding->type == REF_DEFENSE ||
+-                                                                       psBuilding->type == REF_WALL))
 								{
 									psStruct = getTileStructure(i,j);
 									if (psStruct != NULL &&
@@ -5074,11 +5049,10 @@ void removeStructFromMap(STRUCTURE *psStruct)
 		for (j = 0; j < psStruct->pStructureType->baseBreadth; j++)
 		{
 			psTile = mapTile(mapX+i, mapY+j);
-			SET_TILE_EMPTY(psTile);
+			psTile->psObject = NULL;
 			CLEAR_TILE_NODRAW(psTile);
 			CLEAR_TILE_TALLSTRUCTURE(psTile);
 			CLEAR_TILE_NOTBLOCKING(psTile);
-			CLEAR_TILE_SMALLSTRUCTURE(psTile);
 		}
 	}
 }

src/action.c
@@ -1095,7 +1095,7 @@ BOOL actionRouteBlockingPos(DROID *psDroid, SDWORD tx, SDWORD ty)
 			if (tileOnMap(i,j))
 			{
 				psTile = mapTile(i,j);
-				if (psTile->tileInfoBits & BITS_WALL)
+				if (TILE_HAS_WALL(psTile))
 				{
 					psWall = getTileStructure((UDWORD)i,(UDWORD)j);
 					//Watermelon:fixes AI try to destroy ally's wall bug

src/feature.c
@@ -725,7 +725,6 @@ FEATURE * buildFeature(FEATURE_STATS *psStats, UDWORD x, UDWORD y,BOOL FromSave)
 	FEATURE		*psFeature;
 	UDWORD		mapX, mapY;
 	UDWORD		width,breadth, foundationMin,foundationMax, height;
-	MAPTILE		*psTile;
 	UDWORD		startX,startY,max,min;
 	SDWORD		i;
 	UBYTE		vis;
@@ -843,14 +842,12 @@ FEATURE * buildFeature(FEATURE_STATS *psStats, UDWORD x, UDWORD y,BOOL FromSave)
 	if(psFeature->psStats->subType==FEAT_BUILD_WRECK)
 	{
 
-//		psFeature->sDisplay.imd = wreckageImds[rand()%MAX_WRECKAGE];
 		psFeature->sDisplay.imd = getRandomWreckageImd();
 
 	}
 	else
 	{
 		psFeature->sDisplay.imd = psStats->psImd;
-//DBPRINTF(("%d %d\n",psStats->psImd->ymin,psStats->psImd->ymax);
   	}
 
 
@@ -860,8 +857,9 @@ FEATURE * buildFeature(FEATURE_STATS *psStats, UDWORD x, UDWORD y,BOOL FromSave)
 	{
 		for (breadth = 0; breadth <= psStats->baseBreadth; breadth++)
 		{
-			//check not outside of map - for load save game
+			MAPTILE *psTile = mapTile(mapX + width, mapY + breadth);
 
+			//check not outside of map - for load save game
 			ASSERT( (mapX+width) < mapWidth,
 				"x coord bigger than map width - %s, id = %d",
 				getName(psFeature->psStats->pName), psFeature->id );
@@ -869,14 +867,14 @@ FEATURE * buildFeature(FEATURE_STATS *psStats, UDWORD x, UDWORD y,BOOL FromSave)
 				"y coord bigger than map height - %s, id = %d",
 				getName(psFeature->psStats->pName), psFeature->id );
 
-			psTile = mapTile(mapX+width, mapY+breadth);
 			if (width != psStats->baseWidth && breadth != psStats->baseBreadth)
 			{
-				ASSERT( !(TILE_HAS_FEATURE(mapTile(mapX+width,mapY+breadth))),
+				ASSERT( !(TILE_HAS_FEATURE(psTile)),
 					"buildFeature - feature- %d already found at %d, %d",
 					psFeature->id, mapX+width,mapY+breadth );
 
-				SET_TILE_FEATURE(psTile);
+				psTile->psObject = (BASE_OBJECT*)psFeature;
+
 				// if it's a tall feature then flag it in the map.
 				if(psFeature->sDisplay.imd->ymax > TALLOBJECT_YMAX) {
 					SET_TILE_TALLSTRUCTURE(psTile);
@@ -911,7 +909,6 @@ FEATURE * buildFeature(FEATURE_STATS *psStats, UDWORD x, UDWORD y,BOOL FromSave)
 //	else
 //	{
 //		psFeature->sDisplay.imd = psStats->psImd;
-//DBPRINTF(("%d %d\n",psStats->psImd->ymin,psStats->psImd->ymax);
 // 	}
 
 	// add the feature to the grid
@@ -957,7 +954,6 @@ void featureUpdate(FEATURE *psFeat)
 void removeFeature(FEATURE *psDel)
 {
 	UDWORD		mapX, mapY, width,breadth, player;
-	MAPTILE		*psTile;
 	MESSAGE		*psMessage;
 	Vector3i pos;
 
@@ -986,9 +982,10 @@ void removeFeature(FEATURE *psDel)
 	{
 		for (breadth = 0; breadth < psDel->psStats->baseBreadth; breadth++)
 		{
-			psTile = mapTile(mapX+width, mapY+breadth);
-			/* Don't need to worry about clearing structure bits - they should not be there! */
-			SET_TILE_EMPTY(psTile);
+			MAPTILE *psTile = mapTile(mapX + width, mapY + breadth);
+
+			psTile->psObject = NULL;
+
 			CLEAR_TILE_NODRAW(psTile);
 			CLEAR_TILE_TALLSTRUCTURE(psTile);
 			CLEAR_TILE_NOTBLOCKING(psTile);
@@ -1056,14 +1053,11 @@ void destroyFeature(FEATURE *psDel)
 	Vector3i pos;
 	UDWORD			width,breadth;
 	UDWORD			mapX,mapY;
-	MAPTILE			*psTile;
 	UDWORD			texture;
-
 
 	ASSERT( psDel != NULL,
 		"destroyFeature: invalid feature pointer\n" );
 
-//---------------------------------------------------------------------------------------
  	/* Only add if visible and damageable*/
 	if(psDel->visible[selectedPlayer] && psDel->psStats->damageable)
 	{
@@ -1107,20 +1101,20 @@ void destroyFeature(FEATURE *psDel)
 			mapX = (psDel->x - psDel->psStats->baseWidth * TILE_UNITS / 2) >> TILE_SHIFT;
 			mapY = (psDel->y - psDel->psStats->baseBreadth * TILE_UNITS / 2) >> TILE_SHIFT;
 //			if(psDel->sDisplay.imd->ymax>300)
-		if(psDel->psStats->subType == FEAT_SKYSCRAPER)
+			if (psDel->psStats->subType == FEAT_SKYSCRAPER)
 			{
 				for (width = 0; width < psDel->psStats->baseWidth; width++)
 				{
 					for (breadth = 0; breadth < psDel->psStats->baseBreadth; breadth++)
 					{
-						psTile = mapTile(mapX+width,mapY+breadth);
+						MAPTILE *psTile = mapTile(mapX+width,mapY+breadth);
 						// stops water texture chnaging for underwateer festures
 					 	if(TERRAIN_TYPE(psTile) != TER_WATER)
 						{
 							if(TERRAIN_TYPE(psTile) != TER_CLIFFFACE)
 							{
 						   		/* Clear feature bits */
-								SET_TILE_EMPTY(mapTile(mapX+width,mapY+breadth));
+								psTile->psObject = NULL;
 								texture = (psTile->texture & (~TILE_NUMMASK));
 								texture |= DRIVE_OVER_RUBBLE_TILE;//getRubbleTileNum();
 								psTile->texture = (UWORD)texture;
@@ -1128,7 +1122,7 @@ void destroyFeature(FEATURE *psDel)
 							else
 							{
 							   /* This remains a blocking tile */
-								SET_TILE_EMPTY(mapTile(mapX+width,mapY+breadth));
+								psTile->psObject = NULL;
 								texture = (psTile->texture & (~TILE_NUMMASK));
 								texture |= NO_DRIVE_OVER_RUBBLE_TILE;//getRubbleTileNum();
 								psTile->texture = (UWORD)texture;

src/geometry.c
@@ -157,153 +157,6 @@ SDWORD	sum;
 	return 0;
 }
 
-/* Get the structure pointer for a specified tile coord. NULL if no structure */
-STRUCTURE	*getTileStructure(UDWORD x, UDWORD y)
-{
-STRUCTURE	*psStructure;
-STRUCTURE	*psReturn;
-UDWORD		centreX, centreY;
-UDWORD		strX,strY;
-UDWORD		width,breadth;
-UDWORD		i;
-
-	/* No point in checking if there's no structure here! */
-	if(!TILE_HAS_STRUCTURE(mapTile(x,y)))
-	{
-		return(NULL);
-	}
-
-	/* Otherwise - see which one it is! */
-	psReturn = NULL;
-	/* Get the world coords for the tile centre */
-	centreX = (x<<TILE_SHIFT)+(TILE_UNITS/2);
-	centreY = (y<<TILE_SHIFT)+(TILE_UNITS/2);
-
-	/* Go thru' all players - drop out if match though */
-	for(i=0; i<MAX_PLAYERS && !psReturn; i++)
-	{
-		/* Got thru' all structures for this player - again drop out if match */
-		for (psStructure = apsStructLists[i];
-			psStructure && !psReturn; psStructure = psStructure->psNext)
-		{
-			/* Get structure coords */
-			strX = psStructure->x;
-			strY = psStructure->y;
-			/* And extents */
-			width = psStructure->pStructureType->baseWidth*TILE_UNITS;
-			breadth = psStructure->pStructureType->baseBreadth*TILE_UNITS;
-			/* Within x boundary? */
-
-
-			if((centreX > (strX-(width/2))) && (centreX < (strX+(width/2))) )
-			{
-				if((centreY > (strY-(breadth/2))) && (centreY < (strY+(breadth/2))) )
-				{
-					psReturn = psStructure;
-				}
-			}
-
-/*			if((centreX > (strX-width)) && (centreX < (strX+width)) )
-			{
-				if((centreY > (strY-breadth)) && (centreY < (strY+breadth)) )
-				{
-					psReturn = psStructure;
-				}
-			}
-*/
-		}
-	}
-	/* Send back either NULL or structure */
-	return(psReturn);
-}
-
-/* Sends back the feature on the specified tile - NULL if no feature */
-FEATURE	*getTileFeature(UDWORD x, UDWORD y)
-{
-FEATURE		*psFeature;
-FEATURE		*psReturn;
-UDWORD		centreX, centreY;
-UDWORD		strX,strY;
-UDWORD		width,breadth;
-//UDWORD		i;
-
-	/* No point in checking if there's no feature here! */
-	if(!TILE_HAS_FEATURE(mapTile(x,y)))
-	{
-		return(NULL);
-	}
-
-	/* Otherwise - see which one it is! */
-	psReturn = NULL;
-	/* Get the world coords for the tile centre */
-	centreX = (x<<TILE_SHIFT)+(TILE_UNITS/2);
-	centreY = (y<<TILE_SHIFT)+(TILE_UNITS/2);
-
-	/* Go through all features for this player - again drop out if we get one */
-	for (psFeature = apsFeatureLists[0];
-		psFeature && !psReturn; psFeature = psFeature->psNext)
-		{
-			/* Get the features coords */
-			strX = psFeature->x;
-			strY = psFeature->y;
-			/* And it's base dimensions */
-			width = psFeature->psStats->baseWidth*TILE_UNITS;
-			breadth = psFeature->psStats->baseBreadth*TILE_UNITS;
-			/* Does tile centre lie within the area covered by base of feature? */
-			/* First check for x */
-			if((centreX > (strX-(width/2))) && (centreX < (strX+(width/2))) )
-			{
-				/* Got a match on the x - now try y */
-				if((centreY > (strY-(breadth/2))) && (centreY < (strY+(breadth/2))) )
-				{
-					/* Got it! */
-					psReturn = psFeature;
-				}
-			}
-		}
-
-	/* Send back either NULL or feature pointer */
-	return(psReturn);
-}
-
-/*	Will return a base_object pointer to either a structure or feature - depending
-	what's on tile. Returns NULL if nothing */
-BASE_OBJECT	*getTileOccupier(UDWORD x, UDWORD y)
-{
-
-//DBPRINTF(("gto x=%d y=%d (%d,%d)\n",x,y,x*TILE_UNITS,y*TILE_UNITS);
-	/* Firsty - check there is something on it?! */
-	if(!TILE_OCCUPIED(mapTile(x,y)))
-	{
-//DBPRINTF(("gto nothing\n");
-		/* Nothing here at all! */
-		return(NULL);
-	}
-
-	/* Now check we can see it... */
-	if(TEST_TILE_VISIBLE(selectedPlayer,mapTile(x,y)) == FALSE)
-	{
-		return(NULL);
-	}
-
-	/* Has it got a fetaure? */
-	if(TILE_HAS_FEATURE(mapTile(x,y)))
-	{
-//DBPRINTF(("gto feature\n");
-		/* Return the feature */
-		return( (BASE_OBJECT *) getTileFeature(x,y) );
-	}
-	/*	Otherwise check for a structure - we can do else here since a tile cannot
-		have both a feature and structure simultaneously */
-	else if (TILE_HAS_STRUCTURE(mapTile(x,y)))
-	{
-//DBPRINTF(("gto structure\n");
-		/* Send back structure pointer */
-		return( (BASE_OBJECT *) getTileStructure(x,y) );
-	}
-	return NULL;
-}
-
 // Approximates a square root - never more than 11% out...
 UDWORD dirtySqrt( SDWORD x1, SDWORD y1, SDWORD x2, SDWORD y2)
 {

src/geometry.h
@@ -22,6 +22,9 @@
 #ifndef _geometry_h
 #define _geometry_h
 
+#include "map.h"
+#include "hci.h"
+
 typedef struct _t_tri
 {
 POINT	coords[3];
@@ -32,10 +35,6 @@ typedef struct _t_quad
 POINT	coords[4];
 } QUAD;
 
-extern BASE_OBJECT	*getTileOccupier(UDWORD x, UDWORD y);
-extern STRUCTURE	*getTileStructure(UDWORD x, UDWORD y);
-extern FEATURE		*getTileFeature(UDWORD x, UDWORD y);
-
 extern	UDWORD			adjustDirection	( SDWORD present, SDWORD difference );
 extern	SDWORD			calcDirection	( UDWORD x0, UDWORD y0, UDWORD x1, UDWORD y1 );
 extern	void			initBulletTable	( void );
@@ -44,5 +43,27 @@ extern DROID			*getNearestDroid ( UDWORD x, UDWORD y, BOOL bSelected );
 extern BOOL	droidOnScreen ( DROID *psDroid, SDWORD tolerance );
 
 extern UDWORD	dirtySqrt	( SDWORD x1, SDWORD y1, SDWORD x2, SDWORD y2 );
+
+static inline STRUCTURE *getTileStructure(UDWORD x, UDWORD y)
+{
+	return (STRUCTURE *)mapTile(x,y)->psObject;
+}
+
+static inline FEATURE *getTileFeature(UDWORD x, UDWORD y)
+{
+	return (FEATURE *)mapTile(x,y)->psObject;
+}
+
+static inline BASE_OBJECT *getTileOccupier(UDWORD x, UDWORD y)
+{
+	MAPTILE *psTile = mapTile(x,y);
+
+	if (TEST_TILE_VISIBLE(selectedPlayer, psTile))
+	{
+		return mapTile(x,y)->psObject;
+	} else {
+		return NULL;
+	}
+}
 
 #endif

src/lighting.c
@@ -207,7 +207,6 @@ static void normalsOnTile(UDWORD tileX, UDWORD tileY, UDWORD quadrant)
 	case 0:
 	case 2:
 		/* Is it flipped? In this case one triangle  */
-//		if(psTile->triangleFlip)
 		if(TRI_FLIPPED(psTile))
 		{
 			if(quadrant==0)
@@ -274,7 +273,6 @@ static void normalsOnTile(UDWORD tileX, UDWORD tileY, UDWORD quadrant)
 	case 1:
 	case 3:
 		/* Is it flipped? In this case two triangles  */
-//		if(psTile->triangleFlip)
 		if(TRI_FLIPPED(psTile))
 		{
 	   	 	corner1.x = tileX<<TILE_SHIFT;

src/map.c
@@ -329,13 +329,10 @@ static BOOL mapLoadV2(char *pFileData, UDWORD fileSize)
 
 //		psMapTiles[i].type = psTileData->type;
 		psMapTiles[i].height = psTileData->height;
-//		psMapTiles[i].onFire = 0;
 		// Changed line - alex
-//		psMapTiles[i].rippleIndex = (UBYTE) (i%RIP_SIZE);
 		//end of change - alex
 		for (j=0; j<MAX_PLAYERS; j++)
 		{
-//			psMapTiles[i].tileVisible[j]=FALSE;
 			psMapTiles[i].tileVisBits =(UBYTE)(( (psMapTiles[i].tileVisBits) &~ (UBYTE)(1<<j) ));
 		}
 		psTileData = (MAP_SAVETILEV2 *)(((UBYTE *)psTileData) + SAVE_TILE_SIZE);
@@ -383,13 +380,10 @@ static BOOL mapLoadV3(char *pFileData, UDWORD fileSize)
 
 //		psMapTiles[i].type = psTileData->type;
 		psMapTiles[i].height = psTileData->height;
-//		psMapTiles[i].onFire = 0;
 		// Changed line - alex
-//		psMapTiles[i].rippleIndex = (UBYTE) (i%RIP_SIZE);
 		//end of change - alex
 		for (j=0; j<MAX_PLAYERS; j++)
 		{
-//			psMapTiles[i].tileVisible[j]=FALSE;
 			psMapTiles[i].tileVisBits =(UBYTE)(( (psMapTiles[i].tileVisBits) &~ (UBYTE)(1<<j) ));
 		}
 		psTileData = (MAP_SAVETILEV2 *)(((UBYTE *)psTileData) + SAVE_TILE_SIZE);

src/map.h
@@ -80,30 +80,26 @@ typedef enum _terrain_type
 
 #define TILE_IS_NOTBLOCKING(x)	(x->texture & TILE_NOTBLOCKING)
 
-#define TILE_IMPOSSIBLE(x)		((TILE_HAS_STRUCTURE(x)) && (TILE_HAS_FEATURE(x)))
-#define BITS_OCCUPIED			(BITS_STRUCTURE | BITS_FEATURE | BITS_WALL)
-#define TILE_OCCUPIED(x)		(x->tileInfoBits & BITS_OCCUPIED)
-#define TILE_HAS_STRUCTURE(x)	(x->tileInfoBits & BITS_STRUCTURE)
-#define TILE_HAS_FEATURE(x)		(x->tileInfoBits & BITS_FEATURE)
+#define TILE_OCCUPIED(x)		(x->psObject)
+#define TILE_HAS_STRUCTURE(x)		(x->psObject && x->psObject->type == OBJ_STRUCTURE)
+#define TILE_HAS_FEATURE(x)		(x->psObject && x->psObject->type == OBJ_FEATURE)
+#define TILE_HAS_WALL(x)		(TILE_HAS_STRUCTURE(x) \
+					&& (((STRUCTURE*)x->psObject)->pStructureType->type == REF_WALL \
+					    || ((STRUCTURE*)x->psObject)->pStructureType->type == REF_WALLCORNER))
 #define TILE_DRAW(x)			(!((x)->tileInfoBits & BITS_NODRAWTILE))
 #define TILE_HIGHLIGHT(x)		(x->texture & TILE_HILIGHT)
 #define TILE_HAS_TALLSTRUCTURE(x)	(x->tileInfoBits & BITS_TALLSTRUCTURE)
-#define TILE_HAS_SMALLSTRUCTURE(x)	(x->tileInfoBits & BITS_SMALLSTRUCTURE)
+#define TILE_HAS_SMALLSTRUCTURE(x)	(TILE_HAS_STRUCTURE(x) && ((STRUCTURE*)x->psObject)->pStructureType->height == 1)
 
 #define SET_TILE_NOTBLOCKING(x)	(x->texture |= TILE_NOTBLOCKING)
 #define CLEAR_TILE_NOTBLOCKING(x)	(x->texture &= ~TILE_NOTBLOCKING)
 
-#define SET_TILE_STRUCTURE(x)	(x->tileInfoBits = (UBYTE)(((x->tileInfoBits & (~BITS_OCCUPIED))) | BITS_STRUCTURE))
-#define SET_TILE_FEATURE(x)		(x->tileInfoBits = (UBYTE)(((x->tileInfoBits & (~BITS_OCCUPIED))) | BITS_FEATURE))
-#define SET_TILE_EMPTY(x)		(x->tileInfoBits = (UBYTE) (x->tileInfoBits & (~BITS_OCCUPIED)) )
 #define SET_TILE_NODRAW(x)		(x->tileInfoBits = (UBYTE)((x)->tileInfoBits | BITS_NODRAWTILE))
 #define CLEAR_TILE_NODRAW(x)	(x->tileInfoBits = (UBYTE)((x)->tileInfoBits & (~BITS_NODRAWTILE)))
 #define SET_TILE_HIGHLIGHT(x)	(x->texture = (UWORD)((x)->texture | TILE_HILIGHT))
 #define CLEAR_TILE_HIGHLIGHT(x)	(x->texture = (UWORD)((x)->texture & (~TILE_HILIGHT)))
 #define SET_TILE_TALLSTRUCTURE(x)	(x->tileInfoBits = (UBYTE)((x)->tileInfoBits | BITS_TALLSTRUCTURE))
 #define CLEAR_TILE_TALLSTRUCTURE(x)	(x->tileInfoBits = (UBYTE)((x)->tileInfoBits & (~BITS_TALLSTRUCTURE)))
-#define SET_TILE_SMALLSTRUCTURE(x)	(x->tileInfoBits = (UBYTE)((x)->tileInfoBits | BITS_SMALLSTRUCTURE))
-#define CLEAR_TILE_SMALLSTRUCTURE(x)	(x->tileInfoBits = (UBYTE)((x)->tileInfoBits & (~BITS_SMALLSTRUCTURE)))
 
 // Multiplier for the tile height
 #define	ELEVATION_SCALE	2
@@ -137,14 +133,8 @@ typedef struct _maptile
 	UBYTE			bMaxed;
 	UBYTE			level;
 	UBYTE			inRange;		// sensor range display.
+	BASE_OBJECT		*psObject;		// Any object sitting on the location (e.g. building)
 
-									// This is also used to store the tile flip flags
-//  What's been removed - 46 bytes per tile so far
-//	BASE_OBJECT		*psObject;		// Any object sitting on the location (e.g. building)
-//	UBYTE			onFire;			// Is tile on fire?
-//	UBYTE			rippleIndex;	// Current value in ripple table?
-//	BOOL			tileVisible[MAX_PLAYERS]; // Which players can see the tile?
-//	BOOL			triangleFlip;	// Is the triangle flipped?
 //	TYPE_OF_TERRAIN	type;			// The terrain type for the tile
 } MAPTILE;
 

commit 712fc51db172cf8e32e9f71292ed7693ddd02b15

Fix a bug broken in r1734  (doesn't make any sense to check a negative value for building type)
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7506 4a71c877-e1ca-e34f-864e-861f7616d084

src/structure.c
@@ -4274,9 +4274,7 @@ BOOL validLocation(BASE_STATS *psStats, UDWORD x, UDWORD y, UDWORD player,
 							psTile = mapTile(i,j);
 							if (TileIsOccupied(psTile))
 							{
-								if (TileHasWall(psTile)
-								    && (psBuilding->type == REF_DEFENSE ||
--                                                                       psBuilding->type == REF_WALL))
+								if (TileHasWall(psTile) && (psBuilding->type == REF_DEFENSE || psBuilding->type == REF_WALL))
 								{
 									psStruct = getTileStructure(i,j);
 									if (psStruct != NULL &&