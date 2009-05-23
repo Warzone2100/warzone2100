c541cc63f00e95cce9834bd14037d6d9d82b72bd
Buginator <buginator@gna.org>
2009-05-23 19:47:52 +0000

    Only use abort() for fatal conditions: abort() cleanup patch.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7520 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/init.c
+++ src/init.c
@@ -531,7 +531,7 @@ init_ObjectDead( void * psObj )
 
 			default:
 				debug( LOG_ERROR, "init_ObjectAnimRemoved: unrecognised object type" );
-				abort();
+				return false;
 		}
 	}
 
@@ -899,7 +899,7 @@ BOOL stageTwoInitialise(void)
 	if(!initMiscImds())			/* Set up the explosions */
 	{
 		iV_ShutDown();
-		debug( LOG_ERROR, "Can't find all the explosions PCX's" );
+		debug( LOG_ERROR, "Can't find all the explosions graphics?" );
 		abort();
 		return false;
 	}
