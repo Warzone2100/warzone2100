Warzone2100/warzone2100

commit dc98cd6e16f30d631d3c1b2c9d67ec2362a45a81

Add additional checks in isLasSat(), since it appears we can have invalid pointers being passed.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7747 4a71c877-e1ca-e34f-864e-861f7616d084

src/structure.h
@@ -210,6 +210,8 @@ extern BOOL placeDroid(STRUCTURE *psStructure, UDWORD *droidX, UDWORD *droidY);
 /* is this a lassat structure? */
 static inline BOOL isLasSat(STRUCTURE_STATS *pStructureType)
 {
+	ASSERT_OR_RETURN(false, pStructureType != NULL, "LasSat is invalid?");
+	ASSERT_OR_RETURN(false, pStructureType->psWeapStat[0] != NULL, "This doesn't appear to be a LasSat?");
 	return (pStructureType->psWeapStat[0]
 	        && pStructureType->psWeapStat[0]->weaponSubClass == WSC_LAS_SAT);
 }

commit 0c77636d5e022651ba0bb017c874864d6f14f294

Remove 1 Assert check that was introduced in r7747.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7751 4a71c877-e1ca-e34f-864e-861f7616d084

src/structure.h
@@ -211,7 +211,7 @@ extern BOOL placeDroid(STRUCTURE *psStructure, UDWORD *droidX, UDWORD *droidY);
 static inline BOOL isLasSat(STRUCTURE_STATS *pStructureType)
 {
 	ASSERT_OR_RETURN(false, pStructureType != NULL, "LasSat is invalid?");
-	ASSERT_OR_RETURN(false, pStructureType->psWeapStat[0] != NULL, "This doesn't appear to be a LasSat?");
+
 	return (pStructureType->psWeapStat[0]
 	        && pStructureType->psWeapStat[0]->weaponSubClass == WSC_LAS_SAT);
 }