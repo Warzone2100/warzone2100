Warzone2100/warzone2100

commit 3f9f9bfe37bbe5c2f07549566b28b658e250b3f9

When we have a level change, we must reset the firebit, or that tile will 'burn' ~forever, since the effects were cleared prior to the level change.
closes ticket:611


git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7737 4a71c877-e1ca-e34f-864e-861f7616d084

src/effects.c
@@ -254,7 +254,7 @@ static void effectStructureUpdates(void);
 static void effectDroidUpdates(void);
 
 static UDWORD effectGetNumFrames(EFFECT *psEffect);
-
+static void killEffect(EFFECT *e);
 
 /*!
  * Initialise memory between first and last as singly linked list
@@ -266,7 +266,8 @@ static void initEffectPool(EFFECT *first, EFFECT *last)
 	EFFECT *it;
 	for (it = first; it < last; it++)
 	{
-		 // We do not need a double-linked-list for inactiveeffects, since we always pick from the front:
+		killEffect(it);		// We must clear the fire bit on tiles.
+		// We do not need a double-linked-list for inactiveeffects, since we always pick from the front:
 		it->prev = NULL;
 		it->next = it+1;
 	}
@@ -310,7 +311,7 @@ static EFFECT *Effect_malloc(void)
 	if (inactiveList.first == NULL)
 	{
 		/* Allocate new effect chunk */
-		EffectChunk *chunk = malloc(sizeof(EffectChunk));
+		EffectChunk *chunk = calloc(1, sizeof(EffectChunk));
 
 		debug(LOG_MEMORY, "%zd effects in use, allocating %d extra", activeList.num, EFFECT_CHUNK_SIZE);
 
@@ -375,10 +376,16 @@ static void Effect_free(void *self)
 void shutdownEffectsSystem(void)
 {
 	EffectChunk *chunk;
+	int i = 0;
 
 	for (chunk = chunkList.first; chunk != NULL;)
 	{
 		EffectChunk *chunkNext = chunk->next;
+		for (i=0; i < EFFECT_CHUNK_SIZE; i++)
+		{
+			// clear the blasted fire effects!
+			killEffect(&chunk->effects[i]);
+		}
 		free(chunk);
 		chunk = chunkNext;
 	}
@@ -398,7 +405,7 @@ void initEffectsSystem(void)
 	shutdownEffectsSystem();
 
 	/* Allocate new chunk */
-	chunk = malloc(sizeof(EffectChunk));
+	chunk = calloc(1, sizeof(EffectChunk));
 
 	/* Deal with out-of-memory conditions */
 	if (chunk == NULL)

src/levels.c
@@ -45,6 +45,7 @@
 #include "scripttabs.h"
 #include "research.h"
 #include "lib/framework/lexer_input.h"
+#include "effects.h"
 
 extern int lev_get_lineno(void);
 extern char* lev_get_text(void);
@@ -487,6 +488,8 @@ BOOL levReleaseAll(void)
 {
 	SDWORD i;
 
+	// clear out old effect data first
+	initEffectsSystem();
 	// release old data if any was loaded
 	if (psCurrLevel != NULL)
 	{

commit b59f996595b5caf87105b0141bc143a64fe0246b

Fix bug introduced in r7737 where we would iterate wrongly over uninitialized effects during shutdown, and badly initialize new nodes in the effect chunks. This caused a counter underflow and crash during any game save. This closes ticket:640 for real this time.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7801 4a71c877-e1ca-e34f-864e-861f7616d084

src/effects.c
@@ -266,7 +266,6 @@ static void initEffectPool(EFFECT *first, EFFECT *last)
 	EFFECT *it;
 	for (it = first; it < last; it++)
 	{
-		killEffect(it);		// We must clear the fire bit on tiles.
 		// We do not need a double-linked-list for inactiveeffects, since we always pick from the front:
 		it->prev = NULL;
 		it->next = it+1;
@@ -370,24 +369,21 @@ static void Effect_free(void *self)
 
 	/* Adjust counts */
 	inactiveList.num++;
+	ASSERT_OR_RETURN(, activeList.num > 0, "Underflow");
 	activeList.num--;
 }
 
 void shutdownEffectsSystem(void)
 {
-	EffectChunk *chunk;
-	int i = 0;
+	EFFECT *eff;
 
-	for (chunk = chunkList.first; chunk != NULL;)
+	/* Traverse the list */
+	for (eff = activeList.first; eff;)
 	{
-		EffectChunk *chunkNext = chunk->next;
-		for (i=0; i < EFFECT_CHUNK_SIZE; i++)
-		{
-			// clear the blasted fire effects!
-			killEffect(&chunk->effects[i]);
-		}
-		free(chunk);
-		chunk = chunkNext;
+		EFFECT *effNext = eff->next;
+
+		killEffect(eff);
+		eff = effNext;
 	}
 	chunkList.first = NULL;
 	chunkList.last = NULL;