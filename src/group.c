cfda22f528ecac91265722b8bc60b411ab0ac1fa
Guangcong Luo <zarel@wz2100.net>
2009-07-19 22:24:45 +0000

    Fix compile error introduced by r7884
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7886 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/group.c
+++ src/group.c
@@ -208,8 +208,8 @@ void grpLeave(DROID_GROUP *psGroup, DROID *psDroid)
 {
 	DROID	*psPrev, *psCurr;
 
-	ASSERT_OR_RETURN(false, grpInitialized, "Group code not initialized yet");
-	ASSERT_OR_RETURN(false, psGroup != NULL,
+	ASSERT_OR_RETURN(, grpInitialized, "Group code not initialized yet");
+	ASSERT_OR_RETURN(, psGroup != NULL,
 		"grpLeave: invalid group pointer" );
 
 	if (psDroid != NULL && psDroid->psGroup != psGroup)
