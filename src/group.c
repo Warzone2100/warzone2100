Warzone2100/warzone2100

commit fbf69897a92ab8e1d8923c1ab80303c981e660df

Fix crash when attempting to leave a null group.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7884 4a71c877-e1ca-e34f-864e-861f7616d084

src/group.c
@@ -208,8 +208,8 @@ void grpLeave(DROID_GROUP *psGroup, DROID *psDroid)
 {
 	DROID	*psPrev, *psCurr;
 
-	ASSERT(grpInitialized, "Group code not initialized yet");
-	ASSERT( psGroup != NULL,
+	ASSERT_OR_RETURN(false, grpInitialized, "Group code not initialized yet");
+	ASSERT_OR_RETURN(false, psGroup != NULL,
 		"grpLeave: invalid group pointer" );
 
 	if (psDroid != NULL && psDroid->psGroup != psGroup)

commit c0a37d56754bf6bc60b4c05dfcb5d4e72e2d84bf

Fix compile error introduced by r7884
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7886 4a71c877-e1ca-e34f-864e-861f7616d084

src/group.c
@@ -208,8 +208,8 @@ void grpLeave(DROID_GROUP *psGroup, DROID *psDroid)
 {
 	DROID	*psPrev, *psCurr;
 
-	ASSERT_OR_RETURN(false, grpInitialized, "Group code not initialized yet");
-	ASSERT_OR_RETURN(false, psGroup != NULL,
+	ASSERT_OR_RETURN(, grpInitialized, "Group code not initialized yet");
+	ASSERT_OR_RETURN(, psGroup != NULL,
 		"grpLeave: invalid group pointer" );
 
 	if (psDroid != NULL && psDroid->psGroup != psGroup)