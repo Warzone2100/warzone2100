3d230678d35def71adf887a1e10b5814bde7baa3
Guangcong Luo <zarel@wz2100.net>
2009-08-02 23:03:51 +0000

    General order/action code cleanup: - rewrite secondaryGetState to just return the state instead of pushing it into a pointer - disallow WSS turret from targeting things it can't see - introduce a "temporary hold" order - fix the bug that considered keyboard shortcuts cheats that could not be used in multiplayer - make trucks and repair turrets hold their position when in "Hold Position" mode (this involves moving the #define of REPAIR_RANGE from action.c to action.h, in case that affects anyone)
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7926 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/ai.c
+++ src/ai.c
@@ -650,7 +650,6 @@ BOOL aiChooseTarget(BASE_OBJECT *psObj, BASE_OBJECT **ppsTarget, int weapon_slot
 {
 	BASE_OBJECT		*psTarget = NULL;
 	DROID			*psCommander;
-	SECONDARY_STATE		state;
 	SDWORD			curTargetWeight=-1;
 
 	/* Get the sensor range */
@@ -700,7 +699,7 @@ BOOL aiChooseTarget(BASE_OBJECT *psObj, BASE_OBJECT **ppsTarget, int weapon_slot
 		        || curTargetWeight <= 0		// attacker had no valid target, use new one
 			|| newTargetWeight > curTargetWeight + OLD_TARGET_THRESHOLD)	// updating and new target is better
 		    && validTarget(psObj, psTarget, weapon_slot)
-		    && (aiDroidHasRange((DROID *)psObj, psTarget, weapon_slot) || (secondaryGetState((DROID *)psObj, DSO_HALTTYPE, &state) && state != DSS_HALT_HOLD)))
+		    && (aiDroidHasRange((DROID *)psObj, psTarget, weapon_slot) || (secondaryGetState((DROID *)psObj, DSO_HALTTYPE) != DSS_HALT_HOLD)))
 		{
 			ASSERT(!isDead(psTarget), "aiChooseTarget: Droid found a dead target!");
 			*ppsTarget = psTarget;
@@ -912,7 +911,6 @@ BOOL aiChooseSensorTarget(BASE_OBJECT *psObj, BASE_OBJECT **ppsTarget)
 void aiUpdateDroid(DROID *psDroid)
 {
 	BASE_OBJECT	*psTarget;
-	SECONDARY_STATE	state;
 	BOOL		lookForTarget,updateTarget;
 
 	ASSERT(psDroid != NULL, "Invalid droid pointer");
@@ -984,8 +982,7 @@ void aiUpdateDroid(DROID *psDroid)
 	if ((psDroid->order == DORDER_NONE) &&
 		(psDroid->player == selectedPlayer) &&
 		!isVtolDroid(psDroid) &&
-		secondaryGetState(psDroid, DSO_HALTTYPE, &state) &&
-		(state == DSS_HALT_GUARD))
+		secondaryGetState(psDroid, DSO_HALTTYPE) == DSS_HALT_GUARD)
 	{
 		lookForTarget = false;
 		updateTarget = false;
@@ -1019,12 +1016,9 @@ void aiUpdateDroid(DROID *psDroid)
 	}
 
 	// do not attack if the attack level is wrong
-	if (secondaryGetState(psDroid, DSO_ATTACK_LEVEL, &state))
+	if (secondaryGetState(psDroid, DSO_ATTACK_LEVEL) != DSS_ALEV_ALWAYS)
 	{
-		if (state != DSS_ALEV_ALWAYS)
-		{
-			lookForTarget = false;
-		}
+		lookForTarget = false;
 	}
 
 	/* Don't rebuild 'Naybor' list too often */
