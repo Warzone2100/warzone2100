cdf00a1da41ac06ed95bb82ebbbc919d6a7b0ff7
Per Inge Mathisen <per@wz2100.net>
2009-08-08 12:29:25 +0000

    Remove erronous comment and definitions belonging to long since removed force editor.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7940 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/multiint.h
+++ src/multiint.h
@@ -229,7 +229,7 @@ void loadMapPreview(bool hideInterface);
 #define MULTIOP_POWER			10296
 #define MULTIOP_ALLIANCES		10298
 #define MULTIOP_BASETYPE		10300
-#define MULTIOP_TECHLEVEL		10302	// WARNING 10300+ used in forceditor.
+#define MULTIOP_TECHLEVEL		10302
 #define MULTIOP_COMPUTER		10304
 #define	MULTIOP_FOG				10306
 
@@ -251,11 +251,9 @@ void loadMapPreview(bool hideInterface);
 #define MULTIOP_PASSWORD	920010
 #define MULTIOP_PASSWORD_BUT 920012
 #define MULTIOP_PASSWORD_EDIT 920013
+
 // ///////////////////////////////
 // Many Button Variations..
-
-#define FORCE_BUTW			56
-#define FORCE_BUTH			38
 
 #define CON_BUTWIDTH			60
 #define CON_BUTHEIGHT			46
