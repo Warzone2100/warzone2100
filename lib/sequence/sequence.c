cd6fd9282be5a2c5c032b622f56d3c9444572cd1
Christian Ohm <chr.ohm@gmx.net>
2009-08-15 17:05:08 +0000

    Increase video buffer size from 4K to 256K.
    This fixes playback of videos with a bitrate larger than ~2000kbps.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2@7981 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/sequence/sequence.c
+++ lib/sequence/sequence.c
@@ -173,9 +173,10 @@ static int ScrnvidYpos = 0;
 // Helper; just grab some more compressed bitstream and sync it for page extraction
 static int buffer_data(PHYSFS_file* in, ogg_sync_state* oy)
 {
-	// read in 4K chunks
-	char *buffer = ogg_sync_buffer(oy, 4096);
-	int bytes = PHYSFS_read(in, buffer, 1, 4096);
+	// read in 256K chunks
+	const int size = 262144;
+	char *buffer = ogg_sync_buffer(oy, size);
+	int bytes = PHYSFS_read(in, buffer, 1, size);
 
 	ogg_sync_wrote(oy, bytes);
 	return(bytes);
