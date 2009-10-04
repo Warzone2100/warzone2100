Warzone2100/warzone2100

commit 2bd86a7427b0785e55706c41c2e346ee060596ad

Draw the skybox after the terrain but before the transparent objects. This fixes the black smoke that could be seen against the sky.
From: Gerard Krol <gerard_@gna.org>

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@7631 4a71c877-e1ca-e34f-864e-861f7616d084

src/display3d.c
@@ -396,9 +396,6 @@ void draw3DScene( void )
 	// draw terrain
 	displayTerrain();
 
-	// draw skybox
-	renderSurroundings();
-
 	pie_BeginInterface();
 	updateLightLevels();
 	drawDroidSelections();
@@ -823,6 +820,9 @@ static void drawTiles(iView *player)
 	pie_TranslateTextureEnd();
 	// and to the warzone modelview transform
 	glPopMatrix();
+
+	// draw skybox
+	renderSurroundings();
 	
 	// clear the terrain highlight
 	for (i = 0; i < visibleTiles.y; i++)
@@ -3611,9 +3611,6 @@ static void renderSurroundings(void)
 	static float wind = 0.0f;
 	const float skybox_scale = 20000.0f;
 
-	// set up matrices and textures
-	pie_PerspectiveBegin();
-
 	// Push identity matrix onto stack
 	pie_MatBegin();
 
@@ -3646,7 +3643,6 @@ static void renderSurroundings(void)
 
 	// Load Saved State
 	pie_MatEnd();
-	pie_PerspectiveEnd();
 }
 
 /// Flattens an imd to the landscape and handles 4 different rotations

commit 36397110bbeb25b3d8f4d1a17bb9c1f74e65d83c

Fix the skybox so that it doesn't move around (got broken in r7631).
From: Gerard Krol <gerard_@gna.org>

git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8197 4a71c877-e1ca-e34f-864e-861f7616d084

src/display3d.c
@@ -3677,12 +3677,6 @@ static void renderSurroundings(void)
 	// Set the camera position
 	pie_MATTRANS(0, 0, distance);
 
-	// Rotate for the player and for the wind
-	pie_MatRotZ(player.r.z);
-	pie_MatRotX(player.r.x);
-	pie_MatRotY(player.r.y);
-
-	// Skybox //
 	// rotate it
 	pie_MatRotY(DEG(1) * wind);
 