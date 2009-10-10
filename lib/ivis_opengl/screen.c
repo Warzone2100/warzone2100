d67909d9ed62e8b9249e83108e2f661a2b9a928d
Buginator <buginator@gna.org>
2009-10-10 00:33:47 +0000

    Add GLSL version to crash dumps to find out what people have, and also add a few more openGL 2+ checks to the LOG_3D dump.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8240 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/ivis_opengl/screen.c
+++ lib/ivis_opengl/screen.c
@@ -170,6 +170,10 @@ BOOL screenInitialise(
 		addDumpInfo(buf);
 		ssprintf(buf, "OpenGL Version : %s", glGetString(GL_VERSION));
 		addDumpInfo(buf);
+		ssprintf(buf, "OpenGL GLSL Version : %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+		addDumpInfo(buf);
+		ssprintf(buf, "Video Mode %d x %d (%d bpp) (%s)", width, height, bpp, fullScreen ? "fullscreen" : "window");
+		addDumpInfo(buf);
 		/* Dump information about OpenGL implementation to the console */
 		debug(LOG_3D, "OpenGL Vendor : %s", glGetString(GL_VENDOR));
 		debug(LOG_3D, "OpenGL Renderer : %s", glGetString(GL_RENDERER));
@@ -189,7 +193,10 @@ BOOL screenInitialise(
 		debug(LOG_3D, "  * Stencil wrap %s supported.", GLEE_EXT_stencil_wrap ? "is" : "is NOT");
 		debug(LOG_3D, "  * Anisotropic filtering %s supported.", GLEE_EXT_texture_filter_anisotropic ? "is" : "is NOT");
 		debug(LOG_3D, "  * Rectangular texture %s supported.", GLEE_ARB_texture_rectangle ? "is" : "is NOT");
-		debug(LOG_3D, "  * FrameBuffer Object (FBO) %s supported.", GLEE_EXT_framebuffer_object  ? "is" : "is NOT");
+		debug(LOG_3D, "  * FrameBuffer Object (FBO) %s supported.", GLEE_EXT_framebuffer_object ? "is" : "is NOT");
+		debug(LOG_3D, "  * Shader Objects %s supported.", GL_ARB_shader_objects ? "is" : "is NOT");
+		debug(LOG_3D, "  * Vertex Buffer Object (VBO) %s supported.", GL_ARB_vertex_buffer_object ? "is" : "is NOT");
+		debug(LOG_3D, "  * OpenGL GLSL Version : %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
 	}
 	
 #ifndef WZ_OS_MAC
