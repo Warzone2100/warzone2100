f4f06338be13f20b20d64f5a633064609e2a8d35
Buginator <buginator@gna.org>
2009-10-31 23:11:00 +0000

    Update version string for the test branch
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.2_net@8347 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/netplay/netplay.c
+++ lib/netplay/netplay.c
@@ -248,7 +248,7 @@ static bool playerPasswordFlag[MAX_PLAYERS] = {false};		// we kick on false
  **			   ie ("trunk", "2.1.3", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "trunk-check"; 
+char VersionString[VersionStringSize] = "2.2-net-merge"; 
 static int NETCODE_VERSION_MAJOR = 2;	// unused for now
 static int NETCODE_VERSION_MINOR = 24;	// unused for now
 static int NUMBER_OF_MODS = 0;			// unused for now
