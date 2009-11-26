d6e70a3c0041ba2b719428be12f1dc7604702f97
Buginator <buginator@gna.org>
2009-11-26 18:16:52 +0000

    Filter out non-ASCII in our debug line (r8380)
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8519 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/input.c
+++ lib/framework/input.c
@@ -266,7 +266,7 @@ void inputHandleKeyEvent(SDL_KeyboardEvent * keyEvent)
 					break;
 			}
 
-			debug( LOG_INPUT, "Key Code (pressed): 0x%x, %d, [%c] SDLkey=[%s]", vk, vk, vk < 255 ? (char) vk : '?' , SDL_GetKeyName(keyCodeToSDLKey(keyEvent->keysym.sym)));
+			debug( LOG_INPUT, "Key Code (pressed): 0x%x, %d, [%c] SDLkey=[%s]", vk, vk, (vk < 128) && (vk > 31) ? (char) vk : '?' , SDL_GetKeyName(keyCodeToSDLKey(keyEvent->keysym.sym)));
 			if (char_code < 32)
 			{
 				char_code = 0;
