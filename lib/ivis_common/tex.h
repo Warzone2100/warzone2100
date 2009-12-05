6519ce48e958d4a3a94c2ad77320e97d653b1fce
Guangcong Luo <zarel@wz2100.net>
2009-12-05 07:39:49 +0000

    Use GLuint correctly (fixes build errors and warnings caused by other fixes).
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@8608 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/ivis_common/tex.h
+++ lib/ivis_common/tex.h
@@ -21,7 +21,11 @@
 #define _tex_
 
 #include "png_util.h"
+#if defined __APPLE__ && defined __MACH__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 
 //*************************************************************************
 
