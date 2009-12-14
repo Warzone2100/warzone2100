8395cf5ac6f57358d045337af60d6cc6742f494a
Guangcong Luo <zarel@wz2100.net>
2009-12-14 18:53:18 +0000

    2.3: Fix bug #1193: Crash when sending messages.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@8688 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/multiplay.c
+++ src/multiplay.c
@@ -1053,6 +1053,7 @@ BOOL sendTextMessage(const char *pStr, BOOL all)
 	memset(display,0x0, sizeof(display));	//clear buffer
 	memset(msg,0x0, sizeof(display));		//clear buffer
 	memset(sendto,0x0, sizeof(sendto));		//clear private flag
+	memset(posTable,0x0, sizeof(posTable));		//clear buffer
 	sstrcpy(msg, curStr);
 
 	if (!all)
@@ -1081,7 +1082,7 @@ BOOL sendTextMessage(const char *pStr, BOOL all)
 		}
 		for (; curStr[0] >= '0' && curStr[0] <= '7'; curStr++)		// for each 0..7 numeric char encountered
 		{
-			sendto[ posTable[curStr[0]-'0'] ] = true;
+			i = posTable[curStr[0]-'0'];
 			if (!all)
 			{
 				if (normal)
@@ -1095,6 +1096,7 @@ BOOL sendTextMessage(const char *pStr, BOOL all)
 				if ((isHumanPlayer(i) || (game.type == SKIRMISH && i<game.maxPlayers && game.skDiff[i] ) ))
 				{
 					sstrcat(display, getPlayerName(posTable[curStr[0]-'0']));
+					sendto[i] = true;
 				}
 				else
 				{
