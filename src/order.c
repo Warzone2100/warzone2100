9068852f9878aff62b75a783420029bef27399fa
Guangcong Luo <zarel@wz2100.net>
2009-12-21 07:29:56 +0000

    2.3: Don't let trucks and other noncombatants guard anything since they'll just get confused and spin in circles. Also let super cyborgs guard things just like regular cyborgs and tanks. Fixes bug #1198.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@8808 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/order.c
+++ src/order.c
@@ -2904,7 +2904,8 @@ DROID_ORDER chooseOrderObj(DROID *psDroid, BASE_OBJECT *psObj)
 			  ((DROID *)psObj)->droidType == DROID_SENSOR ||
 			  (((DROID *)psObj)->droidType == DROID_COMMAND && psObj->player != psDroid->player)) &&
 			 (psDroid->droidType == DROID_WEAPON ||
-			  psDroid->droidType == DROID_CYBORG) &&
+			  psDroid->droidType == DROID_CYBORG ||
+			  psDroid->droidType == DROID_CYBORG_SUPER) &&
 			 proj_Direct(asWeaponStats + psDroid->asWeaps[0].nStat))
 	{
 		order = DORDER_GUARD;
@@ -3013,7 +3014,11 @@ DROID_ORDER chooseOrderObj(DROID *psDroid, BASE_OBJECT *psObj)
 					}
 				}
 			}
-			else
+			// Some droids shouldn't be guarding
+			else if ((psDroid->droidType == DROID_WEAPON ||
+			          psDroid->droidType == DROID_CYBORG ||
+			          psDroid->droidType == DROID_CYBORG_SUPER) &&
+			         proj_Direct(asWeaponStats + psDroid->asWeaps[0].nStat)
 			{
 				order = DORDER_GUARD;
 			}
