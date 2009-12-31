78387361a14facacccafccc255868bcf9537373e
Guangcong Luo <zarel@wz2100.net>
2009-12-31 09:39:33 +0000

    2.3: Fix bug #1292 - auto-repair is sometimes instantaneous.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@8898 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/action.c
+++ src/action.c
@@ -2865,6 +2865,7 @@ static void actionDroidBase(DROID *psDroid, DROID_ACTION_DATA *psAction)
 		setDroidActionTarget(psDroid, psAction->psObj, 0);
 		//initialise the action points
 		psDroid->actionPoints  = 0;
+		psDroid->actionStarted = gameTime;
 		if (secondaryGetState(psDroid, DSO_HALTTYPE) == DSS_HALT_HOLD &&
 		    (psDroid->order == DORDER_NONE || psDroid->order == DORDER_TEMP_HOLD))
 		{
