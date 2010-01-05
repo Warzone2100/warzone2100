d234d949b990ab259abd76a45fabcec31f8897df
Per Inge Mathisen <per@wz2100.net>
2010-01-05 18:29:58 +0000

    2.3: Add back traffic shaping for outgoing sync messages. Removed as a test in r8522
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9006 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/multisync.c
+++ src/multisync.c
@@ -102,9 +102,8 @@ static UDWORD				PingSend[MAX_PLAYERS];	//stores the time the ping was called.
 // test traffic level.
 static BOOL okToSend(void)
 {
-	//update checks	& go no further if any exceeded.
-	// NOTE: for this BETA test, just testing bytes sent!
-	if (NETgetRecentBytesSent() >= MAX_BYTESPERSEC)		// + NETgetRecentBytesRecvd()
+	// Update checks and go no further if any exceeded.
+	if (NETgetRecentBytesSent() + NETgetRecentBytesRecvd() >= MAX_BYTESPERSEC)
 	{
 		return false;
 	}
