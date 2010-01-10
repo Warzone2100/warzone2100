00f22e9a042d9a8e9c8e41b0eb81bdb9df91f42e
Guangcong Luo <zarel@wz2100.net>
2010-01-10 09:50:37 +0000

    2.3: Add feedback for the 'mouseflip' and 'shakey' commands.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9087 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/keybind.c
+++ src/keybind.c
@@ -2389,10 +2389,12 @@ void	kf_ToggleMouseInvert( void )
 	if(getInvertMouseStatus())
 	{
 		setInvertMouseStatus(false);
+		CONPRINTF(ConsoleString,(ConsoleString,_("Vertical rotation direction: Normal")));
 	}
 	else
 	{
 		setInvertMouseStatus(true);
+		CONPRINTF(ConsoleString,(ConsoleString,_("Vertical rotation direction: Flipped")));
 	}
 }
 // --------------------------------------------------------------------------
@@ -2401,10 +2403,12 @@ void	kf_ToggleShakeStatus( void )
 	if(getShakeStatus())
 	{
 		setShakeStatus(false);
+		CONPRINTF(ConsoleString,(ConsoleString,_("Screen shake when things die: Off")));
 	}
 	else
 	{
 		setShakeStatus(true);
+		CONPRINTF(ConsoleString,(ConsoleString,_("Screen shake when things die: On")));
 	}
 }
 // --------------------------------------------------------------------------
