37cc99d797f3df39652652c5c49964cc1ca954fe
Christian Ohm <chr.ohm@gmx.net>
2010-01-12 21:10:28 +0000

    Decrease loading screen updates to 20 fps.
    Was 60 fps, and with vsync enabled, that slowed down loading significantly
    (discovered by Cyp). Also remove a useless debug message.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9163 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/wrappers.c
+++ src/wrappers.c
@@ -274,17 +274,12 @@ void loadingScreenCallback(void)
 	UDWORD			currTick;
 	PIELIGHT		colour;
 
-	if(SDL_GetTicks()-lastTick < 16)
+	currTick = SDL_GetTicks();
+	if (currTick - lastTick < 50)
 	{
 		return;
 	}
-	currTick = SDL_GetTicks();
-	if ((currTick - lastTick) > 500)
-	{
-		currTick -= lastTick;
-		debug( LOG_NEVER, "loadingScreenCallback: pause %d\n", currTick );
-	}
-	lastTick = SDL_GetTicks();
+	lastTick = currTick;
 	colour.byte.r = 1;
 	colour.byte.g = 1;
 	colour.byte.b = 1;
