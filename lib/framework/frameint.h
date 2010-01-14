9c6b55f2d62f576e26a721e137f84247ce2287dc
Per Inge Mathisen <per@wz2100.net>
2010-01-14 12:37:03 +0000

    Remove unused definitions.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@9187 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/frameint.h
+++ lib/framework/frameint.h
@@ -30,19 +30,6 @@
 #error Framework header files MUST be included from Frame.h ONLY.
 #endif
 
-
-/* Define the style and extended style of the window.
- * Need these to calculate the size the window should be when returning to
- * window mode.
- *
- * create a title bar, minimise button on the title bar,
- * automatic ShowWindow, get standard system menu on title bar
- */
-#define WIN_STYLE (WS_CAPTION | WS_MINIMIZEBOX | WS_VISIBLE | WS_SYSMENU)
-
-#define WIN_EXSTYLE	 WS_EX_APPWINDOW	// Go on task bar when iconified
-
-
 /* Initialise the double buffered display */
 extern BOOL screenInitialise(UDWORD		width,			// Display width
 							 UDWORD		height,			// Display height
@@ -55,13 +42,6 @@ extern BOOL screenInitialise(UDWORD		width,			// Display width
 
 /* Release the DD objects */
 extern void screenShutDown(void);
-
-/* In full screen mode flip to the GDI buffer.
- * Use this if you want the user to see any GDI output.
- * This is mainly used so that ASSERTs and message boxes appear
- * even in full screen mode.
- */
-extern void screenFlipToGDI(void);
 
 /* This is called once a frame so that the system can tell
  * whether a key was pressed this turn or held down from the last frame.
