0e82730bd39f7ab59346bfda05c6348df68c6f5d
Guangcong Luo <zarel@wz2100.net>
2010-01-18 22:13:42 +0000

    2.3: Fix structures so they're illuminated correctly (they were previously illuminated based on whether or not their owner could see them, rather than whether or not you could).
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9343 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/display3d.c
+++ src/display3d.c
@@ -2032,7 +2032,7 @@ void	renderFeature(FEATURE *psFeature)
 	{
 		brightness = pal_SetBrightness(avGetObjLightLevel((BASE_OBJECT*)psFeature, brightness.byte.r));
 	}
-	if (!hasSensorOnTile(mapTile(map_coord(psFeature->pos.x), map_coord(psFeature->pos.y)), psFeature->player))
+	if (!hasSensorOnTile(mapTile(map_coord(psFeature->pos.x), map_coord(psFeature->pos.y)), selectedPlayer))
 	{
 		brightness.byte.r /= 2;
 		brightness.byte.g /= 2;
@@ -2365,7 +2365,7 @@ void	renderStructure(STRUCTURE *psStructure)
 		{
 			buildingBrightness = pal_SetBrightness(avGetObjLightLevel((BASE_OBJECT*)psStructure, buildingBrightness.byte.r));
 		}
-		if (!hasSensorOnTile(mapTile(map_coord(psStructure->pos.x), map_coord(psStructure->pos.y)), psStructure->player))
+		if (!hasSensorOnTile(mapTile(map_coord(psStructure->pos.x), map_coord(psStructure->pos.y)), selectedPlayer))
 		{
 			buildingBrightness.byte.r /= 2;
 			buildingBrightness.byte.g /= 2;
@@ -2803,7 +2803,7 @@ static BOOL	renderWallSection(STRUCTURE *psStructure)
 			{
 				buildingBrightness = pal_SetBrightness(avGetObjLightLevel((BASE_OBJECT*)psStructure, buildingBrightness.byte.r));
 			}
-			if (!hasSensorOnTile(mapTile(map_coord(psStructure->pos.x), map_coord(psStructure->pos.y)), psStructure->player))
+			if (!hasSensorOnTile(mapTile(map_coord(psStructure->pos.x), map_coord(psStructure->pos.y)), selectedPlayer))
 			{
 				buildingBrightness.byte.r /= 2;
 				buildingBrightness.byte.g /= 2;
