36261c68b34c56f96cb72d701fc940a3fd281377
Guangcong Luo <zarel@wz2100.net>
2010-01-22 06:57:20 +0000

    2.3: Fix bug that caused players not to send data checks (and subsequently get kicked) sometimes.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9378 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/multiplay.c
+++ src/multiplay.c
@@ -255,23 +255,23 @@ BOOL multiPlayerLoop(void)
 		{
 			if(bDisplayMultiJoiningStatus)
 			{
-				if (!NetPlay.isHost)
-				{
-					sendDataCheck();
-				}
 				bDisplayMultiJoiningStatus = 0;
 				setWidgetsStatus(true);
 			}
 			if (!ingame.TimeEveryoneIsInGame)
 			{
 				ingame.TimeEveryoneIsInGame = gameTime;
-		}
+				if (!NetPlay.isHost)
+				{
+					sendDataCheck();
+				}
+			}
 			// Only have to do this on a true MP game
 			if (NetPlay.isHost && !ingame.isAllPlayersDataOK && NetPlay.bComms)
 			{
-				if (gameTime - ingame.TimeEveryoneIsInGame > GAME_TICKS_PER_SEC * 60)
+				if (gameTime - ingame.TimeEveryoneIsInGame > GAME_TICKS_PER_SEC * 30)
 				{
-					// we waited 60 secs to make sure people didn't bypass the data integrity checks
+					// we waited 30 secs to make sure people didn't bypass the data integrity checks
 					int index;
 					for (index=0; index < MAX_PLAYERS; index++)
 					{
