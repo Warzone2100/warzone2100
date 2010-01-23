ce12726d6ed06c3a1fbf09174f12bba345c608e2
Guangcong Luo <zarel@wz2100.net>
2010-01-23 20:30:42 +0000

    2.3: Fix bug #406 - AIs used human player's truck template.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9426 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/droid.c
+++ src/droid.c
@@ -3514,7 +3514,7 @@ DROID_TEMPLATE * getTemplateFromTranslatedNameNoPlayer(char *pName)
 		{
 			if (strcmp(psCurr->aName, pName) == 0)
 			{
-				//if template is a human player's it must be a CYBORG or we ignore it
+				// if template is a human player's it must be a CYBORG or we ignore it
 				if (isHumanPlayer(player)
 				 && !(psCurr->droidType == DROID_CYBORG
 				   || psCurr->droidType == DROID_CYBORG_SUPER
