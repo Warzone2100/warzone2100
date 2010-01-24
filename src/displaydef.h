c84d9a0318b7e2e461a9c11808eb65eb855ceebf
Christian Ohm <chr.ohm@gmx.net>
2010-01-24 00:19:17 +0000

    Increase the scrolling area from 16 to 22 pixels.
    Makes scrolling a bit easier with larger resolutions. 24 is where other stuff
    starts (radar...), so we can't increase it more.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9443 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/displaydef.h
+++ src/displaydef.h
@@ -27,8 +27,8 @@
 #include "lib/ivis_common/imd.h"
 #include "lib/ivis_common/pieclip.h"
 
-#define	BOUNDARY_X			(16)
-#define BOUNDARY_Y			(16)
+#define	BOUNDARY_X			(22)
+#define BOUNDARY_Y			(22)
 //#define BOUNDARY_X		(DISP_WIDTH/20)	   // proportional to resolution - Alex M
 //#define	BOUNDARY_Y		(DISP_WIDTH/16)
 
