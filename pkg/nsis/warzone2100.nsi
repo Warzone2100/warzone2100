Warzone2100/warzone2100

commit b09ba3c72d44bbd18f83122f05be0e83bdf43fbc

2.3: Rename "original.wz" to "old-1.10-balance.wz" - I had an agreement with Delphinio that the mod could be named anything but "original", which was vague and confusing to new users.
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9538 4a71c877-e1ca-e34f-864e-861f7616d084

pkg/nsis/warzone2100.nsi
@@ -293,12 +293,14 @@ Section $(TEXT_SecOriginalMod) SecOriginalMod
 
   SetOutPath "$INSTDIR\mods\multiplay"
 
-  File "${TOP_BUILDDIR}\data\mods\multiplay\original.wz"
+  File "${TOP_BUILDDIR}\data\mods\multiplay\old-1.10-balance.wz"
+  Delete "${TOP_BUILDDIR}\data\mods\multiplay\original.wz"
 
   SetOutPath "$INSTDIR"
 
   !insertmacro MUI_STARTMENU_WRITE_BEGIN "Application"
-    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\${PACKAGE_NAME} - Original.lnk" "$INSTDIR\${PACKAGE}.exe" "--mod_mp original.wz"
+    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\${PACKAGE_NAME} - Old 1.10 Balance.lnk" "$INSTDIR\${PACKAGE}.exe" "--mod_mp old-1.10-balance.wz"
+    Delete "$SMPROGRAMS\$STARTMENU_FOLDER\${PACKAGE_NAME} - Original.lnk"
   !insertmacro MUI_STARTMENU_WRITE_END
 
 SectionEnd
@@ -553,8 +555,8 @@ FunctionEnd
   LangString TEXT_SecNTWMod ${LANG_ENGLISH} "NTW"
   LangString DESC_SecNTWMod ${LANG_ENGLISH} "NTW: New Team War mod. Modifies most of the weapons and research."
 
-  LangString TEXT_SecOriginalMod ${LANG_ENGLISH} "1.10 balance"
-  LangString DESC_SecOriginalMod ${LANG_ENGLISH} "Play the game with the original 1.10 version balance stats."
+  LangString TEXT_SecOriginalMod ${LANG_ENGLISH} "Old 1.10 balance"
+  LangString DESC_SecOriginalMod ${LANG_ENGLISH} "Play the game as it was back in the 1.10 days."
 
   LangString TEXT_RunWarzone ${LANG_ENGLISH} "Run ${PACKAGE_NAME}"
   LangString TEXT_Readme ${LANG_ENGLISH} "$INSTDIR\Readme.en.html"
@@ -764,7 +766,7 @@ Section "Uninstall"
 
   Delete "$INSTDIR\mods\multiplay\ntw.wz"
   Delete "$INSTDIR\mods\multiplay\aivolution.wz"
-  Delete "$INSTDIR\mods\multiplay\original.wz"
+  Delete "$INSTDIR\mods\multiplay\old-1.10-balance.wz"
 
   RMDir "$INSTDIR\mods\multiplay"
   RMDir "$INSTDIR\mods\music"
@@ -891,7 +893,7 @@ Section "Uninstall"
   Delete "$SMPROGRAMS\$MUI_TEMP\${PACKAGE_NAME}.lnk"
   Delete "$SMPROGRAMS\$MUI_TEMP\${PACKAGE_NAME} - Aivolution.lnk"
   Delete "$SMPROGRAMS\$MUI_TEMP\${PACKAGE_NAME} - NTW.lnk"
-  Delete "$SMPROGRAMS\$MUI_TEMP\${PACKAGE_NAME} - Original.lnk"
+  Delete "$SMPROGRAMS\$MUI_TEMP\${PACKAGE_NAME} - Old 1.10 Balance.lnk"
 ;  RMDir "$SMPROGRAMS\$STARTMENU_FOLDER"
 
   ;Delete empty start menu parent diretories

data/mods/multiplay/Makefile.am
@@ -13,7 +13,7 @@ OLD110LIST = \
 	stats \
 	wrf
 	
-OLD110ARCHIVE = original.wz
+OLD110ARCHIVE = old-1.10-balance.wz
 
 
 NTWLIST = \

data/mods/multiplay/makefile.win32
@@ -10,7 +10,7 @@ OLD110LIST= \
 	stats \
 	wrf
 	
-OLD110ARCHIVE=original.wz
+OLD110ARCHIVE=old-1.10-balance.wz
 
 NTWLIST= \
 	addon.lev \

commit 35a5e8125568cdf35ae81176008981564312355b

Fix NSIS that was broken in r9538
git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9642 4a71c877-e1ca-e34f-864e-861f7616d084

pkg/nsis/warzone2100.nsi
@@ -292,15 +292,11 @@ SectionEnd
 Section $(TEXT_SecOriginalMod) SecOriginalMod
 
   SetOutPath "$INSTDIR\mods\multiplay"
-
   File "${TOP_BUILDDIR}\data\mods\multiplay\old-1.10-balance.wz"
-  Delete "${TOP_BUILDDIR}\data\mods\multiplay\old-1.10-balance.wz"
-
   SetOutPath "$INSTDIR"
 
   !insertmacro MUI_STARTMENU_WRITE_BEGIN "Application"
     CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\${PACKAGE_NAME} - Old 1.10 Balance.lnk" "$INSTDIR\${PACKAGE}.exe" "--mod_mp old-1.10-balance.wz"
-    Delete "$SMPROGRAMS\$STARTMENU_FOLDER\${PACKAGE_NAME} - Original.lnk"
   !insertmacro MUI_STARTMENU_WRITE_END
 
 SectionEnd