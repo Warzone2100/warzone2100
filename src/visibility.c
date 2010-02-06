63648d0bffa11bfffa5505faba37b8f017712379
Buginator <buginator@gna.org>
2010-02-06 00:18:35 +0000

    play the artifact detected sample only for the person that found it.
    fixes ticket:1496
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9643 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/visibility.c
+++ src/visibility.c
@@ -726,7 +726,7 @@ void processVisibility(BASE_OBJECT *psObj)
 					{
 						psMessage->pViewData = (MSG_VIEWDATA *)psObj;
 					}
-					if (!bInTutorial)
+					if (!bInTutorial && player == selectedPlayer)
 					{
 						// play message to indicate been seen
 						audio_QueueTrackPos( ID_SOUND_ARTEFACT_DISC, psObj->pos.x, psObj->pos.y, psObj->pos.z );
