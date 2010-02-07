392c08cd3c8745d5e5bcbbec4950924b41487c0c
Giel van Schijndel <giel@wz2100.net>
2010-02-07 21:19:08 +0000

    Use LOG_INFO to report non-existant playlist files (instead of LOG_ERROR)
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@9732 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/sound/playlist.c
+++ lib/sound/playlist.c
@@ -73,7 +73,7 @@ bool PlayList_Read(const char* path)
 	debug(LOG_WZ, "Reading...[directory: %s] %s", PHYSFS_getRealDir(listName), listName);
 	if (fileHandle == NULL)
 	{
-		debug(LOG_ERROR, "PHYSFS_openRead(\"%s\") failed with error: %s\n", listName, PHYSFS_getLastError());
+		debug(LOG_INFO, "PHYSFS_openRead(\"%s\") failed with error: %s\n", listName, PHYSFS_getLastError());
 		return false;
 	}
 
