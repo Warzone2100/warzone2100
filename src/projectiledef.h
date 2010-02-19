a2dc1ffcde9a89772f38386dd5c114f34f33b476
Cyp <cyp@wz2100.net>
2010-02-19 11:16:44 +0000

    Use list of damaged objects instead of single damaged object, to avoid damaging the same pair of overlapping objects twice.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@9863 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/projectiledef.h
+++ src/projectiledef.h
@@ -49,7 +49,9 @@ typedef struct PROJECTILE
 	WEAPON_STATS*   psWStats;               ///< firing weapon stats
 	BASE_OBJECT*    psSource;               ///< what fired the projectile
 	BASE_OBJECT*    psDest;                 ///< target of this projectile
-	BASE_OBJECT*    psDamaged;              ///< the target it already dealt damage to (don't damage the same target twice)
+	BASE_OBJECT **  psDamaged;              ///< the targets that have already been dealt damage to (don't damage the same target twice)
+	unsigned        psNumDamaged;
+
 	UDWORD          startX, startY;         ///< Where projectile started
 	UDWORD          tarX, tarY;             ///< The target coordinates
 	SDWORD          vXY, vZ;                ///< axis velocities
