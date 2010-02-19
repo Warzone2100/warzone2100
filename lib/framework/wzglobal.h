0947080cc0388757701ce2f8fe9030b0a793d6ac
Giel van Schijndel <giel@wz2100.net>
2010-02-19 17:13:26 +0000

    Backport r9868 and r9869 from trunk into the 2.3 branch:
     * Win32 crossbuild script: Make sure that win32/builds/libs exists
     * #undefine NOMINMAX before #defining it
      - Prevents warnings about #redefining previously #defined macros
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9870 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/wzglobal.h
+++ lib/framework/wzglobal.h
@@ -505,6 +505,8 @@
 
 #  define WIN32_LEAN_AND_MEAN
 #  define WIN32_EXTRA_LEAN
+#  undef NOMINMAX
+#  define NOMINMAX 1		// disable the min / max macros
 #  include <windows.h>
 
 #  if defined(WZ_CC_MSVC)
