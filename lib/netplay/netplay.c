6315fea412f19dcf833784045958adcaee0b82a1
Guangcong Luo <zarel@wz2100.net>
2010-02-21 17:55:42 +0000

    2.3: Increment netcode version; set netcode VersionString to "2.3 RC1a".
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@9947 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/netplay/netplay.c
+++ lib/netplay/netplay.c
@@ -262,9 +262,9 @@ extern LOBBY_ERROR_TYPES LobbyError;		// from src/multiint.c
  **			   ie ("trunk", "2.1.3", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "2.3 svn"; // used for display in the lobby, not the actual version check
+char VersionString[VersionStringSize] = "2.3 RC1a"; // used for display in the lobby, not the actual version check
 static int NETCODE_VERSION_MAJOR = 2;                // major netcode version, used for compatibility check
-static int NETCODE_VERSION_MINOR = 51;               // minor netcode version, used for compatibility check
+static int NETCODE_VERSION_MINOR = 52;               // minor netcode version, used for compatibility check
 static int NETCODE_HASH = 0;			// unused for now
 
 static int checkSockets(const SocketSet* set, unsigned int timeout);
