debeeb618c82d41457ccbf46650dbabea3db276c
Buginator <buginator@gna.org>
2010-02-24 05:16:05 +0000

    change the version string back to '2.3 svn' :P
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@10002 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/netplay/netplay.c
+++ lib/netplay/netplay.c
@@ -262,7 +262,7 @@ extern LOBBY_ERROR_TYPES LobbyError;		// from src/multiint.c
  **			   ie ("trunk", "2.1.3", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "2.3 RC1a"; // used for display in the lobby, not the actual version check
+char VersionString[VersionStringSize] = "2.3 svn"; // used for display in the lobby, not the actual version check
 static int NETCODE_VERSION_MAJOR = 2;                // major netcode version, used for compatibility check
 static int NETCODE_VERSION_MINOR = 60;               // minor netcode version, used for compatibility check
 static int NETCODE_HASH = 0;			// unused for now
