dd5413fc5c6923147239190206ce29c0230ade2b
Christian Ohm <chr.ohm@gmx.net>
2010-03-02 21:41:16 +0000

    Include/declaration cleanup for src/cluster.c/h.
    Removes unused clustDisplay function.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10099 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/cluster.h
+++ src/cluster.h
@@ -35,9 +35,6 @@ extern "C"
 // maximum number of clusters in a game
 #define CLUSTER_MAX		UBYTE_MAX
 
-// distance between units for them to be in the same cluster
-//#define CLUSTER_DIST	(TILE_UNITS*4)
-
 // cluster information flags
 #define CLUSTER_PLAYER_MASK		0x07
 #define CLUSTER_DROID			0x08
@@ -45,15 +42,6 @@ extern "C"
 
 // Indirect the cluster ID to an actual cluster number
 extern UBYTE	aClusterMap[CLUSTER_MAX];
-
-// number of droids in a cluster
-extern UWORD	aClusterUsage[CLUSTER_MAX];
-
-// whether a cluster can be seen by a player
-extern UBYTE	aClusterVisibility[CLUSTER_MAX];
-
-// when a cluster was last attacked
-extern UDWORD	aClusterAttacked[CLUSTER_MAX];
 
 // information about the cluster
 extern UBYTE	aClusterInfo[CLUSTER_MAX];
@@ -79,9 +67,6 @@ extern void clustUpdateCluster(BASE_OBJECT *psList, SDWORD cluster);
 // remove an object from the cluster system
 extern void clustRemoveObject(BASE_OBJECT *psObj);
 
-// display the current clusters
-extern void clustDisplay(void);
-
 // tell the cluster system that an objects visibility has changed
 extern void clustObjectSeen(BASE_OBJECT *psObj, BASE_OBJECT *psViewer);
 
@@ -90,9 +75,6 @@ extern void clustObjectAttacked(BASE_OBJECT *psObj);
 
 // get the cluster ID for an object
 extern SDWORD clustGetClusterID(BASE_OBJECT *psObj);
-
-// get the actual cluster number from a cluster ID
-extern SDWORD clustGetClusterFromID(SDWORD clusterID);
 
 // find the center of a cluster
 extern void clustGetCenter(BASE_OBJECT *psObj, SDWORD *px, SDWORD *py);
