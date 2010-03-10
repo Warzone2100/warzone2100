21fb1a18edbccaa902ea8bd15e53d53f2c3904f1
Cyp <cyp@wz2100.net>
2010-03-10 23:09:27 +0100

    Fix mac warnings, hopefully (effectively reverting my previous changes to tex.h)...

--- lib/ivis_common/tex.h
+++ lib/ivis_common/tex.h
@@ -21,7 +21,11 @@
 #define _tex_
 
 //#include <GLee.h>
+#if defined __APPLE__ && defined __MACH__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 #include "png_util.h"
 
 //*************************************************************************
