b2a73377e69d0fbc010b88b4650684da8acee9cc
Cyp <cyp@wz2100.net>
2010-03-13 00:07:57 +0100

    newnet: Avoid using the double type.

--- src/wavecast.cpp
+++ src/wavecast.cpp
@@ -73,7 +73,7 @@ static std::vector<WavecastTile> generateWavecastTable(unsigned radius)
 				{
 					continue;
 				}
-				tile.invRadius = 92681 / sqrt((double)tileRadiusSq) + 0.5;  // +0.5 = round to nearest. Result is at most 92681 / sqrt(2), approximately 65536.
+				tile.invRadius = i64Sqrt((uint64_t)2*65536*65536 / tileRadiusSq);  // Result is at most 65536, inversely proportional to the radius.
 
 				const int minCorner[4][2] = {{1, 0}, {1, 1}, {0, 1}, {0, 0}};
 				const int mcdx = minCorner[quadrant][0], mcdy = minCorner[quadrant][1];  // Corner of the tile which the minimum angle.
