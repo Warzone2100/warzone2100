468f3936cbb4a97f78367807edcf69f998522902
Cyp <cyp@wz2100.net>
2010-03-14 20:53:00 +0100

    newnet: Assert on broken float primitive datatype.

--- src/main.c
+++ src/main.c
@@ -981,8 +981,9 @@ static void mainLoop(void)
 	}
 }
 
-static void fptest1(void)
+static bool fptest1(void)
 {
+	bool wtf = false;
 	float a = 5.19258240356725201562535524577016477814756008082239;
 	uint8_t aexpect[] = {5, 5, 5, 5, 5, 3, 3, 2, 1, 10, 1, 4, 6, 42, 6, 3, 2, 2, 5, 2, 1, 1, 1, 1, 2, 26, 1, 3, 1, 11, 2, 2, 1, 1, 3, 2, 2, 1, 2, 9, 1, 1, 9, 25, 1, 4, 4, 2, 8, 2, 14, 139, 1, 1, 1, 19, 1, 1, 10, 2, 1, 10, 1, 1, 1, 28, 11, 4, 4, 1, 2, 12, 59, 1, 4, 4, 8, 2, 2, 37, 1, 85, 1, 1, 15, 8, 1, 100, 1, 1, 2, 9, 1, 5, 1, 17, 6, 15, 3, 1};
 	float b = 5.03937732811384756748132236757401955859521845805786;
@@ -995,7 +996,8 @@ static void fptest1(void)
 	{
 		if ((unsigned)a != aexpect[n])
 		{
-			debug(LOG_WARNING, "My 32-bit floating point works differently.");
+			debug(LOG_ERROR, "My 32-bit floating point works differently.");
+			wtf = true;
 			break;
 		}
 		a = 1/(a - (unsigned)a);
@@ -1004,7 +1006,8 @@ static void fptest1(void)
 	{
 		if ((unsigned)b != bexpect[n])
 		{
-			debug(LOG_WARNING, "My 32-bit floating point square roots work differently.");
+			debug(LOG_ERROR, "My 32-bit floating point square roots work differently.");
+			wtf = true;
 			break;
 		}
 		b = 1/sqrtf(b - (unsigned)b);
@@ -1016,18 +1019,23 @@ static void fptest1(void)
 		cr = t;
 		if ((uint64_t)(cr*2000000000*2000000000)%251 != cexpect[n])
 		{
-			debug(LOG_WARNING, "My 32-bit floating point mandelbrots work differently.");
+			debug(LOG_ERROR, "My 32-bit floating point mandelbrots work differently.");
+			wtf = true;
 			break;
 		}
 	}
 	if (d2 + d1 - d2 != 0.0f)
 	{
-		debug(LOG_WARNING, "My 32-bit addition works differently.");
+		debug(LOG_ERROR, "My 32-bit addition works differently.");
+		wtf = true;
 	}
+
+	return !wtf;
 }
 
-static void fptest2(void)
+static bool fptest2(void)
 {
+	bool wtf = false;
 	double a = 5.19258240356725201562535524577016477814756008082239;
 	uint16_t aexpect[] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 1, 55, 1, 3, 1, 2, 1, 1, 2, 2, 4, 2, 17, 1, 1, 5, 1, 28, 26, 1, 6, 4, 2, 1, 1, 2, 2, 4, 1, 11, 2, 1, 32, 5, 4, 2, 1, 1, 4, 1, 1, 1, 2, 8, 1, 4, 2, 2, 2, 2, 31, 1, 1, 90, 3, 1, 4, 1, 1, 273, 1, 4, 167, 5, 1, 1, 4, 1, 1, 1, 1, 6, 3, 1, 10, 1, 1, 4, 1, 1, 1, 1, 3, 3, 1, 2, 2, 1};
 	double b = 5.03937732811384756748132236757401955859521845805786;
@@ -1040,7 +1048,8 @@ static void fptest2(void)
 	{
 		if ((unsigned)a != aexpect[n])
 		{
-			debug(LOG_WARNING, "My 64-bit floating point works differently.");
+			debug(LOG_ERROR, "My 64-bit floating point works differently.");
+			wtf = true;
 			break;
 		}
 		a = 1/(a - (unsigned)a);
@@ -1049,7 +1058,8 @@ static void fptest2(void)
 	{
 		if ((unsigned)b != bexpect[n])
 		{
-			debug(LOG_WARNING, "My 64-bit floating point square roots work differently.");
+			debug(LOG_ERROR, "My 64-bit floating point square roots work differently.");
+			wtf = true;
 			break;
 		}
 		b = 1/sqrt(b - (unsigned)b);
@@ -1061,18 +1071,24 @@ static void fptest2(void)
 		cr = t;
 		if ((uint64_t)(cr*2000000000*2000000000)%251 != cexpect[n])
 		{
-			debug(LOG_WARNING, "My 64-bit floating point mandelbrots work differently.");
+			debug(LOG_ERROR, "My 64-bit floating point mandelbrots work differently.");
+			wtf = true;
 			break;
 		}
 	}
 	if (d2 + d1 - d2 != 0.0)
 	{
-		debug(LOG_WARNING, "My 64-bit addition works differently.");
+		debug(LOG_ERROR, "My 64-bit addition works differently.");
+		wtf = true;
 	}
+
+	return !wtf;
 }
 
 int main(int argc, char *argv[])
 {
+	int ok;
+
 	setupExceptionHandler(argc, argv);
 
 	debug_init();
@@ -1081,9 +1097,10 @@ int main(int argc, char *argv[])
 	debug_register_callback( debug_callback_win32debug, NULL, NULL, NULL );
 #endif // WZ_OS_WIN && DEBUG_INSANE
 
-	// Sanity check that floating point math is the same on all machines. (x86 will probably need to be compiled with the "-msse -mfpmath=sse" compiler flags.)
-	fptest1();
-	fptest2();
+	// Sanity check that floating point math is the same on all machines. (x86 needs to be compiled at least with the "-msse -mfpmath=sse" compiler flags, in addition to "-mno-fused-madd".)
+	ok = fptest1();
+	fptest2();  // Multiplayer seems to work, even if doubles are broken. (Should avoid use of doubles, or require "-msse2".)
+	ASSERT(ok, "Your compiler and compiler flag combination breaks 32-bit floats. Multiplayer will be broken.");
 
 	/*** Initialize PhysicsFS ***/
 	initialize_PhysicsFS(argv[0]);
