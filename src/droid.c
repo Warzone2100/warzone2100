1c4abc62118b70d589ab998003849a3d784a731e
Per Inge Mathisen <per@wz2100.net>
2010-03-15 21:00:27 +0000

    Remove useless no-op check in pick-a-tile function.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10294 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/droid.c
+++ src/droid.c
@@ -3472,14 +3472,11 @@ BOOL	pickATileGenThreat(UDWORD *x, UDWORD *y, UBYTE numIterations, SDWORD threat
 	SDWORD		startX, endX, startY, endY;
 	UDWORD		passes;
 	Vector3i	origin = { world_coord(*x), world_coord(*y), 0 };
-	Vector3i	firstPos = { world_coord(*x), world_coord(*y), 0 };
 
 	ASSERT_OR_RETURN(false, *x<mapWidth,"x coordinate is off-map for pickATileGen" );
 	ASSERT_OR_RETURN(false, *y<mapHeight,"y coordinate is off-map for pickATileGen" );
 
-	if (function(*x,*y) 
-	    && fpathCheck(origin, firstPos, PROPULSION_TYPE_WHEELED)
-	    && ((threatRange <=0) || (!ThreatInRange(player, threatRange, *x, *y, false))))	//TODO: vtol check really not needed?
+	if (function(*x,*y) && ((threatRange <=0) || (!ThreatInRange(player, threatRange, *x, *y, false))))	//TODO: vtol check really not needed?
 	{
 		return(true);
 	}
