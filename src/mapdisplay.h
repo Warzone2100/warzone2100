19c2e551eb0203165564c3227791a15113029b33
Christian Ohm <chr.ohm@gmx.net>
2010-03-16 00:51:31 +0000

    2.3: Include/declaration cleanup for src/mapdisplay.c/h.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@10304 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/mapdisplay.h
+++ src/mapdisplay.h
@@ -21,26 +21,15 @@
 #ifndef __INCLUDED_SRC_MAPDISPLAY_H__
 #define __INCLUDED_SRC_MAPDISPLAY_H__
 
-#include "lib/ivis_common/pietypes.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif //__cplusplus
 
-#define BUFFER_GRIDX	22
-#define BUFFER_GRIDY	22
+void renderResearchToBuffer(RESEARCH *psResearch, UDWORD OriginX, UDWORD OriginY);
 
-
-/*Flag to switch code for bucket sorting in renderFeatures etc
-  for the renderMapToBuffer code */
-  /*This is no longer used but may be useful for testing so I've left it in - maybe
-  get rid of it eventually? - AB 1/4/98*/
-extern BOOL	doBucket;
-
-// The surface to render Message information into for the Intelligence Screen
-extern iSurface	*mapSurface;
-
-/* renders up to two IMDs into the surface - used by message display in Intelligence Map */
-extern void renderIMDToBuffer(iSurface *pSurface, iIMDShape *pIMD,
-							  iIMDShape *pIMD2, UDWORD WindowX,UDWORD WindowY,
-							  UDWORD OriginX,UDWORD OriginY);
-extern void renderResearchToBuffer(RESEARCH *psResearch,
-                            UDWORD OriginX, UDWORD OriginY);
+#ifdef __cplusplus
+}
+#endif //__cplusplus
 
 #endif // __INCLUDED_SRC_MAPDISPLAY_H__
