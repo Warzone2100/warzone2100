0e81296ddbe030466ff7f8dce63813af36aff994
dak180 <dak180@users.sourceforge.net>
2010-03-23 04:59:31 +0000

    Add better error reporting to build rules.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10411 4a71c877-e1ca-e34f-864e-861f7616d084

--- macosx/configs/bison.sh
+++ macosx/configs/bison.sh
@@ -3,11 +3,13 @@
 # System Bison executable
 SYS_BISON="bison"
 # Local Warzone Bison executable (relative to macosx/)
-LOCAL_BISON="`pwd`/external/bison/built/bin/bison" 
+LOCAL_BISON="external/bison/built/bin/bison" 
 
 # If a Warzone Bison executable exists, use instead of the system executable
 if [ -e "$LOCAL_BISON" ]; then
-	$LOCAL_BISON "$@"
+	${LOCAL_BISON} ${@}
+    exit ${?}
 else
-	$SYS_BISON "$@"
+	${SYS_BISON} ${@}
+    exit ${?}
 fi
