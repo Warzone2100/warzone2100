901930360b04151444673770853cc10cf07aca87
Buginator <buginator@gna.org>
2010-04-03 21:38:41 +0000

    Update Mac dialog text (yet) again.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10461 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/debug.c
+++ lib/framework/debug.c
@@ -444,10 +444,11 @@ void _debug( code_part part, const char *function, const char *str, ... )
 			GetStandardAlertDefaultParams( &param, kStdCFStringAlertVersionOne );
 			param.movable = true;
 			
-			ssprintf(aBuffer, "%s\n\nPlease check your logs for more details.\n\n Run Console.app and search for wz2100 and copy that to a file. \
-							\n\nFor Crash report on 10.4/10.5 check ~/Library/Logs/CrashReporter, and on 10.6 check ~/Library/Logs/DiagnosticReports", useInputBuffer1 ? inputBuffer[1] : inputBuffer[0] );
+			ssprintf(aBuffer, "%s\n\nPlease check your logs for more details.\n\n", useInputBuffer1 ? inputBuffer[1] : inputBuffer[0] );
 			
-			err = CreateStandardAlert( kAlertStopAlert, CFStringCreateWithCString( nil, aBuffer, kCFStringEncodingMacRoman), CFSTR( "Do not forget to upload and attach those to a bug report at http://developer.wz2100.net/newticket  Thanks!" ), &param, &dialog );
+			err = CreateStandardAlert( kAlertStopAlert, CFStringCreateWithCString( nil, aBuffer, kCFStringEncodingMacRoman),
+				CFSTR( "Run Console.app and search for wz2100 and copy that to a file.\n\nFor the Crash report on\n10.4/10.5 check ~/Library/Logs/CrashReporter,\non 10.6 check ~/Library/Logs/DiagnosticReports \
+					\n\nDo not forget to upload and attach those to a bug report at http://developer.wz2100.net/newticket \n\nThanks!" ), &param, &dialog );
 			SetWindowTitleWithCFString( GetDialogWindow( dialog ), CFSTR( "Warzone has terminated unexpectedly" ) );
 			
 			RunStandardAlert( dialog, NULL, &itemHit );
