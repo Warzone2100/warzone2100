78ff8a4d1c5697722ab2635eb7b8f30dc1d97508
Steven Koenig <kreuvf@gna.org>
2010-04-18 17:23:55 +0000

    ChangeLog2BBCode Converter script now  * uses /bin/sh instead of /bin/bash,  * uses -r (GNU extension, tell me, if we do not want this!) so we have less backslashes,  * uses | as a separator now, so / does not need to be escaped,  * does not escape characters that do not need to be escaped (spaces) and therefore  * has an overall improved readability. There should not be any changes to the behaviour/actions of sed, it is a purely cosmetic commit.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10627 4a71c877-e1ca-e34f-864e-861f7616d084

--- build_tools/changelog2bbcode.sh
+++ build_tools/changelog2bbcode.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-sed -e 's/r\([0-9]\+\)/[rev]\1[\/rev]/g' -e 's/ticket:\([0-9]\+\)/[ticket]\1[\/ticket]/g' -e 's/\ \ \*/\ */g' -e 's/^\ \*\ \(.*\):/\n[b]\1[\/b]/g'
+#!/bin/sh
+sed -r -e 's|r([0-9]+)|[rev]\1[/rev]|g' -e 's|ticket:([0-9]+)|[ticket]\1[/ticket]|g' -e 's|   \*|  *|g' -e 's|^ \* (.*):|\n[b]\1[/b]|g'
