315c75870e96e026f4cbf11f50b14a65fdba1ecf
Christian Ohm <chr.ohm@gmx.net>
2010-04-19 20:33:42 +0000

    Draw the subtitles with shadows to increase readability.
    Closes #1782.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10647 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/seqdisp.c
+++ src/seqdisp.c
@@ -381,6 +381,10 @@ BOOL seq_UpdateFullScreenVideo(int *pbClear)
 				{
 					aSeqList[currentPlaySeq].aText[i].x = 20 + D_W2;
 				}
+				iV_SetTextColour(WZCOL_GREY);
+				iV_DrawText(&(aSeqList[currentPlaySeq].aText[i].pText[0]),
+						aSeqList[currentPlaySeq].aText[i].x + 1, aSeqList[currentPlaySeq].aText[i].y + 1);
+				iV_SetTextColour(WZCOL_TEXT_BRIGHT);
 				iV_DrawText(&(aSeqList[currentPlaySeq].aText[i].pText[0]),
 						aSeqList[currentPlaySeq].aText[i].x, aSeqList[currentPlaySeq].aText[i].y);
 			}
@@ -390,6 +394,10 @@ BOOL seq_UpdateFullScreenVideo(int *pbClear)
 				{
 					aSeqList[currentPlaySeq].aText[i].x = 20 + D_W2;
 				}
+				iV_SetTextColour(WZCOL_GREY);
+				iV_DrawText(&(aSeqList[currentPlaySeq].aText[i].pText[0]),
+						aSeqList[currentPlaySeq].aText[i].x + 1, aSeqList[currentPlaySeq].aText[i].y + 1);
+				iV_SetTextColour(WZCOL_TEXT_BRIGHT);
 				iV_DrawText(&(aSeqList[currentPlaySeq].aText[i].pText[0]),
 						aSeqList[currentPlaySeq].aText[i].x, aSeqList[currentPlaySeq].aText[i].y);
 			}
