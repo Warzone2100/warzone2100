143818e5d9e8eeb949f2b45dc18375059a3baa20
Guangcong Luo <zarel@wz2100.net>
2010-05-02 03:11:14 +0000

    2.3: Always show a delivery point when cheating droids with the debug menu.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@10756 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/display3d.c
+++ src/display3d.c
@@ -1709,7 +1709,7 @@ void displayBlueprints(void)
 		{
 			if (deliveryPointToMove)
 			{
-				// it's a delivery point
+				// it's a droid (from the debug menu) or a delivery point
 				FLAG_POSITION pos = *deliveryPointToMove;
 				pos.coords.x = world_coord(sBuildDetails.x)+world_coord(1)/2;
 				pos.coords.y = world_coord(sBuildDetails.y)+world_coord(1)/2;
@@ -1718,9 +1718,15 @@ void displayBlueprints(void)
 			}
 			else
 			{
-				// it's a droid or a delivery point (from the debug menu)
-				// display nothing for now
-				;
+				// it's a droid (from the debug menu)
+				// no delivery point is known, so show a fake one
+				FLAG_POSITION pos;
+				pos.coords.x = world_coord(sBuildDetails.x)+world_coord(1)/2;
+				pos.coords.y = world_coord(sBuildDetails.y)+world_coord(1)/2;
+				pos.coords.z = map_Height(pos.coords.x, pos.coords.y) + world_coord(1)/8;
+				pos.factoryType = FACTORY_FLAG;
+				pos.factoryInc = 0;
+				renderDeliveryPoint(&pos, true);
 			}
 		}
 	}
