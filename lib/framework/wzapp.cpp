3626ef502ed15bb37f3ecbc6e58892f7d23a20fd
Buginator <buginator@gna.org>
2010-05-09 20:29:21 +0000

    partial revert of r10782 to fix broken MSVC builds. QT inlcudes *must* come first, before platform specific ones.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/qt@10790 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/wzapp.cpp
+++ lib/framework/wzapp.cpp
@@ -20,16 +20,16 @@
  *  Qt-related functions.
  */
 
-// Get platform defines before checking for them!
-// If this is not included here mac builds break!
-#include "frame.h"
-
 #include <QtGui/QImageReader>
 #include <QtGui/QBitmap>
 #include <QtGui/QPainter>
 #include <QtGui/QMouseEvent>
 #include <QtGui/QClipboard>
 #include <QtGui/QDesktopWidget>
+
+// Get platform defines before checking for them.
+// Qt headers MUST come before platform specific stuff!
+#include "frame.h"
 
 #include "wzapp.h"
 #if defined(WZ_CC_MSVC)
