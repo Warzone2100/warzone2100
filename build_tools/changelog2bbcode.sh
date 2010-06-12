cf8976e36ef9806d6a1ea15a16e33b61bd3d76ab
Christian Ohm <chr.ohm@gmx.net>
2010-06-12 23:05:51 +0000

    Improve the changelog script.
    Change all items to [*], and enclose them in list tags. Just one thing needs to
    be done manually, moving the "[/list]" tag at the beginning to the end.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@10968 4a71c877-e1ca-e34f-864e-861f7616d084

--- build_tools/changelog2bbcode.sh
+++ build_tools/changelog2bbcode.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sed -e 's/r\([0-9]\+\)/[rev]\1[\/rev]/g' -e 's/ticket:\([0-9]\+\)/[ticket]\1[\/ticket]/g' -e 's/\ \ \*/\ */g' -e 's/^\ \*\ \(.*\):/\n[b]\1[\/b]/g'
+sed -e 's/r\([0-9]\+\)/[rev]\1[\/rev]/g' \
+    -e 's/ticket:\([0-9]\+\)/[ticket]\1[\/ticket]/g'\
+    -e 's/\ \ \*/\ */g' \
+    -e 's/^\ \*\ \(.*\):/\n[\/list][b]\1[\/b][list]/g' \
+    -e 's/^\ \ \*/\ [*]/'
