803b52125e03ff95e26751159ffd9e7bf2499af7
Buginator <buginator@gna.org>
2010-07-27 03:42:11 +0000

    fix / add headers.  merge sync: r11298
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/qt-trunk@11300 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/framework/wzapp.cpp
+++ lib/framework/wzapp.cpp
@@ -1,6 +1,7 @@
 /*
 	This file is part of Warzone 2100.
-	Copyright (C) 2005-2009  Warzone Resurrection Project
+	Copyright (C) 1999-2004  Eidos Interactive
+	Copyright (C) 2005-2010  Warzone 2100 Project
 
 	Warzone 2100 is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -16,6 +17,7 @@
 	along with Warzone 2100; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
+
 /** @file wzapp.cpp
  *  Qt-related functions.
  */
@@ -1272,3 +1274,4 @@ static int WZkeyToQtKey(int code)
 
 	return 0;	// nothing found (should never happen)
 }
+
