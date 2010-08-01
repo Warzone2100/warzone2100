105a176b08b143b71740a7aeccd7b1c918ab8c9c
Buginator <buginator@gna.org>
2010-08-01 01:05:32 +0000

    Bump netcode version to 2.3.3 & 80
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/tags/2.3.3@11328 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/netplay/netplay.c
+++ lib/netplay/netplay.c
@@ -253,9 +253,9 @@ extern LOBBY_ERROR_TYPES LobbyError;		// from src/multiint.c
  **			   ie ("trunk", "2.1.3", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "2.3 svn"; // used for display in the lobby, not the actual version check
+char VersionString[VersionStringSize] = "2.3.3"; // used for display in the lobby, not the actual version check
 static int NETCODE_VERSION_MAJOR = 2;                // major netcode version, used for compatibility check
-static int NETCODE_VERSION_MINOR = 73;               // minor netcode version, used for compatibility check
+static int NETCODE_VERSION_MINOR = 80;               // minor netcode version, used for compatibility check
 static int NETCODE_HASH = 0;			// unused for now
 
 static int checkSockets(const SocketSet* set, unsigned int timeout);
