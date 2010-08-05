94320034ae8610294cb8cee1da4ed16d0faea7aa
Giel van Schijndel <giel@wz2100.net>
2010-08-05 17:27:00 +0000

    Assume that SDL headers are directly in the #include path, instead of assuming the SDL directory is in the #include path.
    Closes #1941.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/trunk@11385 4a71c877-e1ca-e34f-864e-861f7616d084

--- lib/ivis_opengl/piestate.c
+++ lib/ivis_opengl/piestate.c
@@ -27,7 +27,7 @@
 #include <SDL.h>
 #include <SDL_mouse.h>
 #include <physfs.h>
-#include <SDL/SDL_opengl.h>
+#include <SDL_opengl.h>
 
 #include "lib/ivis_common/piestate.h"
 #include "lib/ivis_common/piedef.h"
