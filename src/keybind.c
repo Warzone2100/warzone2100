837f0b53f4d63b7d9a32a6a21b59e61b43ce998f
Christian Ohm <chr.ohm@gmx.net>
2010-08-22 23:53:39 +0000

    Change "resource extractors" to "oil derricks".
    Refs #2102.
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@11520 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/keybind.c
+++ src/keybind.c
@@ -1713,7 +1713,7 @@ SDWORD	xJump,yJump;
 	}
 	else
 	{
-		addConsoleMessage(_("Unable to locate any resource extractors!"),LEFT_JUSTIFY, SYSTEM_MESSAGE);
+		addConsoleMessage(_("Unable to locate any oil derricks!"),LEFT_JUSTIFY, SYSTEM_MESSAGE);
 	}
 
 }
