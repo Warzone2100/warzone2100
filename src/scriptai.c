c5090cdc900f3352e00e1c218eb550dff66773b3
Buginator <buginator@gna.org>
2010-09-02 23:56:53 +0000

    Change log line to make it obvious it is only enabled in debug builds.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@11574 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/scriptai.c
+++ src/scriptai.c
@@ -1684,8 +1684,8 @@ BOOL scrSkDoResearch(void)
 		}
 #if defined (DEBUG)
 		{
-			char				sTemp[128];
-			sprintf(sTemp,"player:%d starts topic: %s",player, asResearch[i].pName );
+			char	sTemp[128];
+			sprintf(sTemp,"[debug]player:%d starts topic: %s",player, asResearch[i].pName );
 			NETlogEntry(sTemp, SYNC_FLAG, 0);
 		}
 #endif
