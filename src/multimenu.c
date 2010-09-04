7a083b154dab6b6fa8f421042db0670a671127f5
Buginator <buginator@gna.org>
2010-09-04 05:55:43 +0000

    Allow the host to kick the AI only in 'cheat mode' in skirmish games.
    Closes ticket:2139
    
    
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@11596 4a71c877-e1ca-e34f-864e-861f7616d084

--- src/multimenu.c
+++ src/multimenu.c
@@ -1488,12 +1488,16 @@ void intProcessMultiMenu(UDWORD id)
 			{
 				char buf[250];
 				
-				ssprintf(buf, _("The host has kicked %s from the game!"), getPlayerName((unsigned int) i));
-				sendTextMessage(buf, true);
-				ssprintf(buf, _("kicked %s : %s from the game, and added them to the banned list!"), getPlayerName((unsigned int) i), NetPlay.players[i].IPtextAddress);
-				NETlogEntry(buf, SYNC_FLAG, (unsigned int) i);
-				kickPlayer((unsigned int) i, "you are unwanted by the host.", ERROR_KICKED);
-				return;
+				// Allow the host to kick the AI only in a MP game, or if they activated cheats in a skirmish game
+				if (NetPlay.bComms || Cheated)
+				{
+					ssprintf(buf, _("The host has kicked %s from the game!"), getPlayerName((unsigned int) i));
+					sendTextMessage(buf, true);
+					ssprintf(buf, _("kicked %s : %s from the game, and added them to the banned list!"), getPlayerName((unsigned int) i), NetPlay.players[i].IPtextAddress);
+					NETlogEntry(buf, SYNC_FLAG, (unsigned int) i);
+					kickPlayer((unsigned int) i, "you are unwanted by the host.", ERROR_KICKED);
+					return;
+				}
 			}
 
 		if (openchannels[i])
