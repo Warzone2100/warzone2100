07b694465362e7471b402c931ec99abebb1fa2df
dak180 <dak180@users.sourceforge.net>
2010-09-19 16:44:26 +0000

    Add build bot scripts.
    git-svn-id: https://warzone2100.svn.sourceforge.net/svnroot/warzone2100/branches/2.3@11695 4a71c877-e1ca-e34f-864e-861f7616d084

---
+++ macosx/BuildBot/01_configure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Note:
+# This script is meant to be run from the root of the working copy.
+# 
+# This just tries to build warzone.  It does it twice because there are race conditions that may otherwise be triggered.
+
+# Config
+
+cd macosx
+
+if ! xcodebuild -project Warzone.xcodeproj -parallelizeTargets -target "Warzone" -configuration Release; then
+    if ! xcodebuild -project Warzone.xcodeproj -parallelizeTargets -target "Warzone" -configuration "Release" -PBXBuildsContinueAfterErrors=NO; then
+	    exit ${?}
+	fi
+fi
+
+exit 0
