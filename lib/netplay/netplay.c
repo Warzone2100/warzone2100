ba2c761af2d52ff8724fb147f4e9b399bf7a66ca
buginator <buginator@gna.org>
2010-11-14 22:59:04 -0500

    Bumping version strings to master_20101114 and netcode version to 1008. Set for release build.

--- lib/netplay/netplay.c
+++ lib/netplay/netplay.c
@@ -166,9 +166,9 @@ unsigned NET_PlayerConnectionStatus[CONNECTIONSTATUS_NORMAL][MAX_PLAYERS];
  **            ie ("trunk", "2.1.3", "3.0", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "master, netcode 4.1007";
+char VersionString[VersionStringSize] = "master_1008";
 static int NETCODE_VERSION_MAJOR = 4;
-static int NETCODE_VERSION_MINOR = 1007;
+static int NETCODE_VERSION_MINOR = 1008;
 
 bool NETisCorrectVersion(uint32_t game_version_major, uint32_t game_version_minor)
 {
