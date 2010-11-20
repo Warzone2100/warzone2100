Warzone2100/warzone2100

commit 09ea0be7c30236809536db332be78b2967d3114d

Use the host's SP color as the default color when hosting games.

lib/netplay/netplay.c
@@ -46,6 +46,7 @@
 #include "src/multijoin.h"
 #include "src/multiint.h"
 #include "src/multiplay.h"
+#include "src/warzoneconfig.h"
 
 #if   defined(WZ_OS_UNIX)
 # include <arpa/inet.h>
@@ -1510,6 +1511,11 @@ void NET_InitPlayers()
 	for (i = 0; i < MAX_CONNECTED_PLAYERS; ++i)
 	{
 		NET_InitPlayer(i, true);
+	}
+	// Now switch player color of the host to what they normally use for SP games
+	if ( getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
+	{
+		changeColour(NET_HOST_ONLY, war_GetSPcolor());
 	}
 	NetPlay.hostPlayer = NET_HOST_ONLY;	// right now, host starts always at index zero
 	NetPlay.playercount = 0;

src/multiint.c
@@ -1682,7 +1682,7 @@ static BOOL changePosition(UBYTE player, UBYTE position)
 	return false;
 }
 
-static BOOL changeColour(UBYTE player, UBYTE col)
+BOOL changeColour(UBYTE player, UBYTE col)
 {
 	int i;
 
@@ -3343,10 +3343,15 @@ BOOL startMultiOptions(BOOL bReenter)
 	{
 		teamChooserUp = -1;
 		allowChangePosition = true;
-		for(i=0;i<MAX_PLAYERS;i++)
+		for(i=0; i < MAX_PLAYERS; i++)
 		{
 			game.skDiff[i] = (DIFF_SLIDER_STOPS / 2);	// reset AI (turn it on again)
 			setPlayerColour(i,i);						//reset all colors as well
+		}
+		// Now switch player color of the host to what they normally use
+		if (getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
+		{
+			changeColour(NET_HOST_ONLY, war_GetSPcolor());
 		}
 
 		if(!NetPlay.bComms)			// force skirmish if no comms.

src/multiint.h
@@ -44,7 +44,7 @@ extern	BOOL	startMultiOptions		(BOOL bReenter);
 extern	void	frontendMultiMessages	(void);
 
 extern BOOL addMultiBut(W_SCREEN *screen, UDWORD formid, UDWORD id, UDWORD x, UDWORD y, UDWORD width, UDWORD height, const char* tipres, UDWORD norm, UDWORD down, UDWORD hi);
-
+extern BOOL changeColour(UBYTE player, UBYTE col);
 extern	char	sPlayer[128];
 
 void	kickPlayer(uint32_t player_id, const char *reason, LOBBY_ERROR_TYPES type);

commit fd2843e6431e43cc72b635c422fd18bb19d42c47

Fix bug introduced in 09ea0be7c30236809536db332be78b2967d3114d Changelog: Fix kicking of non-hosts who didn't have default color values.
Only change colors if you are the host on the setup screen.
Fixes ticket:2329

lib/netplay/netplay.c
@@ -1512,11 +1512,7 @@ void NET_InitPlayers()
 	{
 		NET_InitPlayer(i, true);
 	}
-	// Now switch player color of the host to what they normally use for SP games
-	if ( getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
-	{
-		changeColour(NET_HOST_ONLY, war_GetSPcolor());
-	}
+
 	NetPlay.hostPlayer = NET_HOST_ONLY;	// right now, host starts always at index zero
 	NetPlay.playercount = 0;
 	NetPlay.pMapFileHandle = NULL;
@@ -1557,6 +1553,8 @@ static void NETSendPlayerInfoTo(uint32_t index, unsigned to)
 void NETSendAllPlayerInfoTo(unsigned int to)
 {
 	static uint32_t indices[MAX_PLAYERS] = {0, 1, 2, 3, 4, 5, 6, 7};
+	ASSERT_OR_RETURN( , NetPlay.isHost == true, "Invalid call for non-host");
+
 	NETSendNPlayerInfoTo(indices, ARRAY_SIZE(indices), to);
 }
 
@@ -3702,6 +3700,11 @@ BOOL NEThostGame(const char* SessionName, const char* PlayerName,
 		NetPlay.players[0].connection	= -1;
 		NetPlay.playercount		= 1;
 		debug(LOG_NET, "Hosting but no comms");
+		// Now switch player color of the host to what they normally use for SP games
+		if ( getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
+		{
+			changeColour(NET_HOST_ONLY, war_GetSPcolor());
+		}
 		return true;
 	}
 
@@ -3770,6 +3773,12 @@ BOOL NEThostGame(const char* SessionName, const char* PlayerName,
 	ASSERT(selectedPlayer == NET_HOST_ONLY, "For now, host must start at player index zero, was %d", (int)selectedPlayer);
 
 	MultiPlayerJoin(selectedPlayer);
+
+	// Now switch player color of the host to what they normally use for SP games
+	if ( getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
+	{
+		changeColour(NET_HOST_ONLY, war_GetSPcolor());
+	}
 
 	allow_joining = true;
 

src/multiint.c
@@ -3348,11 +3348,6 @@ BOOL startMultiOptions(BOOL bReenter)
 			game.skDiff[i] = (DIFF_SLIDER_STOPS / 2);	// reset AI (turn it on again)
 			setPlayerColour(i,i);						//reset all colors as well
 		}
-		// Now switch player color of the host to what they normally use
-		if (getPlayerColour(NET_HOST_ONLY) != war_GetSPcolor())
-		{
-			changeColour(NET_HOST_ONLY, war_GetSPcolor());
-		}
 
 		if(!NetPlay.bComms)			// force skirmish if no comms.
 		{