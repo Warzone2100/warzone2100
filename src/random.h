7574f25f5b08b39b590013c2eb3287e7d054e876
Cyp <cyp@wz2100.net>
2010-12-20 13:45:03 +0100

    Remove MersenneTwister C wrapper.

--- src/random.h
+++ src/random.h
@@ -22,8 +22,6 @@
 
 #include "lib/framework/types.h"
 
-#ifdef __cplusplus
-
 // Pseudorandom number generator with 19937 bit state and period of 2**19937 - 1. Equidistributed in up to 623 dimensions.
 // See http://en.wikipedia.org/wiki/Mersenne_twister
 // If all clients use the same seed, they will generate the same pseudorandom number sequence.
@@ -40,10 +38,6 @@ private:
 	uint32_t state[624];  ///< 19937 bit state, rounded up to next 32 bits.
 };
 
-extern "C"
-{
-#endif //_cplusplus
-
 /// Seeds the random number generator. TODO The seed should be sent over the network, so that all clients generate the same number sequence.
 void gameSRand(uint32_t seed);
 
@@ -54,13 +48,5 @@ uint32_t gameRandU32(void);
 /// Generates a random number in the interval [0...limit - 1]. Not equidistributed for large non-powers of 2.
 /// Must not be called from graphics routines, only for making game decisions.
 int32_t gameRand(uint32_t limit);
-
-void *newMersenneTwister(uint32_t seed);
-void deleteMersenneTwister(void *this_);
-uint32_t mersenneTwisterU32(void *this_);
-
-#ifdef __cplusplus
-}
-#endif //_cplusplus
 
 #endif //_RAND_H_
