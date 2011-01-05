f00b6149d9cf1ee658c6200543778e3bb15b06e0
Cyp <cyp@wz2100.net>
2011-01-05 19:18:12 +0100

    Bump MAX_PLAYERS to allow 10 normal players + 1 scavenger player.

--- lib/framework/frame.h
+++ lib/framework/frame.h
@@ -47,8 +47,8 @@
 
 extern uint32_t selectedPlayer;      ///< The player number corresponding to this client.
 extern uint32_t realSelectedPlayer;  ///< The player number corresponding to this client (same as selectedPlayer, unless changing players in the debug menu).
-#define MAX_PLAYERS         8                  ///< Maximum number of players in the game.
-#define MAX_PLAYERS_IN_GUI  8                  ///< One player should be reserved for baba.
+#define MAX_PLAYERS         11                 ///< Maximum number of players in the game.
+#define MAX_PLAYERS_IN_GUI  (MAX_PLAYERS - 1)  ///< One player reserved for scavengers.
 #define PLAYER_FEATURE      (MAX_PLAYERS + 1)
 #define MAX_PLAYER_SLOTS    (MAX_PLAYERS + 2)  ///< Max players plus 1 baba and 1 reserved for features. Actually, if baba is a regular player, then it's plus 1 unused?
 
