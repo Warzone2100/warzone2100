276fa532881218a11ee81e3877669c727ad01216
Per Inge Mathisen <per@wz2100.net>
2011-01-27 20:51:03 +0100

    2.3: Quick and dirty workaround for ticket:2459 - vision asserts in campaign

--- src/visibility.c
+++ src/visibility.c
@@ -286,8 +286,11 @@ void visRemoveVisibility(BASE_OBJECT *psObj)
 			const TILEPOS pos = psObj->watchedTiles[i];
 			MAPTILE *psTile = mapTile(pos.x, pos.y);
 
-			ASSERT(psTile->watchers[psObj->player] > 0, "Not watching watched tile (%d, %d)", (int)pos.x, (int)pos.y);
-			psTile->watchers[psObj->player]--;
+			//ASSERT(psTile->watchers[psObj->player] > 0, "Not watching watched tile (%d, %d)", (int)pos.x, (int)pos.y);
+			if (psTile->watchers[psObj->player] > 0)
+			{
+				psTile->watchers[psObj->player]--;
+			}
 		}
 		free(psObj->watchedTiles);
 		psObj->watchedTiles = NULL;
