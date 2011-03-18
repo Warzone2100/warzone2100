Warzone2100/warzone2100

commit 2f780609a2893cdfa2f83876e2c183523659e649

Try to fix isatty linkage errors on Windows.
Hope this works, somehow.

lib/framework/frame.h
@@ -45,11 +45,6 @@
 #include "trig.h"
 #include "cursors.h"
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif //__cplusplus
-
 extern uint32_t selectedPlayer;      ///< The player number corresponding to this client.
 extern uint32_t realSelectedPlayer;  ///< The player number corresponding to this client (same as selectedPlayer, unless changing players in the debug menu).
 #define MAX_PLAYERS		8	/**< Maximum number of players in the game. */
@@ -113,16 +108,15 @@ extern UDWORD frameGetAverageRate(void);
 extern UDWORD HashString( const char *String );
 extern UDWORD HashStringIgnoreCase( const char *String );
 
-#ifdef __cplusplus
-}
-#endif //__cplusplus
-
 #if defined(WZ_OS_WIN)
 # include <winsock2.h> /* for struct timeval */
 
 struct timezone;
 extern int gettimeofday(struct timeval* tv, struct timezone* tz);
+
+extern "C" int isatty(int);  // Apparently flex declares isatty with C++ linkage on Windows. Don't ask why. Declaring here instead.
 #endif
+extern "C" int isatty(int);  // Apparently flex declares isatty with C++ linkage on Windows. Don't ask why. Declaring here instead.
 
 static inline WZ_DECL_CONST const char * bool2string(bool var)
 {

src/multiplay.h
@@ -29,11 +29,6 @@
 #include "featuredef.h"
 #include "droid.h"  // For INITIAL_DROID_ORDERS.
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif //__cplusplus
-
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // Game Options Structure. Enough info to completely describe the static stuff in amultiplay game.
 typedef struct {
@@ -91,7 +86,6 @@ extern MULTIPLAYERINGAME	ingame;						// the game description.
 
 extern BOOL					bMultiPlayer;				// true when more than 1 player.
 extern BOOL					bMultiMessages;				// == bMultiPlayer unless multi messages are disabled
-extern UDWORD				selectedPlayer;
 extern BOOL					openchannels[MAX_PLAYERS];
 extern UBYTE				bDisplayMultiJoiningStatus;	// draw load progress?
 
@@ -235,9 +229,5 @@ extern	void startMultiplayerGame	(void);
 extern	void resetReadyStatus		(bool bSendOptions);
 
 extern	BOOL bPlayerReadyGUI[MAX_PLAYERS];
-
-#ifdef __cplusplus
-}
-#endif //__cplusplus
 
 #endif // __INCLUDED_SRC_MULTIPLAY_H__

commit 31502ccdce373e12142fdd55dc6102a988f718d0

Let us try this again, leave the never-interactive option for the lexers.
Set never-interactive option in lexers so that Flex doesn't use isatty() function from unistd.
Define YY_NO_UNISTD_H when no unistd.h is detected.
Remove attempt to fix isatty error on Windows introduced in  2f780609.
cherry picked from commit c046bb88e5b2562d8159e9555e397b24ab9b7f16

lib/framework/frame.h
@@ -123,10 +123,7 @@ extern UDWORD HashStringIgnoreCase( const char *String );
 
 struct timezone;
 extern int gettimeofday(struct timeval* tv, struct timezone* tz);
-
-extern "C" int isatty(int);  // Apparently flex declares isatty with C++ linkage on Windows. Don't ask why. Declaring here instead.
 #endif
-extern "C" int isatty(int);  // Apparently flex declares isatty with C++ linkage on Windows. Don't ask why. Declaring here instead.
 
 static inline WZ_DECL_CONST const char * bool2string(bool var)
 {

lib/framework/resource_lexer.l
@@ -80,7 +80,7 @@ extern void res_set_debug(int bdebug);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option prefix="res_"
 
 %x COMMENT

lib/framework/strres_lexer.l
@@ -84,7 +84,7 @@ extern void strres_set_debug(int bdebug);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option prefix="strres_"
 
 %x COMMENT

lib/gamelib/audp_lexer.l
@@ -84,7 +84,7 @@ extern void audp_set_extra(YY_EXTRA_TYPE user_defined);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option prefix="audp_"
 
 %x COMMENT

lib/script/chat_lexer.l
@@ -80,6 +80,7 @@ static SDWORD playerIndex;
 %option prefix="chat_"
 %option nounput
 %option case-insensitive
+%option never-interactive
 
 %%
 

lib/script/script_lexer.l
@@ -458,6 +458,7 @@ static void pushInclude(const char *pIncludePath)
 %option prefix="scr_"
 %option nounput
 %option yylineno
+%option never-interactive
 
 %x COMMENT
 %x SLCOMMENT

src/level_lexer.l
@@ -87,7 +87,7 @@ extern void lev_set_extra(YY_EXTRA_TYPE user_defined);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option prefix="lev_"
 
 %x COMMENT

src/message_lexer.l
@@ -79,7 +79,7 @@ extern void message_set_debug(int bdebug);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option prefix="message_"
 
 %x COMMENT

src/scriptvals_lexer.l
@@ -92,7 +92,7 @@ extern void scrv_set_extra(YY_EXTRA_TYPE user_defined);
 
 %}
 
-%option yylineno noyywrap nounput
+%option yylineno noyywrap nounput never-interactive
 %option warn nodefault
 %option prefix="scrv_"
 