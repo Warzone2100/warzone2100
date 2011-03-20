b80cd7a9071f07548d329096d3df8011880a74af
Cyp <cyp@wz2100.net>
2011-03-20 21:38:54 +0100

    Bumping version strings to master_20110320 and netcode version to 20110320.

--- lib/netplay/netplay.cpp
+++ lib/netplay/netplay.cpp
@@ -168,9 +168,9 @@ unsigned NET_PlayerConnectionStatus[CONNECTIONSTATUS_NORMAL][MAX_PLAYERS];
  **            ie ("trunk", "2.1.3", "3.0", ...)
  ************************************************************************************
 **/
-char VersionString[VersionStringSize] = "master, netcode 4.1010";
+char VersionString[VersionStringSize] = "master_20110320";
 static int NETCODE_VERSION_MAJOR = 4;
-static int NETCODE_VERSION_MINOR = 1010;
+static int NETCODE_VERSION_MINOR = 20110320;
 
 bool NETisCorrectVersion(uint32_t game_version_major, uint32_t game_version_minor)
 {
