8c570022edb60a70653a096aae9064558ef246c5
cybersphinx <chr.ohm@gmx.net>
2011-05-20 19:05:37 +0200

    Don't include moced files in translations.

--- po/update-po.sh
+++ po/update-po.sh
@@ -10,7 +10,7 @@ EOF
 
 find lib src data -type f |
 	grep -e '\.c\(pp\|xx\)\?$' -e 'data.*strings.*\.txt$' -e 'data.*sequenceaudio.*\.tx.$' -e '\.slo$' -e '\.rmsg$' -e '\.js$' |
-	grep -v -e '\.lex\.c\(pp\|xx\)\?$' -e '\.tab\.c\(pp\|xx\)\?$' -e 'lib/netplay/miniupnpc/*' -e 'lib/betawidget/*' |
+	grep -v -e '\.lex\.c\(pp\|xx\)\?$' -e '\.tab\.c\(pp\|xx\)\?$' -e 'lib/netplay/miniupnpc/*' -e 'lib/betawidget/*' -e '_moc\.' |
 	sort >> po/POTFILES.in
 
 cd po
