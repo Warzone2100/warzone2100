1454529c5ec1bb34a9ebc0a10e194cc76a060c47
Rene Jochum <from_github@pc-dummy.net>
2011-07-13 18:15:45 +0200

    Don't call po/update-custom.py in po/update-po.sh to remove python ... dependecies for building warzone.

--- po/update-po.sh
+++ po/update-po.sh
@@ -8,10 +8,7 @@ cat > po/POTFILES.in << EOF
 # List of source files which contain translatable strings.
 EOF
 
-find lib src data -type f |
-	grep -e '\.c\(pp\|xx\)\?$' -e 'data.*strings.*\.txt$' -e 'data.*sequenceaudio.*\.tx.$' -e '\.slo$' -e '\.rmsg$' -e '\.js$' |
-	grep -v -e '\.lex\.c\(pp\|xx\)\?$' -e '\.tab\.c\(pp\|xx\)\?$' -e 'lib/netplay/miniupnpc/*' -e 'lib/betawidget/*' -e '_moc\.' |
+find lib src data po -type f |
+	grep -e '\.c\(pp\|xx\)\?$' -e 'data.*strings.*\.txt$' -e 'data.*sequenceaudio.*\.tx.$' -e '\.slo$' -e '\.rmsg$' -e 'po/custom/.*\.txt' -e '\.js$' |
+	grep -v -e '\.lex\.c\(pp\|xx\)\?$' -e '\.tab\.c\(pp\|xx\)\?$' -e 'lib/netplay/miniupnpc/*' -e 'lib/betawidget/*' -e '_moc\.' -e 'po/custom/files.js' |
 	sort >> po/POTFILES.in
-
-cd po
-./update-custom.py filelist >> POTFILES.in
