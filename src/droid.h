810463edfadd9ad3a54801eb99bf1e3e736b0775
vexed <vexed@wz2100.net>
2011-07-17 17:45:59 -0400

    Revert "Bump max structure limits, now that UI supports them."
    This reverts commit 61ed2a1505cf2ae8a44e4a23d6abafb142235160.
    Our engine can't currently support all these units.
    
    Once a new engine is in place, we can increase limits again.
    
    Closes ticket:2812

--- src/droid.h
+++ src/droid.h
@@ -38,7 +38,7 @@
 
 #define DROID_RESISTANCE_FACTOR     30
 
-#define MAX_MP_DROIDS 450
+#define MAX_MP_DROIDS 150
 #define MAX_SP_DROIDS 100
 #define MAX_SP_AI_DROIDS 999
 // Changing this breaks campaign saves!
@@ -53,7 +53,7 @@ extern DROID_TEMPLATE			*apsStaticTemplates;			// for AIs and scripts
 
 /* Define max number of allowed droids per droid type */
 #define MAX_COMMAND_DROIDS		10		// max number of commanders a player can have
-#define MAX_CONSTRUCTOR_DROIDS          90              // max number of constructors a player can have
+#define MAX_CONSTRUCTOR_DROIDS	15		// max number of constructors a player can have
 
 /* Experience modifies */
 #define EXP_REDUCE_DAMAGE		6		// damage of a droid is reduced by this value per experience level, in %
