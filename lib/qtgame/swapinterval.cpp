866aeb950b1f7b55bb6eea1a8bd273bcc1c7302c
safety0ff <safety0ff@users.sourceforge.net>
2011-07-18 21:03:05 +0000

    Make swapinterval.cpp independant of glew, this fixes issues caused by accessing glew vars before glew is initiliazed. Closes #2826.

--- lib/qtgame/swapinterval.cpp
+++ lib/qtgame/swapinterval.cpp
@@ -4,7 +4,7 @@
 
 #if defined(WZ_WS_X11)
 
-#include <GL/glxew.h>
+#include <GL/glx.h> // GLXDrawable
 // X11 polution
 #ifdef Status
 #undef Status
@@ -16,15 +16,32 @@
 #undef Bool
 #endif
 
+#ifndef GLX_SWAP_INTERVAL_EXT
+#define GLX_SWAP_INTERVAL_EXT 0x20F1
+#endif
+
 #include <QtGui/QX11Info>
 #include <QtOpenGL/QGLWidget>
 
 
 void setSwapInterval(QGLWidget const &glWidget, int * interval)
 {
+	typedef void (* PFNGLXQUERYDRAWABLEPROC) (Display *, GLXDrawable, int, unsigned int *);
+	typedef void ( * PFNGLXSWAPINTERVALEXTPROC) (Display*, GLXDrawable, int);
+	typedef int (* PFNGLXGETSWAPINTERVALMESAPROC)(void);
+	typedef int (* PFNGLXSWAPINTERVALMESAPROC)(unsigned);
+	typedef int ( * PFNGLXSWAPINTERVALSGIPROC) (int);
+	PFNGLXSWAPINTERVALEXTPROC glXSwapIntervalEXT;
+	PFNGLXQUERYDRAWABLEPROC glXQueryDrawable;
+	PFNGLXGETSWAPINTERVALMESAPROC glXGetSwapIntervalMESA;
+	PFNGLXSWAPINTERVALMESAPROC glXSwapIntervalMESA;
+	PFNGLXSWAPINTERVALSGIPROC glXSwapIntervalSGI;
 	QGLContext const &context = *glWidget.context();
 	QX11Info const &xinfo = glWidget.x11Info();
-	if (GLXEW_EXT_swap_control)
+
+	glXSwapIntervalEXT = (PFNGLXSWAPINTERVALEXTPROC) context.getProcAddress("glXSwapIntervalEXT");
+	glXQueryDrawable = (PFNGLXQUERYDRAWABLEPROC) context.getProcAddress("glXQueryDrawable");
+	if (glXSwapIntervalEXT && glXQueryDrawable)
 	{
 		unsigned clampedInterval;
 		if (*interval < 0)
@@ -32,23 +49,22 @@ void setSwapInterval(QGLWidget const &glWidget, int * interval)
 		glXSwapIntervalEXT(xinfo.display(), glWidget.winId(), *interval);
 		glXQueryDrawable(xinfo.display(), glWidget.winId(), GLX_SWAP_INTERVAL_EXT, &clampedInterval);
 		*interval = clampedInterval;
+		return;
 	}
-	else if (xinfo.display() && strstr(glXQueryExtensionsString(xinfo.display(), xinfo.screen()), "GLX_MESA_swap_control"))
-	{
-		typedef int (* PFNGLXSWAPINTERVALMESAPROC)(unsigned);
-		typedef int (* PFNGLXGETSWAPINTERVALMESAPROC)(void);
-		PFNGLXSWAPINTERVALMESAPROC glXSwapIntervalMESA = (PFNGLXSWAPINTERVALMESAPROC) context.getProcAddress("glXSwapIntervalMESA");
-		PFNGLXGETSWAPINTERVALMESAPROC glXGetSwapIntervalMESA = (PFNGLXGETSWAPINTERVALMESAPROC) context.getProcAddress("glXGetSwapIntervalMESA");
 
-		if (glXSwapIntervalMESA && glXGetSwapIntervalMESA)
-		{
-			if (*interval < 0)
-				*interval = 0;
-			glXSwapIntervalMESA(*interval);
-			*interval = glXGetSwapIntervalMESA();
-		}
+	glXSwapIntervalMESA = (PFNGLXSWAPINTERVALMESAPROC) context.getProcAddress("glXSwapIntervalMESA");
+	glXGetSwapIntervalMESA = (PFNGLXGETSWAPINTERVALMESAPROC) context.getProcAddress("glXGetSwapIntervalMESA");
+	if (glXSwapIntervalMESA && glXGetSwapIntervalMESA)
+	{
+		if (*interval < 0)
+			*interval = 0;
+		glXSwapIntervalMESA(*interval);
+		*interval = glXGetSwapIntervalMESA();
+		return;
 	}
-	else if (GLXEW_SGI_swap_control)
+
+	glXSwapIntervalSGI = (PFNGLXSWAPINTERVALSGIPROC) context.getProcAddress("glXSwapIntervalSGI");
+	if (glXSwapIntervalSGI)
 	{
 		if (*interval < 1)
 			*interval = 1;
@@ -58,20 +74,27 @@ void setSwapInterval(QGLWidget const &glWidget, int * interval)
 			*interval = 1;
 			glXSwapIntervalSGI(1);
 		}
+		return;
 	}
-	else
-	{
-		*interval = -1;
-	}
+
+	*interval = -1;
 }
 
 #elif defined(WZ_WS_WIN)
-#include <GL/wglew.h>
 #include <QtOpenGL/QGLWidget>
 
-void setSwapInterval(QGLWidget const &, int * interval)
+void setSwapInterval(QGLWidget const &glWidget, int * interval)
 {
-	if (WGLEW_EXT_swap_control)
+	typedef int (WINAPI * PFNWGLGETSWAPINTERVALEXTPROC) (void);
+	typedef BOOL (WINAPI * PFNWGLSWAPINTERVALEXTPROC) (int);
+	PFNWGLGETSWAPINTERVALEXTPROC wglGetSwapIntervalEXT;
+	PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT;
+	QGLContext const &context = *glWidget.context();
+
+	wglGetSwapIntervalEXT = (PFNWGLGETSWAPINTERVALEXTPROC) context.getProcAddress("wglGetSwapIntervalEXT");
+	wglSwapIntervalEXT = (PFNWGLSWAPINTERVALEXTPROC) context.getProcAddress("wglSwapIntervalEXT");
+
+	if (wglGetSwapIntervalEXT && wglSwapIntervalEXT)
 	{
 		if (*interval < 0)
 			*interval = 0;
