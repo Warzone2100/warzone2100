bc26936424803cbdf8e326a183a7eeba18dbe112
safety0ff <safety0ff@users.sourceforge.net>
2011-07-30 17:24:35 +0000

    Further improve documentation of map/pngsave.h.

--- tools/map/pngsave.h
+++ tools/map/pngsave.h
@@ -8,6 +8,7 @@
  * The pixels argument should such that
  * pixels[0] is the bottom left corner and
  * pixels[h-1] is the top left corner
+ * Pixel components which exceed one byte are expected in network byte order.
  */
 bool savePng(const char *filename, uint8_t *pixels, int w, int h);
 bool savePngARGB32(const char *filename, uint8_t *pixels, int w, int h);
