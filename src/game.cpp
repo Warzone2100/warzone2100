0d6a27380de7dc286d6e0131b3e4e2a26c200f1b
vexed <vexed@wz2100.net>
2011-09-08 17:40:07 -0400

    When reading in map values from a map, don't do any conversions on said map for the player's starting position.  They should __not__ be remaped.
    fixes ticket:2859
    refs: 09eb6e79f3cb095b83a526d2024e87f33585085b

--- src/game.cpp
+++ src/game.cpp
@@ -6340,7 +6340,7 @@ bool plotStructurePreview16(char *backDropSprite, Vector2i playeridpos[])
 			ini.beginGroup(list[i]);
 			QString name = ini.value("name").toString();
 			Position pos = ini.vector3i("position");
-			playerid = getPlayer(ini);
+			playerid = ini.value("startpos").toInt();		// No conversion should be going on, this is the map makers position when player X should be.
 
 			if (name.startsWith("A0CommandCentre"))
 			{
