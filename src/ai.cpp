faecbdd14b0a926d9b9b5431ba0de2261fb9a000
Jorge Cardoso Leitão <jocl1@sapo.pt>
2011-10-01 15:16:50 +0100

    Small fix on aiBestNearestToRepair().

--- src/ai.cpp
+++ src/ai.cpp
@@ -668,10 +668,9 @@ DROID *aiBestNearestToRepair(DROID *psDroid)
 	// if guarding a unit, check that first
 	DROID* guardedDroid = castDroid(orderStateObj(psDroid, DORDER_GUARD));
 	if (guardedDroid != NULL
-	    && guardedDroid->type == OBJ_DROID
 	    && droidIsDamaged(guardedDroid)
 	    && !aiObjectIsProbablyDoomed(guardedDroid)
-		)
+	    )
 	{
 		return guardedDroid;
 	}
@@ -695,34 +694,38 @@ DROID *aiBestNearestToRepair(DROID *psDroid)
 	// the range to search is given by REPAIR_RANGE in case is on hold, or REPAIR_MAXDIST^2 if not.
 	int droidRange = ((psDroid->order==DORDER_NONE && secondaryGetState(psDroid, DSO_HALTTYPE)==DSS_HALT_HOLD) ? REPAIR_RANGE : REPAIR_MAXDIST*REPAIR_MAXDIST);
 
+
+	int64_t bestDroidRepairWeight = (int64_t)100*1073741824; // the weight of the best droid to repair.
+
 	// iterate on all objects within range
 	gridStartIterate(psDroid->pos.x, psDroid->pos.y, droidRange);
 	for (BASE_OBJECT* psTarget = gridIterate(); psTarget != NULL; psTarget = gridIterate())
 	{
-        int64_t bestDroidRepairWeight = 0; // the weight of the best droid to repair.
-
 		// if it is of the same alliance, it is a droid, it is damaged, is visible and is not probably doomed, then we can repair it
 		if (aiCheckAlliances(psTarget->player,psDroid->player)
 		                     && psTarget->type == OBJ_DROID
-                             && psTarget != psDroid
-                             && droidIsDamaged((DROID*)psTarget)
-                             && !(isVtolDroid((DROID*)psTarget) && isFlying((DROID*)psTarget)) // vtol and flying
-                             && visibleObject(psDroid, psTarget, false)
-                             && !aiObjectIsProbablyDoomed(psTarget))
+		                     && psTarget != psDroid
+		                     && droidIsDamaged((DROID*)psTarget)
+		                     && !(isVtolDroid((DROID*)psTarget) && isFlying((DROID*)psTarget)) // vtol and flying
+		                     && visibleObject(psDroid, psTarget, false)
+		                     && !aiObjectIsProbablyDoomed(psTarget))
 		{
-			// this part of the code is used to minimize the weighted sum = targetHealth*healthWeight + targetArmor*(1-healthWeight). The healthWeight is a value [0,1] which states the force that the targetHealth should have. For instance, if healthWeight = 1, then sum = targetHealth, which means that the distance is not taken into account for the calculation
+			// this part of the code is used to minimize the weighted sum = targetHealth*healthWeight + targetDistance*(100-healthWeight). The healthWeight is a value [0,100] which states the force that the targetHealth should have. For instance, if healthWeight = 100, then sum = targetHealth, which means that the distance is not taken into account for the calculation
 
-			const int64_t droidToTargetDistance = ((int64_t) droidSqDist(psDroid, psTarget))*1073741824/droidRange; // relative distance
+			const int64_t targetDistance = ((int64_t) droidSqDist(psDroid, psTarget))*1073741824/droidRange; // relative distance
 			const int64_t targetHealth = ((int64_t) (psTarget->body))*1073741824/((DROID*)psTarget)->originalBody; // relative health
 
-			// this is the relative amount of weight that is given to armor
+			// this is the relative amount of weight that is given to health
 			const int64_t healthWeight = 75;
 
-			// if this target is on a worse condition than the previous one
-			if (targetHealth*healthWeight + droidToTargetDistance*(100 - healthWeight) > bestDroidRepairWeight)
+			// total weight given to the current droid.
+			const int64_t droidRepairWeight = targetHealth*healthWeight + targetDistance*(100 - healthWeight);
+
+			// if this target is on a worse condition than the worst potential repair droid
+			if (droidRepairWeight < bestDroidRepairWeight)
 			{
 				// then use this as the most likely target
-                bestDroidRepairWeight = targetHealth*healthWeight + droidToTargetDistance*(100 - healthWeight);
+				bestDroidRepairWeight = droidRepairWeight;
 				psMostLikelyTarget = (DROID*)psTarget;
 			}
 		}
