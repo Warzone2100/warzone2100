048a97e3e6f75cd915ea49ad268d7631eef75e55
cybersphinx <chr.ohm@gmx.net>
2012-01-09 01:41:57 +0100

    Restore verbose lobby error message.

--- lib/netplay/netplay.cpp
+++ lib/netplay/netplay.cpp
@@ -2035,7 +2035,8 @@ static void NETregisterServer(int state)
 				{
 					debug(LOG_ERROR, "Cannot connect to masterserver \"%s:%d\": %s", masterserver_name, masterserver_port, strSockError(getSockErr()));
 					free(NetPlay.MOTD);
-					if (asprintf(&NetPlay.MOTD, _("Could not communicate with lobby server! Is TCP port %u open for outgoing traffic?"), masterserver_port) == -1)
+					if (asprintf(&NetPlay.MOTD, _("Error connecting to the lobby server: %s. Make sure port %d can receive incoming connections. If you're using a router configure it to use UPnP, or to forward the port to your system."),
+					    strSockError(getSockErr()), masterserver_port) == -1)
 						NetPlay.MOTD = NULL;
 					server_not_there = true;
 					return;
