395cb1bf5075b334a2725c17f656183673abe37b
cybersphinx <chr.ohm@gmx.net>
2012-01-31 21:10:05 +0100

    Disentangle some Mac includes.

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,10 @@
 #  include <shellapi.h> /* CommandLineToArgvW */
 #elif defined(WZ_OS_UNIX)
 #  include <errno.h>
+#elif defined(WZ_OS_MAC)
+# include <CoreServices/CoreServices.h>
+# include <unistd.h>
+# include "cocoa_wrapper.h"
 #endif // WZ_OS_WIN
 
 #include "lib/framework/input.h"
@@ -97,9 +101,6 @@ enum FOCUS_STATE
 #if defined(WZ_OS_WIN)
 # define WZ_WRITEDIR "Warzone 2100 master"
 #elif defined(WZ_OS_MAC)
-# include <CoreServices/CoreServices.h>
-# include <unistd.h>
-# include "cocoa_wrapper.h"
 # define WZ_WRITEDIR "Warzone 2100 master"
 #else
 # define WZ_WRITEDIR ".warzone2100-master"
