Warzone2100/warzone2100

commit 5506daef71feb4e3c513b2a35788da5b4858af61

Move WZ_WRITEDIR to frame.h and use it for the crashdumps as well.
Closes #3095.

lib/exceptionhandler/exchndl.cpp
@@ -1203,7 +1203,8 @@ void ExchndlSetup(const char *packageVersion)
 	// NOTE: CSIDL_PERSONAL =  C:\Users\user name\Documents
 	if ( SUCCEEDED( SHGetFolderPathW( NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, miniDumpPath ) ))
 	{
-		PathAppendW( miniDumpPath, L"Warzone 2100 master\\logs" );
+		PathAppendW( miniDumpPath, WZ_WRITEDIR);
+		PathAppendW( miniDumpPath, L"\\logs" );
 
 		if( !PathFileExistsW( miniDumpPath ) )
 		{

lib/framework/frame.h
@@ -61,6 +61,14 @@ typedef uint16_t PlayerMask;
 #error Warzone 2100 is not a MMO.
 #endif
 
+#if defined(WZ_OS_WIN)
+# define WZ_WRITEDIR "Warzone 2100 master"
+#elif defined(WZ_OS_MAC)
+# define WZ_WRITEDIR "Warzone 2100 master"
+#else
+# define WZ_WRITEDIR ".warzone2100-master"
+#endif
+
 enum QUEUE_MODE
 {
 	ModeQueue,      ///< Sends a message on the game queue, which will get synchronised, by sending a GAME_ message.

src/main.cpp
@@ -98,14 +98,6 @@ enum FOCUS_STATE
 	FOCUS_IN,		// Window has got the focus
 };
 
-#if defined(WZ_OS_WIN)
-# define WZ_WRITEDIR "Warzone 2100 master"
-#elif defined(WZ_OS_MAC)
-# define WZ_WRITEDIR "Warzone 2100 master"
-#else
-# define WZ_WRITEDIR ".warzone2100-master"
-#endif
-
 bool customDebugfile = false;		// Default false: user has NOT specified where to store the stdout/err file.
 
 char datadir[PATH_MAX] = ""; // Global that src/clparse.c:ParseCommandLine can write to, so it can override the default datadir on runtime. Needs to be empty on startup for ParseCommandLine to work!

commit 1b0a7b7fb02a535ebe527fe067b704dd0787b4f4

Fix cross-compilation.
Broken in 5506daef71feb4e3c513b2a35788da5b4858af61, which was supposed
to fix #3108.

lib/exceptionhandler/exchndl.cpp
@@ -40,6 +40,8 @@
 #define HAVE_BFD	1
 #endif
 
+#define WSTRING(x) CONCAT(L, x)
+
 // Declare the static variables
 static wchar_t szLogFileName[MAX_PATH] = L"";
 static LPTOP_LEVEL_EXCEPTION_FILTER prevExceptionFilter = NULL;
@@ -1203,7 +1205,7 @@ void ExchndlSetup(const char *packageVersion)
 	// NOTE: CSIDL_PERSONAL =  C:\Users\user name\Documents
 	if ( SUCCEEDED( SHGetFolderPathW( NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, miniDumpPath ) ))
 	{
-		PathAppendW( miniDumpPath, WZ_WRITEDIR);
+		PathAppendW( miniDumpPath, WSTRING(WZ_WRITEDIR));
 		PathAppendW( miniDumpPath, L"\\logs" );
 
 		if( !PathFileExistsW( miniDumpPath ) )

lib/framework/frame.h
@@ -46,6 +46,9 @@
 #include "trig.h"
 #include "cursors.h"
 
+#define REALCONCAT(x, y) x ## y
+#define CONCAT(x, y) REALCONCAT(x, y)
+
 extern uint32_t selectedPlayer;      ///< The player number corresponding to this client.
 extern uint32_t realSelectedPlayer;  ///< The player number corresponding to this client (same as selectedPlayer, unless changing players in the debug menu).
 #define MAX_PLAYERS         11                 ///< Maximum number of players in the game.