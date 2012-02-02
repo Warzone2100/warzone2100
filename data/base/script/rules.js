4a71db9e9b9f5884324943350776a7099b478250
cybersphinx <chr.ohm@gmx.net>
2012-02-02 22:57:43 +0100

    Add general rules script for the campaign.
    Enable design and minimap only when an HQ exists.

---
+++ data/base/script/rules.js
@@ -0,0 +1,37 @@
+// General rules for the campaign
+//
+// * Enable unit design and minimap only when an HQ exists
+
+function eventStartLevel()
+{
+	// Disable by default
+	setMiniMap(false);
+	setDesign(false);
+
+	var structlist = enumStruct(me);
+	for (var i = 0; i < structlist.length; i++)
+	{
+		// Simulate build events to enable minimap/unit design when an HQ exists
+		eventStructureBuilt(structlist[i]);
+	}
+}
+
+function eventStructureBuilt(struct)
+{
+	if (struct.player == selectedPlayer && struct.type == STRUCTURE && struct.stattype == HQ)
+	{
+		// Enable unit design and minimap when an HQ gets built
+		setMiniMap(true);
+		setDesign(true);
+	}
+}
+
+function eventDestroyed(victim)
+{
+	if (victim.player == selectedPlayer && victim.type == STRUCTURE && victim.stattype == HQ)
+	{
+		// Disable unit design and minimap when the HQ gets destroyed
+		setMiniMap(false);
+		setDesign(false);
+	}
+}
