fb7c7ccdf1c5e3d0a11ac047391ef5e3a2eeab1b
dak180 <dak180@users.sourceforge.net>
2012-02-01 23:57:39 -0500

    Fix the help bundle.
    The info.plist file is now once again included in the bundle.

--- macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
+++ macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DirectorY="WarzoneHelp-8d40cbe"
+DirectorY="WarzoneHelp-44d8bf7"
 OutDir="WarzoneHelp"
-FileName="WarzoneHelp-8d40cbe.tgz"
-BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/WarzoneHelp-8d40cbe.tgz"
-MD5Sum="b5f5622d51fe8c2c364469192b483816"
+FileName="WarzoneHelp-44d8bf7.tgz"
+BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/WarzoneHelp-44d8bf7.tgz"
+MD5Sum="606a2d131a9d5de498eb24e5887ae4ae"
 
 configs/FetchPrebuilt.sh "${DirectorY}" "${OutDir}" "${FileName}" "${BuiltDLP}" "${MD5Sum}"
 exit ${?}
