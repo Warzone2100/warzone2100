c7f3d99d54c647147d9a7a9c375f1ce7a0bd66e8
dak180 <dak180@users.sourceforge.net>
2012-02-04 15:25:40 -0500

    Update the docs for xcode builds.
    Also restructure the script so it will require less changes in the future.

--- macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
+++ macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DirectorY="WarzoneHelp-44d8bf7"
 OutDir="WarzoneHelp"
-FileName="WarzoneHelp-44d8bf7.tgz"
-BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/WarzoneHelp-44d8bf7.tgz"
-MD5Sum="606a2d131a9d5de498eb24e5887ae4ae"
+DirectorY="${OutDir}-94d7d61"
+FileName="${DirectorY}.tgz"
+BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/${FileName}"
+MD5Sum="1d39a208b38537d401db310ac362b46a"
 
 configs/FetchPrebuilt.sh "${DirectorY}" "${OutDir}" "${FileName}" "${BuiltDLP}" "${MD5Sum}"
 exit ${?}
