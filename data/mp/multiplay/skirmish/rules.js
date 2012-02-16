Warzone2100/warzone2100

commit f35de0d51304f68e104d1673d4e284e335472f99

qtscript: Generalize attacked event to cover droids as well. Fire off droid built event also if droids are built through cheating. Make trigger function names consistent.

src/cluster.cpp
@@ -561,6 +561,7 @@ void clustObjectAttacked(BASE_OBJECT *psObj)
 	{
 		psScrCBTarget = psObj;
 		eventFireCallbackTrigger((TRIGGER_TYPE)CALL_ATTACKED);
+		triggerEventAttacked(psObj, g_pProjLastAttacker);
 
 		switch (psObj->type)
 		{
@@ -572,7 +573,6 @@ void clustObjectAttacked(BASE_OBJECT *psObj)
 			case OBJ_STRUCTURE:
 				psLastStructHit = (STRUCTURE *)psObj;
 				eventFireCallbackTrigger((TRIGGER_TYPE)CALL_STRUCT_ATTACKED);
-				triggerStructureAttacked((STRUCTURE *)psObj, g_pProjLastAttacker);
 				psLastStructHit = NULL;
 				break;
 			default:

data/base/multiplay/script/scavfact.js
@@ -117,16 +117,16 @@ function eventDroidBuilt(droid, fac1)
 }
 
 // watch for structures being attacked. Send the cavalry as required.
-function eventStructureAttacked(structure, attacker)
+function eventAttacked(victim, attacker)
 {
-	if ((gameTime - lastAttack) > 3000)
+	if (victim.type == STRUCTURE && (gameTime - lastAttack) > 3000)
 	{
 		lastAttack = gameTime;
 		var droidlist = enumGroup(attackGroup);
 		for (var i = 0; i < droidlist.length; i++)
 		{
 			var droid = droidlist[i];
-			if (distBetweenTwoPoints(structure.x, structure.y, attacker.x, attacker.y) < (24 * 128))
+			if (distBetweenTwoPoints(victim.x, victim.y, attacker.x, attacker.y) < (24 * 128))
 			{
 				orderDroidLoc(droid, DORDER_MOVE, attacker.x, attacker.y);
 			}

data/base/multiplay/skirmish/semperfi.js
@@ -224,7 +224,7 @@ function eventGameInit()
 {
 }
 
-function eventStructureAttacked(struct, attacker)
+function eventAttacked(victim, attacker)
 {
 }
 

src/hci.cpp
@@ -78,6 +78,7 @@
 #include "template.h"
 #include "wrappers.h"
 #include "keybind.h"
+#include "qtscript.h"
 
 //#define DEBUG_SCROLLTABS 	//enable to see tab scroll button info for buttons
 
@@ -2027,6 +2028,8 @@ INT_RETVAL intRunWidgets(void)
 						psScrCBNewDroidFact = NULL;
 						eventFireCallbackTrigger((TRIGGER_TYPE)CALL_NEWDROID);	// notify scripts so it will get assigned jobs
 						psScrCBNewDroid = NULL;
+
+						triggerEventDroidBuilt(psDroid, NULL);
 					}
 					else
 					{

src/qtscript.cpp
@@ -591,10 +591,10 @@ bool triggerEventStructBuilt(STRUCTURE *psStruct, DROID *psDroid)
 	return true;
 }
 
-//__ \subsection{eventStructureAttacked(structure, attacker)}
-//__ An event that is run when a structure is attacked. The attacker parameter may be
-//__ either a structure or a droid.
-bool triggerStructureAttacked(STRUCTURE *psVictim, BASE_OBJECT *psAttacker)
+//__ \subsection{eventAttacked(victim, attacker)}
+//__ An event that is run when an object belonging to the script's controlling player is
+//__ attacked. The attacker parameter may be either a structure or a droid.
+bool triggerEventAttacked(BASE_OBJECT *psVictim, BASE_OBJECT *psAttacker)
 {
 	if (!psAttacker)
 	{
@@ -609,9 +609,9 @@ bool triggerStructureAttacked(STRUCTURE *psVictim, BASE_OBJECT *psAttacker)
 		if (player == psVictim->player)
 		{
 			QScriptValueList args;
-			args += convStructure(psVictim, engine);
-			args += convObj(psAttacker, engine);
-			callFunction(engine, "eventStructureAttacked", args);
+			args += convMax(psVictim, engine);
+			args += convMax(psAttacker, engine);
+			callFunction(engine, "eventAttacked", args);
 		}
 	}
 	return true;
@@ -620,7 +620,7 @@ bool triggerStructureAttacked(STRUCTURE *psVictim, BASE_OBJECT *psAttacker)
 //__ \subsection{eventResearched(research[, structure])}
 //__ An event that is run whenever a new research is available. The structure
 //__ parameter is set if the research comes from a research lab.
-bool triggerResearched(RESEARCH *psResearch, STRUCTURE *psStruct, int player)
+bool triggerEventResearched(RESEARCH *psResearch, STRUCTURE *psStruct, int player)
 {
 	for (int i = 0; i < scripts.size() && psStruct; ++i)
 	{

src/qtscript.h
@@ -70,8 +70,8 @@ bool triggerEvent(SCRIPT_TRIGGER_TYPE trigger);
 
 // For each trigger with function parameters, a function to trigger it here
 bool triggerEventDroidBuilt(DROID *psDroid, STRUCTURE *psFactory);
-bool triggerStructureAttacked(STRUCTURE *psVictim, BASE_OBJECT *psAttacker);
-bool triggerResearched(RESEARCH *psResearch, STRUCTURE *psStruct, int player);
+bool triggerEventAttacked(BASE_OBJECT *psVictim, BASE_OBJECT *psAttacker);
+bool triggerEventResearched(RESEARCH *psResearch, STRUCTURE *psStruct, int player);
 bool triggerEventStructBuilt(STRUCTURE *psStruct, DROID *psDroid);
 
 // bool triggerEventReachedLocation(ORDER order, DROID *psDroid);

src/research.cpp
@@ -1176,7 +1176,7 @@ void researchResult(UDWORD researchIndex, UBYTE player, bool bDisplay, STRUCTURE
 		CBResFacilityOwner = -1;
 		psCBLastResearch = NULL;
 
-		triggerResearched(pResearch, psResearchFacility, player);
+		triggerEventResearched(pResearch, psResearchFacility, player);
 	}
 
 #ifdef DEBUG

src/structure.cpp
@@ -2307,8 +2307,6 @@ static bool structPlaceDroid(STRUCTURE *psStructure, DROID_TEMPLATE *psTempl,
 			return false;
 		}
 
-		//set the droids order to that of the factory - AB 22/04/99
-		//psNewDroid->secondaryOrder = psStructure->pFunctionality->factory.secondaryOrder;
 		if (myResponsibility(psStructure->player))
 		{
 			uint32_t newState = psStructure->pFunctionality->factory.secondaryOrder;

commit 001ef35faf1b1207db0e45ed18fbf072356372f6

Play the correct sounds when we are getting attacked.
Broken in f35de0d51304f68e104d1673d4e284e335472f99

NOTE: see aforementioned commit b18b76ea8a553b06c4f7258fc8432c7db280b9aa
on why we should be spitting out warnings/errors, not ignore these issues.

Fixes ticket:3102

data/mp/multiplay/skirmish/rules.js
@@ -224,18 +224,18 @@ function checkEndConditions()
 // /////////////////////////////////////////////////////////////////
 // WARNING MESSAGES
 // Base Under Attack
-function eventStructureAttacked(hitStruct, attackerObj)
+function eventAttacked(victimObj, attackerObj)
 {
 	if (gameTime > lastHitTime + 10)
 	{
 		lastHitTime = gameTime;
-		if (hitStruct)
+		if (victimObj.type == STRUCTURE)
 		{
-			playSound("pcv337.ogg", hitStruct.x, hitStruct.y, hitStruct.z);	// show position if still alive
+			playSound("pcv337.ogg", victimObj.x, victimObj.y, victimObj.z);	// show position if still alive
 		}
 		else
 		{
-			playSound("pcv337.ogg");
+			playSound("pcv399.ogg", victimObj.x, victimObj.y, victimObj.z);
 		}
 	}
 }

src/cluster.cpp
@@ -578,7 +578,8 @@ void clustObjectAttacked(BASE_OBJECT *psObj)
 				ASSERT(!"invalid object type", "clustObjectAttacked: invalid object type");
 				return;
 		}
-
+		// and fire the sound effect (and/or...) for the added callback trigger we just processed
+		triggerEventAttacked(psObj, g_pProjLastAttacker);
 		aClusterAttacked[psObj->cluster] = gameTime;
 	}
 }