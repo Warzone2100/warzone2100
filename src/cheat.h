Warzone2100/warzone2100

commit 68639f734fd3d5a8b5f29b0becd7dc03b5ca0c1f

Synchronise enabling of debug mode.

src/cheat.h
@@ -23,4 +23,6 @@
 
 bool attemptCheatCode(const char* cheat_name);
 
+void sendProcessDebugMappings(bool val);
+
 #endif // __INCLUDED_SRC_CHEAT_H__

src/multiplay.cpp
@@ -609,6 +609,9 @@ bool recvMessage(void)
 			case GAME_LASSAT:
 				recvLasSat(queue);
 				break;
+			case GAME_DEBUG_MODE:
+				recvProcessDebugMappings(queue);
+				break;
 			default:
 				processedMessage1 = false;
 				break;

src/multirecv.h
@@ -27,6 +27,7 @@
 #ifndef __INCLUDED_SRC_MULTIRECV_H__
 #define __INCLUDED_SRC_MULTIRECV_H__
 
+bool recvProcessDebugMappings(NETQUEUE queue);
 extern bool recvDroid               (NETQUEUE queue);
 extern bool recvDroidInfo           (NETQUEUE queue);
 extern bool recvDestroyDroid        (NETQUEUE queue);

lib/netplay/netplay.cpp
@@ -3475,8 +3475,8 @@ const char *messageTypeToString(unsigned messageType_)
 		case GAME_LASSAT:                   return "GAME_LASSAT";
 		case GAME_GAME_TIME:                return "GAME_GAME_TIME";
 		case GAME_PLAYER_LEFT:              return "GAME_PLAYER_LEFT";
-		// The following messages (not including GAME_MAX_TYPE) are currently redundant, and should probably at some point not be
-		// sent, except (some of them) when using cheats in debug mode.
+		// The following messages are used for debug mode.
+		case GAME_DEBUG_MODE:               return "GAME_DEBUG_MODE";
 		case GAME_DROID:                    return "GAME_DROID";
 		case GAME_BUILDFINISHED:            return "GAME_BUILDFINISHED";
 		case GAME_FEATURES:                 return "GAME_FEATURES";

lib/netplay/netplay.h
@@ -103,8 +103,8 @@ enum MESSAGE_TYPES
 	GAME_LASSAT,                    ///< lassat firing.
 	GAME_GAME_TIME,                 ///< Game time. Used for synchronising, so that all messages are executed at the same gameTime on all clients.
 	GAME_PLAYER_LEFT,               ///< Player has left or dropped.
-	// The following messages (not including GAME_MAX_TYPE) are currently redundant, and should probably at some point not be
-	// sent, except (some of them) when using cheats in debug mode.
+	// The following messages are used for debug mode.
+	GAME_DEBUG_MODE,                ///< Request enable/disable debug mode.
 	GAME_DROID,                     ///< a new droid
 	GAME_BUILDFINISHED,             ///< a building is complete.
 	GAME_FEATURES,                  ///< information regarding features.
@@ -113,7 +113,7 @@ enum MESSAGE_TYPES
 	GAME_FEATUREDEST,               ///< destroy a game feature.
 	GAME_RESEARCH,                  ///< Research has been completed.
 	GAME_DROIDDISEMBARK,            ///< droid disembarked from a Transporter
-	// End of redundant messages.
+	// End of debug messages.
 	GAME_MAX_TYPE                   ///< Maximum+1 valid GAME_ type, *MUST* be last.
 };
 //#define SYNC_FLAG (NUM_GAME_PACKETS * NUM_GAME_PACKETS)	//special flag used for logging.

src/cheat.cpp
@@ -25,10 +25,12 @@
 
 #include "lib/framework/frame.h"
 #include "lib/exceptionhandler/dumpinfo.h"
+#include "lib/netplay/netplay.h"
 
 #include "cheat.h"
 #include "keybind.h"
 #include "keymap.h"
+#include "multiplay.h"
 
 struct CHEAT_ENTRY
 {
@@ -133,4 +135,43 @@ bool attemptCheatCode(const char* cheat_name)
 	}
 
 	return false;
+}
+
+void sendProcessDebugMappings(bool val)
+{
+	NETbeginEncode(NETgameQueue(selectedPlayer), GAME_DEBUG_MODE);
+		NETbool(&val);
+	NETend();
+}
+
+void recvProcessDebugMappings(NETQUEUE queue)
+{
+	bool val = false;
+	NETbeginDecode(queue, GAME_DEBUG_MODE);
+		NETbool(&val);
+	NETend();
+
+	bool oldDebugMode = getDebugMappingStatus();
+	processDebugMappings(queue.index, val);
+	bool newDebugMode = getDebugMappingStatus();
+
+	char const *cmsg;
+	if (val)
+	{
+		sasprintf((char**)&cmsg, _("%s wants to enable debug mode. Enabled: %s, Disabled: %s."), getPlayerName(queue.index), getWantedDebugMappingStatuses(true).c_str(), getWantedDebugMappingStatuses(false).c_str());
+	}
+	else
+	{
+		sasprintf((char**)&cmsg, _("%s wants to disable debug mode. Enabled: %s, Disabled: %s."), getPlayerName(queue.index), getWantedDebugMappingStatuses(true).c_str(), getWantedDebugMappingStatuses(false).c_str());
+	}
+	addConsoleMessage(cmsg, DEFAULT_JUSTIFY,  SYSTEM_MESSAGE);
+
+	if (!oldDebugMode && newDebugMode)
+	{
+		addConsoleMessage(_("Debug mode now enabled!"), DEFAULT_JUSTIFY,  SYSTEM_MESSAGE);
+	}
+	else if (oldDebugMode && !newDebugMode)
+	{
+		addConsoleMessage(_("Debug mode now disabled!"), DEFAULT_JUSTIFY,  SYSTEM_MESSAGE);
+	}
 }

src/init.cpp
@@ -1267,5 +1267,8 @@ static void	initMiscVars(void)
 	enableConsoleDisplay(true);
 
 	setSelectedGroup(UBYTE_MAX);
-	processDebugMappings(false);
+	for (unsigned n = 0; n < MAX_PLAYERS; ++n)
+	{
+		processDebugMappings(n, false);
+	}
 }

src/keybind.cpp
@@ -1214,29 +1214,7 @@ void	kf_TogglePowerBar( void )
 /* Toggles whether we process debug key mappings */
 void	kf_ToggleDebugMappings( void )
 {
-	const char* cmsg;
-
-#ifndef DEBUG
-	// Prevent cheating in multiplayer when not compiled in debug mode by
-	// bailing out if we're running a _true_ multiplayer game
-	if (runningMultiplayer())
-	{
-		noMPCheatMsg();
-		return;
-	}
-#endif
-
-	if (getDebugMappingStatus())
-	{
-		processDebugMappings(false);
-	}
-	else
-	{
-		processDebugMappings(true);
-	}
-	sasprintf((char**)&cmsg, _("(Player %u) is using cheat :%s"), selectedPlayer,
-		 getDebugMappingStatus() ? _("CHEATS ARE NOW ENABLED!") : _("CHEATS ARE NOW DISABLED!"));
-	sendTextMessage(cmsg, true);
+	sendProcessDebugMappings(!getWantedDebugMappingStatus(selectedPlayer));
 }
 // --------------------------------------------------------------------------
 

commit 9a66b4e0d64a1d95e466b0347ec65b79adef42c1

Correctly define the function for compilers that aren't broken enough to allow bool blah() == void blah() which should throw a error since we don't overload it.
Broken in 68639f734fd3d5a8b5f29b0becd7dc03b5ca0c1f

src/cheat.h
@@ -24,5 +24,5 @@
 bool attemptCheatCode(const char* cheat_name);
 
 void sendProcessDebugMappings(bool val);
-
+void recvProcessDebugMappings(NETQUEUE queue);
 #endif // __INCLUDED_SRC_CHEAT_H__

src/multiplay.cpp
@@ -69,6 +69,7 @@
 #include "multigifts.h"								// gifts and alliances.
 #include "multiint.h"
 #include "keymap.h"
+#include "cheat.h"
 
 // ////////////////////////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////////////////////////////

src/multirecv.h
@@ -27,7 +27,6 @@
 #ifndef __INCLUDED_SRC_MULTIRECV_H__
 #define __INCLUDED_SRC_MULTIRECV_H__
 
-bool recvProcessDebugMappings(NETQUEUE queue);
 extern bool recvDroid               (NETQUEUE queue);
 extern bool recvDroidInfo           (NETQUEUE queue);
 extern bool recvDestroyDroid        (NETQUEUE queue);