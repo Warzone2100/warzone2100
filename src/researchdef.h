311ca52811e4e951043858574492e9819458a5b1
Per Inge Mathisen <per@wz2100.net>
2012-03-04 13:05:21 +0100

    Wild guess attempt at fixing the savegame bug reported in ticket:3200

--- src/researchdef.h
+++ src/researchdef.h
@@ -59,7 +59,7 @@ struct RESEARCH : public BASE_STATS
 	iIMDShape		*pIMD2;		/* the 2nd IMD for base plates/turrets*/
 	int index;		///< Unique index for this research, set incrementally
 
-	RESEARCH() : pViewData(NULL), iconID(0), psStat(NULL), pIMD(NULL), pIMD2(NULL) {}
+	RESEARCH() : pViewData(NULL), iconID(0), psStat(NULL), pIMD(NULL), pIMD2(NULL) { pName = NULL; }
 };
 
 struct PLAYER_RESEARCH
