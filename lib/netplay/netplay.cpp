f2ec5b4bde2c71e18a211badd1615f91e2070f2a
cybersphinx <chr.ohm@gmx.net>
2012-03-13 00:06:13 +0100

    Update version numbers for 3.1_beta5.

--- lib/netplay/netplay.cpp
+++ lib/netplay/netplay.cpp
@@ -165,7 +165,7 @@ unsigned NET_PlayerConnectionStatus[CONNECTIONSTATUS_NORMAL][MAX_PLAYERS];
 **/
 static char const *versionString = version_getVersionString();
 static int NETCODE_VERSION_MAJOR = 6;
-static int NETCODE_VERSION_MINOR = 5;
+static int NETCODE_VERSION_MINOR = 20120313;
 
 bool NETisCorrectVersion(uint32_t game_version_major, uint32_t game_version_minor)
 {
