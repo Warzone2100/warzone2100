Warzone2100/warzone2100

commit 91614562b0969c47ffed7020caa1e16c8ede5823

Move radar handling from processInput() to processRadarInput().
Fixes #3283.

src/display.cpp
@@ -298,7 +298,7 @@ bool dispInitialise(void)
 }
 
 
-void ProcessRadarInput(void)
+bool ProcessRadarInput()
 {
 	int PosX, PosY;
 	int x = mouseX();
@@ -380,8 +380,21 @@ void ProcessRadarInput(void)
 					requestRadarTrack(PosX*TILE_UNITS,PosY*TILE_UNITS);
 				}
 			}
+			// ctrl-alt-scroll changes game speed
+			if (!keyDown(KEY_LCTRL) || !keyDown(KEY_LALT))
+			{
+				if (mousePressed(MOUSE_WUP))
+				{
+					kf_RadarZoomIn();
+				}
+				else if (mousePressed(MOUSE_WDN))
+				{
+					kf_RadarZoomOut();
+				}
+			}
 		}
 	}
+	return mouseOverRadar;
 }
 
 
@@ -397,17 +410,11 @@ void resetInput(void)
 /* Process the user input. This just processes the key input and jumping around the radar*/
 void processInput(void)
 {
-	bool mOverRadar = false;
 	bool mOverConstruction = false;
 
 	if (InGameOpUp || isInGamePopupUp)
 	{
 		dragBox3D.status = DRAG_RELEASED;	// disengage the dragging since it stops menu input
-	}
-
-	if (radarOnScreen && radarPermitted && CoordInRadar(mouseX(), mouseY()))
-	{
-		mOverRadar = true;
 	}
 
 	if(CoordInBuild(mouseX(), mouseY()))
@@ -432,11 +439,6 @@ void processInput(void)
 		{
 			kf_SpeedUp();
 		}
-		/* Decide if radar or world zoom in */
-		else if (mOverRadar)
-		{
-			kf_RadarZoomIn();
-		}
 		else if (mOverConstruction)
 		{
 			kf_BuildPrevPage();
@@ -453,11 +455,6 @@ void processInput(void)
 		if (keyDown(KEY_LCTRL) && keyDown(KEY_LALT))
 		{
 			kf_SlowDown();
-		}
-		/* Decide if radar or world zoom out */
-		else if (mOverRadar)
-		{
-			kf_RadarZoomOut();
 		}
 		else if (mOverConstruction)
 		{

src/display.h
@@ -33,7 +33,7 @@ extern bool dispInitialise(void);
 extern void shakeStart(void);
 extern void shakeStop(void);
 
-extern void ProcessRadarInput(void);
+bool ProcessRadarInput();
 
 extern void processInput(void);
 /*don't want to do any of these whilst in the Intelligence Screen*/

src/loop.cpp
@@ -305,16 +305,20 @@ static GAMECODE renderLoop()
 	{
 		if (!gameUpdatePaused())
 		{
+			bool done = false;
 			if (dragBox3D.status != DRAG_DRAGGING
 			    && wallDrag.status != DRAG_DRAGGING
 			    && intRetVal != INT_INTERCEPT)
 			{
-				ProcessRadarInput();
+				done = ProcessRadarInput();
 			}
-			processInput();
+			if (!done)
+			{
+				processInput();
+			}
 
 			//no key clicks or in Intelligence Screen
-			if (intRetVal == INT_NONE && !InGameOpUp && !isInGamePopupUp)
+			if (!done && intRetVal == INT_NONE && !InGameOpUp && !isInGamePopupUp)
 			{
 				processMouseClickInput();
 			}

commit 23a2d96eb29e90ccca95185018c12e554ceedef9

Make escape work again when the mouse is over the menu.
Broken in 91614562b0969c47ffed7020caa1e16c8ede5823.

src/display.cpp
@@ -165,6 +165,11 @@ static bool bLasSatStruct;
 static MOUSE_TARGET	itemUnderMouse(BASE_OBJECT **ppObjUnderCursor);
 static bool	bShakingPermitted = true;
 
+bool isMouseOverRadar()
+{
+	return mouseOverRadar;
+}
+
 void setMouseScroll(bool scroll)
 {
 	mouseScroll = scroll;
@@ -298,7 +303,7 @@ bool dispInitialise(void)
 }
 
 
-bool ProcessRadarInput()
+void ProcessRadarInput()
 {
 	int PosX, PosY;
 	int x = mouseX();
@@ -394,7 +399,6 @@ bool ProcessRadarInput()
 			}
 		}
 	}
-	return mouseOverRadar;
 }
 
 
@@ -432,7 +436,7 @@ void processInput(void)
 	ignoreRMBC = false;
 
 	/* Process all of our key mappings */
-	if (mousePressed(MOUSE_WUP))
+	if (mousePressed(MOUSE_WUP) && !isMouseOverRadar())
 	{
 		/* Ctrl+Alt+WheelUp makes game speed up */
 		if (keyDown(KEY_LCTRL) && keyDown(KEY_LALT))
@@ -449,7 +453,7 @@ void processInput(void)
 		}
 	}
 
-	if (mousePressed(MOUSE_WDN))
+	if (mousePressed(MOUSE_WDN) && !isMouseOverRadar())
 	{
 		/* Ctrl+Alt+WheelDown makes game slow down */
 		if (keyDown(KEY_LCTRL) && keyDown(KEY_LALT))

src/display.h
@@ -33,7 +33,7 @@ extern bool dispInitialise(void);
 extern void shakeStart(void);
 extern void shakeStop(void);
 
-bool ProcessRadarInput();
+void ProcessRadarInput();
 
 extern void processInput(void);
 /*don't want to do any of these whilst in the Intelligence Screen*/
@@ -51,6 +51,8 @@ extern void clearSel(void);
 extern void clearSelection(void);
 // deal with selecting a droid
 extern void dealWithDroidSelect(DROID *psDroid, bool bDragBox);
+
+bool isMouseOverRadar();
 
 extern	void	setInvertMouseStatus( bool val );
 extern bool	getInvertMouseStatus( void );

src/loop.cpp
@@ -305,20 +305,16 @@ static GAMECODE renderLoop()
 	{
 		if (!gameUpdatePaused())
 		{
-			bool done = false;
 			if (dragBox3D.status != DRAG_DRAGGING
 			    && wallDrag.status != DRAG_DRAGGING
 			    && intRetVal != INT_INTERCEPT)
 			{
-				done = ProcessRadarInput();
+				ProcessRadarInput();
 			}
-			if (!done)
-			{
-				processInput();
-			}
+			processInput();
 
 			//no key clicks or in Intelligence Screen
-			if (!done && intRetVal == INT_NONE && !InGameOpUp && !isInGamePopupUp)
+			if (!isMouseOverRadar() && intRetVal == INT_NONE && !InGameOpUp && !isInGamePopupUp)
 			{
 				processMouseClickInput();
 			}