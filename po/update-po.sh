b63f7a0b09d20037878a39da7d72237fb02db8bc
cybersphinx <chr.ohm@gmx.net>
2012-03-19 00:48:44 +0100

    Update files to translate.

--- po/update-po.sh
+++ po/update-po.sh
@@ -11,4 +11,5 @@ EOF
 find lib src data po -type f |
 	grep -e '\.c\(pp\|xx\)\?$' -e 'data.*strings.*\.txt$' -e 'data.*sequenceaudio.*\.tx.$' -e '\.slo$' -e '\.rmsg$' -e 'po/custom/.*\.txt' -e '\.js$' |
 	grep -v -e '\.lex\.c\(pp\|xx\)\?$' -e '\.tab\.c\(pp\|xx\)\?$' -e 'lib/netplay/miniupnpc/*' -e 'lib/betawidget/*' -e '_moc\.' -e 'po/custom/files.js' |
+	grep -v -e '_lexer\.cpp' -e '_parser\.cpp' |
 	sort >> po/POTFILES.in
