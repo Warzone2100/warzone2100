0007657291832d003639eb3f408d069881ed3b39
dak180 <dak180@users.sourceforge.net>
2012-06-19 19:01:40 -0400

    Update the mac doc bundle.

--- macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
+++ macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 OutDir="WarzoneHelp"
-DirectorY="${OutDir}-00f437c"
+DirectorY="${OutDir}-b66426c"
 FileName="${DirectorY}.tgz"
 BuiltDLP="http://downloads.sourceforge.net/project/warzone2100/build-tools/mac/${FileName}"
-MD5Sum="ec7759f4702cf93f54ed9eb69406421d"
+MD5Sum="e0f3b5d15efa4a063461c1ffb21769b8"
 
 configs/FetchPrebuilt.sh "${DirectorY}" "${OutDir}" "${FileName}" "${BuiltDLP}" "${MD5Sum}"
 exit ${?}
