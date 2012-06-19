Warzone2100/warzone2100

commit 15c04ab10f87de89ca7dcae2813834bd1a03247b

Handle different maps with identical names, instead of a desynch.
Fixes ticket:3531 and ticket:3180.

src/game.cpp
@@ -336,7 +336,8 @@ static bool serializeMultiplayerGame(PHYSFS_file* fileHandle, const MULTIPLAYERG
 	 || !PHYSFS_writeUBE32(fileHandle, serializeMulti->power)
 	 || !PHYSFS_writeUBE8(fileHandle, serializeMulti->base)
 	 || !PHYSFS_writeUBE8(fileHandle, serializeMulti->alliance)
-	 || !PHYSFS_writeUBE8(fileHandle, 0)
+	 || !PHYSFS_writeUBE8(fileHandle, serializeMulti->hash.Bytes)
+	 || !PHYSFS_write(fileHandle, serializeMulti->hash.bytes, serializeMulti->hash.Bytes, 1)
 	 || !PHYSFS_writeUBE16(fileHandle, 0)	// dummy, was bytesPerSec
 	 || !PHYSFS_writeUBE8(fileHandle, 0)	// dummy, was packetsPerSec
 	 || !PHYSFS_writeUBE8(fileHandle, challengeActive))	// reuse available field, was encryptKey
@@ -360,6 +361,9 @@ static bool deserializeMultiplayerGame(PHYSFS_file* fileHandle, MULTIPLAYERGAME*
 	uint8_t dummy8;
 	uint16_t dummy16;
 	char dummy8c[8];
+	uint8_t hashSize;
+
+	serializeMulti->hash.setZero();
 
 	if (!PHYSFS_readUBE8(fileHandle, &serializeMulti->type)
 	 || PHYSFS_read(fileHandle, serializeMulti->map, 1, 128) != 128
@@ -370,7 +374,8 @@ static bool deserializeMultiplayerGame(PHYSFS_file* fileHandle, MULTIPLAYERGAME*
 	 || !PHYSFS_readUBE32(fileHandle, &serializeMulti->power)
 	 || !PHYSFS_readUBE8(fileHandle, &serializeMulti->base)
 	 || !PHYSFS_readUBE8(fileHandle, &serializeMulti->alliance)
-	 || !PHYSFS_readUBE8(fileHandle, &dummy8)
+	 || !PHYSFS_readUBE8(fileHandle, &hashSize)
+	 || (hashSize == serializeMulti->hash.Bytes && !PHYSFS_read(fileHandle, serializeMulti->hash.bytes, serializeMulti->hash.Bytes, 1))
 	 || !PHYSFS_readUBE16(fileHandle, &dummy16)	// dummy, was bytesPerSec
 	 || !PHYSFS_readUBE8(fileHandle, &dummy8)	// dummy, was packetsPerSec
 	 || !PHYSFS_readUBE8(fileHandle, &dummy8))	// reused for challenge, was encryptKey
@@ -3191,7 +3196,7 @@ bool gameLoadV7(PHYSFS_file* fileHandle)
 		//copy the level name across
 		sstrcpy(aLevelName, saveGame.levelName);
 		//load up the level dataset
-		if (!levLoadData(aLevelName, saveGameName, (GAME_TYPE)gameType))
+		if (!levLoadData(aLevelName, NULL, saveGameName, (GAME_TYPE)gameType))
 		{
 			return false;
 		}
@@ -3629,7 +3634,7 @@ bool gameLoadV(PHYSFS_file* fileHandle, unsigned int version)
 		//copy the level name across
 		sstrcpy(aLevelName, saveGameData.levelName);
 		//load up the level dataset
-		if (!levLoadData(aLevelName, saveGameName, (GAME_TYPE)gameType))
+		if (!levLoadData(saveGameData.sGame.map /*aLevelName*/, &saveGameData.sGame.hash, saveGameName, (GAME_TYPE)gameType))
 		{
 			return false;
 		}
@@ -6385,7 +6390,7 @@ bool plotStructurePreview16(char *backDropSprite, Vector2i playeridpos[])
 	PIELIGHT color = WZCOL_BLACK ;
 	bool HQ = false;
 
-	psLevel = levFindDataSet(game.map);
+	psLevel = levFindDataSet(game.map, &game.hash);
 	strcpy(aFileName, psLevel->apDataFiles[0]);
 	aFileName[strlen(aFileName) - 4] = '\0';
 	strcat(aFileName, "/struct.bjo");
@@ -6587,7 +6592,7 @@ static void plotFeature(char *backDropSprite)
 	const PIELIGHT colourOil = WZCOL_MAP_PREVIEW_OIL;
 	const PIELIGHT colourBarrel = WZCOL_MAP_PREVIEW_BARREL;
 
-	psLevel = levFindDataSet(game.map);
+	psLevel = levFindDataSet(game.map, &game.hash);
 	strcpy(aFileName, psLevel->apDataFiles[0]);
 	aFileName[strlen(aFileName) - 4] = '\0';
 	strcat(aFileName, "/feat.bjo");

lib/framework/crc.cpp
@@ -124,7 +124,7 @@ static void sha256SumBlock(uint32_t *w, uint32_t *h)
 	h[7] += j;
 }
 
-std::vector<uint8_t> sha256Sum(void const *data, size_t dataLen)
+Sha256 sha256Sum(void const *data, size_t dataLen)
 {
 	uint32_t h[8];
 	std::copy(sha256TableH, sha256TableH + 8, h);
@@ -158,7 +158,35 @@ std::vector<uint8_t> sha256Sum(void const *data, size_t dataLen)
 	{
 		h[i] = htonl(h[i]);
 	}
-	std::vector<uint8_t> ret(32);
-	memcpy(&ret[0], h, 32);
+	Sha256 ret;
+	memcpy(ret.bytes, h, 32);
 	return ret;
+}
+
+bool Sha256::operator ==(Sha256 const &b) const
+{
+	return memcmp(bytes, b.bytes, Bytes) == 0;
+}
+
+bool Sha256::isZero() const
+{
+	return bytes[0] == 0x00 && memcmp(bytes, bytes + 1, Bytes - 1) == 0;
+}
+
+void Sha256::setZero()
+{
+	memset(bytes, 0x00, Bytes);
+}
+
+std::string Sha256::toString() const
+{
+	std::string str;
+	str.resize(Bytes*2);
+	char const *hexDigits = "0123456789abcdef";
+	for (unsigned n = 0; n < Bytes; ++n)
+	{
+		str[n*2    ] = hexDigits[bytes[n] >> 4];
+		str[n*2 + 1] = hexDigits[bytes[n] & 15];
+	}
+	return str;
 }

lib/framework/crc.h
@@ -23,12 +23,24 @@
 #include "types.h"
 #include "vector.h"
 #include <vector>
+#include <string>
 
 
 uint32_t crcSum(uint32_t crc, const void *data, size_t dataLen);
 uint32_t crcSumU16(uint32_t crc, const uint16_t *data, size_t dataLen);
 uint32_t crcSumVector2i(uint32_t crc, const Vector2i *data, size_t dataLen);
 
-std::vector<uint8_t> sha256Sum(void const *data, size_t dataLen);
+struct Sha256
+{
+	static const int Bytes = 32;
+
+	bool operator ==(Sha256 const &b) const;
+	bool isZero() const;
+	void setZero();
+	std::string toString() const;
+
+	uint8_t bytes[Bytes];
+};
+Sha256 sha256Sum(void const *data, size_t dataLen);
 
 #endif //_CRC_H_

lib/framework/file.h
@@ -21,6 +21,9 @@
 
 #include <physfs.h>
 
+#include "crc.h"
+
+
 /*! Open a file for reading */
 extern PHYSFS_file* openLoadFile(const char* fileName, bool hard_fail);
 
@@ -41,5 +44,7 @@ extern bool loadFileToBuffer(const char *pFileName, char *pFileBuffer, UDWORD bu
 /** Load a file from disk, but returns quietly if no file found. */
 extern bool loadFileToBufferNoError(const char *pFileName, char *pFileBuffer, UDWORD bufferSize,
                              UDWORD *pSize);
+
+Sha256 findHashOfFile(char const *realFileName);
 
 #endif // _file_h

lib/framework/frame.cpp
@@ -183,17 +183,22 @@ PHYSFS_file* openLoadFile(const char* fileName, bool hard_fail)
 static bool loadFile2(const char *pFileName, char **ppFileData, UDWORD *pFileSize,
                       bool AllocateMem, bool hard_fail)
 {
-	PHYSFS_file *pfile;
-	PHYSFS_sint64 filesize;
-	PHYSFS_sint64 length_read;
+	if (PHYSFS_isDirectory(pFileName))
+	{
+		return false;
+	}
 
-	pfile = openLoadFile(pFileName, hard_fail);
+	PHYSFS_file *pfile = openLoadFile(pFileName, hard_fail);
 	if (!pfile)
 	{
 		return false;
 	}
 
-	filesize = PHYSFS_fileLength(pfile);
+	PHYSFS_sint64 filesize = PHYSFS_fileLength(pfile);
+	if (filesize < 0)
+	{
+		return false;  // File size could not be determined. Is a directory?
+	}
 
 	//debug(LOG_WZ, "loadFile2: %s opened, size %i", pFileName, filesize);
 
@@ -220,7 +225,7 @@ static bool loadFile2(const char *pFileName, char **ppFileData, UDWORD *pFileSiz
 	}
 
 	/* Load the file data */
-	length_read = PHYSFS_read(pfile, *ppFileData, 1, filesize);
+	PHYSFS_sint64 length_read = PHYSFS_read(pfile, *ppFileData, 1, filesize);
 	if (length_read != filesize)
 	{
 		if (AllocateMem)
@@ -334,6 +339,16 @@ bool loadFileToBufferNoError(const char *pFileName, char *pFileBuffer, UDWORD bu
 {
 	*pSize = bufferSize;
 	return loadFile2(pFileName, &pFileBuffer, pSize, false, false);
+}
+
+Sha256 findHashOfFile(char const *realFileName)
+{
+	char *realFileData = NULL;
+	uint32_t realFileSize = 0;
+	loadFile(realFileName, &realFileData, &realFileSize);
+	Sha256 realFileHash = sha256Sum(realFileData, realFileSize);
+	free(realFileData);
+	return realFileHash;
 }
 
 

lib/netplay/netplay.cpp
@@ -52,6 +52,7 @@
 #include "src/multiplay.h"
 #include "src/warzoneconfig.h"
 #include "src/version.h"
+#include "src/loadsave.h"
 
 #ifdef WZ_OS_LINUX
 #include <execinfo.h>  // Nonfatal runtime backtraces.
@@ -1838,7 +1839,7 @@ bool NETrecvGame(NETQUEUE *queue, uint8_t *type)
 *         NET_BUFFER_SIZE is at 16k.  (also remember text chat, plus all the other cruff)
 */
 #define MAX_FILE_TRANSFER_PACKET 2048
-UBYTE NETsendFile(char *fileName, UDWORD player)
+int NETsendFile(char *fileName, Sha256 const &fileHash, UDWORD player)
 {
 	uint32_t        bytesToRead = 0;
 	uint8_t		sendto = 0;
@@ -1862,7 +1863,11 @@ UBYTE NETsendFile(char *fileName, UDWORD player)
 		NETint32_t(&NetPlay.players[player].wzFile.fileSize_32);		// total bytes in this file. (we don't support 64bit yet)
 		NETuint32_t(&bytesToRead);                                                // bytes in this packet
 		NETint32_t(&NetPlay.players[player].wzFile.currPos);			// start byte
-		NETstring(fileName, 256);										//256 = max filename size
+		if (NetPlay.players[player].wzFile.currPos == 0)
+		{
+			NETstring(fileName, 256);  //256 = max filename size
+			NETbin(const_cast<uint8_t *>(fileHash.bytes), fileHash.Bytes);  // const_cast ok since we're encoding, not decoding.
+		}
 		NETbin(inBuff, bytesToRead);
 	NETend();
 
@@ -1883,11 +1888,9 @@ UBYTE NETrecvFile(NETQUEUE queue)
 {
 	uint32_t        bytesToRead = 0;
 	int32_t		fileSize = 0, currPos = 0;
-	char		fileName[256];
 	uint8_t         outBuff[MAX_FILE_TRANSFER_PACKET];
 	static bool isLoop = false;
 
-	memset(fileName, 0x0, sizeof(fileName));
 	memset(outBuff, 0x0, sizeof(outBuff));
 
 	//read incoming bytes.
@@ -1895,11 +1898,27 @@ UBYTE NETrecvFile(NETQUEUE queue)
 	NETint32_t(&fileSize);		// total bytes in this file.
 	NETuint32_t(&bytesToRead);      // bytes in this packet
 	NETint32_t(&currPos);		// start byte
-	NETstring(fileName, 256);	// read filename (only valid on 1st packet)
-	debug(LOG_NET, "Creating new file %s, position is %d", fileName, currPos);
 
 	if (currPos == 0)	// first packet!
 	{
+		char mapName[256];
+		Sha256 fileHash;
+		memset(mapName, 0x0, sizeof(mapName));
+		fileHash.setZero();
+		// Read filename and hash (only valid on 1st packet)
+		NETstring(mapName, 256);
+		NETbin(fileHash.bytes, fileHash.Bytes);
+
+		removeWildcards(mapName);
+		char fileName[256];
+		if (strlen(mapName) >= 3 && mapName[strlen(mapName) - 3] == '-' && mapName[strlen(mapName) - 2] == 'T' && unsigned(mapName[strlen(mapName) - 1] - '1') < 3)
+		{
+			mapName[strlen(mapName) - 3] = '\0';  // Cut off "-T1", "-T2" or "-T3".
+		}
+		snprintf(fileName, sizeof(fileName), "maps/%dc-%s-%s.wz", game.maxPlayers, mapName, fileHash.toString().c_str());  // Wonder whether game.maxPlayers is initialised already?
+
+		debug(LOG_NET, "Creating new file %s hash %s", fileName, fileHash.toString().c_str());
+
 		if (PHYSFS_exists(fileName))
 		{
 			PHYSFS_file *fin;
@@ -1907,6 +1926,8 @@ UBYTE NETrecvFile(NETQUEUE queue)
 			fin = PHYSFS_openRead(fileName);
 			if (!fin)
 			{
+				NETend();
+
 				// the file exists, but we can't open it, and I have no clue how to fix this...
 				debug(LOG_FATAL, "PHYSFS_openRead(\"%s\") failed with error: %s\n", fileName, PHYSFS_getLastError());
 
@@ -1925,7 +1946,8 @@ UBYTE NETrecvFile(NETQUEUE queue)
 			{
 				fsize = PHYSFS_fileLength(fin);
 			}
-			if ((int32_t) fsize == fileSize)
+			Sha256 ourHash = findHashOfFile(fileName);
+			if (ourHash == fileHash)
 			{
 				uint32_t reason = ALREADY_HAVE_FILE;
 				debug(LOG_NET, "We already have the file %s! ", fileName);
@@ -1954,21 +1976,23 @@ UBYTE NETrecvFile(NETQUEUE queue)
 					PHYSFS_close(NetPlay.pMapFileHandle);
 					NetPlay.pMapFileHandle = NULL;
 					debug(LOG_FATAL, "Something is really wrong with the file's (%s) data, game can't detect it?", fileName);
-					return 100;
 				}
+				return 100;
 			}
 			PHYSFS_close(fin);
 
-			debug(LOG_NET, "We already have the file %s, but different size %d vs %d.  Redownloading", fileName, (int32_t) fsize, fileSize);
+			debug(LOG_NET, "We already have the file %s, but wrong hash, ours %s vs theirs %s.  Redownloading", fileName, ourHash.toString().c_str(), fileHash.toString().c_str());
 
 		}
 		NetPlay.pMapFileHandle = PHYSFS_openWrite(fileName);	// create a new file.
 	}
 
+	debug(LOG_NET, "New file position is %d", currPos);
+
 	if (!NetPlay.pMapFileHandle) // file can't be opened
 	{
 		debug(LOG_FATAL, "Fatal error while creating file: %s", PHYSFS_getLastError());
-		debug(LOG_FATAL, "Either we do not have write permission, or the host sent us a invalid file (%s)!", fileName);
+		debug(LOG_FATAL, "Either we do not have write permission, or the host sent us a invalid file!");
 		abort();
 	}
 

lib/netplay/netplay.h
@@ -26,6 +26,7 @@
 #ifndef _netplay_h
 #define _netplay_h
 
+#include "lib/framework/crc.h"
 #include "nettypes.h"
 #include <physfs.h>
 
@@ -291,7 +292,7 @@ extern bool NETrecvNet(NETQUEUE *queue, uint8_t *type);                  ///< re
 extern bool NETrecvGame(NETQUEUE *queue, uint8_t *type);                 ///< recv a message from the game queues which is sceduled to execute by time, if possible.
 void NETflush(void);                                                     ///< Flushes any data stuck in compression buffers.
 
-extern UBYTE   NETsendFile(char *fileName, UDWORD player);	// send file chunk.
+int NETsendFile(char *mapName, Sha256 const &fileHash, UDWORD player);  // send file chunk.
 extern UBYTE   NETrecvFile(NETQUEUE queue);                     // recv file chunk
 
 extern int NETclose(void);					// close current game

src/configuration.cpp
@@ -98,12 +98,14 @@ bool loadConfig()
 	if (ini.contains("mapName") && ini.contains("maxPlayers"))
 	{
 		sstrcpy(game.map, ini.value("mapName").toString().toUtf8().constData());
+		game.hash.setZero();
 		game.maxPlayers = ini.value("maxPlayers").toInt();	// FIXME: horrible kluge, MUST match map above
 	}
 	else
 	{
 		// Set a default map to prevent hosting games without a map.
 		sstrcpy(game.map, "Sk-Rush");
+		game.hash.setZero();
 		game.maxPlayers = 4;
 	}
 	game.power = ini.value("power", LEV_MED).toInt();
@@ -283,6 +285,7 @@ bool reloadMPConfig(void)
 	if (ini.contains("mapName") && ini.contains("maxPlayers"))
 	{
 		sstrcpy(game.map, ini.value("mapName").toString().toUtf8().constData());
+		game.hash.setZero();
 		game.maxPlayers = ini.value("maxPlayers").toInt();	// FIXME: horrible kluge, MUST match map above
 	}
 	game.power = ini.value("power", LEV_MED).toInt();

commit 1461b3a5a81b72381bc3cc3dbc57bbd2f5ce22a7

Fix loading of campaign savegames.
Apparently, the game.map variable is set to "Sk-Rush" in campaign. And the actual map name is saved somewhere else.

Broken in 15c04ab10f87de89ca7dcae2813834bd1a03247b.

src/game.cpp
@@ -3634,7 +3634,9 @@ bool gameLoadV(PHYSFS_file* fileHandle, unsigned int version)
 		//copy the level name across
 		sstrcpy(aLevelName, saveGameData.levelName);
 		//load up the level dataset
-		if (!levLoadData(saveGameData.sGame.map /*aLevelName*/, &saveGameData.sGame.hash, saveGameName, (GAME_TYPE)gameType))
+		//if (!levLoadData(saveGameData.sGame.map /*aLevelName*/, &saveGameData.sGame.hash, saveGameName, (GAME_TYPE)gameType))
+		// Not sure what aLevelName is, in relation to game.map. But need to use aLevelName here, to be able to start the right map for campaign, and need game.hash, to start the right non-campaign map, if there are multiple identically named maps.
+		if (!levLoadData(aLevelName, &saveGameData.sGame.hash, saveGameName, (GAME_TYPE)gameType))
 		{
 			return false;
 		}