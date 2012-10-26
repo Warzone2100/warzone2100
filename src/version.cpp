83c71993920c196bcdcc1591b985964514c18da7
dak180 <dak180@users.sourceforge.net>
2012-10-26 12:03:21 -0400

    Use the latest version of autorevision.
    From https://github.com/Autorevision/autorevision

--- src/version.cpp
+++ src/version.cpp
@@ -27,18 +27,8 @@
 
 #include "src/autorevision.h"
 
-extern const char *BACKEND;
-
-// Two-step process to put quotes around anything, including preprocessor definitions.
-#define EXPAND(token) #token
-#define QUOTE(token) EXPAND(token)
-
-#define VCS_SHORT_HASH_QUOTED QUOTE(VCS_SHORT_HASH)
-#define VCS_URI_QUOTED QUOTE(VCS_URI)
-#define VCS_DATE_QUOTED QUOTE(VCS_DATE)
-
-static const char vcs_date_cstr[] = QUOTE(VCS_DATE);
-static const char vcs_uri_cstr[] = QUOTE(VCS_URI);
+static const char vcs_date_cstr[] = VCS_DATE;
+static const char vcs_branch_cstr[] = VCS_BRANCH;
 
 unsigned int version_getRevision()
 {
@@ -51,29 +41,29 @@ const char* version_getVersionString()
 
 	if (version_string == NULL)
 	{
-		if (strncmp(vcs_uri_cstr, "tags/", strlen("tags/")) == 0)
+		if (strncmp(vcs_branch_cstr, "tags/", strlen("tags/")) == 0)
 		{
-			version_string = vcs_uri_cstr + strlen("tags/");
+			version_string = vcs_branch_cstr + strlen("tags/");
 		}
-		else if (strcmp(vcs_uri_cstr, "trunk") == 0)
+		else if (strcmp(vcs_branch_cstr, "trunk") == 0)
 		{
-			version_string = "TRUNK " VCS_SHORT_HASH_QUOTED;
+			version_string = "TRUNK " VCS_SHORT_HASH;
 		}
-		else if (strncmp(vcs_uri_cstr, "branches/", strlen("branches/")) == 0)
+		else if (strncmp(vcs_branch_cstr, "branches/", strlen("branches/")) == 0)
 		{
-			version_string = (VCS_URI_QUOTED " branch " VCS_SHORT_HASH_QUOTED) + strlen("branches/");
+			version_string = (VCS_BRANCH " branch " VCS_SHORT_HASH) + strlen("branches/");
 		}
-		else if (strncmp(vcs_uri_cstr, "refs/heads/", strlen("refs/heads/")) == 0)
+		else if (strncmp(vcs_branch_cstr, "refs/heads/", strlen("refs/heads/")) == 0)
 		{
-			version_string = (VCS_URI_QUOTED " branch " VCS_SHORT_HASH_QUOTED) + strlen("refs/heads/");
+			version_string = (VCS_BRANCH " branch " VCS_SHORT_HASH) + strlen("refs/heads/");
 		}
 		else if (VCS_NUM != 0)
 		{
-			version_string = VCS_URI_QUOTED " " VCS_SHORT_HASH_QUOTED;
+			version_string = VCS_BRANCH " " VCS_SHORT_HASH;
 		}
 		else
 		{
-			version_string = VCS_SHORT_HASH_QUOTED;
+			version_string = VCS_SHORT_HASH;
 		}
 	}
 
@@ -116,7 +106,7 @@ const char* version_getVcsTime()
 #if (VCS_NUM == 0)
 	return "";
 #else
-	return VCS_DATE_QUOTED + sizeof(VCS_DATE_QUOTED) - 8 - 1;
+	return VCS_DATE + sizeof(VCS_DATE) - 8 - 1;
 #endif
 }
 
@@ -142,7 +132,7 @@ const char* version_getFormattedVersionString()
 
 		const char* build_date = NULL;
 
-		if (strncmp(vcs_uri_cstr, "tags/", strlen("tags/")) != 0)
+		if (strncmp(vcs_branch_cstr, "tags/", strlen("tags/")) != 0)
 		{
 			sasprintf((char**)&build_date, _(" - Built %s"), version_getBuildDate());
 		}
@@ -154,7 +144,7 @@ const char* version_getFormattedVersionString()
 		// Construct the version string
 		// TRANSLATORS: This string looks as follows when expanded.
 		// "Version <version name/number> <working copy state><BUILD DATE><BUILD TYPE>"
-		snprintf(versionString, MAX_STR_LENGTH, _("Version %s-%s%s%s%s"), BACKEND, version_getVersionString(), wc_state, build_date, build_type);
+		snprintf(versionString, MAX_STR_LENGTH, _("Version %s%s%s%s"), version_getVersionString(), wc_state, build_date, build_type);
 	}
 
 	return versionString;
