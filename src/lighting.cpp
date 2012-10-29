85ecd9a3937e7843d9eb4798572651f950bd7555
Subsentient <thinkingrodent@gmail.com>
2012-10-28 23:47:27 -0700

    Add fix for backwards terrain shadows by Berg

--- src/lighting.cpp
+++ src/lighting.cpp
@@ -216,8 +216,8 @@ static void calcTileIllum(UDWORD tileX, UDWORD tileY)
 	{
 		finalVector = finalVector + normals[i];
 	}
-
-	dotProduct = normalise(finalVector) * theSun;
+	Vector3f sunVector(theSun.x, theSun.y, -theSun.z);
+	dotProduct = normalise(finalVector) * sunVector;
 
 	val = abs(dotProduct) / 16;
 	if (val == 0) val = 1;
