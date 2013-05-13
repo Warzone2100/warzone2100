3c21376ac97550751d80dfc190b18c24051b82a3
dak180 <dak180@users.sourceforge.net>
2013-05-13 16:11:33 -0400

    Update zlib to 1.2.8 for the mac builds.

--- macosx/configs/fetchscripts/Zlib-FetchSource.sh
+++ macosx/configs/fetchscripts/Zlib-FetchSource.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+VerLib="1.2.8"
 OutDir="zlib"
-DirectorY="${OutDir}-1.2.7"
+DirectorY="${OutDir}-${VerLib}"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://zlib.net/${FileName}"
-MD5Sum="60df6a37c56e7c1366cca812414f7b85"
+MD5Sum="44d667c142d7cda120332623eab69f40"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
