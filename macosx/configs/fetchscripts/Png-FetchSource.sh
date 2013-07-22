874df70e1f6a41df65abeeb402099b98bf740b27
dak180 <dak180@users.sourceforge.net>
2013-07-22 14:18:55 -0400

    Update png to 1.5.17.

--- macosx/configs/fetchscripts/Png-FetchSource.sh
+++ macosx/configs/fetchscripts/Png-FetchSource.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VerLib="1.5.16"
+VerLib="1.5.17"
 OutDir="libpng"
 DirectorY="${OutDir}-${VerLib}"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://downloads.sourceforge.net/project/libpng/libpng15/${VerLib}/${FileName}"
-MD5Sum="836cc4673e282cb089018e70c3c4a7fa"
+MD5Sum="d2e27dbd8c6579d1582b3f128fd284b4"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
