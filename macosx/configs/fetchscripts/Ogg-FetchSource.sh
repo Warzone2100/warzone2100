10652103259ace901aa4958887de5dff5d5a1d92
dak180 <dak180@users.sourceforge.net>
2013-07-25 15:03:40 -0400

    Update ogg to 1.3.1 for the mac build.

--- macosx/configs/fetchscripts/Ogg-FetchSource.sh
+++ macosx/configs/fetchscripts/Ogg-FetchSource.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 OutDir="libogg"
-DirectorY="${OutDir}-1.3.0"
+DirectorY="${OutDir}-1.3.1"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://downloads.xiph.org/releases/ogg/${FileName}"
-MD5Sum="0a7eb40b86ac050db3a789ab65fe21c2"
+MD5Sum="ba526cd8f4403a5d351a9efaa8608fbc"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${MD5Sum}"
 exit ${?}
