Warzone2100/warzone2100

commit de42fd35476edbff7af7acdcfb2de254129443d2

Fix saving of effects

lib/ivis_opengl/imdload.cpp
@@ -110,6 +110,7 @@ void modelShutdown()
 	{
 		iV_IMDRelease(i.value());
 	}
+	models.clear();
 }
 
 static bool tryLoad(const QString &path, const QString &filename)
@@ -132,6 +133,20 @@ static bool tryLoad(const QString &path, const QString &filename)
 		return true;
 	}
 	return false;
+}
+
+const QString &modelName(iIMDShape *model)
+{
+	for (MODELMAP::iterator i = models.begin(); i != models.end(); i = models.erase(i))
+	{
+		if (i.value() == model)
+		{
+			return i.key();
+		}
+	}
+	ASSERT(false, "An IMD pointer could not be backtraced to a filename!");
+	static QString error;
+	return error;
 }
 
 iIMDShape *modelGet(const QString &filename)

lib/ivis_opengl/imd.h
@@ -47,6 +47,11 @@
 //*************************************************************************
 
 void modelShutdown();
+
+/// Get filename of model pointer. This is really slow, so do not abuse for logging
+/// purposes, for example.
+const QString &modelName(iIMDShape *model);
+
 iIMDShape *modelGet(const QString &filename);
 
 #endif

src/effects.cpp
@@ -2537,8 +2537,8 @@ bool writeFXData(const char *fileName)
 		ini.setValue("lifeSpan", it->lifeSpan);
 		ini.setValue("radius", it->radius);
 
-		const char *imd_name = resGetNamefromData("IMD", it->imd);
-		if (imd_name && *imd_name)
+		const QString &imd_name = modelName(it->imd);
+		if (!imd_name.isEmpty())
 		{
 			ini.setValue("imd_name", imd_name);
 		}

commit 491c1c3f4a0bd448f12c208689639b6c33fe8fe5

Fix copy-paste bug introduced in de42fd35476edbff7af7acdcfb2de254129443d2 whereby saved effects would be erased. Closes ticket:4099. Closes ticket:4103.

lib/ivis_opengl/imdload.cpp
@@ -137,7 +137,7 @@ static bool tryLoad(const QString &path, const QString &filename)
 
 const QString &modelName(iIMDShape *model)
 {
-	for (MODELMAP::iterator i = models.begin(); i != models.end(); i = models.erase(i))
+	for (MODELMAP::iterator i = models.begin(); i != models.end(); ++i)
 	{
 		if (i.value() == model)
 		{