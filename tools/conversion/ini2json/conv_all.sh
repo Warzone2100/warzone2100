3af2ff4211d867e425ee78424ec747af4d2c898a
per <per@wz2100.net>
2014-01-19 15:14:31 +0100

    Add -t option to python interpreter to warn about mixing tabs and spaces. Closes ticket:4172 for now.

--- tools/conversion/ini2json/conv_all.sh
+++ tools/conversion/ini2json/conv_all.sh
@@ -12,8 +12,8 @@ BASE="../../../data"
 
 function dict
 {
-	python ini2dict.py ${BASE}/base/stats/$1.ini > id_dict.json
-	python ini2json_generic.py ${BASE}/mp/stats/$1.ini  > ${BASE}/mp/stats/$1.json
+	python -t ini2dict.py ${BASE}/base/stats/$1.ini > id_dict.json
+	python -t ini2json_generic.py ${BASE}/mp/stats/$1.ini  > ${BASE}/mp/stats/$1.json
 }
 rm -f id_dict_*.json
 python ini2dict.py ${BASE}/base/stats/ecm.ini ${BASE}/base/stats/sensor.ini ${BASE}/base/stats/weapons.ini ${BASE}/base/stats/repair.ini \
@@ -29,8 +29,8 @@ python ini2dict.py ${BASE}/mp/stats/ecm.ini ${BASE}/mp/stats/sensor.ini ${BASE}/
 
 function generic
 {
-	python ini2json_generic.py ${BASE}/base/stats/$1.ini id_dict_base.json  > ${BASE}/base/stats/$1.json
-	python ini2json_generic.py ${BASE}/mp/stats/$1.ini id_dict_mp.json  > ${BASE}/mp/stats/$1.json
+	python -t ini2json_generic.py ${BASE}/base/stats/$1.ini id_dict_base.json  > ${BASE}/base/stats/$1.json
+	python -t ini2json_generic.py ${BASE}/mp/stats/$1.ini id_dict_mp.json  > ${BASE}/mp/stats/$1.json
 }
 generic ecm
 generic sensor
@@ -40,9 +40,9 @@ generic construction
 generic propulsion
 generic structure
 generic templates
-python ini2json_body.py ${BASE}/base/stats/body.ini ${BASE}/base/stats/bodypropulsionimd.ini id_dict_base.json > ${BASE}/base/stats/body.json
-python ini2json_body.py ${BASE}/mp/stats/body.ini ${BASE}/mp/stats/bodypropulsionimd.ini id_dict_base.json > ${BASE}/mp/stats/body.json
-python ini2json_research.py ${BASE}/base/stats/research_cam1.ini id_dict_base.json > ${BASE}/base/stats/research_cam1.json
-python ini2json_research.py ${BASE}/base/stats/research_cam2.ini id_dict_base.json > ${BASE}/base/stats/research_cam2.json
-python ini2json_research.py ${BASE}/base/stats/research_cam3.ini id_dict_base.json > ${BASE}/base/stats/research_cam3.json
-python ini2json_research.py ${BASE}/mp/stats/research.ini id_dict_mp.json > ${BASE}/mp/stats/research.json
+python -t ini2json_body.py ${BASE}/base/stats/body.ini ${BASE}/base/stats/bodypropulsionimd.ini id_dict_base.json > ${BASE}/base/stats/body.json
+python -t ini2json_body.py ${BASE}/mp/stats/body.ini ${BASE}/mp/stats/bodypropulsionimd.ini id_dict_base.json > ${BASE}/mp/stats/body.json
+python -t ini2json_research.py ${BASE}/base/stats/research_cam1.ini id_dict_base.json > ${BASE}/base/stats/research_cam1.json
+python -t ini2json_research.py ${BASE}/base/stats/research_cam2.ini id_dict_base.json > ${BASE}/base/stats/research_cam2.json
+python -t ini2json_research.py ${BASE}/base/stats/research_cam3.ini id_dict_base.json > ${BASE}/base/stats/research_cam3.json
+python -t ini2json_research.py ${BASE}/mp/stats/research.ini id_dict_mp.json > ${BASE}/mp/stats/research.json
