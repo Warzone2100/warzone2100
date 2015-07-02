288f3dc1e94a3e8be0e110ef31e943e360e78137
gamemanj <gamemanj@hotmail.co.uk>
2015-07-02 10:06:28 +0100

    Revert "Removed restrictions on team setup."
    This reverts commit 9920fcbb59b88ccb6cf02b8f0675228a0836ac70.
    It was probably a stupid idea...

--- src/multiint.cpp
+++ src/multiint.cpp
@@ -2270,6 +2270,7 @@ int allPlayersOnSameTeam(int except)
 static void addTeamChooser(UDWORD player)
 {
 	UDWORD i;
+	int disallow = allPlayersOnSameTeam(player);
 
 	debug(LOG_NET, "Opened team chooser for %d, current team: %d", player, NetPlay.players[player].team);
 
@@ -2284,9 +2285,15 @@ static void addTeamChooser(UDWORD player)
 	int spaceDiv = game.maxPlayers;
 	space = std::min(space, 3 * spaceDiv);
 
-	// add the teams. Used to block "all on 1 team" configurations, but they're useful for learning.
+	// add the teams, skipping the one we CAN'T be on (if applicable)
 	for (i = 0; i < game.maxPlayers; i++)
-		addMultiBut(psWScreen, MULTIOP_TEAMCHOOSER_FORM, MULTIOP_TEAMCHOOSER + i, i * (teamW * spaceDiv + space) / spaceDiv + 3, 6, teamW, teamH, _("Team"), IMAGE_TEAM0 + i , IMAGE_TEAM0_HI + i, IMAGE_TEAM0_HI + i);
+	{
+		if (i != disallow)
+		{
+			addMultiBut(psWScreen, MULTIOP_TEAMCHOOSER_FORM, MULTIOP_TEAMCHOOSER + i, i * (teamW * spaceDiv + space) / spaceDiv + 3, 6, teamW, teamH, _("Team"), IMAGE_TEAM0 + i , IMAGE_TEAM0_HI + i, IMAGE_TEAM0_HI + i);
+		}
+		// may want to add some kind of 'can't do' icon instead of being blank?
+	}
 
 	// add a kick button
 	if (player != selectedPlayer && NetPlay.bComms && NetPlay.isHost && NetPlay.players[player].allocated)
@@ -2311,6 +2318,8 @@ static void closeTeamChooser(void)
 
 static void drawReadyButton(UDWORD player)
 {
+	int disallow = allPlayersOnSameTeam(-1);
+
 	// delete 'ready' botton form
 	widgDelete(psWScreen, MULTIOP_READY_FORM_ID + player);
 
@@ -2333,6 +2342,11 @@ static void drawReadyButton(UDWORD player)
 	else if (!NetPlay.players[player].allocated)
 	{
 		return;	// closed or open
+	}
+
+	if (disallow != -1)
+	{
+		return;
 	}
 
 	bool isMe = player == selectedPlayer;
@@ -2394,6 +2408,7 @@ void addPlayerBox(bool players)
 	if (players)
 	{
 		int  team = -1;
+		bool allOnSameTeam = true;
 
 		for (int i = 0; i < game.maxPlayers; i++)
 		{
@@ -2406,6 +2421,7 @@ void addPlayerBox(bool players)
 				}
 				else if (myTeam != team)
 				{
+					allOnSameTeam = false;
 					break;  // We just need to know if we have enough to start a game
 				}
 			}
@@ -2488,7 +2504,10 @@ void addPlayerBox(bool players)
 
 			if (ingame.localOptionsReceived)
 			{
-				drawReadyButton(i);
+				if (!allOnSameTeam)
+				{
+					drawReadyButton(i);
+				}
 
 				// draw player info box
 				W_BUTINIT sButInit;
