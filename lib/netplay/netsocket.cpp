a85dd20a67d1b745e3d42e2c132d9ff81b4dbfe1
vexed <vexed@wz2100.net>
2015-07-14 22:01:48 -0400

    Fix coverity bug #1108992 RESOURCE_LEAK

--- lib/netplay/netsocket.cpp
+++ lib/netplay/netsocket.cpp
@@ -1076,6 +1076,7 @@ Socket *socketAccept(Socket *sock)
 			{
 				debug(LOG_NET, "Couldn't set socket (%p) blocking status (false).  Closing.", conn);
 				socketClose(conn);
+				free(conn);
 				return NULL;
 			}
 
