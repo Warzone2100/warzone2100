906f6f3fa8b9006918684a6d716e7b37fab5b8d2
vexed <vexed@wz2100.net>
2015-07-15 00:13:36 -0400

    Fix coverity bug #1108912 (FORWARD_NULL)

--- src/order.cpp
+++ src/order.cpp
@@ -1594,6 +1594,7 @@ void orderDroidBase(DROID *psDroid, DROID_ORDER_DATA *psOrder)
 			psDroid->order = DroidOrder(DORDER_NONE);
 			break;
 		}
+		ASSERT_OR_RETURN(, psOrder->psObj != NULL, "Can't command a NULL");
 		cmdDroidAddDroid((DROID *)psOrder->psObj, psDroid);
 		break;
 	case DORDER_RETREAT:
