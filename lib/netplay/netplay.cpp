Warzone2100/warzone2100

commit 9300d7e3c6f13b11d004b059b2d219e27eed6a93

teams: Improve default teams.

lib/netplay/netplay.cpp
@@ -287,12 +287,24 @@ static size_t NET_fillBuffer(Socket **pSocket, SocketSet *socket_set, uint8_t *b
 	return 0;
 }
 
+static int playersPerTeam()
+{
+	for (int v = game.maxPlayers - 1; v > 1; --v)
+	{
+		if (game.maxPlayers%v == 0)
+		{
+			return v;
+		}
+	}
+	return 1;
+}
+
 void NET_InitPlayer(int i, bool initPosition)
 {
 	NetPlay.players[i].allocated = false;
 	NetPlay.players[i].autoGame = false;
 	NetPlay.players[i].heartattacktime = 0;
-	NetPlay.players[i].heartbeat = true;		// we always start with a hearbeat
+	NetPlay.players[i].heartbeat = true;  // we always start with a heartbeat
 	NetPlay.players[i].kick = false;
 	if (ingame.localJoiningInProgress)
 	{
@@ -304,7 +316,7 @@ void NET_InitPlayer(int i, bool initPosition)
 		NetPlay.players[i].colour = i;
 		setPlayerColour(i, i);  // PlayerColour[] in component.c must match this! Why is this in more than one place??!
 		NetPlay.players[i].position = i;
-		NetPlay.players[i].team = i;
+		NetPlay.players[i].team = i/playersPerTeam();
 	}
 	NetPlay.players[i].ready = false;
 	NetPlay.players[i].needFile = false;

lib/ivis_opengl/png_util.cpp
@@ -190,7 +190,7 @@ bool iV_loadImage_PNG(const char *fileName, iV_Image *image)
 
 static void internal_saveImage_PNG(const char *fileName, const iV_Image *image, int color_type)
 {
-	unsigned char **scanlines = NULL;
+	unsigned char **volatile scanlines = NULL;  // Must be volatile to reliably preserve value if modified between setjmp/longjmp.
 	png_infop info_ptr = NULL;
 	png_structp png_ptr = NULL;
 	PHYSFS_file *fileHandle;

src/multiint.cpp
@@ -2143,8 +2143,8 @@ static bool changePosition(UBYTE player, UBYTE position)
 		{
 			debug(LOG_NET, "Swapping positions between players %d(%d) and %d(%d)",
 			      player, NetPlay.players[player].position, i, NetPlay.players[i].position);
-			NetPlay.players[i].position = NetPlay.players[player].position;
-			NetPlay.players[player].position = position;
+			std::swap(NetPlay.players[i].position, NetPlay.players[player].position);
+			std::swap(NetPlay.players[i].team, NetPlay.players[player].team);
 			NETBroadcastTwoPlayerInfo(player, i);
 			netPlayersUpdated = true;
 			return true;

commit 255009d7c18e8013e5f39b471e20a96bf7c9a9b4

teams: Fix extra alliances on loading games.
Broken in 9300d7e3c6f13b11d004b059b2d219e27eed6a93. Fixes ticket:4334.

lib/netplay/netplay.cpp
@@ -299,7 +299,7 @@ static int playersPerTeam()
 	return 1;
 }
 
-void NET_InitPlayer(int i, bool initPosition)
+void NET_InitPlayer(int i, bool initPosition, bool initTeams)
 {
 	NetPlay.players[i].allocated = false;
 	NetPlay.players[i].autoGame = false;
@@ -316,7 +316,7 @@ void NET_InitPlayer(int i, bool initPosition)
 		NetPlay.players[i].colour = i;
 		setPlayerColour(i, i);  // PlayerColour[] in component.c must match this! Why is this in more than one place??!
 		NetPlay.players[i].position = i;
-		NetPlay.players[i].team = i/playersPerTeam();
+		NetPlay.players[i].team = initTeams? i/playersPerTeam() : i;
 	}
 	NetPlay.players[i].ready = false;
 	NetPlay.players[i].needFile = false;
@@ -334,13 +334,13 @@ void NET_InitPlayer(int i, bool initPosition)
 	ingame.JoiningInProgress[i] = false;
 }
 
-void NET_InitPlayers()
+void NET_InitPlayers(bool initTeams)
 {
 	unsigned int i;
 
 	for (i = 0; i < MAX_CONNECTED_PLAYERS; ++i)
 	{
-		NET_InitPlayer(i, true);
+		NET_InitPlayer(i, true, initTeams);
 		NetPlay.players[i].name[0] = '\0';
 		NETinitQueue(NETnetQueue(i));
 	}
@@ -1115,7 +1115,7 @@ int NETinit(bool bFirstCall)
 {
 	debug(LOG_NET, "NETinit");
 	NETlogEntry("NETinit!", SYNC_FLAG, selectedPlayer);
-	NET_InitPlayers();
+	NET_InitPlayers(true);
 
 	SOCKETinit();
 
@@ -2686,7 +2686,7 @@ bool NEThostGame(const char *SessionName, const char *PlayerName,
 	mapDownloadProgress = 100;
 	netPlayersUpdated = true;
 
-	NET_InitPlayers();
+	NET_InitPlayers(true);
 	for (unsigned n = 0; n < MAX_PLAYERS_IN_GUI; ++n)
 	{
 		changeColour(n, rand() % (n + 1), true); // Put colours in random order.

lib/netplay/netplay.h
@@ -337,8 +337,8 @@ void NETBroadcastTwoPlayerInfo(uint32_t index1, uint32_t index2);
 extern bool NETisCorrectVersion(uint32_t game_version_major, uint32_t game_version_minor);
 extern int NETGetMajorVersion(void);
 extern int NETGetMinorVersion(void);
-void NET_InitPlayer(int i, bool initPosition);
-extern void NET_InitPlayers(void);
+void NET_InitPlayer(int i, bool initPosition, bool initTeams = false);
+extern void NET_InitPlayers(bool initTeams = false);
 
 void NETGameLocked(bool flag);
 void NETresetGamePassword(void);