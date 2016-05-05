297b3e43bf389c89acf7881741c1eaed0139bc41
Cyp <cyp@wz2100.net>
2016-05-05 23:33:09 +0200

    Fix race condition in pathfinding.
    Invalid read of size 8
       at 0x4778FC: isBlocked (astar.cpp:165)
       by 0x4778FC: fpathAStarExplore(PathfindContext&, PathCoord) (astar.cpp:414)
       by 0x478329: fpathAStarRoute(MOVE_CONTROL*, PATHJOB*) (astar.cpp:477)
       by 0x4C25EE: fpathExecute (fpath.cpp:515)
       by 0x4C25EE: fpathThreadFunc(void*) (fpath.cpp:104)
       by 0x6DAA62: WZ_THREAD::run() (wzapp_qt.h:99)
     Address 0x1e8896a8 is 344 bytes inside a block of size 1,064 free'd
       by 0x478910: clear (stl_list.h:1304)
       by 0x478910: fpathSetBlockingMap(PATHJOB*) (astar.cpp:617)
       by 0x4C3538: fpathRoute(MOVE_CONTROL*, int, int, int, int, int, PROPULSION_TYPE, DROID_TYPE, FPATH_MOVETYPE, int, bool, StructureBounds const&) (fpath.cpp:443)
       by 0x4C3C21: fpathDroidRoute(DROID*, int, int, FPATH_MOVETYPE) (fpath.cpp:509)
       by 0x53A4F0: moveDroidToBase(DROID*, unsigned int, unsigned int, bool, FPATH_MOVETYPE) (move.cpp:155)
       by 0x46F854: actionDroidBase(DROID*, DROID_ACTION_DATA*) (action.cpp:2038)
       by 0x46F996: actionDroid(DROID*, DROID_ACTION, BASE_OBJECT*) (action.cpp:2237)
       by 0x476252: aiUpdateDroid(DROID*) (ai.cpp:1200)
       by 0x4B7038: droidUpdate(DROID*) (droid.cpp:865)
       by 0x52365B: gameStateUpdate (loop.cpp:617)
       by 0x52365B: gameLoop() (loop.cpp:692)
       by 0x524CFA: runGameLoop (main.cpp:886)
       by 0x524CFA: mainLoop() (main.cpp:992)
     Block was alloc'd at
       by 0x478DCA: resize (stl_bvector.h:953)
       by 0x478DCA: fpathSetBlockingMap(PATHJOB*) (astar.cpp:638)
       by 0x4C3538: fpathRoute(MOVE_CONTROL*, int, int, int, int, int, PROPULSION_TYPE, DROID_TYPE, FPATH_MOVETYPE, int, bool, StructureBounds const&) (fpath.cpp:443)
       by 0x4C3C21: fpathDroidRoute(DROID*, int, int, FPATH_MOVETYPE) (fpath.cpp:509)
       by 0x53A4F0: moveDroidToBase(DROID*, unsigned int, unsigned int, bool, FPATH_MOVETYPE) (move.cpp:155)
       by 0x46F854: actionDroidBase(DROID*, DROID_ACTION_DATA*) (action.cpp:2038)
       by 0x46F996: actionDroid(DROID*, DROID_ACTION, BASE_OBJECT*) (action.cpp:2237)
       by 0x476252: aiUpdateDroid(DROID*) (ai.cpp:1200)
       by 0x4B7038: droidUpdate(DROID*) (droid.cpp:865)
       by 0x52365B: gameStateUpdate (loop.cpp:617)
       by 0x52365B: gameLoop() (loop.cpp:692)
       by 0x524CFA: runGameLoop (main.cpp:886)
       by 0x524CFA: mainLoop() (main.cpp:992)

--- src/fpath.h
+++ src/fpath.h
@@ -26,6 +26,9 @@
 
 #include "droiddef.h"
 
+#include <memory>
+
+
 /** Return values for routing
  *
  *  @ingroup pathfinding
@@ -51,7 +54,7 @@ struct PATHJOB
 	UDWORD		droidID;
 	FPATH_MOVETYPE	moveType;
 	int		owner;		///< Player owner
-	PathBlockingMap *blockingMap;   ///< Map of blocking tiles.
+	std::shared_ptr<PathBlockingMap> blockingMap;   ///< Map of blocking tiles.
 	bool		acceptNearest;
 	bool            deleted;        ///< Droid was deleted, so throw away result when complete. Must still process this PATHJOB, since processing order can affect resulting paths (but can't affect the path length).
 };
