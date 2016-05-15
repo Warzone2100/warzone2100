Warzone2100/warzone2100

commit 07e33362d4ee5d9c4b1d9f3ccd63ecc88f73d93b

Fix droid bounding boxes at all resolutions.
Not sure why this works, but fixes ticket:4157.

lib/ivis_opengl/piematrix.cpp
@@ -344,7 +344,7 @@ void pie_GetMatrix(float *matrix)
  */
 int32_t pie_RotateProject(const Vector3i *v3d, Vector2i *v2d)
 {
-	float hackScaleFactor = 3 * 330 / 1024.0;  // HACK: This seems to work by experimentation, not sure why.
+	float hackScaleFactor = 1.0 / (3 * 330);  // HACK: This seems to work by experimentation, not sure why.
 
 	/*
 	 * v = curMatrix . v3d
@@ -355,20 +355,20 @@ int32_t pie_RotateProject(const Vector3i *v3d, Vector2i *v2d)
 	    v3d->x * psMatrix->c + v3d->y * psMatrix->f + v3d->z * psMatrix->i + psMatrix->l
 	);
 
-	const int zz = v.z >> STRETCHED_Z_SHIFT;
+	const float zz = v.z * hackScaleFactor;
 
-	if (zz < MIN_STRETCHED_Z)
+	if (zz < MIN_STRETCHED_Z * hackScaleFactor)
 	{
 		v2d->x = LONG_WAY; //just along way off screen
 		v2d->y = LONG_WAY;
 	}
 	else
 	{
-		v2d->x = rendSurface.xcentre + (hackScaleFactor / zz * v.x);
-		v2d->y = rendSurface.ycentre - (hackScaleFactor / zz * v.y);
+		v2d->x = rendSurface.xcentre + v.x / zz;
+		v2d->y = rendSurface.ycentre - v.y / zz;
 	}
 
-	return zz;
+	return zz / (1 << STRETCHED_Z_SHIFT);
 }
 
 void pie_PerspectiveBegin(void)

src/display3d.cpp
@@ -3363,16 +3363,13 @@ void calcScreenCoords(DROID *psDroid)
 	origin = Vector3i(0, wsRadius, 0); // take the center of the object
 
 	/* get the screen coordinates */
-	// FP12_SHIFT-STRETCHED_Z_SHIFT is the shift of the scaling on the depth returned
-	const float cZ = pie_RotateProject(&origin, &center) / (float)(1 << (FP12_SHIFT - STRETCHED_Z_SHIFT));
+	float magic = float(1 << STRETCHED_Z_SHIFT) / 40;  // The 40 comes from experimentation, it makes the boxes have approximately the right size.
+	const float cZ = pie_RotateProject(&origin, &center) * magic;
 
 	//Watermelon:added a crash protection hack...
 	if (cZ >= 0)
 	{
-		// 330 is the near plane depth from pie_PerspectiveBegin
-		// not sure where magic comes from, could be another 1<<FP12_SHIFT-STRETCHED_Z_SHIFT
-		const int magic = 4;
-		radius = (wsRadius * 330 * magic) / cZ;
+		radius = wsRadius / cZ * pie_GetResScalingFactor();
 	}
 	else
 	{

commit ced3a11c8ba2861e4e938e1057c2b5aeb5f7b0b5

Fix invisible super cyborgs.
Broken in 07e33362d4ee5d9c4b1d9f3ccd63ecc88f73d93b.

lib/ivis_opengl/piematrix.cpp
@@ -368,7 +368,7 @@ int32_t pie_RotateProject(const Vector3i *v3d, Vector2i *v2d)
 		v2d->y = rendSurface.ycentre - v.y / zz;
 	}
 
-	return zz / (1 << STRETCHED_Z_SHIFT);
+	return zz;
 }
 
 void pie_PerspectiveBegin(void)

src/display3d.cpp
@@ -3363,7 +3363,7 @@ void calcScreenCoords(DROID *psDroid)
 	origin = Vector3i(0, wsRadius, 0); // take the center of the object
 
 	/* get the screen coordinates */
-	float magic = float(1 << STRETCHED_Z_SHIFT) / 40;  // The 40 comes from experimentation, it makes the boxes have approximately the right size.
+	float magic = 0.025f;  // The 0.025f comes from experimentation, it makes the boxes have approximately the right size.
 	const float cZ = pie_RotateProject(&origin, &center) * magic;
 
 	//Watermelon:added a crash protection hack...