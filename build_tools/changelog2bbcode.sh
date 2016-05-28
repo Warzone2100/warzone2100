1ef241f08a5ea0ca1745480b4769d4f90cb00539
Per Inge Mathisen <per.mathisen@gmail.com>
2016-05-29 00:21:42 +0200

    Update the changelog tool

--- build_tools/changelog2bbcode.sh
+++ build_tools/changelog2bbcode.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-sed -e 's/r\([0-9]\+\)/[rev]\1[\/rev]/g' \
-    -e 's/ticket:\([0-9]\+\)/[ticket]\1[\/ticket]/g'\
-    -e 's/commit:\(\([a-f0-9]\{10\}\)[a-f0-9]\{30\}\)/[url=http:\/\/developer.wz2100.net\/changeset\/\1]\2[\/url]/g' \
-    -e 's/\ \ \*/\ */g' \
-    -e 's/^\ \*\ \(.*\):/\n[\/list][b]\1[\/b][list]/g' \
-    -e 's/^\ \ \*/\ [*]/'
+sed -e 's/  \*/ [\*]/g' \
+    -e 's/ \* \(.*\):/[\/list][b]\1[\/b][list]/g' \
+    -e 's/commit\:\([a-f0-9]\{10\}\)[a-f0-9]*/\[githash\]\1\[\/githash\]/g' \
+    -e 's/ticket\:\([a-f0-9]*\)/\[ticket\]\1\[\/ticket\]/g'
