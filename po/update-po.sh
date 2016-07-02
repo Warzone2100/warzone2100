6b6833ba35435abf1fbee53caf84f94e1cd80a55
cybersphinx <chr.ohm@gmx.net>
2016-07-02 21:42:31 +0200

    Remove some strings that should not be translated from fromJson.txt.

--- po/update-po.sh
+++ po/update-po.sh
@@ -6,6 +6,7 @@ cd "`dirname "$0"`/.."
 find data -name '*.json' -type f '-!' -path 'data/mp/multiplay/maps/*' -exec \
 	grep '"name": ".*"' {} + |
 	sed -r 's/.*"name": "([^"]*)".*/_("\1")/' |
+	grep -v -e "\*" -e "NULL" -e "CAM" |
 	sort | uniq > po/custom/fromJson.txt
 
 # Add the comment to the top of the file
