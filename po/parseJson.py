2ee0737aae96d21f494f7a245ea2013d9b326ba0
Per Inge Mathisen <per.mathisen@gmail.com>
2016-08-31 23:09:38 +0200

    parseJson.py: Make it compatible with python 2.7

--- po/parseJson.py
+++ po/parseJson.py
@@ -2,7 +2,7 @@ import json, sys, re
 
 def printString(s, begin, end):
 	if not re.match(r'^(\*.*\*|CAM[0-9] .*|Z ?NULL.*)$', s):
-		print('{}_({}){}'.format(begin, json.dumps(s, ensure_ascii=False), end), end='')
+		sys.stdout.write('{}_({}){}'.format(begin, json.dumps(s, ensure_ascii=False), end))
 
 def parse(obj):
 	if isinstance(obj, dict):
@@ -11,7 +11,7 @@ def parse(obj):
 			if k == 'name' and isinstance(v, str):
 				printString(v, '', '\n')
 			elif k == 'text' and isinstance(v, list):
-				print('ZZ', end='')
+				sys.stdout.write('ZZ')
 				for s in v:
 					if isinstance(s, str):
 						printString(s, ' ', '')
@@ -20,5 +20,4 @@ def parse(obj):
 		for v in obj:
 			parse(v)
 
-#parse(json.load(sys.stdin))
 parse(json.load(open(sys.argv[1], 'r')))
