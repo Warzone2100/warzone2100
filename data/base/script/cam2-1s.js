5def11978ef1b2f02215effb6344df10f476b405
CristianAlpha93 <cristian.odorico@gmail.com>
2016-12-27 18:57:15 +0100

    Fixed sub_2_1s JScam file

---
+++ data/base/script/cam2-1s.js
@@ -0,0 +1,28 @@
+/* 
+SUB_2_1S campaign script
+Author: Cristian Odorico / Alpha93
+ */
+include("script/campaign/libcampaign.js");
+
+function eventLevelStart()
+{
+    //Variables initialization
+    var startpos = getObject("startingPosition");
+    var lz = getObject("landingZone");
+    var takeOff = getObject("transporterTakeOff");
+    var exitPoint = getObject("transporterExit");
+    
+    //Setup Transport
+    camSetupTransporter(takeOff.x, takeOff.y, exitPoint.x, exitPoint.y);
+    //Centre View on Area
+    centreView(startpos.x, startpos.y);
+    //Setup Landing Zone
+    setNoGoArea(lz.x, lz.y, lz.x2, lz.y2);
+    //Set Mission Time
+    setMissionTime(1800);
+    //Give player briefings
+    hackAddMessage("MB2_1MSG", MISS_MSG, 0, true);
+    hackAddMessage("MB2_1MSG2", MISS_MSG, 0, false);
+    //Set Victory Conditions
+    camSetStandardWinLossConditions(CAM_VICTORY_PRE_OFFWORLD, "SUB_2_1");   
+}
