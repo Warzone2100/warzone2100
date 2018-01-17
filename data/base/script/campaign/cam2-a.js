51ff947b4473b80b8a381e42ecf6c1c75af471f2
KJeff01 <kjeff1997@gmail.com>
2018-01-17 10:35:09 -0600

    Make cam2-a mission give less alloys to the AI.
    The Collective get one less kinetic alloy research for the first Beta mission.

--- data/base/script/campaign/cam2-a.js
+++ data/base/script/campaign/cam2-a.js
@@ -188,7 +188,7 @@ function cam2Setup()
 		"R-Wpn-MG1Mk1", "R-Sys-Engineering02",
 		"R-Defense-WallUpgrade03", "R-Struc-Materials03",
 		"R-Struc-Factory-Upgrade03", "R-Struc-Factory-Cyborg-Upgrade03",
-		"R-Vehicle-Engine03", "R-Vehicle-Metals04", "R-Cyborg-Metals04",
+		"R-Vehicle-Engine03", "R-Vehicle-Metals03", "R-Cyborg-Metals03",
 		"R-Wpn-Cannon-Accuracy01", "R-Wpn-Cannon-Damage03",
 		"R-Wpn-Cannon-ROF01", "R-Wpn-Flamer-Damage03", "R-Wpn-Flamer-ROF01",
 		"R-Wpn-MG-Damage04", "R-Wpn-MG-ROF02", "R-Wpn-Mortar-Acc01",
