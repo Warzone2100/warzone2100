9e16dee06aa20a9039b5218dfc92c4d5f8898eb8
past-due <30942300+past-due@users.noreply.github.com>
2018-02-14 23:36:55 -0500

    [macOS] Generate a proper build # & version # for the app bundle
    Uses the new values provided by `autorevision` to construct a properly-formatted build # and version # for the Warzone.app bundle.
    
    The build # automatically increments alongside the # of commits in the `master` branch of the Git repo (until branched), with a third component provided by the # of commits since branched from `master` (if on a branch).
    
    The version # uses the most recent "version-like" tag in the Git repo. Hence, simply tagging a new version will yield future builds that adopt the new version #. ("beta", "rc", and other similar suffixes are ignored, since Info.plist version #s must have a specific format.)

--- macosx/configs/autorevision.sh
+++ macosx/configs/autorevision.sh
@@ -28,15 +28,79 @@ fi
 . "${cauto}"
 
 
-if [[ ! "${VCS_TICK}" = "0" ]]; then
-	# If we are not exactly on a tag make the branch look better and use the value for the tag too.
+# Process the values for Xcode
+
+extractVersionNumberFromGitTag() {
+	local version_tag="$1"
+
+	# Remove "v/" or "v" prefix (as in "v3.2.2"), if present
+	version_tag="$(echo "${version_tag}" | sed -e 's:^v/::' -e 's:^v::')"
+
+	# Remove _beta* or _rc* suffix, if present
+	version_tag="$(echo "${version_tag}" | sed -e 's:_beta.*$::' -e 's:_rc.*$::')"
+
+	# Extract up to a 3-component version # from the beginning of the tag (i.e. 3.2.2)
+	version_tag="$(echo "${version_tag}" | grep -Eo '^\d+(.\d+){0,2}')"
+
+	if [ -n "${version_tag}" ]; then
+		echo "${version_tag}"
+		return 0
+	else
+		return 1 # failed to extract version #
+	fi
+}
+
+# Make the branch look better.
+if [ -n "${VCS_BRANCH}" ]; then
 	N_VCS_BRANCH="$(echo "${VCS_BRANCH}" | sed -e 's:remotes/:remote/:' -e 's:master:Master:')"
-	sed -e "s:\"${VCS_BRANCH}\":\"${N_VCS_BRANCH}\":" -e "s:\"${VCS_TAG}\":\"${N_VCS_BRANCH}\":" "${cauto}" > "${tauto}"
-else
+	sed -e "s:\"${VCS_BRANCH}\":\"${N_VCS_BRANCH}\":" "${cauto}" > "${tauto}"
+fi
+
+if [ -n "${VCS_TAG}" ]; then
 	# When exactly on a tag make the value suitable for users.
-	N_VCS_TAG="$(echo "${VCS_TAG}" | sed -e 's:^v::' -e 's:^v/::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :')"
+	N_VCS_TAG="$(echo "${VCS_TAG}" | sed -e 's:^v/::' -e 's:^v::' | sed -e 's:_beta: Beta :' -e 's:_rc: RC :')"
 	sed -e "s:\"${VCS_TAG}\":\"${N_VCS_TAG}\":" "${cauto}" > "${tauto}"
 fi
+
+# Extract a version number from the most recent version-like tag (to be used as the CFBundleShortVersionString)
+MAC_VERSION_NUMBER="$(extractVersionNumberFromGitTag "${VCS_MOST_RECENT_TAGGED_VERSION}")"
+if [ -z "${MAC_VERSION_NUMBER}" ]; then
+	# Tag does not start with identifiable version # components
+	echo "warning: The VCS_MOST_RECENT_TAGGED_VERSION tag does not seem to include a version #; defaulting to 0.0.0"
+	MAC_VERSION_NUMBER="0.0.0"
+fi
+
+## To increment the 3rd component of the Version# if there are commits since the most recent tagged version,
+## uncomment the code below
+#if [ "$VCS_COMMIT_COUNT_SINCE_MOST_RECENT_TAGGED_VERSION" -gt "0" ]; then
+#	# Since there have been commits since the most recent tag, increment the MAC_VERSION_NUMBER
+#
+#	VERSION_A=$(echo "${MAC_VERSION_NUMBER}" | awk -F. '{print $1}')
+#	VERSION_B=$(echo "${MAC_VERSION_NUMBER}" | awk -F. '{print $2}')
+#	VERSION_C=$(echo "${MAC_VERSION_NUMBER}" | awk -F. '{print $3}')
+#
+#	if [ -n "${VERSION_C}" ]; then
+#		VERSION_C=$(($VERSION_C + 1))
+#	elif [ -n "${VERSION_B}" ]; then
+#		VERSION_C="1"
+#	elif [ -n "${VERSION_A}" ]; then
+#		VERSION_C="1"
+#		VERSION_B="0"
+#	fi
+#	MAC_VERSION_NUMBER="${VERSION_A}.${VERSION_B}.${VERSION_C}"
+#fi
+echo "info: Using Version # \"${MAC_VERSION_NUMBER}\" (most recent version tag: ${VCS_MOST_RECENT_TAGGED_VERSION})"
+
+# Construct a build number (for the CFBundleVersion)
+# Format: "1." (commits-on-master[-until-branch "." commits-on-branch])
+if [ "${VCS_BRANCH}" = "master" ]; then
+	# "1.{commits-on-master}"
+	MAC_BUILD_NUMBER="1.${VCS_COMMIT_COUNT}"
+else
+	# "1.{commits-on-master-until-branch}.{commits-on-branch}"
+	MAC_BUILD_NUMBER="1.${VCS_COMMIT_COUNT_ON_MASTER_UNTIL_BRANCH}.${VCS_BRANCH_COMMIT_COUNT}"
+fi
+echo "info: Using Build # \"${MAC_BUILD_NUMBER}\""
 
 
 # Output for src/autorevision.h.
@@ -48,5 +112,10 @@ fi
 
 # Output for info.plist prepossessing.
 ./build_tools/autorevision -f -o "${tauto}" -t xcode > "${xauto}"
+
+# Modify the output header for info.plist preprocessing to insert the MAC_BUILD_NUMBER and MAC_VERSION_NUMBER
+infoplist_header_additions="#define MAC_BUILD_NUMBER	${MAC_BUILD_NUMBER}\\
+#define MAC_VERSION_NUMBER	${MAC_VERSION_NUMBER}"
+perl -i.bak -pe 's/## AUTOINSERT MARK/'"${infoplist_header_additions}"'/g' "${xauto}"
 
 exit ${?}
