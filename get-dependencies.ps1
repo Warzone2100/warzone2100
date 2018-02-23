4b08f70e53146c83cccece7f52f7e351c4f24cac
past-due <30942300+past-due@users.noreply.github.com>
2018-02-23 16:35:26 -0500

    [Windows] Add required dependencies to get-dependencies.ps1

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -1,5 +1,5 @@
 git clone https://github.com/Microsoft/vcpkg.git;
 pushd vcpkg;
 .\bootstrap-vcpkg.bat;
-.\vcpkg install physfs harfbuzz libiconv libogg libtheora libvorbis libpng openal-soft sdl2 glew openssl
+.\vcpkg install physfs harfbuzz libiconv libogg libtheora libvorbis libpng openal-soft sdl2 glew freetype gettext zlib
 popd;
