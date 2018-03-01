1476719abc805b1bfa4011a1bd58d3992213a007
past-due <30942300+past-due@users.noreply.github.com>
2018-03-01 15:06:51 -0500

    fixes #4693: replace deprecated function PHYSFS_removeFromSearchPath()
    replace deprecated function PHYSFS_removeFromSearchPath() with function WZ_PHYSFS_unmount() to avoid compiler warnings with physfs >= 2.1
    
    Co-Authored-By: Forgon2100 <forgon2100@users.noreply.github.com>

--- src/modding.cpp
+++ src/modding.cpp
@@ -141,7 +141,7 @@ void removeSubdirs(const char *basedir, const char *subdir)
 #ifdef DEBUG
 		debug(LOG_NEVER, "Removing [%s] from search path", tmpstr);
 #endif // DEBUG
-		if (!PHYSFS_removeFromSearchPath(tmpstr))
+		if (!WZ_PHYSFS_unmount(tmpstr))
 		{
 #ifdef DEBUG	// spams a ton!
 			debug(LOG_NEVER, "Couldn't remove %s from search path because %s", tmpstr, WZ_PHYSFS_getLastError());
