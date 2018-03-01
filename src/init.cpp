ea648d31bfab184fc8083adb6837b9de5f78b983
past-due <30942300+past-due@users.noreply.github.com>
2018-03-01 15:06:51 -0500

    fixes #4693: replace deprecated function PHYSFS_removeFromSearchPath()
    replace deprecated function PHYSFS_removeFromSearchPath() with function WZ_PHYSFS_unmount() to avoid compiler warnings with physfs >= 2.1
    
    Co-Authored-By: Forgon2100 <forgon2100@users.noreply.github.com>

--- src/init.cpp
+++ src/init.cpp
@@ -291,26 +291,26 @@ bool rebuildSearchPath(searchPathMode mode, bool force, const char *current_map)
 				// Remove multiplay patches
 				sstrcpy(tmpstr, curSearchPath->path);
 				sstrcat(tmpstr, "mp");
-				PHYSFS_removeFromSearchPath(tmpstr);
+				WZ_PHYSFS_unmount(tmpstr);
 				sstrcpy(tmpstr, curSearchPath->path);
 				sstrcat(tmpstr, "mp.wz");
-				PHYSFS_removeFromSearchPath(tmpstr);
+				WZ_PHYSFS_unmount(tmpstr);
 
 				// Remove plain dir
-				PHYSFS_removeFromSearchPath(curSearchPath->path);
+				WZ_PHYSFS_unmount(curSearchPath->path);
 
 				// Remove base files
 				sstrcpy(tmpstr, curSearchPath->path);
 				sstrcat(tmpstr, "base");
-				PHYSFS_removeFromSearchPath(tmpstr);
+				WZ_PHYSFS_unmount(tmpstr);
 				sstrcpy(tmpstr, curSearchPath->path);
 				sstrcat(tmpstr, "base.wz");
-				PHYSFS_removeFromSearchPath(tmpstr);
+				WZ_PHYSFS_unmount(tmpstr);
 
 				// remove video search path as well
 				sstrcpy(tmpstr, curSearchPath->path);
 				sstrcat(tmpstr, "sequences.wz");
-				PHYSFS_removeFromSearchPath(tmpstr);
+				WZ_PHYSFS_unmount(tmpstr);
 				curSearchPath = curSearchPath->higherPriority;
 			}
 			break;
@@ -344,7 +344,7 @@ bool rebuildSearchPath(searchPathMode mode, bool force, const char *current_map)
 				addSubdirs(curSearchPath->path, "mods", PHYSFS_APPEND, use_override_mods ? &override_mods : &global_mods, true);
 				addSubdirs(curSearchPath->path, "mods/autoload", PHYSFS_APPEND, use_override_mods ? &override_mods : nullptr, true);
 				addSubdirs(curSearchPath->path, "mods/campaign", PHYSFS_APPEND, use_override_mods ? &override_mods : &campaign_mods, true);
-				if (!PHYSFS_removeFromSearchPath(curSearchPath->path))
+				if (!WZ_PHYSFS_unmount(curSearchPath->path))
 				{
 					debug(LOG_WZ, "* Failed to remove path %s again", curSearchPath->path);
 				}
@@ -411,7 +411,7 @@ bool rebuildSearchPath(searchPathMode mode, bool force, const char *current_map)
 						addSubdirs(curSearchPath->path, "mods/downloads", PHYSFS_APPEND, &hashList, true);
 					}
 				}
-				PHYSFS_removeFromSearchPath(curSearchPath->path);
+				WZ_PHYSFS_unmount(curSearchPath->path);
 
 				// Add multiplay patches
 				sstrcpy(tmpstr, curSearchPath->path);
@@ -450,7 +450,7 @@ bool rebuildSearchPath(searchPathMode mode, bool force, const char *current_map)
 		}
 
 		// User's home dir must be first so we always see what we write
-		PHYSFS_removeFromSearchPath(PHYSFS_getWriteDir());
+		WZ_PHYSFS_unmount(PHYSFS_getWriteDir());
 		PHYSFS_mount(PHYSFS_getWriteDir(), NULL, PHYSFS_PREPEND);
 
 #ifdef DEBUG
@@ -491,7 +491,7 @@ static MapFileList listMapFiles()
 	{
 		debug(LOG_WZ, "    [%s]", *i);
 		oldSearchPath.push_back(*i);
-		PHYSFS_removeFromSearchPath(*i);
+		WZ_PHYSFS_unmount(*i);
 	}
 	PHYSFS_freeList(searchPath);
 
@@ -526,7 +526,7 @@ static MapFileList listMapFiles()
 			{
 				filtered.push_back(realFileName);
 			}
-			PHYSFS_removeFromSearchPath(realFilePathAndName.c_str());
+			WZ_PHYSFS_unmount(realFilePathAndName.c_str());
 		}
 		else
 		{
@@ -616,7 +616,7 @@ static bool CheckInMap(const char *archive, const char *mountpoint, const char *
 	}
 	PHYSFS_freeList(filelist);
 
-	if (!PHYSFS_removeFromSearchPath(archive))
+	if (!WZ_PHYSFS_unmount(archive))
 	{
 		debug(LOG_ERROR, "Could not unmount %s, %s", archive, WZ_PHYSFS_getLastError());
 	}
@@ -657,7 +657,7 @@ bool buildMapList()
 		}
 		PHYSFS_freeList(filelist);
 
-		if (PHYSFS_removeFromSearchPath(realFilePathAndName.c_str()) == 0)
+		if (WZ_PHYSFS_unmount(realFilePathAndName.c_str()) == 0)
 		{
 			debug(LOG_ERROR, "Could not unmount %s, %s", realFilePathAndName.c_str(), WZ_PHYSFS_getLastError());
 		}
