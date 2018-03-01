59880a377c1bf6590a4ef70fa1c31d783e95e39a
past-due <30942300+past-due@users.noreply.github.com>
2018-03-01 16:27:09 -0500

    [macOS] Improve JS doc Markdown generation
    Since other JS doc generation scripts do not use `[[:space:]]` (or an equivalent that matches *any* whitespace character), revert to use of a literal space to better match the output.

--- macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
+++ macosx/configs/fetchscripts/SetupPrebuiltComponents-Docs.sh
@@ -100,14 +100,14 @@ else
 fi
 
 # javascript doc
-	grep src/qtscript.cpp -e '//==' | sed -E 's:.*//==[[:space:]]?::' > doc/js-globals.md
-	grep src/qtscriptfuncs.cpp -e '//==' | sed -E 's:.*//==[[:space:]]?::' >> doc/js-globals.md
-	grep src/qtscript.cpp -e '//__' | sed -E 's:.*//__[[:space:]]?::' > doc/js-events.md
-	grep src/qtscript.cpp -e '//--' | sed -E 's:.*//--[[:space:]]?::' > doc/js-functions.md
-	grep src/qtscriptfuncs.cpp -e '//--' | sed -E 's:.*//--[[:space:]]?::' >> doc/js-functions.md
-	grep src/qtscript.cpp -e '//;;' | sed -E 's:.*//;;[[:space:]]?::' > doc/js-objects.md
-	grep src/qtscriptfuncs.cpp -e '//;;' | sed -E 's:.*//;;[[:space:]]?::' >> doc/js-objects.md
-	grep data/base/script/campaign/libcampaign.js -e '.*//;;' | sed -E 's://;;[[:space:]]?::' > doc/js-campaign.md
+	grep src/qtscript.cpp -e '//==' | sed -E 's:.*//== ?::' > doc/js-globals.md
+	grep src/qtscriptfuncs.cpp -e '//==' | sed -E 's:.*//== ?::' >> doc/js-globals.md
+	grep src/qtscript.cpp -e '//__' | sed -E 's:.*//__ ?::' > doc/js-events.md
+	grep src/qtscript.cpp -e '//--' | sed -E 's:.*//-- ?::' > doc/js-functions.md
+	grep src/qtscriptfuncs.cpp -e '//--' | sed -E 's:.*//-- ?::' >> doc/js-functions.md
+	grep src/qtscript.cpp -e '//;;' | sed -E 's:.*//;; ?::' > doc/js-objects.md
+	grep src/qtscriptfuncs.cpp -e '//;;' | sed -E 's:.*//;; ?::' >> doc/js-objects.md
+	grep data/base/script/campaign/libcampaign.js -e '.*//;;' | sed -E 's://;; ?::' > doc/js-campaign.md
 cp -af doc/Scripting.md ${mWarzoneHelpLproj}
 cp -af doc/js-{globals,events,functions,objects,campaign}*.md ${mWarzoneHelpLproj}
 
