d6137dc6db5f8b75a013d7e0744f8d55cb3b34e7
past-due <30942300+past-due@users.noreply.github.com>
2018-05-22 12:44:03 -0400

    [macOS] Update HarfBuzz to 1.7.6

--- macosx/configs/fetchscripts/harfbuzz-FetchSource.sh
+++ macosx/configs/fetchscripts/harfbuzz-FetchSource.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-VerLib="1.4.8"
+VerLib="1.7.6"
 OutDir="harfbuzz"
 DirectorY="${OutDir}-${VerLib}"
 FileName="${DirectorY}.tar.bz2"
 SourceDLP="https://www.freedesktop.org/software/harfbuzz/release/${FileName}"
-SHA256Sum="ccec4930ff0bb2d0c40aee203075447954b64a8c2695202413cc5e428c907131"
+SHA256Sum="da7bed39134826cd51e57c29f1dfbe342ccedb4f4773b1c951ff05ff383e2e9b"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${SHA256Sum}"
 exit ${?}
