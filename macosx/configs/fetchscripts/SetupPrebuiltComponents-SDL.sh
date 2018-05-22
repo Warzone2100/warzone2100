30f691481f9823e4a054f8081ec30a7b4fc1ca04
past-due <30942300+past-due@users.noreply.github.com>
2018-05-22 11:52:23 -0400

    [macOS] Update SDL to 2.0.8

--- macosx/configs/fetchscripts/SetupPrebuiltComponents-SDL.sh
+++ macosx/configs/fetchscripts/SetupPrebuiltComponents-SDL.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-DirectorY="SDL-2.0.5"
+DirectorY="SDL-2.0.8"
 OutDir="SDL2"
-FileName="SDL2-2.0.5.dmg"
-BuiltDLP="https://www.libsdl.org/release/SDL2-2.0.5.dmg"
-SHA256Sum="09309e5af6739ce91e8e5db443604a0d0d85e0b728652423ba1a00e26363c30c"
+FileName="SDL2-2.0.8.dmg"
+BuiltDLP="https://www.libsdl.org/release/SDL2-2.0.8.dmg"
+SHA256Sum="74dd2cb6b18e35e8181523590115f10f1da774939c21ce27768a2a80ba57ad5f"
 
 configs/FetchPrebuilt.sh "${DirectorY}" "${OutDir}" "${FileName}" "${BuiltDLP}" "${SHA256Sum}" "-dmg"
 exit ${?}
 
-# tar -czf SDL-2.0.5.tgz --exclude '.DS_Store' SDL-2.0.5
+# tar -czf SDL-2.0.8.tgz --exclude '.DS_Store' SDL-2.0.8
