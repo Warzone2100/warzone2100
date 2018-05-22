24687e563ab964cc37877a604258106bcb869934
past-due <30942300+past-due@users.noreply.github.com>
2018-05-22 12:07:36 -0400

    [macOS] Update libogg to 1.3.3

--- macosx/configs/fetchscripts/Ogg-FetchSource.sh
+++ macosx/configs/fetchscripts/Ogg-FetchSource.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 OutDir="libogg"
-DirectorY="${OutDir}-1.3.2"
+DirectorY="${OutDir}-1.3.3"
 FileName="${DirectorY}.tar.gz"
 SourceDLP="http://downloads.xiph.org/releases/ogg/${FileName}"
-SHA256Sum="e19ee34711d7af328cb26287f4137e70630e7261b17cbe3cd41011d73a654692"
+SHA256Sum="c2e8a485110b97550f453226ec644ebac6cb29d1caef2902c007edab4308d985"
 
 configs/FetchSource.sh "${DirectorY}" "${OutDir}" "${FileName}" "${SourceDLP}" "${SHA256Sum}"
 exit ${?}
