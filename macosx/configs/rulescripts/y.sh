b525bb3acdfcc703a2ef7af0aac529abd4df9b11
past-due <30942300+past-due@users.noreply.github.com>
2018-12-09 17:13:35 -0500

    [macOS] Clean up old build scripts / configs / Xcode project

--- macosx/configs/rulescripts/y.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-bison --output-file="${DERIVED_FILES_DIR}/${INPUT_FILE_BASE}.tab.cpp" "${INPUT_FILE_PATH}" || exit 1
-cp "${DERIVED_FILES_DIR}/${INPUT_FILE_BASE}.tab.hpp" "${DERIVED_FILES_DIR}/${INPUT_FILE_BASE}.h"
-exit ${?}
