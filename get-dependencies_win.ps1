ef6f119488da9afde7304bd42dc8ff089ab97e78
past-due <30942300+past-due@users.noreply.github.com>
2019-01-19 16:40:27 -0500

    [Windows] Update vcpkg commit in get-dependencies_win.ps1

--- get-dependencies_win.ps1
+++ get-dependencies_win.ps1
@@ -5,7 +5,7 @@ param([string]$VCPKG_BUILD_TYPE = "")
 ############################
 
 # To ensure reproducible builds, pin to a specific vcpkg commit
-$VCPKG_COMMIT_SHA = "99b06bba5d86a6982f2491d9f4ea7ef637ff3747";
+$VCPKG_COMMIT_SHA = "c6db2c880a064b29e0b38eda29f19510235fb4c9";
 
 # To ensure the proper dump_syms.exe is downloaded, specify the commit + hash
 $DUMP_SYMS_EXE_COMMIT = "aebee55695eeb40d788f5421bf32eaaa7227aba0";
