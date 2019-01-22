b6de4e4ed3c310307da65092a2d86e2b9649ae6d
past-due <30942300+past-due@users.noreply.github.com>
2019-01-22 12:07:21 -0500

    Split `gfx_api_gl.cpp` into .cpp and .h files

--- lib/ivis_opengl/gfx_api_gl.cpp
+++ lib/ivis_opengl/gfx_api_gl.cpp
@@ -18,11 +18,7 @@
 */
 
 #include "lib/framework/frame.h"
-#include "gfx_api.h"
-
-#include <GL/glew.h>
-#include <algorithm>
-#include <cmath>
+#include "gfx_api_gl.h"
 
 static GLenum to_gl(const gfx_api::pixel_format& format)
 {
@@ -72,113 +68,103 @@ static GLenum to_gl(const gfx_api::buffer::usage& usage)
 	return GL_INVALID_ENUM;
  }
 
-struct gl_texture : public gfx_api::texture
+// MARK: gl_texture
+
+gl_texture::gl_texture()
 {
-private:
-	friend struct gl_context;
-	GLuint _id;
+	glGenTextures(1, &_id);
+}
 
-	gl_texture()
-	{
-		glGenTextures(1, &_id);
-	}
-
-	~gl_texture()
-	{
-		glDeleteTextures(1, &_id);
-	}
-public:
-	virtual void bind() override
-	{
-		glBindTexture(GL_TEXTURE_2D, _id);
-	}
-
-	virtual void upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data) override
-	{
-		bind();
-		glTexSubImage2D(GL_TEXTURE_2D, mip_level, offset_x, offset_y, width, height, to_gl(buffer_format), GL_UNSIGNED_BYTE, data);
-	}
-
-	virtual unsigned id() override
-	{
-		return _id;
-	}
-
-	virtual void generate_mip_levels() override
-	{
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-
-};
-
-struct gl_buffer : public gfx_api::buffer
+gl_texture::~gl_texture()
 {
-	gfx_api::buffer::usage usage;
-	gfx_api::context::buffer_storage_hint hint;
-	GLuint buffer = 0;
-	size_t buffer_size = 0;
+	glDeleteTextures(1, &_id);
+}
 
-	virtual ~gl_buffer() override
-	{
-		glDeleteBuffers(1, &buffer);
-	}
-
-	gl_buffer(const gfx_api::buffer::usage& usage, const gfx_api::context::buffer_storage_hint& hint)
-	: usage(usage)
-	, hint(hint)
-	{
-		glGenBuffers(1, &buffer);
-	}
-
-	void bind() override
-	{
-		glBindBuffer(to_gl(usage), buffer);
-	}
-
-	virtual void upload(const size_t & size, const void * data) override
-	{
-		glBindBuffer(to_gl(usage), buffer);
-		glBufferData(to_gl(usage), size, data, to_gl(hint));
-		buffer_size = size;
-	}
-
-	virtual void update(const size_t & start, const size_t & size, const void * data) override
-	{
-		ASSERT(start < buffer_size, "Starting offset (%zu) is past end of buffer (length: %zu)", start, buffer_size);
-		ASSERT(start + size <= buffer_size, "Attempt to write past end of buffer");
-		if (size == 0)
-		{
-			debug(LOG_WARNING, "Attempt to update buffer with 0 bytes of new data");
-			return;
-		}
-		glBindBuffer(to_gl(usage), buffer);
-		glBufferSubData(to_gl(usage), start, size, data);
-	}
-
-};
-
-struct gl_context : public gfx_api::context
+void gl_texture::bind()
 {
-	virtual gfx_api::texture* create_texture(const size_t & width, const size_t & height, const gfx_api::pixel_format & internal_format, const std::string& filename) override
-	{
-		auto* new_texture = new gl_texture();
-		new_texture->bind();
-		if (!filename.empty() && (GLEW_VERSION_4_3 || GLEW_KHR_debug))
-		{
-			glObjectLabel(GL_TEXTURE, new_texture->id(), -1, filename.c_str());
-		}
-		for (unsigned i = 0; i < floor(log(std::max(width, height))) + 1; ++i)
-		{
-			glTexImage2D(GL_TEXTURE_2D, i, to_gl(internal_format), width >> i, height >> i, 0, to_gl(internal_format), GL_UNSIGNED_BYTE, nullptr);
-		}
-		return new_texture;
-	}
+	glBindTexture(GL_TEXTURE_2D, _id);
+}
 
-	virtual gfx_api::buffer * create_buffer_object(const gfx_api::buffer::usage &usage, const buffer_storage_hint& hint = buffer_storage_hint::static_draw) override
+void gl_texture::upload(const size_t& mip_level, const size_t& offset_x, const size_t& offset_y, const size_t & width, const size_t & height, const gfx_api::pixel_format & buffer_format, const void * data)
+{
+	bind();
+	glTexSubImage2D(GL_TEXTURE_2D, mip_level, offset_x, offset_y, width, height, to_gl(buffer_format), GL_UNSIGNED_BYTE, data);
+}
+
+unsigned gl_texture::id()
+{
+	return _id;
+}
+
+void gl_texture::generate_mip_levels()
+{
+	glGenerateMipmap(GL_TEXTURE_2D);
+}
+
+// MARK: gl_buffer
+
+gl_buffer::gl_buffer(const gfx_api::buffer::usage& usage, const gfx_api::context::buffer_storage_hint& hint)
+: usage(usage)
+, hint(hint)
+{
+	glGenBuffers(1, &buffer);
+}
+
+gl_buffer::~gl_buffer()
+{
+	glDeleteBuffers(1, &buffer);
+}
+
+void gl_buffer::bind()
+{
+	glBindBuffer(to_gl(usage), buffer);
+}
+
+void gl_buffer::upload(const size_t & size, const void * data)
+{
+	glBindBuffer(to_gl(usage), buffer);
+	glBufferData(to_gl(usage), size, data, to_gl(hint));
+	buffer_size = size;
+}
+
+void gl_buffer::update(const size_t & start, const size_t & size, const void * data)
+{
+	ASSERT(start < buffer_size, "Starting offset (%zu) is past end of buffer (length: %zu)", start, buffer_size);
+	ASSERT(start + size <= buffer_size, "Attempt to write past end of buffer");
+	if (size == 0)
 	{
-		return new gl_buffer(usage, hint);
+		debug(LOG_WARNING, "Attempt to update buffer with 0 bytes of new data");
+		return;
 	}
-};
+	glBindBuffer(to_gl(usage), buffer);
+	glBufferSubData(to_gl(usage), start, size, data);
+}
+
+// MARK: gl_context
+
+gl_context::~gl_context()
+{
+}
+
+gfx_api::texture* gl_context::create_texture(const size_t & width, const size_t & height, const gfx_api::pixel_format & internal_format, const std::string& filename)
+{
+	auto* new_texture = new gl_texture();
+	new_texture->bind();
+	if (!filename.empty() && (GLEW_VERSION_4_3 || GLEW_KHR_debug))
+	{
+		glObjectLabel(GL_TEXTURE, new_texture->id(), -1, filename.c_str());
+	}
+	for (unsigned i = 0; i < floor(log(std::max(width, height))) + 1; ++i)
+	{
+		glTexImage2D(GL_TEXTURE_2D, i, to_gl(internal_format), width >> i, height >> i, 0, to_gl(internal_format), GL_UNSIGNED_BYTE, nullptr);
+	}
+	return new_texture;
+}
+
+gfx_api::buffer * gl_context::create_buffer_object(const gfx_api::buffer::usage &usage, const buffer_storage_hint& hint /*= buffer_storage_hint::static_draw*/)
+{
+	return new gl_buffer(usage, hint);
+}
 
 gfx_api::context& gfx_api::context::get()
 {
