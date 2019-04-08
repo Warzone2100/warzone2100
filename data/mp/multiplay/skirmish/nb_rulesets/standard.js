051dfeaa6e6b0540b011f4c45808bb1a97977887
KJeff01 <kjeff1997@gmail.com>
2019-04-08 11:26:33 -0500

    Nullbot research improvements.
    - Include path extras since they are otherwise never used.
    - Don't include engine upgrades for cannon extras since that
    makes it focus too much on body, armor, and engine upgrades.

--- data/mp/multiplay/skirmish/nb_rulesets/standard.js
+++ data/mp/multiplay/skirmish/nb_rulesets/standard.js
@@ -300,7 +300,6 @@ const weaponStats = {
 		],
 		extras: [
 			"R-Wpn-Cannon-ROF06",
-			"R-Vehicle-Engine09", // cannons are heavy
 			"R-Wpn-Rail-Damage03", // sure it's required by gauss, but what if our AI uses only cyborgs and vtols?
 			"R-Wpn-Rail-ROF03",
 		],
