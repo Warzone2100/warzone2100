005c720841d2284caba6ab6a187f7a117d8038a8
past-due <30942300+past-due@users.noreply.github.com>
2019-04-28 16:58:22 -0400

    Improve export_build_output_desc.sh

--- build_tools/ci/travis/export_build_output_desc.sh
+++ build_tools/ci/travis/export_build_output_desc.sh
@@ -19,7 +19,7 @@
 # For tags/releases, base on the tag name: <TAG_NAME>-
 if [ -n "${TRAVIS_TAG}" ] && [ -z "${TRAVIS_PULL_REQUEST_BRANCH}" ]; then
 	# Replace "/" so the TRAVIS_TAG can be used in a filename
-	TRAVIS_TAG_SANITIZED="$(echo "${TRAVIS_TAG}" | sed -e 's:/:_:' -e 's:-:_:')"
+	TRAVIS_TAG_SANITIZED="$(echo "${TRAVIS_TAG}" | sed -e 's:/:_:g' -e 's:-:_:g')"
 	export WZ_BUILD_DESC_PREFIX="${TRAVIS_TAG_SANITIZED}"
 else
 	# Collect current working copy Git information
@@ -34,7 +34,7 @@ else
 	fi
 
 	# Replace "/" so the GIT_BRANCH can be used in a filename
-	GIT_BRANCH_SANITIZED="$(echo "${GIT_BRANCH}" | sed -e 's:/:_:' -e 's:-:_:')"
+	GIT_BRANCH_SANITIZED="$(echo "${GIT_BRANCH}" | sed -e 's:/:_:g' -e 's:-:_:g')"
 	export WZ_BUILD_DESC_PREFIX="${GIT_BRANCH_SANITIZED}"
 fi
 
