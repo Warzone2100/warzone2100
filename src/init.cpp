96087b568b96907c851984d8a4141bcbcd7374b1
KJeff01 <kjeff1997@gmail.com>
2019-06-15 22:55:50 -0500

    Include mod paths when the host is setting the game up.
    Previewing 3rd party maps causes a search path cleanup (and if switching back to in-game afterwards) so
    they need to be added again.

--- src/init.cpp
+++ src/init.cpp
@@ -397,7 +397,7 @@ bool rebuildSearchPath(searchPathMode mode, bool force, const char *current_map)
 				// Add global and multiplay mods
 				PHYSFS_mount(curSearchPath->path, NULL, PHYSFS_APPEND);
 				addSubdirs(curSearchPath->path, "mods/music", PHYSFS_APPEND, nullptr, false);
-				if (NetPlay.isHost || !NetPlay.bComms)
+				if (NetPlay.isHost || !NetPlay.bComms || ingame.bHostSetup)
 				{
 					addSubdirs(curSearchPath->path, "mods/global", PHYSFS_APPEND, use_override_mods ? &override_mods : &global_mods, true);
 					addSubdirs(curSearchPath->path, "mods", PHYSFS_APPEND, use_override_mods ? &override_mods : &global_mods, true);
