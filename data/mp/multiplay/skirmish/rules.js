3ab6a996f5e97bb5e564add171ef3710591a0563
KJeff01 <kjeff1997@gmail.com>
2020-09-05 13:04:25 -0500

    Let Hard AIs get a power bonus.
    Hard difficulty AI would simply match the modifier of the power level.

--- data/mp/multiplay/skirmish/rules.js
+++ data/mp/multiplay/skirmish/rules.js
@@ -302,6 +302,10 @@ function eventGameInit()
 		{
 			setPowerModifier(200 + 15 * powerType, playnum);
 		}
+		else if (playerData[playnum].difficulty == HARD)
+		{
+			setPowerModifier(150 + 10 * powerType, playnum);
+		}
 		else if (playerData[playnum].difficulty == EASY)
 		{
 			setPowerModifier(70 + 5 * powerType, playnum);
@@ -331,7 +335,7 @@ function eventGameInit()
 	const timeBaseTech = 4.5*60;		// after Power Module
 	const timeAdvancedBaseTech = 7.9*60;	// after Mortar and Repair Facility
 	const timeT2 = 17*60;
-	const timeT3 = 26*60;			// after Needle Gun and Scourge Missile 
+	const timeT3 = 26*60;			// after Needle Gun and Scourge Missile
 
 	for (var playnum = 0; playnum < maxPlayers; playnum++)
 	{
