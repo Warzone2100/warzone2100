e46fcb4f85299086485f7a7ee4f3b3826f5c4aaf
Thiago Barcala <thiago.barcala@gmail.com>
2020-10-11 13:16:25 +0200

    Refactor animation to make it more readable
    - The method `start` used to accept `initialData`/`finalData`, and while
    it makes the usage of the animation shorter, these parameters are a bit
    arbitrary, and maybe not all animations require resetting both (or any)
    of them.
    - The `time` parameter is now a const ref instead of a pointer.
    - New easing functions added.
    - The setters are now using a fluent interface.
    - The method `isFinished` was renamed to `isActive`, because if the
    animation was never started (and therefore never finished), the method
    still returns `true`
    - Const function members were properly declared.

--- src/animation.cpp
+++ src/animation.cpp
@@ -2,55 +2,95 @@
 
 static uint16_t calculateEasing(EasingType easingType, uint16_t progress)
 {
-    switch (easingType)
-    {
-        case LINEAR:
-            return progress;
-        case EASE_IN_OUT:
-            return MAX(0, MIN(UINT16_MAX, iCos(UINT16_MAX / 2 + progress / 2) / 2 + (1 << 15)));
-    }
+	switch (easingType)
+	{
+	case LINEAR:
+		return progress;
+	case EASE_IN_OUT:
+		return MAX(0, MIN(UINT16_MAX, iCos(UINT16_MAX / 2 + progress / 2) / 2 + (1 << 15)));
+	case EASE_IN:
+		return (progress * (uint32_t)progress) / (UINT16_MAX * (uint32_t)UINT16_MAX);
+	case EASE_OUT:
+		return 2 * progress - (progress * (uint32_t)progress) / (UINT16_MAX - 1);
+	}
 
-    return UINT16_MAX;
+	return UINT16_MAX;
 }
 
 template <class AnimatableData>
-void Animation<AnimatableData>::start(AnimatableData initial, AnimatableData final)
+void Animation<AnimatableData>::start()
 {
-	startTime = *time;
-    initialData = initial;
-    finalData = final;
-}
-
-template <class AnimatableData>
-void Animation<AnimatableData>::setDuration(uint32_t durationMilliseconds)
-{
-	duration = durationMilliseconds * 0.001 * GAME_TICKS_PER_SEC;
+	startTime = time;
+	progress = 0;
 }
 
 template <class AnimatableData>
 void Animation<AnimatableData>::update()
 {
-    if (duration > 0) {
-        auto deltaTime = *time - (int64_t)startTime;
-        progress = MAX(0, MIN(UINT16_MAX, UINT16_MAX * deltaTime / duration));
-    } else {
-        progress = UINT16_MAX;
-    }
+	if (duration > 0)
+	{
+		auto deltaTime = time - (int64_t)startTime;
+		progress = MAX(0, MIN(UINT16_MAX, UINT16_MAX * deltaTime / duration));
+	}
+	else
+	{
+		progress = UINT16_MAX;
+	}
 
-	auto easedProgress = calculateEasing(easingType, calculateEasing(easingType, progress));
-    currentData = initialData + (finalData - initialData) * (easedProgress / (float)UINT16_MAX);
+	currentData = initialData + (finalData - initialData) * (getEasedProgress() / (float)UINT16_MAX);
 }
 
 template <class AnimatableData>
-AnimatableData Animation<AnimatableData>::getCurrent()
+bool Animation<AnimatableData>::isActive() const
 {
-    return currentData;
+	return progress < UINT16_MAX;
 }
 
 template <class AnimatableData>
-bool Animation<AnimatableData>::isFinished()
+const AnimatableData &Animation<AnimatableData>::getCurrent() const
 {
-    return progress == UINT16_MAX;
+	return currentData;
+}
+
+template <class AnimatableData>
+const AnimatableData &Animation<AnimatableData>::getFinalData() const
+{
+	return finalData;
+}
+
+template <class AnimatableData>
+uint16_t Animation<AnimatableData>::getEasedProgress() const
+{
+	return calculateEasing(easingType, calculateEasing(easingType, progress));
+}
+
+template <class AnimatableData>
+Animation<AnimatableData> &Animation<AnimatableData>::setInitialData(AnimatableData initial)
+{
+	initialData = initial;
+	return *this;
+}
+
+template <class AnimatableData>
+Animation<AnimatableData> &Animation<AnimatableData>::setFinalData(AnimatableData final)
+{
+	finalData = final;
+	return *this;
+}
+
+template <class AnimatableData>
+Animation<AnimatableData> &Animation<AnimatableData>::setEasing(EasingType easing)
+{
+	easingType = easing;
+	return *this;
+}
+
+template <class AnimatableData>
+Animation<AnimatableData> &Animation<AnimatableData>::setDuration(uint32_t durationMilliseconds)
+{
+	duration = durationMilliseconds * 0.001 * GAME_TICKS_PER_SEC;
+	return *this;
 }
 
 template class Animation<Vector3f>;
+template class Animation<float>;
--- src/animation.h
+++ src/animation.h
@@ -6,30 +6,38 @@
 
 enum EasingType
 {
-    LINEAR,
-    EASE_IN_OUT
+	LINEAR,
+	EASE_IN_OUT,
+	EASE_IN,
+	EASE_OUT,
 };
 
 template <class AnimatableData>
 class Animation
 {
 public:
-    Animation(const uint32_t *time, EasingType easingType = LINEAR): time(time), easingType(easingType) {}
-    void start(AnimatableData initialData, AnimatableData finalData);
-    void setDuration(uint32_t durationMilliseconds);
-    void update();
-    AnimatableData getCurrent();
-    bool isFinished();
+	Animation(uint32_t const &time, EasingType easingType = LINEAR, uint16_t duration = 0) : time(time), easingType(easingType), duration(duration) {}
+	void start();
+	void update();
+	bool isActive() const;
+	const AnimatableData &getCurrent() const;
+	const AnimatableData &getFinalData() const;
+	Animation<AnimatableData> &setInitialData(AnimatableData initial);
+	Animation<AnimatableData> &setFinalData(AnimatableData final);
+	Animation<AnimatableData> &setEasing(EasingType easing);
+	Animation<AnimatableData> &setDuration(uint32_t durationMilliseconds);
 
 private:
-	const uint32_t *time;
+	uint16_t getEasedProgress() const;
+
+	uint32_t const &time;
+	EasingType easingType;
+	uint32_t duration;
 	uint32_t startTime = 0;
-	uint32_t duration = 0;
-	uint16_t progress = 0;
-    EasingType easingType;
-    AnimatableData initialData;
-    AnimatableData finalData;
-    AnimatableData currentData;
+	uint16_t progress = UINT16_MAX;
+	AnimatableData initialData;
+	AnimatableData finalData;
+	AnimatableData currentData;
 };
 
 #endif // __INCLUDED_SRC_ANIMATION_H__
