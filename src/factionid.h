aecf3ecfc6502e90a2d006ae60fe01936c298140
past-due <30942300+past-due@users.noreply.github.com>
2020-11-22 12:29:07 -0500

    Preload faction models

--- src/factionid.h
+++ src/factionid.h
@@ -7,4 +7,14 @@ enum FactionID : uint8_t {
 	FACTION_COLLECTIVE = 2
 };
 
+namespace std {
+	template <> struct hash<FactionID>
+	{
+		size_t operator()(const FactionID& faction) const
+		{
+			return hash<uint8_t>()(static_cast<uint8_t>(faction));
+		}
+	};
+}
+
 #endif
