fc6edf649cbfc1fe816c132a12758fe6274c3200
past-due <30942300+past-due@users.noreply.github.com>
2020-12-21 10:51:53 -0500

    Add overload for addSideText()

--- src/frontend.h
+++ src/frontend.h
@@ -74,7 +74,8 @@ void addBottomForm();
 W_FORM *addBackdrop();
 W_FORM *addBackdrop(const std::shared_ptr<W_SCREEN> &screen);
 void addTextButton(UDWORD id, UDWORD PosX, UDWORD PosY, const std::string &txt, unsigned int style);
-void addSideText(UDWORD id, UDWORD PosX, UDWORD PosY, const char *txt);
+W_LABEL *addSideText(UDWORD id, UDWORD PosX, UDWORD PosY, const char *txt);
+W_LABEL *addSideText(const std::shared_ptr<W_SCREEN> &screen, UDWORD formId, UDWORD id, UDWORD PosX, UDWORD PosY, const char *txt);
 void addFESlider(UDWORD id, UDWORD parent, UDWORD x, UDWORD y, UDWORD stops, UDWORD pos);
 
 void displayTextOption(WIDGET *psWidget, UDWORD xOffset, UDWORD yOffset);
