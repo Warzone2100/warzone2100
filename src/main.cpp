c3d620ffbfab9c3574aef6509eabab694f2e2e67
past-due <30942300+past-due@users.noreply.github.com>
2021-02-09 09:52:37 -0500

    Ensure PHYSFS_permitSymbolicLinks is disabled

--- src/main.cpp
+++ src/main.cpp
@@ -570,7 +570,8 @@ static void initialize_ConfigDir()
 	// Config dir first so we always see what we write
 	PHYSFS_mount(PHYSFS_getWriteDir(), NULL, PHYSFS_PREPEND);
 
-	PHYSFS_permitSymbolicLinks(1);
+	// Do not follow symlinks *inside* search paths / archives
+	PHYSFS_permitSymbolicLinks(0);
 
 	debug(LOG_WZ, "Write dir: %s", PHYSFS_getWriteDir());
 	debug(LOG_WZ, "Base dir: %s", PHYSFS_getBaseDir());
