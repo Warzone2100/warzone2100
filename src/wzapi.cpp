02e0f870b98acc58e491d26ef8a7356ef93f81bf
maxsupermanhd <q3.max.2011@ya.ru>
2021-04-18 18:41:37 +0300

    Add public key to JS backend

--- src/wzapi.cpp
+++ src/wzapi.cpp
@@ -4437,6 +4437,7 @@ nlohmann::json wzapi::constructStaticPlayerData()
 		vector["isAI"] = !NetPlay.players[i].allocated && NetPlay.players[i].ai >= 0;
 		vector["isHuman"] = NetPlay.players[i].allocated;
 		vector["type"] = SCRIPT_PLAYER;
+		vector["identity"] = playerStats[i].identity.publicHashString();
 		playerData.push_back(std::move(vector));
 	}
 	return playerData;
