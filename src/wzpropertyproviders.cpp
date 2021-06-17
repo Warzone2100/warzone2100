52013cd150eb32f46ee7f13ecfa3ee6a3c74bb42
past-due <30942300+past-due@users.noreply.github.com>
2021-06-17 12:02:44 -0400

    BuildPropertyProvider: Add support for WIN_PACKAGE_FULLNAME

--- src/wzpropertyproviders.cpp
+++ src/wzpropertyproviders.cpp
@@ -1,6 +1,6 @@
 /*
 	This file is part of Warzone 2100.
-	Copyright (C) 2020  Warzone 2100 Project
+	Copyright (C) 2020-2021  Warzone 2100 Project
 
 	Warzone 2100 is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
 #include "wzpropertyproviders.h"
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include "lib/framework/wzglobal.h" // required for config.h
 #include "lib/framework/frame.h"
@@ -48,8 +49,97 @@ enum class BuildProperty {
 	VERSION_STRING,
 	PLATFORM,
 	PARENTPROCESS,
-	ANCESTORS
+	ANCESTORS,
+	// WZ 4.1.0+
+	WIN_PACKAGE_FULLNAME
 };
+
+#if defined(WZ_OS_WIN)
+
+#include <ntverp.h>				// Windows SDK - include for access to VER_PRODUCTBUILD
+#if VER_PRODUCTBUILD >= 9200
+	// 9200 is the Windows SDK 8.0 (which introduced family support)
+	#include <winapifamily.h>	// Windows SDK
+#else
+	// Earlier SDKs don't have the concept of families - provide simple implementation
+	// that treats everything as "desktop"
+	#if !defined(WINAPI_PARTITION_DESKTOP)
+		#define WINAPI_PARTITION_DESKTOP			0x00000001
+	#endif
+	#if !defined(WINAPI_FAMILY_PARTITION)
+		#define WINAPI_FAMILY_PARTITION(Partition)	((WINAPI_PARTITION_DESKTOP & Partition) == Partition)
+	#endif
+#endif
+
+static std::string Get_WinPackageFullName()
+{
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+	typedef LONG (WINAPI *GetCurrentPackageFullNameFunc)(
+		UINT32 *packageFullNameLength,
+		PWSTR  packageFullName
+	);
+#if !defined(APPMODEL_ERROR_NO_PACKAGE)
+#  define APPMODEL_ERROR_NO_PACKAGE 15700
+#endif
+
+	HMODULE hKernel32 = GetModuleHandleW(L"kernel32");
+
+	GetCurrentPackageFullNameFunc _GetCurrentPackageFullName = reinterpret_cast<GetCurrentPackageFullNameFunc>(reinterpret_cast<void*>(GetProcAddress(hKernel32, "GetCurrentPackageFullName")));
+
+	if (!_GetCurrentPackageFullName)
+	{
+		return "";
+	}
+
+	UINT32 length = 0;
+	LONG rc = _GetCurrentPackageFullName(&length, NULL);
+	if (rc != ERROR_INSUFFICIENT_BUFFER)
+	{
+		if (rc == APPMODEL_ERROR_NO_PACKAGE)
+		{
+			// Process has no package identity
+			return "";
+		}
+		else
+		{
+			// Unexpected error
+			debug(LOG_WARNING, "Error %ld in GetCurrentPackageFullName", rc);
+			return "";
+		}
+	}
+
+	std::vector<wchar_t> fullNameW(length, 0);
+	rc = _GetCurrentPackageFullName(&length, &fullNameW[0]);
+	if (rc != ERROR_SUCCESS)
+	{
+		debug(LOG_WARNING, "Error %ld retrieving PackageFullName", rc);
+		return "";
+	}
+
+	// convert UTF-16 fullNameW to UTF-8
+	std::vector<char> utf8Buffer;
+	int utf8Len = WideCharToMultiByte(CP_UTF8, 0, fullNameW.data(), -1, NULL, 0, NULL, NULL);
+	if ( utf8Len <= 0 )
+	{
+		// Encoding conversion error
+		debug(LOG_ERROR, "WideCharToMultiByte[1] failed");
+		return "";
+	}
+	utf8Buffer.resize(utf8Len, 0);
+	if ( (utf8Len = WideCharToMultiByte(CP_UTF8, 0, fullNameW.data(), -1, &utf8Buffer[0], utf8Len, NULL, NULL)) <= 0 )
+	{
+		// Encoding conversion error
+		debug(LOG_ERROR, "WideCharToMultiByte[2] failed");
+		return "";
+	}
+	std::string fullName = std::string(utf8Buffer.data(), utf8Len - 1);
+	return fullName;
+#else
+	// For now, return empty string
+	return "";
+#endif
+}
+#endif
 
 static std::string GetCurrentBuildPropertyValue(const BuildProperty& property)
 {
@@ -93,6 +183,12 @@ static std::string GetCurrentBuildPropertyValue(const BuildProperty& property)
 			}
 			return ancestorsStr;
 		}
+		case BP::WIN_PACKAGE_FULLNAME:
+#if defined(WZ_OS_WIN)
+			return Get_WinPackageFullName();
+#else
+			return "";
+#endif
 	}
 	return ""; // silence warning
 }
@@ -110,7 +206,8 @@ static const std::unordered_map<std::string, BuildProperty> strToBuildPropertyMa
 	{"VERSION_STRING", BuildProperty::VERSION_STRING},
 	{"PLATFORM", BuildProperty::PLATFORM},
 	{"PARENTPROCESS", BuildProperty::PARENTPROCESS},
-	{"ANCESTORS", BuildProperty::ANCESTORS}
+	{"ANCESTORS", BuildProperty::ANCESTORS},
+	{"WIN_PACKAGE_FULLNAME", BuildProperty::WIN_PACKAGE_FULLNAME}
 };
 
 BuildPropertyProvider::~BuildPropertyProvider() { }
