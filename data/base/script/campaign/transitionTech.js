e8a3720e9a585f2e9633e2ea9917fa7d06db4a83
alfred007 <highlander1599@gmail.com>
2021-07-26 16:34:52 +0200

    Adding new Twin MG Tower to transitionTech.js
    And also moving the second Cannon damage upgrade back to Alpha 04.

--- data/base/script/campaign/transitionTech.js
+++ data/base/script/campaign/transitionTech.js
@@ -10,6 +10,7 @@ const ALPHA_RESEARCH_NEW = [
 	// 2
 	"R-Wpn-Flamer-Damage01", "R-Wpn-MG2Mk1", "R-Sys-Sensor-Turret01", "R-Struc-PowerModuleMk1",
 	"R-Wpn-MG-Damage02", "R-Sys-Sensor-Tower01", "R-Wpn-Flamer-ROF01", "R-Wpn-Flamer-Range01",
+	"R-Defense-Tower02",
 
 	// 3
 	"R-Wpn-MG3Mk1", "R-Defense-Tower03", "R-Wpn-MG-Damage03",
@@ -17,7 +18,7 @@ const ALPHA_RESEARCH_NEW = [
 	// 4
 	"R-Wpn-Flamer-Damage02", "R-Wpn-Mortar01Lt", "R-Vehicle-Prop-Halftracks",
 	"R-Wpn-Cannon1Mk1", "R-Wpn-Mortar-Damage01", "R-Wpn-Mortar-Damage02",
-	"R-Wpn-Mortar-ROF01","R-Wpn-Cannon-Damage01",
+	"R-Wpn-Mortar-ROF01","R-Wpn-Cannon-Damage01", "R-Wpn-Cannon-Damage02",
 
 	// 5
 	"R-Struc-Factory-Module", "R-Wpn-Flamer-Damage03",
@@ -32,7 +33,7 @@ const ALPHA_RESEARCH_NEW = [
 	// 6
 	"R-Wpn-Cannon2Mk1", "R-Defense-WallTower03", "R-Wpn-Rocket05-MiniPod", "R-Struc-Research-Module",
 	"R-Vehicle-Prop-Tracks", "R-Vehicle-Engine01", "R-Defense-Tower06", "R-Defense-Pillbox06",
-	"R-Wpn-Rocket-Damage01", "R-Wpn-Rocket-Damage02", "R-Wpn-Rocket-ROF01", "R-Wpn-Cannon-Damage02",
+	"R-Wpn-Rocket-Damage01", "R-Wpn-Rocket-Damage02", "R-Wpn-Rocket-ROF01",
 	"R-Wpn-Rocket-ROF02", "R-Wpn-Rocket-ROF03", "R-Defense-WallTower06",
 	"R-Wpn-Rocket01-LtAT", "R-Wpn-Rocket03-HvAT", "R-Wpn-RocketSlow-Damage01",
 
