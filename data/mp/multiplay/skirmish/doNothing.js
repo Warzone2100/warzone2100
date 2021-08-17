66985247912f2f8b3ddec0e9858d381ac8182bcb
Denis Gantsev <gantsevdenis@gmail.com>
2021-08-17 17:42:29 +0200

    dummy file

--- data/mp/multiplay/skirmish/doNothing.js
+++
@@ -1,7 +0,0 @@
-// You can redefine these paths when you make a customized AI
-// for a map or a challenge.
-NB_PATH = "/multiplay/skirmish/";
-NB_INCLUDES = NB_PATH + "nb_includes/";
-
-// please don't touch this line
-include(NB_INCLUDES + "_head.js");
