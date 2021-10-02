106ebecb66ee5345864350a81a63fc58cb8a2506
past-due <30942300+past-due@users.noreply.github.com>
2021-10-02 18:18:45 -0400

    crc.h/.cpp: Various tweaks / new functions

--- lib/framework/crc.cpp
+++ lib/framework/crc.cpp
@@ -134,17 +134,22 @@ void Sha256::setZero()
 	memset(bytes, 0x00, Bytes);
 }
 
-std::string Sha256::toString() const
+static inline std::string bytesToHex(const uint8_t* bytes, size_t numBytes)
 {
 	std::string str;
-	str.resize(Bytes * 2);
+	str.resize(numBytes * 2);
 	char const *hexDigits = "0123456789abcdef";
-	for (int n = 0; n < Bytes; ++n)
+	for (size_t n = 0; n < numBytes; ++n)
 	{
 		str[n * 2    ] = hexDigits[bytes[n] >> 4];
 		str[n * 2 + 1] = hexDigits[bytes[n] & 15];
 	}
 	return str;
+}
+
+std::string Sha256::toString() const
+{
+	return bytesToHex(bytes, Bytes);
 }
 
 void Sha256::fromString(std::string const &s)
@@ -437,6 +442,16 @@ std::string EcKey::publicHashString(size_t truncateToLength /*= 0*/) const
 	return shaStr;
 }
 
+std::string EcKey::publicKeyHexString(size_t truncateToLength /*= 0*/) const
+{
+	auto bytes = toBytes(EcKey::Public);
+	if (bytes.empty())
+	{
+		return std::string{};
+	}
+	return bytesToHex(bytes.data(), bytes.size());
+}
+
 
 //================================================================================
 // MARK: - Base64
@@ -498,6 +513,22 @@ std::vector<uint8_t> base64Decode(std::string const &str)
 		}
 	}
 	return bytes;
+}
+
+std::string b64Tob64UrlSafe(const std::string& inputb64)
+{
+	std::string b64urlsafe = inputb64;
+	std::replace(b64urlsafe.begin(), b64urlsafe.end(), '+', '-');
+	std::replace(b64urlsafe.begin(), b64urlsafe.end(), '/', '_');
+	return b64urlsafe;
+}
+
+std::string b64UrlSafeTob64(const std::string& inputb64urlsafe)
+{
+	std::string b64 = inputb64urlsafe;
+	std::replace(b64.begin(), b64.end(), '-', '+');
+	std::replace(b64.begin(), b64.end(), '_', '/');
+	return b64;
 }
 
 //================================================================================
