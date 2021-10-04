e87ebbc2105176e1ef81333d82e58f303efc1178
past-due <30942300+past-due@users.noreply.github.com>
2021-10-04 11:43:49 -0400

    WzConfig: More robust handling of load failure

--- lib/framework/wzconfig.cpp
+++ lib/framework/wzconfig.cpp
@@ -71,8 +71,8 @@ static nlohmann::json jsonMerge(nlohmann::json original, const nlohmann::json& o
 WzConfig::WzConfig(const WzString &name, WzConfig::warning warning)
 : mArray(nlohmann::json::array())
 {
-	UDWORD size;
-	char *data;
+	UDWORD size = 0;
+	char *data = nullptr;
 
 	mFilename = name;
 	mStatus = true;
@@ -98,8 +98,11 @@ WzConfig::WzConfig(const WzString &name, WzConfig::warning warning)
 	}
 	if (!loadFile(name.toUtf8().c_str(), &data, &size))
 	{
+		mStatus = false;
 		debug(LOG_FATAL, "Could not open \"%s\"", name.toUtf8().c_str());
+		return;
 	}
+	ASSERT_OR_RETURN(, data != nullptr, "Null data?");
 
 	try {
 		mRoot = nlohmann::json::parse(data, data + size);
