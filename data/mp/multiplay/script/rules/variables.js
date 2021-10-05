14636165f38eb7c32e3532c4c82b1051402ce0b0
Tipchik <50970280+Tipchik87@users.noreply.github.com>
2021-10-06 01:03:39 +0300

    AdvancedBaseTech 6.5 -> 6.4
    For a balanced start with the Advanced Base

--- data/mp/multiplay/script/rules/variables.js
+++ data/mp/multiplay/script/rules/variables.js
@@ -13,6 +13,6 @@ const TRANSFER_LIKE_EVENT = 2;
 
 const cleanTech = 1;
 const timeBaseTech = 3*60;		// after Half-tracked Propulsion and Light Cannon
-const timeAdvancedBaseTech = 6.5*60;	// after Factory Module and HEAT Cannon Shells Mk2
+const timeAdvancedBaseTech = 6.4*60;	// after Factory Module and HEAT Cannon Shells Mk2
 const timeT2 = 17*60;
 const timeT3 = 26*60;			// after Needle Gun and Scourge Missile
