871eb86af01f0e9599c255d3e402fca923c9989e
past-due <30942300+past-due@users.noreply.github.com>
2021-10-18 19:29:02 -0400

    gameTimeInit: Clear additional state

--- lib/gamelib/gtime.cpp
+++ lib/gamelib/gtime.cpp
@@ -113,11 +113,13 @@ void gameTimeInit(void)
 	wantedLatency = GAME_TICKS_PER_UPDATE * 2;
 	for (player = 0; player < MAX_GAMEQUEUE_SLOTS; ++player)
 	{
+		gameQueueCheckTime[player] = 0;
 		gameQueueCheckCrc[player] = 0;
 		wantedLatencies[player] = 0;
 	}
 
 	// Don't let syncDebug from previous games cause a desynch dump at gameTime 102.
+	crcError = false;
 	resetSyncDebug();
 }
 
