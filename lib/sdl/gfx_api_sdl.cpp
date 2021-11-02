d69deb7b872a83b9e77257fdf017d5096be44b15
past-due <30942300+past-due@users.noreply.github.com>
2021-11-02 12:54:32 -0400

    [SDL backend] Immediately return nullptr if window is invalid

--- lib/sdl/gfx_api_sdl.cpp
+++ lib/sdl/gfx_api_sdl.cpp
@@ -37,14 +37,14 @@ std::unique_ptr<gfx_api::backend_Null_Impl> SDL_gfx_api_Impl_Factory::createNull
 
 std::unique_ptr<gfx_api::backend_OpenGL_Impl> SDL_gfx_api_Impl_Factory::createOpenGLBackendImpl() const
 {
-	ASSERT(window != nullptr, "Invalid SDL_Window*");
+	ASSERT_OR_RETURN(nullptr, window != nullptr, "Invalid SDL_Window*");
 	return std::unique_ptr<gfx_api::backend_OpenGL_Impl>(new sdl_OpenGL_Impl(window, config.useOpenGLES, config.useOpenGLESLibrary));
 }
 
 #if defined(WZ_VULKAN_ENABLED)
 std::unique_ptr<gfx_api::backend_Vulkan_Impl> SDL_gfx_api_Impl_Factory::createVulkanBackendImpl() const
 {
-	ASSERT(window != nullptr, "Invalid SDL_Window*");
+	ASSERT_OR_RETURN(nullptr, window != nullptr, "Invalid SDL_Window*");
 #if defined(HAVE_SDL_VULKAN_H)
 	return std::unique_ptr<gfx_api::backend_Vulkan_Impl>(new sdl_Vulkan_Impl(window));
 #else // !defined(HAVE_SDL_VULKAN_H)
