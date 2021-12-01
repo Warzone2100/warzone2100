921ac6a22416c03917268f8ee7f18d7bcba51c56
KJeff01 <kjeff1997@gmail.com>
2021-12-01 15:22:07 -0600

    Add a time range to cam3-1 launch/detonate sounds
    Do not play any of these sounds if the current time is over the desired time by a bit.

--- data/base/script/campaign/cam3-1.js
+++ data/base/script/campaign/cam3-1.js
@@ -181,6 +181,7 @@ function setupNextMission()
 //Play countdown sounds. Elements are shifted out of the missile launch/detonation arrays as they play.
 function getCountdown()
 {
+	const ACCEPTABLE_TIME_DIFF = 2;
 	var silosDestroyed = missileSilosDestroyed();
 	var countdownObject = silosDestroyed ? detonateInfo : launchInfo;
 	var skip = false;
@@ -190,7 +191,7 @@ function getCountdown()
 		var currentTime = getMissionTime();
 		if (currentTime <= countdownObject[0].time)
 		{
-			if (len > 1 && (currentTime <= countdownObject[1].time))
+			if (currentTime < (countdownObject[0].time - ACCEPTABLE_TIME_DIFF))
 			{
 				skip = true; //Huge time jump?
 			}
