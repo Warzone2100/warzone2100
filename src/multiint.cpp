Warzone2100/warzone2100

commit 8692cb6ac4067d33ff3b30c995bb457c598c48df

Fix AI stats not being reset across games

src/multiint.cpp
@@ -7131,6 +7131,11 @@ void WzMultiplayerOptionsTitleUI::start()
 		ingame.localOptionsReceived = false;
 
 		PLAYERSTATS	nullStats;
+		for (size_t i = 0; i < MAX_CONNECTED_PLAYERS; i++)
+		{
+			setMultiStats(i, nullStats, true);
+		}
+
 		loadMultiStats((char*)sPlayer, &nullStats);
 		lookupRatingAsync(selectedPlayer);
 

commit d91ff175420fef8d53e6c529a20bd7dd7da321d2

Fix: multiStats being improperly reset for clients joining a game
Fixes issue introduced in 8692cb6ac4067d33ff3b30c995bb457c598c48df

src/multiint.cpp
@@ -7163,9 +7163,20 @@ void WzMultiplayerOptionsTitleUI::start()
 		ingame.localOptionsReceived = false;
 
 		PLAYERSTATS	nullStats;
-		for (size_t i = 0; i < MAX_CONNECTED_PLAYERS; i++)
+		if (!NetPlay.bComms)
 		{
-			setMultiStats(i, nullStats, true);
+			// ONLY DO THIS FOR LOCAL SKIRMISH (NOT MULTIPLAY)
+			// This is probably not the right place for this, and breaks multiplayer games (without the bComms check above), but // TODO FIXME: Figure out a better place to put this
+			// See: https://github.com/Warzone2100/warzone2100/pull/2527
+			for (size_t i = 0; i < MAX_CONNECTED_PLAYERS; i++)
+			{
+				if (i == realSelectedPlayer)
+				{
+					continue;
+				}
+
+				setMultiStats(i, nullStats, true);
+			}
 		}
 
 		loadMultiStats((char*)sPlayer, &nullStats);