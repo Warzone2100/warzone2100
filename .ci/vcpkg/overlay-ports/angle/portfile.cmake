5db4070b472047d28570ed75142bee8f01a324ab
past-due <30942300+past-due@users.noreply.github.com>
2022-05-05 18:56:05 -0400

    vcpkg overlay port for angle: Fix msys2/clang64 builds

--- .ci/vcpkg/overlay-ports/angle/portfile.cmake
+++ .ci/vcpkg/overlay-ports/angle/portfile.cmake
@@ -25,7 +25,8 @@ vcpkg_from_github(
         001-fix-uwp.patch
         002-fix-builder-error.patch
         wz-mingw-headers.patch
-        wz-mingw-headers-2.patch
+#        wz-mingw-headers-2.patch
+        wz-mingw-headers-3.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
