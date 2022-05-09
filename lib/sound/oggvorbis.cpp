d951314c471b74d92ec8c9b8298a1055dfdc5cf4
Denis Gantsev <gantsevdenis@gmail.com>
2022-05-09 09:40:18 +0200

    lib/sound: move callbacks

--- lib/sound/oggvorbis.cpp
+++ lib/sound/oggvorbis.cpp
@@ -22,7 +22,6 @@
 #include <limits>
 #include "lib/framework/physfs_ext.h"
 #include "oggvorbis.h"
-#include "openal_callbacks.h"
 
 
 #ifdef __BIG_ENDIAN__
@@ -36,6 +35,98 @@
 #endif
 
 #include "oggvorbis.h"
+
+/**
+ *
+ * \param	_stream	    The stream to read from.
+ * \param 	[out]	 _ptr	The buffer to store the data in.
+ * \param _nbytes	    The maximum number of bytes to read.
+	This function may return fewer, though it will not return zero unless it reaches end-of-file.
+*/
+static size_t wz_ogg_read(void *ptr, size_t size, size_t nmemb, void *datasource)
+{
+	PHYSFS_file *fileHandle;
+
+	ASSERT(datasource != nullptr, "NULL decoder passed!");
+
+	fileHandle = ((PHYSFS_file *)datasource);
+	ASSERT(fileHandle != nullptr, "Bad PhysicsFS file handle passed in");
+
+	size_t readLen = size * nmemb;
+	ASSERT(readLen <= static_cast<size_t>(std::numeric_limits<PHYSFS_uint32>::max()), "readLen (%zu) exceeds PHYSFS_uint32::max", readLen);
+	return WZ_PHYSFS_readBytes(fileHandle, ptr, static_cast<PHYSFS_uint32>(readLen));
+}
+
+static int wz_ogg_seek(void *datasource, int64_t offset, int whence)
+{
+	PHYSFS_file *fileHandle;
+	int64_t newPos;
+	ASSERT(datasource != nullptr, "NULL decoder passed!");
+	fileHandle = ((PHYSFS_file *)datasource);
+	ASSERT(fileHandle != nullptr, "Bad PhysicsFS file handle passed in");
+
+	switch (whence)
+	{
+	// Seek to absolute position
+	case SEEK_SET:
+		newPos = offset;
+		break;
+
+	// Seek `offset` ahead
+	case SEEK_CUR:
+		{
+			int64_t curPos = PHYSFS_tell(fileHandle);
+			if (curPos == -1)
+			{
+				return -1;
+			}
+
+			newPos = curPos + offset;
+			break;
+		}
+
+	// Seek backwards from the end of the file
+	case SEEK_END:
+		{
+			int64_t fileSize = PHYSFS_fileLength(fileHandle);
+			if (fileSize == -1)
+			{
+				return -1;
+			}
+
+			newPos = fileSize - 1 - offset;
+			break;
+		}
+
+	// unrecognized seek instruction
+	default:
+		// indicate failure
+		return -1;
+	}
+
+	// PHYSFS_seek return value of non-zero means success
+	if (PHYSFS_seek(fileHandle, newPos) != 0)
+	{
+		// success is NON-ZERO!
+		return newPos;
+	}
+	else
+	{
+		// indicate failure
+		return -1;    // failure
+	}
+}
+
+static long wz_ogg_tell(void *datasource)
+{
+	PHYSFS_file *fileHandle;
+	ASSERT(datasource != nullptr, "NULL decoder passed!");
+
+	fileHandle = ((PHYSFS_file *)datasource);
+	ASSERT(fileHandle != nullptr, "Bad PhysicsFS file handle passed in");
+	const int64_t out = PHYSFS_tell(fileHandle);
+	return out;
+}
 
 const ov_callbacks wz_oggVorbis_callbacks =
 {
