035a3645d0025eb5e1aa7eed0af9356ab580dc5b
highlander1599 <highlander1599@gmail.com>
2022-06-27 22:59:13 +0200

    Alpha 04 changes
    Reverts the throttle changes from the commit before because I misunderstood the code. Silly me.

--- data/base/script/campaign/cam1-2.js
+++ data/base/script/campaign/cam1-2.js
@@ -113,7 +113,7 @@ function eventStartLevel()
 			assembly: "NorthAssembly",
 			order: CAM_ORDER_PATROL,
 			groupSize: 5,
-			throttle: camChangeOnDiff(camSecondsToMilliseconds((difficulty === EASY || difficulty === MEDIUM) ? 20 : 18)),
+			throttle: camChangeOnDiff(camSecondsToMilliseconds((difficulty === EASY || difficulty === MEDIUM) ? 20 : 15)),
 			data: {
 				pos: [
 					camMakePos("NorthAssembly"),
@@ -131,7 +131,7 @@ function eventStartLevel()
 			assembly: "WestAssembly",
 			order: CAM_ORDER_PATROL,
 			groupSize: 5,
-			throttle: camChangeOnDiff(camSecondsToMilliseconds((difficulty === EASY || difficulty === MEDIUM) ? 13 : 12)),
+			throttle: camChangeOnDiff(camSecondsToMilliseconds((difficulty === EASY || difficulty === MEDIUM) ? 13 : 10)),
 			data: {
 				pos: [
 					camMakePos("WestAssembly"),
