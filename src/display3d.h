28dfe21e26a8f24f60235ca7fcd5d184d279b62b
g.denis <gantsevdenis@gmail.com>
2022-07-05 21:26:56 +0200

    show rank for next droid (#2727)

--- src/display3d.h
+++ src/display3d.h
@@ -125,6 +125,7 @@ extern const Vector2i visibleTiles;
 
 /*returns the graphic ID for a droid rank*/
 UDWORD  getDroidRankGraphic(DROID *psDroid);
+UDWORD  getDroidRankGraphicFromLevel(unsigned int level);
 
 void setSkyBox(const char *page, float mywind, float myscale);
 
