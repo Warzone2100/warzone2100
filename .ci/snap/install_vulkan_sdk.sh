b021821e4b0b2951076ca7981ea99c3d663fcaa6
past-due <30942300+past-due@users.noreply.github.com>
2022-09-07 15:22:33 -0400

    [CI] Split glslc compilation into install_glslc.sh

--- .ci/snap/install_vulkan_sdk.sh
+++ .ci/snap/install_vulkan_sdk.sh
@@ -16,35 +16,9 @@ if [ "$TARGET_BUILD_ARCH" = "amd64" ]; then
   apt install --yes vulkan-sdk
 
 else
-  # Compile the necessary components of the Vulkan SDK from source
-  echo "Compiling Vulkan SDK (selected components)"
-
-  mkdir tmp_vulkan_sdk_build
-  cd "tmp_vulkan_sdk_build"
-
-  # glslc
-  DEBIAN_FRONTEND=noninteractive apt-get -y install cmake python3 lcov
-
-  # glslc - clone
-  echo "Fetching shaderc"
-  git clone https://github.com/google/shaderc shaderc
-  cd shaderc
-  git checkout tags/v2022.1 -b v2022.1
-  ./utils/git-sync-deps
-  cd ..
-
-  # glslc - cmake build + install
-  mkdir shaderc_build
-  cd shaderc_build
-  echo "Configuring shaderc"
-  cmake -GNinja -DSHADERC_SKIP_TESTS=ON -DCMAKE_BUILD_TYPE=Release ../shaderc/
-  echo "Compiling shaderc"
-  cmake --build . --target install
-  cd ..
-
-  cd ..
-  rm -rf "tmp_vulkan_sdk_build"
-
+  SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0)"
+  SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+  source "${SCRIPT_DIR}/../vulkan/install_glslc.sh"
 fi
 
 echo "Installing Vulkan SDK - done"
