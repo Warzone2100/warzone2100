b015a75e545faf69b6f400419d84caa6789f41e1
past-due <30942300+past-due@users.noreply.github.com>
2022-09-23 16:22:46 -0400

    Remove pie_Get/SetShaderStretchDepth()

--- lib/ivis_opengl/piedef.h
+++ lib/ivis_opengl/piedef.h
@@ -43,7 +43,7 @@ struct iIMDShape;
  *	Global ProtoTypes
  */
 /***************************************************************************/
-bool pie_Draw3DShape(iIMDShape *shape, int frame, int team, PIELIGHT colour, int pieFlag, int pieFlagData, const glm::mat4 &modelView);
+bool pie_Draw3DShape(iIMDShape *shape, int frame, int team, PIELIGHT colour, int pieFlag, int pieFlagData, const glm::mat4 &modelView, float stretchDepth = 0.f);
 
 void pie_GetResetCounts(size_t *pPieCount, size_t *pPolyCount);
 
