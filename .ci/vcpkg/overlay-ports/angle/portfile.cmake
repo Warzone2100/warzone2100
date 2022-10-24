3174861756853066796d53f076b4ed02c8c454f9
past-due <30942300+past-due@users.noreply.github.com>
2022-10-24 15:53:21 -0400

    Update angle vcpkg port

--- .ci/vcpkg/overlay-ports/angle/portfile.cmake
+++ .ci/vcpkg/overlay-ports/angle/portfile.cmake
@@ -1,6 +1,6 @@
 if (VCPKG_TARGET_IS_LINUX)
     message(WARNING "Building with a gcc version less than 6.1 is not supported.")
-    message(WARNING "${PORT} currently requires the following libraries from the system package manager:\n    libx11-dev\n    libmesa-dev\n    libxi-dev\n    libxext-dev\n\nThese can be installed on Ubuntu systems via apt-get install libx11-dev libmesa-dev libxi-dev libxext-dev.")
+    message(WARNING "${PORT} currently requires the following libraries from the system package manager:\n    libx11-dev\n    mesa-common-dev\n    libxi-dev\n    libxext-dev\n\nThese can be installed on Ubuntu systems via apt-get install libx11-dev mesa-common-dev libxi-dev libxext-dev.")
 endif()
 
 if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
@@ -15,25 +15,56 @@ else()
     message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
 endif()
 
+set(ANGLE_USE_D3D11_COMPOSITOR_NATIVE_WINDOW "OFF")
+if (VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
+  set(ANGLE_BUILDSYSTEM_PORT "Win")
+  set(ANGLE_USE_D3D11_COMPOSITOR_NATIVE_WINDOW "ON")
+elseif (VCPKG_TARGET_IS_OSX)
+  set(ANGLE_BUILDSYSTEM_PORT "Mac")
+elseif (VCPKG_TARGET_IS_LINUX)
+  set(ANGLE_BUILDSYSTEM_PORT "Linux")
+else()
+  # default other platforms to "Linux" config
+  set(ANGLE_BUILDSYSTEM_PORT "Linux")
+endif()
+
+# chromium/5249
+set(ANGLE_COMMIT b8636b57b8f231994ecb3fb14f181c593c83a3fb)
+set(ANGLE_VERSION 5249)
+set(ANGLE_SHA512 a2d3b6ffa49fa1f6f0b93cba9a079f923b78ebf8294e95be2267bedec92041472588033cce47fd0f0361ee35d9c080dbfb8a611d2761afbd3a13d224ada0f7eb)
+set(ANGLE_THIRDPARTY_ZLIB_COMMIT 8d1d3e341948009ed8dc807a545204e7a1854c33)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/angle
-    REF d15be77864e18f407c317be6f6bc06ee2b7d070a # chromium/4472
-    SHA512 aad8563ee65458a7865ec7c668d1f90ac2891583c569a22dcd2c557263b72b26386f56b74a7294398be2cf5c548df513159e4be53f3f096f19819ca06227d9ac
+    REF ${ANGLE_COMMIT}
+    SHA512 ${ANGLE_SHA512}
     # On update check headers against opengl-registry
     PATCHES
         001-fix-uwp.patch
         002-fix-builder-error.patch
+        003-fix-mingw.patch
         wz-mingw-headers.patch
 #        wz-mingw-headers-2.patch
         wz-mingw-headers-3.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/angle_commit.h DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/angle_commit.h DESTINATION ${SOURCE_PATH}/src/common)
+# Generate angle_commit.h
+set(ANGLE_COMMIT_HASH_SIZE 12)
+string(SUBSTRING "${ANGLE_COMMIT}" 0 ${ANGLE_COMMIT_HASH_SIZE} ANGLE_COMMIT_HASH)
+set(ANGLE_COMMIT_DATE "invalid-date")
+set(ANGLE_REVISION "${ANGLE_VERSION}")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/angle_commit.h.in" "${SOURCE_PATH}/angle_commit.h" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/angle_commit.h.in" "${SOURCE_PATH}/src/common/angle_commit.h" @ONLY)
 
-function(checkout_in_path_with_patches PATH URL REF PATCHES)
+# Copy CMake buildsystem into appropriate folders
+file(GLOB MAIN_BUILDSYSTEM "${CMAKE_CURRENT_LIST_DIR}/cmake-buildsystem/CMakeLists.txt" "${CMAKE_CURRENT_LIST_DIR}/cmake-buildsystem/*.cmake" "${CMAKE_CURRENT_LIST_DIR}/cmake-buildsystem/generated/*.cmake")
+file(COPY ${MAIN_BUILDSYSTEM} DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/cmake-buildsystem/include/CMakeLists.txt" DESTINATION "${SOURCE_PATH}/include")
+file(GLOB MODULES "${CMAKE_CURRENT_LIST_DIR}/cmake-buildsystem/cmake/*.cmake")
+file(COPY ${MODULES} DESTINATION "${SOURCE_PATH}/cmake")
+
+function(checkout_in_path PATH URL REF)
     if(EXISTS "${PATH}")
         return()
     endif()
@@ -42,34 +73,33 @@ function(checkout_in_path_with_patches PATH URL REF PATCHES)
         OUT_SOURCE_PATH DEP_SOURCE_PATH
         URL "${URL}"
         REF "${REF}"
-        PATCHES ${PATCHES}
     )
     file(RENAME "${DEP_SOURCE_PATH}" "${PATH}")
     file(REMOVE_RECURSE "${DEP_SOURCE_PATH}")
 endfunction()
 
-checkout_in_path_with_patches(
+checkout_in_path(
     "${SOURCE_PATH}/third_party/zlib"
     "https://chromium.googlesource.com/chromium/src/third_party/zlib"
-    "09490503d0f201b81e03f5ca0ab8ba8ee76d4a8e"
-    "third-party-zlib-far-undef.patch"
+    "${ANGLE_THIRDPARTY_ZLIB_COMMIT}"
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=1
     OPTIONS
         -D${ANGLE_CPU_BITNESS}=1
+        -DPORT=${ANGLE_BUILDSYSTEM_PORT}
+        -DANGLE_USE_D3D11_COMPOSITOR_NATIVE_WINDOW=${ANGLE_USE_D3D11_COMPOSITOR_NATIVE_WINDOW}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-angle TARGET_PATH share/unofficial-angle)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-angle PACKAGE_NAME unofficial-angle)
 
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 # File conflict with opengl-registry! Make sure headers are similar on Update!
 # angle defines some additional entrypoints. 
@@ -97,5 +127,3 @@ foreach(_file ${_double_files})
         file(REMOVE "${CURRENT_PACKAGES_DIR}/${_file}")
     endif()
 endforeach()
-
-
