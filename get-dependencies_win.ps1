5500aaf645c5fcc0d00387c96353993f7a0268cb
past-due <30942300+past-due@users.noreply.github.com>
2022-11-30 21:15:59 -0500

    [Windows] Update vcpkg commit in get-dependencies_win.ps1

--- get-dependencies_win.ps1
+++ get-dependencies_win.ps1
@@ -5,7 +5,7 @@ param([string]$VCPKG_BUILD_TYPE = "")
 ############################
 
 # To ensure reproducible builds, pin to a specific vcpkg commit
-$VCPKG_COMMIT_SHA = "fe5757109de431c2be980144867b26f96aaf422e";
+$VCPKG_COMMIT_SHA = "251741475900c9a57549d80f1b5a5e30e63d1887";
 
 # WZ Windows features (for vcpkg install)
 $VCPKG_INSTALL_FEATURES = @()
