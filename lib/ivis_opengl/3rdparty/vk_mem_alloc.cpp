3e676baec933522704c0590c41691c62a6f2b0fc
Adrian Bunk <bunk@debian.org>
2023-01-11 03:02:52 +0200

    vk_mem_alloc: Ignore -Wcast-align on GCC on all architectures
    The warning is not only on ARM, in Debian it has also appeared
    on mipsel/riscv64/sparc64.

--- lib/ivis_opengl/3rdparty/vk_mem_alloc.cpp
+++ lib/ivis_opengl/3rdparty/vk_mem_alloc.cpp
@@ -44,9 +44,7 @@
 # pragma GCC diagnostic ignored "-Wunused-variable"
 # pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 # pragma GCC diagnostic ignored "-Wmissing-noreturn"
-#  if (defined(__arm__) || defined(__thumb__)) && !defined(__aarch64__)
-#    pragma GCC diagnostic ignored "-Wcast-align"
-#  endif
+# pragma GCC diagnostic ignored "-Wcast-align"
 #elif defined(_MSC_VER)
 # pragma warning( push )
 # pragma warning( disable : 4189 ) // warning C4189: 'identifier' : local variable is initialized but not referenced
