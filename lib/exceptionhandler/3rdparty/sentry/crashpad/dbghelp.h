4d0fc97f185aa83cb0cffa434ecc81a7c1515cc2
past-due <30942300+past-due@users.noreply.github.com>
2023-02-03 13:12:41 -0500

    Update sentry-native mingw patches

--- lib/exceptionhandler/3rdparty/sentry/crashpad/dbghelp.h
+++ lib/exceptionhandler/3rdparty/sentry/crashpad/dbghelp.h
@@ -17,6 +17,7 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-include-next"
+#pragma clang diagnostic ignored "-Wzero-length-array"
 
 #include_next <dbghelp.h>
 #include <stdint.h>
