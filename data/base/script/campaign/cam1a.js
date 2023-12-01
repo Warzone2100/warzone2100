425cdb2713da27c1de3e827d7c1b096a44c668a4
KJeff01 <kjeff1997@gmail.com>
2023-12-01 15:27:37 -0600

    Revert moving artifacts on difficulty on Alpha 1
    In retrospect, this suggestion was really bad.

--- data/base/script/campaign/cam1a.js
+++ data/base/script/campaign/cam1a.js
@@ -209,24 +209,12 @@ function eventStartLevel()
 		},
 	});
 
-	if (difficulty >= HARD)
-	{
-		camSetArtifacts({
-			"base1ArtifactPos": { tech: ["R-Wpn-MG-Damage01", "R-Sys-Engineering01"] },
-			"base2Factory": { tech: ["R-Wpn-Flamer01Mk1", "R-Sys-MobileRepairTurret01"] },
-			"base3Factory": { tech: "R-Wpn-MG-Damage02" },
-			"base4Factory": { tech: "R-Wpn-MG-ROF01" },
-		});
-	}
-	else
-	{
-		camSetArtifacts({
-			"base1ArtifactPos": { tech: ["R-Wpn-MG-Damage01", "R-Sys-Engineering01"] },
-			"base2Factory": { tech: "R-Wpn-Flamer01Mk1" },
-			"base3Factory": { tech: ["R-Wpn-MG-Damage02", "R-Sys-MobileRepairTurret01"] },
-			"base4Factory": { tech: "R-Wpn-MG-ROF01" },
-		});
-	}
+	camSetArtifacts({
+		"base1ArtifactPos": { tech: ["R-Wpn-MG-Damage01", "R-Sys-Engineering01"] },
+		"base2Factory": { tech: ["R-Wpn-Flamer01Mk1", "R-Sys-MobileRepairTurret01"] },
+		"base3Factory": { tech: "R-Wpn-MG-Damage02" },
+		"base4Factory": { tech: "R-Wpn-MG-ROF01" },
+	});
 
 	camSetFactories({
 		"base2Factory": {
