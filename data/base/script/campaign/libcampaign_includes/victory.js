Warzone2100/warzone2100

commit 452e21bc50b79b9d854b974e4256d20ad96b56c6

Utilize the transport timer on Beta End (#3421)

src/wzapi.cpp
@@ -2526,30 +2526,29 @@ wzapi::no_return_value wzapi::setReinforcementTime(WZAPI_PARAMS(int _time))
 	              "The transport timer cannot be set to more than 1 hour!");
 	SCRIPT_ASSERT({}, context, selectedPlayer < MAX_PLAYERS, "Invalid selectedPlayer for current client: %" PRIu32 "", selectedPlayer);
 	mission.ETA = time;
-	if (missionCanReEnforce())
-	{
-		addTransporterTimerInterface();
-	}
+	
 	if (time < 0)
 	{
-		DROID *psDroid;
-
 		intRemoveTransporterTimer();
-		/* Only remove the launch if haven't got a transporter droid since the scripts set the
-		 * time to -1 at the between stage if there are not going to be reinforcements on the submap  */
-		for (psDroid = apsDroidLists[selectedPlayer]; psDroid != nullptr; psDroid = psDroid->psNext)
+	}
+	DROID* psDroid;
+
+	/* Search for a transport that is idle; if we can't find any, remove the launch button
+	 * since there's no transport to launch */
+	for (psDroid = apsDroidLists[selectedPlayer]; psDroid != nullptr; psDroid = psDroid->psNext)
+	{
+		if (isTransporter(psDroid) && !transporterFlying(psDroid))
 		{
-			if (isTransporter(psDroid))
-			{
-				break;
-			}
-		}
-		// if not found a transporter, can remove the launch button
-		if (psDroid ==  nullptr)
-		{
-			intRemoveTransporterLaunch();
+			break;
 		}
 	}
+	// Didn't find an idle transporter, we can remove the launch button
+	if (psDroid == nullptr)
+	{
+		intRemoveTransporterLaunch();
+	}
+	resetMissionWidgets();
+	addTransporterTimerInterface();
 	return {};
 }
 

data/base/script/campaign/libcampaign_includes/events.js
@@ -236,6 +236,11 @@ function cam_eventTransporterExit(transport)
 				setReinforcementTime(__camVictoryData.reinforcements);
 			}
 		}
+		// Show how long until the transporter comes back on Beta End.
+		if (__camWinLossCallback === CAM_VICTORY_TIMEOUT)
+		{
+			setReinforcementTime(__camVictoryData.reinforcements);
+		}
 	}
 
 	if (transport.player !== CAM_HUMAN_PLAYER ||
@@ -256,6 +261,14 @@ function cam_eventTransporterLanded(transport)
 	if (transport.player !== CAM_HUMAN_PLAYER)
 	{
 		__camLandTransporter(transport.player, camMakePos(transport));
+	}
+	else
+	{
+		// Make the transporter timer on Beta End disappear, since the transporter has arrived.
+		if (__camWinLossCallback === CAM_VICTORY_TIMEOUT)
+		{
+			setReinforcementTime(-1);
+		}
 	}
 }
 
@@ -355,6 +368,13 @@ function cam_eventGameLoaded()
 			}
 			break;
 		}
+	}
+
+	if (__camWinLossCallback === CAM_VICTORY_TIMEOUT
+		&& enumDroid(CAM_HUMAN_PLAYER, DROID_SUPERTRANSPORTER).length === 0)
+	{
+		// If the transport is gone on Beta End, put a timer up to show when it'll be back
+		setReinforcementTime(__camVictoryData.reinforcements);
 	}
 
 	//Subscribe to eventGroupSeen again.

src/mission.cpp
@@ -543,8 +543,8 @@ void addTransporterTimerInterface()
 		{
 			bAddInterface = true;
 
-			//check timer is not already on the screen
-			if (!widgGetFromID(psWScreen, IDTRANTIMER_BUTTON))
+			// Check that neither the timer nor the launch button are on screen
+			if (!widgGetFromID(psWScreen, IDTRANTIMER_BUTTON) && !widgGetFromID(psWScreen, IDTRANS_LAUNCH))
 			{
 				intAddTransporterTimer();
 			}
@@ -1434,11 +1434,8 @@ void endMission()
 		abort();
 	}
 
-	if (missionCanReEnforce()) //mission.type == LDS_MCLEAR || mission.type == LDS_MKEEP)
-	{
-		intRemoveMissionTimer();
-		intRemoveTransporterTimer();
-	}
+	intRemoveMissionTimer();
+	intRemoveTransporterTimer();
 
 	//at end of mission always do this
 	intRemoveTransporterLaunch();
@@ -3087,7 +3084,7 @@ void clearMissionWidgets()
 		intRemoveMissionTimer();
 	}
 
-	if (missionCanReEnforce())
+	if (mission.ETA >= 0)
 	{
 		intRemoveTransporterTimer();
 	}
@@ -3131,17 +3128,17 @@ void resetMissionWidgets()
 		stopMissionButtonFlash(IDTIMER_FORM);
 	}
 
-	if (missionCanReEnforce())
+	DROID* transporter = find_transporter();
+
+	// Check if not a reinforcement type mission and we have an transporter
+	if (!missionForReInforcements() && transporter != nullptr && !transporterFlying(transporter))
+	{
+		// Show launch button if the transporter has not already been launched
+		intAddTransporterLaunch(transporter);
+	}
+	else if (mission.ETA >= 0)
 	{
 		addTransporterTimerInterface();
-	}
-	//check not a typical reinforcement mission
-	else if (!missionForReInforcements())
-	{
-		if (auto transporter = find_transporter())
-		{
-			intAddTransporterLaunch(transporter);
-		}
 	}
 
 }

src/order.cpp
@@ -484,11 +484,11 @@ void orderUpdateDroid(DROID *psDroid)
 				}
 				else
 				{
-					//the script can call startMission for this callback for offworld missions
-					triggerEvent(TRIGGER_TRANSPORTER_EXIT, psDroid);
 					/* clear order */
 					psDroid->order = DroidOrder(DORDER_NONE);
 				}
+				//the script can call startMission for this callback for offworld missions (if we want to change level)
+				triggerEvent(TRIGGER_TRANSPORTER_EXIT, psDroid);
 
 				psDroid->sMove.speed = 0; // Prevent radical movement vector when adjusting from home to away map exit and entry coordinates.
 			}
@@ -525,6 +525,7 @@ void orderUpdateDroid(DROID *psDroid)
 			    missionDroidsRemaining(selectedPlayer))
 			{
 				resetTransporter();
+				triggerEvent(TRIGGER_TRANSPORTER_LANDED, psDroid);
 			}
 			else
 			{

src/transporter.cpp
@@ -336,8 +336,9 @@ bool intAddTransporterLaunch(DROID *psDroid)
 	//set up the static transporter
 	psCurrTransporter = psDroid;
 
-	//check the button is not already up
-	if (widgGetFromID(psWScreen, IDTRANS_LAUNCH) != nullptr)
+	// Check that neither the launch button nor the transport timer are currently up
+	if (widgGetFromID(psWScreen, IDTRANS_LAUNCH) != nullptr
+		|| widgGetFromID(psWScreen, IDTRANTIMER_BUTTON) != nullptr)
 	{
 		return true;
 	}

commit 10adfcfe28e4c8aac21badd3051848d4233a8fb5

Prevent launch button disappearing on LZ compromise
A little regression from 452e21bc50b79b9d854b974e4256d20ad96b56c6.

data/base/script/campaign/libcampaign_includes/victory.js
@@ -452,7 +452,7 @@ function __camVictoryOffworld()
 		//Protect against early access to reinforcements GUI if it shouldn't be available yet
 		if (__camVictoryData.reinforcements >= 0)
 		{
-			setReinforcementTime(LZ_COMPROMISED_TIME);
+			setReinforcementTime(LZ_COMPROMISED_TIME, false);
 		}
 		if (__camLZCompromisedTicker === 0)
 		{
@@ -474,7 +474,7 @@ function __camVictoryOffworld()
 		camTrace("LZ clear");
 		const pos = camMakePos(lz);
 		playSound(cam_sounds.lz.LZClear, pos.x, pos.y, 0);
-		setReinforcementTime(__camVictoryData.reinforcements);
+		setReinforcementTime(__camVictoryData.reinforcements, false);
 		__camLZCompromisedTicker = 0;
 		if (__camRTLZTicker === 0)
 		{

src/quickjs_backend.cpp
@@ -3488,7 +3488,7 @@ bool quickjs_scripting_instance::registerFunctions(const std::string& scriptName
 	JS_REGISTER_FUNC(applyLimitSet, 0); // WZAPI
 	JS_REGISTER_FUNC(setMissionTime, 1); // WZAPI
 	JS_REGISTER_FUNC(getMissionTime, 0); // WZAPI
-	JS_REGISTER_FUNC(setReinforcementTime, 1); // WZAPI
+	JS_REGISTER_FUNC2(setReinforcementTime, 1, 2); // WZAPI
 	JS_REGISTER_FUNC2(completeResearch, 1, 3); // WZAPI
 	JS_REGISTER_FUNC2(completeAllResearch, 0, 1); // WZAPI
 	JS_REGISTER_FUNC2(enableResearch, 1, 2); // WZAPI

src/wzapi.cpp
@@ -2511,7 +2511,7 @@ int wzapi::getMissionTime(WZAPI_NO_PARAMS)
 	return (mission.time - (gameTime - mission.startTime)) / GAME_TICKS_PER_SEC;
 }
 
-//-- ## setReinforcementTime(time)
+//-- ## setReinforcementTime(time[, removeLaunch])
 //--
 //-- Set time for reinforcements to arrive. If time is negative, the reinforcement GUI
 //-- is removed and the timer stopped. Time is in seconds.
@@ -2519,30 +2519,32 @@ int wzapi::getMissionTime(WZAPI_NO_PARAMS)
 //-- is set to "--:--" and reinforcements are suppressed until this function is called
 //-- again with a regular time value.
 //--
-wzapi::no_return_value wzapi::setReinforcementTime(WZAPI_PARAMS(int _time))
+wzapi::no_return_value wzapi::setReinforcementTime(WZAPI_PARAMS(int _time, optional<bool> _removeLaunch))
 {
 	int time = _time * GAME_TICKS_PER_SEC;
-	SCRIPT_ASSERT({}, context, time == LZ_COMPROMISED_TIME || time < 60 * 60 * GAME_TICKS_PER_SEC,
-	              "The transport timer cannot be set to more than 1 hour!");
+	bool removeLaunch = _removeLaunch.value_or(true);
+	SCRIPT_ASSERT({}, context, time == LZ_COMPROMISED_TIME || time < 60 * 60 * GAME_TICKS_PER_SEC, "The transport timer cannot be set to more than 1 hour!");
 	SCRIPT_ASSERT({}, context, selectedPlayer < MAX_PLAYERS, "Invalid selectedPlayer for current client: %" PRIu32 "", selectedPlayer);
 	mission.ETA = time;
-	
 	if (time < 0)
 	{
 		intRemoveTransporterTimer();
 	}
-	/* Search for a transport that is idle; if we can't find any, remove the launch button
-	 * since there's no transport to launch */
-	auto droidIt = std::find_if(apsDroidLists[selectedPlayer].begin(), apsDroidLists[selectedPlayer].end(), [](DROID* d)
+	if (removeLaunch)
 	{
-		return isTransporter(d) && !transporterFlying(d);
-	});
-	DROID* psDroid = droidIt != apsDroidLists[selectedPlayer].end() ? *droidIt : nullptr;
+		/* Search for a transport that is idle; if we can't find any, remove the launch button
+		 * since there's no transport to launch */
+		auto droidIt = std::find_if(apsDroidLists[selectedPlayer].begin(), apsDroidLists[selectedPlayer].end(), [](DROID* d)
+		{
+			return isTransporter(d) && !transporterFlying(d);
+		});
+		DROID* psDroid = droidIt != apsDroidLists[selectedPlayer].end() ? *droidIt : nullptr;
 
-	// Didn't find an idle transporter, we can remove the launch button
-	if (psDroid == nullptr)
-	{
-		intRemoveTransporterLaunch();
+		// Didn't find an idle transporter, we can remove the launch button
+		if (psDroid == nullptr)
+		{
+			intRemoveTransporterLaunch();
+		}
 	}
 	resetMissionWidgets();
 	addTransporterTimerInterface();

src/wzapi.h
@@ -1075,7 +1075,7 @@ namespace wzapi
 	bool applyLimitSet(WZAPI_NO_PARAMS);
 	no_return_value setMissionTime(WZAPI_PARAMS(int _time));
 	int getMissionTime(WZAPI_NO_PARAMS);
-	no_return_value setReinforcementTime(WZAPI_PARAMS(int _time));
+	no_return_value setReinforcementTime(WZAPI_PARAMS(int _time, optional<bool> _removeLaunch));
 	no_return_value completeResearch(WZAPI_PARAMS(std::string researchName, optional<int> _player, optional<bool> _forceResearch));
 	no_return_value completeAllResearch(WZAPI_PARAMS(optional<int> _player));
 	bool enableResearch(WZAPI_PARAMS(std::string researchName, optional<int> _player));