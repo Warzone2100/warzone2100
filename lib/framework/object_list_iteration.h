e9525478c74f4e56745f53f9a3eb9ebb3640c3a3
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2024-01-21 17:50:01 +0300

    object_list_iteration: allow passing list iterator to `mutating_list_iterate`
    Use a compile-time helper with some SFINAE tricks
    to allow passing callables which take an iterator
    instead of a pointer type as the argument.
    
    This can be quite convenient when one needs to
    erase from or insert into the list being iterated
    directly inside the handler's body, avoiding
    additional lookups to obtain an iterator to
    the current element.
    
    The SFINAE function signature checks are performed
    using the following trick:
    
    	std::is_convertible<Callable, std::function<Sig>::value
    
    This is based on the ability of raw function
    pointers and lambda expressions to be converted
    to the `std::function` type with a matching
    signature.
    
    Paired with a `std::enable_if_t`, this allows
    to easily compile-time-choose the correct overload
    of the `Invoke` helper function, which would call
    the handler and pass the correct argument to it.
    
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

--- lib/framework/object_list_iteration.h
+++ lib/framework/object_list_iteration.h
@@ -31,21 +31,81 @@ enum class IterationResult
 	CONTINUE_ITERATION
 };
 
+/// <summary>
+/// Utility class used to determine how to invoke `Callable`
+/// in the `mutating_list_iterate` function depending on its signature.
+///
+/// Currently two callable signatures are supported:
+/// * `IterationResult(ObjectType*)`
+/// * `IterationResult(std::list<ObjectType*>::iterator)`
+///
+/// The latter overload is convenient when one needs to erase from or
+/// insert into the list being iterated directly inside the handler's body,
+/// avoiding additional lookups to obtain an iterator to the current element.
+/// </summary>
+/// <typeparam name="Callable">
+/// Can either be a function pointer or a lambda expression (or any other callable).
+/// </typeparam>
+template <typename Callable>
+struct LoopBodyHandlerCallStrategy
+{
+	// Since we are constrained by C++14, we'll need to use SFINAE to determine correct
+	// specialization of `Invoke` to call depending on input type of handler function.
+	//
+	// Here we use a bunch of compile-time tests to determine
+	// whether our callable is convertible to `std::function` of a compatible signature.
+	//
+	// This is the most simple way to constrain and choose a correct overload
+	// of `Invoke` function depending on the callable signature.
+	//
+	// Choosing the correct overload is done via `std::enable_if<Cond, T>` helper from <type_traits>,
+	// which basically provides `using type = T` only when `Cond` is `true`.
+	template <typename ObjectType>
+	static constexpr bool handler_accepts_ptr = std::is_convertible<
+		Callable,
+		std::function<IterationResult(ObjectType*)>>::value;
+	template <typename ObjectType>
+	static constexpr bool handler_accepts_iter = std::is_convertible<
+		Callable,
+		std::function<IterationResult(std::list<ObjectType*>::iterator)>>::value;
+
+	// `Invoke` overload for Callable taking a list iterator as the argument
+	template <typename ObjectType>
+	static std::enable_if_t<handler_accepts_iter<ObjectType>, IterationResult>
+		Invoke(Callable handler, typename std::list<ObjectType*>::iterator iter)
+	{
+		return handler(iter);
+	}
+
+	// `Invoke` overload for Callable taking a pointer to `ObjectType` as the argument
+	template <typename ObjectType>
+	static std::enable_if_t<handler_accepts_ptr<ObjectType>, IterationResult>
+		Invoke(Callable handler, typename std::list<ObjectType*>::iterator iter)
+	{
+		return handler(*iter);
+	}
+};
+
 // Common iteration helper for lists of game objects
 // with an ability to execute loop body handlers which can
 // possibly invalidate the any iterator in the range `[begin(), currentIterator]`.
 template <typename ObjectType, typename MaybeErasingLoopBodyHandler>
 void mutating_list_iterate(std::list<ObjectType*>& list, MaybeErasingLoopBodyHandler handler)
 {
-	static_assert(std::is_same<std::result_of_t<MaybeErasingLoopBodyHandler(ObjectType*)>, IterationResult>::value,
-		"Loop body handler should return IterationResult");
+	using HandlerCallStrategy = LoopBodyHandlerCallStrategy<MaybeErasingLoopBodyHandler>;
+
+	static_assert(
+		   HandlerCallStrategy::handler_accepts_ptr<ObjectType>
+		|| HandlerCallStrategy::handler_accepts_iter<ObjectType>,
+		"Unsupported loop body handler signature: "
+		"should return IterationResult and take either an ObjectType* or an iterator");
 
 	typename std::remove_reference_t<decltype(list)>::iterator it = list.begin(), itNext;
 	while (it != list.end())
 	{
 		itNext = std::next(it);
 		// Can possibly invalidate `it` and anything before it.
-		const auto res = handler(*it);
+		const auto res = HandlerCallStrategy::Invoke<ObjectType>(handler, it);
 		if (res == IterationResult::BREAK_ITERATION)
 		{
 			break;
