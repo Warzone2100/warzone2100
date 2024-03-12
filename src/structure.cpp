310943d244e2dd36b8abd1a1b3d097049565141f
Monsterovich <monsterovich@gmail.com>
2024-03-12 03:57:06 +0200

    Give SelectNewDroid a better name and description

--- src/structure.cpp
+++ src/structure.cpp
@@ -2485,7 +2485,7 @@ static bool structPlaceDroid(STRUCTURE *psStructure, DROID_TEMPLATE *psTempl, DR
 			{
 				psNewDroid->group = psStructure->productToGroup;
 				intGroupsChanged(psNewDroid->group); // update groups UI
-				SelectNewDroid(psNewDroid);
+				SelectGroupDroid(psNewDroid);
 			}
 			else
 			{
