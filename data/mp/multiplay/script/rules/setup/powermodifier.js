8444976c56d78e1a36db7d7b02c3853c6df7f9dc
Arithyce <108166240+Arithyce@users.noreply.github.com>
2024-04-07 07:42:13 -0400

    Update powermodifier.js refactor
    Extend the modding help info.

--- data/mp/multiplay/script/rules/setup/powermodifier.js
+++ data/mp/multiplay/script/rules/setup/powermodifier.js
@@ -1,21 +1,40 @@
-/* powerType is as follows:
-powerType = 0 for Low Power
-powerType = 1 for Medium Power
-powerType = 2 for High Power
-
+/*
 To change how much power each Power Level setting gives,
 adjust the values 85 and 20. 85 is the base value, while 20 is
 how much it changes between Power Level settings.
 
+The math `85 + 20 * powerType`,
+is interpereted in this way:
+Low Power = 85,
+Medium Power = 105,
+High Power = 125.
+
+The variable powerType is as follows:
+powerType = 0 for Low Power,
+powerType = 1 for Medium Power,
+powerType = 2 for High Power,
+powerType is a variable from the source code,
+and is set when a Power Level setting is selected by the player,
+please do not attempt to modify it.
+
 To change the difficulty factor of bots,
 adjust the last number in the bot section below.
+
+The difficulty factor is interpereted in this way:
+Easy = -25% power per second (0.75x),
+Medium = +10% power per second (1.1x),
+Hard = +50% power per second (1.5x),
+Insane = +100% power per second (2x).
 */
 function setupPowerModifier(player)
 {
+	// This sets the player(s) power modifier,
+	// it also sets the bots power modifier briefly,
+	// but that is quickly overwritten in the bot section below.
 	setPowerModifier(85 + 20 * powerType, player);
 
-	// the following sets the power modifier for bots.
-	// insane difficulty is meant to be insane...
+	// The following sets the power modifier for bots.
+	// Insane difficulty is meant to be insane...
 	if (playerData[player].difficulty === INSANE)
 	{
 		setPowerModifier((85 + 20 * powerType) * 2, player);
