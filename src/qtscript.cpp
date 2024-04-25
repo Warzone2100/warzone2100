e3d8d3de246044bb4b61c15cb214772e645896e0
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2024-04-25 11:40:24 +0000

    Fix false-positive warnings from GCC14 when building with optimizations on Fedora 40
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

--- src/qtscript.cpp
+++ src/qtscript.cpp
@@ -176,7 +176,16 @@ scripting_engine::timerNode::timerNode(timerNode&& rhs)           // move constr
 
 void scripting_engine::timerNode::swap(timerNode& _rhs)
 {
+// On Fedora 40, GCC 14 produces false-positive warnings for -Wuninitialized
+// when compiling this code with optimizations. Silence these warnings.
+#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__) && __GNUC__ >= 14 && defined(__OPTIMIZE__)
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wuninitialized"
+#endif
 	std::swap(timerID, _rhs.timerID);
+#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__) && __GNUC__ >= 14 && defined(__OPTIMIZE__)
+# pragma GCC diagnostic pop
+#endif
 	std::swap(function, _rhs.function);
 	std::swap(timerName, _rhs.timerName);
 	std::swap(instance, _rhs.instance);
