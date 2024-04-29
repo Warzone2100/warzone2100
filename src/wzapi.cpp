7cec2a6263719ace87900f570dabfadb7370b574
past-due <30942300+past-due@users.noreply.github.com>
2024-04-29 12:21:45 -0400

    No longer treat "GENERIC" struct type as Wall (for upgrades)
    Instead, support targeting GENERIC separately with:
    
    "filterParameter": "Type",
    "filterValue": "Generic"

--- src/wzapi.cpp
+++ src/wzapi.cpp
@@ -4391,9 +4391,13 @@ nlohmann::json wzapi::constructStatsObject()
 			nlohmann::json strct = nlohmann::json::object();
 			strct["Id"] = psStats->id;
 			if (psStats->type == REF_DEFENSE || psStats->type == REF_WALL || psStats->type == REF_WALLCORNER
-			    || psStats->type == REF_GENERIC || psStats->type == REF_GATE || psStats->type == REF_FORTRESS)
+			    || psStats->type == REF_GATE || psStats->type == REF_FORTRESS)
 			{
 				strct["Type"] = "Wall";
+			}
+			else if (psStats->type == REF_GENERIC)
+			{
+				strct["Type"] = "Generic";
 			}
 			else if (psStats->type != REF_DEMOLISH)
 			{
