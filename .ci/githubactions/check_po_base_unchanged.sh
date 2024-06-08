e5ff72d13280640024448fc29f29f267fcf4f7ae
past-due <30942300+past-due@users.noreply.github.com>
2024-06-08 16:46:16 -0400

    [GitHub Actions] Cleanup untranslated .po files

--- .ci/githubactions/check_po_base_unchanged.sh
+++ .ci/githubactions/check_po_base_unchanged.sh
@@ -2,14 +2,14 @@
 
 # Expects input of $1 base file and $2 .po file
 
-diff -q --ignore-trailing-space --ignore-blank-lines \
+diff --ignore-trailing-space --ignore-blank-lines \
      --ignore-matching-lines='^# .*' \
      --ignore-matching-lines='^#$' \
      --ignore-matching-lines='^#, fuzzy$' \
      --ignore-matching-lines='^".*\:.*"' \
-     "$1" "$2" &>/dev/null
-result=$?
+     "$1" "$2" >/dev/null 2>&1
 
+result=$?
 if [ $result -eq 0 ]; then
   printf '%s\0' "$2"
 fi
