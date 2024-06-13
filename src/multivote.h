68e23da459aae75d2e03be7907c552338ee6c807
past-due <30942300+past-due@users.noreply.github.com>
2024-06-13 12:09:59 -0400

    Fix: startKickVote when triggered by spectator host

--- src/multivote.h
+++ src/multivote.h
@@ -21,6 +21,9 @@
 #define __INCLUDED_WZ_MULTI_VOTE_H__
 
 #include <cstdint>
+#include <nonstd/optional.hpp>
+using nonstd::optional;
+using nonstd::nullopt;
 
 void resetLobbyChangeVoteData();
 void resetLobbyChangePlayerVote(uint32_t player);
@@ -29,7 +32,7 @@ uint8_t getLobbyChangeVoteTotal();
 void sendLobbyChangeVoteData(uint8_t currentVote);
 
 void resetKickVoteData();
-bool startKickVote(uint32_t target_player_id);
+bool startKickVote(uint32_t target_player_id, optional<uint32_t> requester_player_id = nullopt);
 void processPendingKickVotes();
 void cancelOrDismissKickVote(uint32_t targetPlayer);
 
