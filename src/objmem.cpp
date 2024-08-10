97fc9e5a67186abfcf139088c507b62e061e0f7d
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2024-08-10 16:19:12 +0300

    objmem: remove `audio_RemoveObj` call from the dtors of the common in-game objects
    The call to `audio_RemoveObj` inside the destructors of
    `SIMPLE_OBJECT`, `DROID`, `STRUCTURE`, `FEATURE` and `PROJECTILE`
    classes can possibly invoke undefined behavior if the
    warzone process is being forcefully shut down.
    
    The reason for this is that by the time a given game object
    is being destroyed, chances are that the static objects
    from the audio code, which are being accessed by the
    `audio_RemoveObj()`, can already be destroyed (due to the inverse
    of the [Static initialization order fiasco](https://en.cppreference.com/w/cpp/language/siof)).
    
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

--- src/objmem.cpp
+++ src/objmem.cpp
@@ -29,6 +29,7 @@
 #include "objects.h"
 #include "lib/gamelib/gtime.h"
 #include "lib/netplay/sync_debug.h"
+#include "lib/sound/audio.h"
 #include "hci.h"
 #include "map.h"
 #include "power.h"
@@ -85,12 +86,23 @@ bool objmemInitialise()
 	return true;
 }
 
+template<typename T>
+static void objMemShutdownContainerImpl(T& container)
+{
+	for (const auto& entity : container)
+	{
+		// Make sure to get rid of some final references in the sound code to this object first
+		audio_RemoveObj(&entity);
+	}
+	container.clear();
+}
+
 /* Release the object heaps */
 void objmemShutdown()
 {
-	GlobalDroidContainer().clear();
-	GlobalStructContainer().clear();
-	GlobalFeatureContainer().clear();
+	objMemShutdownContainerImpl(GlobalDroidContainer());
+	objMemShutdownContainerImpl(GlobalStructContainer());
+	objMemShutdownContainerImpl(GlobalFeatureContainer());
 }
 
 static const char* objTypeToStr(OBJECT_TYPE type)
@@ -266,6 +278,10 @@ bool objmemDestroy(BASE_OBJECT *psObj, bool checkRefs)
 	default:
 		ASSERT(!"unknown object type", "unknown object type in destroyed list at 0x%p", static_cast<void *>(psObj));
 	}
+
+	// Make sure to get rid of some final references in the sound code to this object first
+	audio_RemoveObj(psObj);
+
 	if (checkRefs && !checkReferences(psObj))
 	{
 		return false;
