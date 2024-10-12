3943cd4f33cb35c6706797ec14d3be29c1a1f012
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2024-10-12 21:00:19 +0300

    netplay: introduce custom error category for locale-indepdendent translation of network errors
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

---
+++ lib/netplay/error_categories.cpp
@@ -0,0 +1,110 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2024  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "error_categories.h"
+
+#include "lib/framework/wzglobal.h"
+
+#ifdef WZ_OS_WIN
+# include <winerror.h>
+# include <ws2tcpip.h>
+#elif defined(WZ_OS_UNIX)
+# include <string.h>
+# include <netdb.h>
+#endif
+
+std::string GenericSystemErrorCategory::message(int ev) const
+{
+#if defined(WZ_OS_WIN)
+	switch (ev)
+	{
+	case 0:                     return "No error";
+	case WSAEINTR:              return "Interrupted system call";
+	case WSAEBADF:              return "Bad file number";
+	case WSAEACCES:             return "Permission denied";
+	case WSAEFAULT:             return "Bad address";
+	case WSAEINVAL:             return "Invalid argument";
+	case WSAEMFILE:             return "Too many open sockets";
+	case WSAEWOULDBLOCK:        return "Operation would block";
+	case WSAEINPROGRESS:        return "Operation now in progress";
+	case WSAEALREADY:           return "Operation already in progress";
+	case WSAENOTSOCK:           return "Socket operation on non-socket";
+	case WSAEDESTADDRREQ:       return "Destination address required";
+	case WSAEMSGSIZE:           return "Message too long";
+	case WSAEPROTOTYPE:         return "Protocol wrong type for socket";
+	case WSAENOPROTOOPT:        return "Bad protocol option";
+	case WSAEPROTONOSUPPORT:    return "Protocol not supported";
+	case WSAESOCKTNOSUPPORT:    return "Socket type not supported";
+	case WSAEOPNOTSUPP:         return "Operation not supported on socket";
+	case WSAEPFNOSUPPORT:       return "Protocol family not supported";
+	case WSAEAFNOSUPPORT:       return "Address family not supported";
+	case WSAEADDRINUSE:         return "Address already in use";
+	case WSAEADDRNOTAVAIL:      return "Can't assign requested address";
+	case WSAENETDOWN:           return "Network is down";
+	case WSAENETUNREACH:        return "Network is unreachable";
+	case WSAENETRESET:          return "Net connection reset";
+	case WSAECONNABORTED:       return "Software caused connection abort";
+	case WSAECONNRESET:         return "Connection reset by peer";
+	case WSAENOBUFS:            return "No buffer space available";
+	case WSAEISCONN:            return "Socket is already connected";
+	case WSAENOTCONN:           return "Socket is not connected";
+	case WSAESHUTDOWN:          return "Can't send after socket shutdown";
+	case WSAETOOMANYREFS:       return "Too many references, can't splice";
+	case WSAETIMEDOUT:          return "Connection timed out";
+	case WSAECONNREFUSED:       return "Connection refused";
+	case WSAELOOP:              return "Too many levels of symbolic links";
+	case WSAENAMETOOLONG:       return "File name too long";
+	case WSAEHOSTDOWN:          return "Host is down";
+	case WSAEHOSTUNREACH:       return "No route to host";
+	case WSAENOTEMPTY:          return "Directory not empty";
+	case WSAEPROCLIM:           return "Too many processes";
+	case WSAEUSERS:             return "Too many users";
+	case WSAEDQUOT:             return "Disc quota exceeded";
+	case WSAESTALE:             return "Stale NFS file handle";
+	case WSAEREMOTE:            return "Too many levels of remote in path";
+	case WSASYSNOTREADY:        return "Network system is unavailable";
+	case WSAVERNOTSUPPORTED:    return "Winsock version out of range";
+	case WSANOTINITIALISED:     return "WSAStartup not yet called";
+	case WSAEDISCON:            return "Graceful shutdown in progress";
+	case WSAHOST_NOT_FOUND:     return "Host not found";
+	case WSANO_DATA:            return "No host data of that type was found";
+	default:                    return "Unknown error";
+	}
+#elif defined(WZ_OS_UNIX)
+	return strerror(ev);
+#endif
+}
+
+std::error_condition GenericSystemErrorCategory::default_error_condition(int ev) const noexcept
+{
+	// Try to map the raw error values either to POSIX or Windows error codes (depending on the OS).
+	// The default system category should capture them all well.
+	return std::system_category().default_error_condition(ev);
+}
+
+const std::error_category& generic_system_error_category()
+{
+	static GenericSystemErrorCategory instance;
+	return instance;
+}
+
+std::error_code make_network_error_code(int ev)
+{
+	return { ev, generic_system_error_category() };
+}
