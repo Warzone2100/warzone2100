adcadc8a5ce709e72fc53997ce5cb56c9540930d
past-due <30942300+past-due@users.noreply.github.com>
2024-10-23 14:48:54 -0400

    Improve HQ terrain water
    Co-Authored-By: MaNGusT- <bart2@bk.ru>

--- data/WZPrebuiltPackages.cmake
+++ data/WZPrebuiltPackages.cmake
@@ -2,5 +2,5 @@
 # Prebuilt package DL info
 
 # terrain_overrides/high.wz
-set(WZ_PREBUILT_TERRAIN_HIGH_DL_URL "https://github.com/Warzone2100/data-terrain-high/releases/download/v1/high.wz")
-set(WZ_PREBUILT_TERRAIN_HIGH_DL_SHA512 "44a39e585344b25ab0fa4ed769f2b48bf23a0e4fc4b23e0fab4e68643cc6e3ebeabba790e8b1e6c96493ad01b6a2f09e92b893063013077dcac2475f0b5373d6")
+set(WZ_PREBUILT_TERRAIN_HIGH_DL_URL "https://github.com/Warzone2100/data-terrain-high/releases/download/v2/high.wz")
+set(WZ_PREBUILT_TERRAIN_HIGH_DL_SHA512 "c96ee757c6771ed7cfd104bf4d86bf80a6ce9f1598a16bd26a711fcf2359ae7b13d3812d9cbe64ff6d1e9d55ed7f28c72a150e1c755cc595928f893029802570")
