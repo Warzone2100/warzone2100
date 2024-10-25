a784526cb1b13f44d3f8f37133f301dda06d9c3d
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2024-10-25 14:55:33 +0300

    netplay: introduce abstractions for client/server-side sockets and connection providers
    This patch introduces multiple high-level abstractions over raw low-level
    sockets, which are necessary for supporting network backends other
    than default legacy `TCP_DIRECT` implementation:
    
    1. `WzConnectionProvider` - abstracts the way WZ establishes
       server-side and client-side connections. This thing effectively
       provides usable listen sockets and client connections to work with,
       hence the name.
    2. `IListenSocket` - abstraction over listen sockets.
    3. `IClientConnection` - abstraction over client-side sockets (and
       also server-side connections to the game clients).
    4. `IConnectionPollGroup` - generalization of socket sets for polling
       multiple connections in one go.
    5. `ConnectionProviderRegistry` - trivial singleton class providing
       storage for connection providers.
    6. `ConnectionAddress` - opaque connection address object, aimed
       to replace direct uses of `addrinfo` and provide a bit more
       abstract way to represent connection credentials. Still looks like
       a crutch right now, but it's better than nothing, nonetheless.
    
    The existing implementation in `netplay/netsocket.h(.cpp)` has been
    moved to the `tcp` subfolder and wrapped entirely into the `tcp`
    namespace.
    
    The patch provides `TCP*`-prefixed implementations of the base
    interfaces mentioned above, which are implemented in terms of the
    old `netsocket` code.
    
    There's now a `ConnectionProviderType::TCP_DIRECT` enumeration
    descriptor for accessing the default connection provider.
    
    All uses in the high-level code (`netplay.cpp`, `joiningscreen.cpp`)
    are amended appropriately to use the all-new high-level abstractions
    instead of old low-level tcp-specific `Socket` and `SocketSet`.
    
    NOTE: there are still a few functions from the `tcp::` namespace
    used directly in the Discord RPC integration code, but these
    shouldn't pose any problem to either extract these into a more generic
    abstraction layer or to be rewritten not to use these functions at all,
    because they don't actually use any low-level stuff that's hard to
    refactor.
    
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

---
+++ lib/netplay/tcp/tcp_connection_poll_group.cpp
@@ -0,0 +1,60 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2024  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "lib/netplay/tcp/tcp_connection_poll_group.h"
+#include "lib/netplay/tcp/tcp_client_connection.h"
+#include "lib/netplay/tcp/netsocket.h"
+#include "lib/framework/wzapp.h"
+#include "lib/framework/debug.h"
+
+namespace tcp
+{
+
+TCPConnectionPollGroup::TCPConnectionPollGroup(SocketSet* sset)
+	: sset_(sset)
+{}
+
+TCPConnectionPollGroup::~TCPConnectionPollGroup()
+{
+	if (sset_)
+	{
+		deleteSocketSet(sset_);
+	}
+}
+
+int TCPConnectionPollGroup::checkSockets(unsigned timeout)
+{
+	return tcp::checkSockets(*sset_, timeout);
+}
+
+void TCPConnectionPollGroup::add(IClientConnection* conn)
+{
+	auto* tcpConn = dynamic_cast<TCPClientConnection*>(conn);
+	ASSERT_OR_RETURN(, tcpConn != nullptr, "Expected to have TCPClientConnection instance");
+	SocketSet_AddSocket(*sset_, tcpConn->socket_);
+}
+
+void TCPConnectionPollGroup::remove(IClientConnection* conn)
+{
+	auto tcpConn = dynamic_cast<TCPClientConnection*>(conn);
+	ASSERT_OR_RETURN(, tcpConn != nullptr, "Expected to have TCPClientConnection instance");
+	SocketSet_DelSocket(*sset_, tcpConn->socket_);
+}
+
+} // namespace tcp
---
+++ lib/netplay/tcp/tcp_connection_provider.cpp
@@ -0,0 +1,153 @@
+/*
+	This file is part of Warzone 2100.
+	Copyright (C) 2024  Warzone 2100 Project
+
+	Warzone 2100 is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Warzone 2100 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Warzone 2100; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "tcp_connection_provider.h"
+
+#include "lib/netplay/tcp/netsocket.h"
+#include "lib/netplay/tcp/tcp_connection_poll_group.h"
+#include "lib/netplay/tcp/tcp_client_connection.h"
+#include "lib/netplay/tcp/tcp_listen_socket.h"
+
+#include "lib/netplay/open_connection_result.h"
+#include "lib/framework/wzapp.h"
+
+namespace tcp
+{
+
+void TCPConnectionProvider::initialize()
+{
+	SOCKETinit();
+}
+
+void TCPConnectionProvider::shutdown()
+{
+	SOCKETshutdown();
+}
+
+net::result<ConnectionAddress> TCPConnectionProvider::resolveHost(const char* host, uint16_t port)
+{
+	return ConnectionAddress::parse(host, port);
+}
+
+net::result<IListenSocket*> TCPConnectionProvider::openListenSocket(uint16_t port)
+{
+	auto res = tcp::socketListen(port);
+	if (!res.has_value())
+	{
+		return tl::make_unexpected(res.error());
+	}
+	return new TCPListenSocket(res.value());
+}
+
+net::result<IClientConnection*> TCPConnectionProvider::openClientConnectionAny(const ConnectionAddress& addr, unsigned timeout)
+{
+	const auto* rawAddr = addr.asRawSocketAddress();
+	auto res = socketOpenAny(rawAddr, timeout);
+	if (!res.has_value())
+	{
+		return tl::make_unexpected(res.error());
+	}
+	return new TCPClientConnection(res.value());
+}
+
+namespace
+{
+
+OpenConnectionResult socketOpenTCPConnectionSync(const char* host, uint32_t port)
+{
+	const auto hostsResult = resolveHost(host, port);
+	SocketAddress* hosts = hostsResult.value_or(nullptr);
+	if (hosts == nullptr)
+	{
+		const auto hostsErr = hostsResult.error();
+		const auto hostsErrMsg = hostsErr.message();
+		return OpenConnectionResult(hostsErr, astringf("Cannot resolve host \"%s\": [%d]: %s", host, hostsErr.value(), hostsErrMsg.c_str()));
+	}
+
+	auto sockResult = socketOpenAny(hosts, 15000);
+	Socket* client_transient_socket = sockResult.value_or(nullptr);
+	deleteSocketAddress(hosts);
+	hosts = nullptr;
+
+	if (client_transient_socket == nullptr)
+	{
+		const auto errValue = sockResult.error();
+		const auto errMsg = errValue.message();
+		return OpenConnectionResult(errValue, astringf("Cannot connect to [%s]:%d, [%d]:%s", host, port, errValue.value(), errMsg.c_str()));
+	}
+
+	return OpenConnectionResult(new TCPClientConnection(client_transient_socket));
+}
+
+struct OpenConnectionRequest
+{
+	std::string host;
+	uint32_t port = 0;
+	OpenConnectionToHostResultCallback callback;
+};
+
+static int openDirectTCPConnectionAsyncImpl(void* data)
+{
+	OpenConnectionRequest* pRequestInfo = (OpenConnectionRequest*)data;
+	if (!pRequestInfo)
+	{
+		return 1;
+	}
+
+	pRequestInfo->callback(socketOpenTCPConnectionSync(pRequestInfo->host.c_str(), pRequestInfo->port));
+	delete pRequestInfo;
+	return 0;
+}
+
+} // anonymous namespace
+
+bool TCPConnectionProvider::openClientConnectionAsync(const std::string& host, uint32_t port, OpenConnectionToHostResultCallback callback)
+{
+	// spawn background thread to handle this
+	auto pRequest = new OpenConnectionRequest();
+	pRequest->host = host;
+	pRequest->port = port;
+	pRequest->callback = callback;
+
+	WZ_THREAD* pOpenConnectionThread = wzThreadCreate(openDirectTCPConnectionAsyncImpl, pRequest);
+	if (pOpenConnectionThread == nullptr)
+	{
+		debug(LOG_ERROR, "Failed to create thread for opening connection");
+		delete pRequest;
+		return false;
+	}
+
+	wzThreadDetach(pOpenConnectionThread);
+	// the thread handles deleting pRequest
+	pOpenConnectionThread = nullptr;
+
+	return true;
+}
+
+IConnectionPollGroup* TCPConnectionProvider::newConnectionPollGroup()
+{
+	auto* sset = allocSocketSet();
+	if (!sset)
+	{
+		return nullptr;
+	}
+	return new TCPConnectionPollGroup(sset);
+}
+
+} // namespace tcp
