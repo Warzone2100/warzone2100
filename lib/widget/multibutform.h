e024010d5d81cadba53e6d94823c2fb4cce40e50
past-due <30942300+past-due@users.noreply.github.com>
2025-01-20 21:40:12 -0500

    MultibuttonWidget: Additional methods

--- lib/widget/multibutform.h
+++ lib/widget/multibutform.h
@@ -51,7 +51,10 @@ public:
 	virtual int32_t idealHeight() override;
 
 	void setLabel(char const *text);
+	void setLabel(const std::shared_ptr<WIDGET>& widget);
 	void addButton(int value, const std::shared_ptr<W_BUTTON>& button);
+	void clearButtons();
+	size_t numButtons() const;
 	void setButtonMinClickInterval(UDWORD interval);
 	void setButtonAlignment(ButtonAlignment alignment);
 	void enable(bool enabled = true);
@@ -60,26 +63,37 @@ public:
 		enable(false);
 	}
 	void setGap(int gap);
+	int getGap() const { return gap_; }
 	void setOuterPaddingX(int left, int right);
 	int currentValue() const
 	{
 		return currentValue_;
 	}
 
+	std::shared_ptr<W_BUTTON> getButtonByValue(int value);
+
+	/* The optional "canChoose" callback function */
+	typedef std::function<bool (MultibuttonWidget& widget, int newValue)> W_CAN_CHOOSE_FUNC;
+
+	void setCanChooseHandler(const W_CAN_CHOOSE_FUNC& canChooseFunc);
+
 	/* The optional "onChoose" callback function */
 	typedef std::function<void (MultibuttonWidget& widget, int newValue)> W_ON_CHOOSE_FUNC;
 
 	void addOnChooseHandler(const W_ON_CHOOSE_FUNC& onChooseFunc);
 
+protected:
+	virtual void stateChanged();
+
 public:
 	void choose(int value);
 
 private:
-	void stateChanged();
 	int32_t widthOfAllButtons() const;
+	void internalHandleButtonClick(int value);
 
 protected:
-	std::shared_ptr<W_LABEL> label;
+	std::shared_ptr<WIDGET> label;
 	std::vector<std::pair<std::shared_ptr<W_BUTTON>, int>> buttons;
 	int currentValue_;
 	bool disabled;
@@ -89,6 +103,7 @@ protected:
 	bool lockCurrent;
 	ButtonAlignment butAlign = ButtonAlignment::RIGHT_ALIGN;
 	std::vector<W_ON_CHOOSE_FUNC> onChooseHandlers;
+	W_CAN_CHOOSE_FUNC canChooseHandler;
 };
 
 class MultichoiceWidget : public MultibuttonWidget
