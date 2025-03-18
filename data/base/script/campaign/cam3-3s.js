ef3b898f41bd1473aac13b1e7ac9da3fbdc64c22
KJeff01 <kjeff1997@gmail.com>
2025-03-18 18:51:26 -0500

    Add bonus Gamma mission tweak option
    Make use of the sub3-3 map as an optional tweak option.
    
    Works as a tower-defense style mission after Gamma 5.

---
+++ data/base/script/campaign/cam3-3s.js
@@ -0,0 +1,18 @@
+include("script/campaign/libcampaign.js");
+
+const mis_Labels = {
+	startPos: {x: 57, y: 119},
+	lz: {x: 55, y: 119, x2: 57, y2: 121},
+	trPlace: {x: 56, y: 120},
+	trExit: {x: 25, y: 87}
+};
+
+function eventStartLevel()
+{
+	camSetupTransporter(mis_Labels.trPlace.x, mis_Labels.trPlace.y, mis_Labels.trExit.x, mis_Labels.trExit.y);
+	centreView(mis_Labels.startPos.x, mis_Labels.startPos.y);
+	setNoGoArea(mis_Labels.lz.x, mis_Labels.lz.y, mis_Labels.lz.x2, mis_Labels.lz.y2, CAM_HUMAN_PLAYER);
+	camSetMissionTimer(camChangeOnDiff(camMinutesToSeconds(50)));
+	camPlayVideos([{video: "MB3_3_MSG", type: CAMP_MSG}]);
+	camSetStandardWinLossConditions(CAM_VICTORY_PRE_OFFWORLD, cam_levels.gammaBonus.offWorld);
+}
