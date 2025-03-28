fd839471c784f64347759edf08afa2f3dc1367ab
KJeff01 <kjeff1997@gmail.com>
2025-03-28 14:36:43 -0500

    Gamma bonus mission and library improvements

--- data/base/script/campaign/libcampaign_includes/events.js
+++ data/base/script/campaign/libcampaign_includes/events.js
@@ -339,10 +339,11 @@ function cam_eventAttacked(victim, attacker)
 			if (camDef(__camGroupInfo[victim.group]))
 			{
 				__camGroupInfo[victim.group].lastHit = gameTime;
-				//Increased Nexus intelligence if struck on cam3-4
-				if (__camNextLevel === CAM_GAMMA_OUT)
+				if (__camGroupInfo[victim.group].order === CAM_ORDER_PATROL)
 				{
-					if (__camGroupInfo[victim.group].order === CAM_ORDER_PATROL)
+					if (camDef(__camGroupInfo[victim.group].data) &&
+						camDef(__camGroupInfo[victim.group].data.reactToAttack) &&
+						__camGroupInfo[victim.group].data.reactToAttack)
 					{
 						__camGroupInfo[victim.group].order = CAM_ORDER_ATTACK;
 					}
