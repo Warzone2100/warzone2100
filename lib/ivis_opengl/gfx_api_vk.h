3f518aef9a8313c3d28620b9b2b1abce9ba05c33
past-due <30942300+past-due@users.noreply.github.com>
2025-05-28 15:31:31 -0400

    [Vulkan] Handle initPixelFormatsSupport failure

--- lib/ivis_opengl/gfx_api_vk.h
+++ lib/ivis_opengl/gfx_api_vk.h
@@ -890,7 +890,7 @@ public:
 	virtual bool debugRecompileAllPipelines() override;
 private:
 	virtual bool _initialize(const gfx_api::backend_Impl_Factory& impl, int32_t antialiasing, swap_interval_mode mode, optional<float> mipLodBias, uint32_t depthMapResolution) override;
-	void initPixelFormatsSupport();
+	bool initPixelFormatsSupport();
 	gfx_api::pixel_format_usage::flags getPixelFormatUsageSupport(gfx_api::pixel_format format) const;
 	std::string calculateFormattedRendererInfoString() const;
 	void set_uniforms_set(const size_t& set_idx, const void* buffer, size_t bufferSize);
