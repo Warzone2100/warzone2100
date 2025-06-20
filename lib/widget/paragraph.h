333e044cdb7e75092d8dfae9b8788f38c7b862e0
past-due <30942300+past-due@users.noreply.github.com>
2025-06-20 16:59:45 -0400

    Additional WzCachedText methods

--- lib/widget/paragraph.h
+++ lib/widget/paragraph.h
@@ -80,6 +80,7 @@ public:
 		}
 		text = _text;
 		font = _fontID;
+		cachedTextWidth.reset();
 		if (cachedText)
 		{
 			cachedText->setText(text, font);
@@ -88,6 +89,26 @@ public:
 
 	inline const WzString& getText() const { return text; }
 	inline iV_fonts getFontID() const { return font; }
+	inline int32_t getTextWidth()
+	{
+		if (!cachedText)
+		{
+			if (!cachedTextWidth.has_value())
+			{
+				cachedTextWidth = iV_GetTextWidth(text, font);
+			}
+			return cachedTextWidth.value();
+		}
+		return cachedText->width();
+	}
+	inline int32_t getTextLineSize() const
+	{
+		if (!cachedText)
+		{
+			return iV_GetTextLineSize(font);
+		}
+		return cachedText->lineSize();
+	}
 
 	WzText *operator ->()
 	{
@@ -103,6 +124,7 @@ public:
 private:
 	WzString text;
 	iV_fonts font;
+	optional<unsigned int> cachedTextWidth = nullopt;
 	uint32_t cacheDurationMs;
 	std::unique_ptr<WzText> cachedText = nullptr;
 	uint32_t cacheExpireAt = 0;
