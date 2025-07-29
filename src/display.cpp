e74baf49ee2fe42b3c244bf1874ea178b9ef2ea9
aco4 <anthonythegu@gmail.com>
2025-07-29 18:28:03 -0400

    Decrease mouse camera pan speed

--- src/display.cpp
+++ src/display.cpp
@@ -1224,7 +1224,7 @@ void displayWorld()
 			int mouseDeltaX = mouseX() - panMouseX;
 			int mouseDeltaY = mouseY() - panMouseY;
 
-			int panningSpeed = war_GetCameraSpeed() / 100;
+			int panningSpeed = war_GetCameraSpeed() / 200;
 
 			float horizontalMovement = panXTracker->setTargetDelta(mouseDeltaX * panningSpeed)->update()->getCurrentDelta();
 			float verticalMovement = -1 * panZTracker->setTargetDelta(mouseDeltaY * panningSpeed)->update()->getCurrentDelta();
