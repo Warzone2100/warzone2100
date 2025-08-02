2bde62b5439f626e8d99add0b0e1f2bdd3748ee9
pastdue <30942300+past-due@users.noreply.github.com>
2025-08-01 20:53:01 -0400

    Update angle vcpkg port to chromium_7258 (#4426)

--- .ci/vcpkg/overlay-ports/angle/cmake-buildsystem/PlatformLinux.cmake
+++ .ci/vcpkg/overlay-ports/angle/cmake-buildsystem/PlatformLinux.cmake
@@ -1,4 +1,4 @@
-list(APPEND ANGLE_DEFINITIONS ANGLE_PLATFORM_LINUX USE_SYSTEM_EGL)
+list(APPEND ANGLE_DEFINITIONS USE_SYSTEM_EGL)
 include(linux.cmake)
 
 if (USE_OPENGL)
@@ -8,7 +8,7 @@ endif ()
 
 if (USE_ANGLE_EGL OR ENABLE_WEBGL)
     list(APPEND ANGLE_SOURCES
-        ${_gl_backend_sources}
+        ${gl_backend_sources}
 
         ${angle_system_utils_sources_linux}
         ${angle_system_utils_sources_posix}
