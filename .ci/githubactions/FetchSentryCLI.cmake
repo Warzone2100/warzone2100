e0185ec7498909e03804a24769268add1780ff5f
past-due <30942300+past-due@users.noreply.github.com>
2025-09-05 11:34:07 -0400

    FetchSentryCLI.cmake: Bump to 2.53.0

--- .ci/githubactions/FetchSentryCLI.cmake
+++ .ci/githubactions/FetchSentryCLI.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.19...3.31)
 
-set(sentry_cli_version "2.46.0") # Note: When updating, must also update all of the sentry_cli_dl_sha512 below!
+set(sentry_cli_version "2.53.0") # Note: When updating, must also update all of the sentry_cli_dl_sha512 below!
 
 # Manually query the CMAKE_HOST_SYSTEM_PROCESSOR
 # See: https://gitlab.kitware.com/cmake/cmake/-/issues/25151
@@ -17,16 +17,16 @@ if(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
   else()
     # just default to x64 otherwise
     set(sentry_cli_dl_url "https://github.com/getsentry/sentry-cli/releases/download/${sentry_cli_version}/sentry-cli-Windows-x86_64.exe")
-    set(sentry_cli_dl_sha512 "1bd5039f1c18c08425a0d001e32f7cad514705c591becd7ab55e0d526f96f6208cde8c0a70a7cf796d2864ce2276255218d5598c9f7c72d4474947cbacd80efc")
+    set(sentry_cli_dl_sha512 "8903da75117f5eab10c08b9b925e6f6442b06685974feb5960e37a91221860a57ca481c50c0bc4eb4e3c5b36d6712a238e209f514487cdd96d7213fa6d893584")
   endif()
   set(_exe_suffix ".exe")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
   set(sentry_cli_dl_url "https://github.com/getsentry/sentry-cli/releases/download/${sentry_cli_version}/sentry-cli-Darwin-universal")
-  set(sentry_cli_dl_sha512 "ca8a0910431ca666c01ba6f3915443aece7b0c4f446e0884600d8beaf809509ff97463b95924651bb259e78f65f9fb656c9b2bc5a7c567fea815c44275f0650f")
+  set(sentry_cli_dl_sha512 "dc9374a26e94f9cb76193dbbe3d29c1baa3e0d2969d1ad2ed09975bdcb7aeb3dc795d8dca745536e8919c3af3b6fdc042649a58174825add0790b34e2852e974")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
   if(CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^x86_64$")
     set(sentry_cli_dl_url "https://github.com/getsentry/sentry-cli/releases/download/${sentry_cli_version}/sentry-cli-Linux-x86_64")
-    set(sentry_cli_dl_sha512 "459f3233038dfdc0f833325e287f9ee156c8a801b486449ad49cc4b71379aafec399da395df7eb3da71a9a0c8e450bc693a8a05256c286bc622496697bd04d9e")
+    set(sentry_cli_dl_sha512 "f9a4f4be6bdb698ca6dd1ca4de7ac95dc7e5360726a36e9c63558692662420e91d86200830d831f6426715087310890e6172c822f5be5e3bc575c2ec0f6d3c2e")
   else()
     message(FATAL_ERROR "Script does not currently support platform: ${CMAKE_HOST_SYSTEM_NAME} and ARCH: ${CMAKE_HOST_SYSTEM_PROCESSOR}")
   endif()
