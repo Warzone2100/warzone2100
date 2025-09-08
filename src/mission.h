cd55975927fc3c25133bd51066d11ae0ed2c319d
past-due <30942300+past-due@users.noreply.github.com>
2025-09-07 21:47:13 -0400

    unloadTransporter: Remove unused goingHome parameter (is always false)

--- src/mission.h
+++ src/mission.h
@@ -102,7 +102,7 @@ void intRemoveMissionResultNoAnim();
 void intProcessMissionResult(UDWORD id);
 void intRunMissionResult();
 
-void unloadTransporter(DROID *psTransporter, UDWORD x, UDWORD y, bool goingHome);
+void unloadTransporter(DROID *psTransporter, UDWORD x, UDWORD y);
 
 /** Sets the appropriate pause states for when the interface is up but the game needs to be paused. */
 void setMissionPauseState();
