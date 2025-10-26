Warzone2100/warzone2100

commit 97660b3ef5d81a29b82af0b7e9fa1532c71c8d84

Bot: NB - Update to improve VTOL Re-arming pads (#4399)

data/mp/multiplay/skirmish/nb_includes/build.js
@@ -301,7 +301,7 @@ function buildModules() {
 }
 
 _global.buildVtols = function() {
-	if (buildMinimum(structures.vtolPads, enumDroid(me, DROID_WEAPON).filter(isVTOL).length / 2), IMPORTANCE.PEACETIME)
+	if (buildMinimum(structures.vtolPads, enumDroid(me, DROID_WEAPON).filter(isVTOL).length), IMPORTANCE.MANDATORY)
 		return true;
 	return false;
 }

commit f8b59d3e5348378f30699df7aaeb9cbeb557cb21

Fix Turtle AI defense building
Restores the defense building behaviour of Turtle AI (and other Nullbot variants).

Commit 97660b3 inadvertently broke this due to some preexisting misplaced brackets.

I have changed the importance of building VTOL rearming pads to PEACETIME rather than MANDATORY, so that Nullbots won't try to build rearming pads in dangerous areas.

data/mp/multiplay/skirmish/nb_includes/build.js
@@ -301,7 +301,7 @@ function buildModules() {
 }
 
 _global.buildVtols = function() {
-	if (buildMinimum(structures.vtolPads, enumDroid(me, DROID_WEAPON).filter(isVTOL).length), IMPORTANCE.MANDATORY)
+	if (buildMinimum(structures.vtolPads, enumDroid(me, DROID_WEAPON).filter(isVTOL).length, IMPORTANCE.PEACETIME))
 		return true;
 	return false;
 }