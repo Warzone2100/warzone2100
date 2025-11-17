50fc98bdf277af8cb484f9b607b3659e4e3a4921
aco4 <anthonythegu@gmail.com>
2025-11-17 05:24:19 -0500

    Optimize performance
    Pass stattype instead of entire structure object, which may improve performance as per @KJeff01. Also increase the verbosity of the function names to help mitigate global scope pollution

--- data/mp/multiplay/script/rules/setup/base.js
+++ data/mp/multiplay/script/rules/setup/base.js
@@ -4,13 +4,13 @@ function setupBase(player)	// inside hackNetOff()
 	{
 		setPower(1300, player);
 		completeResearchOnTime(cleanTech, player);
-		removeStructs(player);
+		removeBaseStructures(player);
 	}
 	else if (baseType === CAMP_BASE)
 	{
 		setPower(2500, player);
 		completeResearchOnTime(timeBaseTech, player);
-		removeStructs(player);
+		removeBaseStructures(player);
 	}
 	else // CAMP_WALLS
 	{
@@ -19,44 +19,44 @@ function setupBase(player)	// inside hackNetOff()
 	}
 }
 
-function removeStructs(player)
+function removeBaseStructures(player)
 {
 	for (const structure of enumStruct(player))
 	{
-		if (shouldRemove(structure, player))
+		if (shouldRemoveBaseStructure(structure.stattype, player))
 		{
 			removeObject(structure);
 		}
 	}
 }
 
-function shouldRemove(structure, player)
+function shouldRemoveBaseStructure(stattype, player)
 {
 	if (baseType === CAMP_CLEAN && playerData[player].difficulty !== INSANE)
 	{
 		return true; // remove everything
 	}
-	if (baseType === CAMP_CLEAN && playerData[player].difficulty === INSANE)
+	else if (baseType === CAMP_CLEAN && playerData[player].difficulty === INSANE)
 	{
-		return structure.stattype !== WALL
-			&& structure.stattype !== DEFENSE
-			&& structure.stattype !== GATE
-			&& structure.stattype !== RESOURCE_EXTRACTOR;
+		return stattype !== WALL
+			&& stattype !== DEFENSE
+			&& stattype !== GATE
+			&& stattype !== RESOURCE_EXTRACTOR;
 	}
 	else if (baseType === CAMP_BASE && playerData[player].difficulty !== INSANE)
 	{
-		return structure.stattype === WALL
-			|| structure.stattype === DEFENSE
-			|| structure.stattype === GATE
-			|| structure.stattype === CYBORG_FACTORY
-			|| structure.stattype === COMMAND_CONTROL;
+		return stattype === WALL
+			|| stattype === DEFENSE
+			|| stattype === GATE
+			|| stattype === CYBORG_FACTORY
+			|| stattype === COMMAND_CONTROL;
 	}
 	else if (baseType === CAMP_BASE && playerData[player].difficulty === INSANE)
 	{
-		return structure.stattype === CYBORG_FACTORY
-			|| structure.stattype === COMMAND_CONTROL;
+		return stattype === CYBORG_FACTORY
+			|| stattype === COMMAND_CONTROL;
 	}
-	else
+	else // CAMP_WALLS
 	{
 		return false; // don't remove anything
 	}
