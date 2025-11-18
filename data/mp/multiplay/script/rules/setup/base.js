8a61dbab079846fd6cc6ea59bc6ef142da8c1586
aco4 <53398545+aco4@users.noreply.github.com>
2025-11-18 02:42:34 -0500

    Let Insane difficulty start with gates (#4640)

--- data/mp/multiplay/script/rules/setup/base.js
+++ data/mp/multiplay/script/rules/setup/base.js
@@ -4,38 +4,60 @@ function setupBase(player)	// inside hackNetOff()
 	{
 		setPower(1300, player);
 		completeResearchOnTime(cleanTech, player);
-		// Keep only some structures for insane AI
-		var structs = enumStruct(player);
-		for (let i = 0; i < structs.length; ++i)
-		{
-			var s = structs[i];
-			if (playerData[player].difficulty !== INSANE
-				|| (s.stattype !== WALL && s.stattype !== DEFENSE && s.stattype !== GATE
-					&& s.stattype !== RESOURCE_EXTRACTOR))
-			{
-				removeObject(s, false);
-			}
-		}
+		removeBaseStructures(player);
 	}
 	else if (baseType === CAMP_BASE)
 	{
 		setPower(2500, player);
 		completeResearchOnTime(timeBaseTech, player);
-		// Keep only some structures
-		var structs = enumStruct(player);
-		for (let i = 0; i < structs.length; ++i)
-		{
-			var s = structs[i];
-			if ((playerData[player].difficulty !== INSANE && (s.stattype === WALL || s.stattype === DEFENSE))
-				|| s.stattype === GATE || s.stattype === CYBORG_FACTORY || s.stattype === COMMAND_CONTROL)
-			{
-				removeObject(s, false);
-			}
-		}
+		removeBaseStructures(player);
 	}
 	else // CAMP_WALLS
 	{
 		setPower(2500, player);
 		completeResearchOnTime(timeAdvancedBaseTech, player);
+	}
+}
+
+function removeBaseStructures(player)
+{
+	for (const structure of enumStruct(player))
+	{
+		if (shouldRemoveBaseStructure(structure.stattype, player))
+		{
+			removeObject(structure);
+		}
+	}
+}
+
+function shouldRemoveBaseStructure(stattype, player)
+{
+	if (baseType === CAMP_CLEAN && playerData[player].difficulty !== INSANE)
+	{
+		return true; // remove everything
+	}
+	else if (baseType === CAMP_CLEAN && playerData[player].difficulty === INSANE)
+	{
+		return stattype !== WALL
+			&& stattype !== DEFENSE
+			&& stattype !== GATE
+			&& stattype !== RESOURCE_EXTRACTOR;
+	}
+	else if (baseType === CAMP_BASE && playerData[player].difficulty !== INSANE)
+	{
+		return stattype === WALL
+			|| stattype === DEFENSE
+			|| stattype === GATE
+			|| stattype === CYBORG_FACTORY
+			|| stattype === COMMAND_CONTROL;
+	}
+	else if (baseType === CAMP_BASE && playerData[player].difficulty === INSANE)
+	{
+		return stattype === CYBORG_FACTORY
+			|| stattype === COMMAND_CONTROL;
+	}
+	else // CAMP_WALLS
+	{
+		return false; // don't remove anything
 	}
 }
