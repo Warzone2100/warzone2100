611502ade35a8be3af2e81e088c62cb77d509b6f
aubergineagain <aubergine18@proton.me>
2026-02-08 00:44:10 +0000

    Add `syncString()` and `eventSyncString()`
    Adds an alternative to `syncRequest()` and `eventSyncRequest()` which instead of passing x,y,obj,obj, passes a string.

--- src/multiplay.h
+++ src/multiplay.h
@@ -402,6 +402,7 @@ STRUCTURE *findResearchingFacilityByResearchIndex(const PerPlayerStructureLists&
 STRUCTURE *findResearchingFacilityByResearchIndex(unsigned player, unsigned index); // checks apsStructLists
 
 void sendSyncRequest(int32_t req_id, int32_t x, int32_t y, const BASE_OBJECT *psObj, const BASE_OBJECT *psObj2);
+void sendSyncString(int32_t req_id, const WzString& str);
 
 
 bool sendBeaconToPlayer(SDWORD locX, SDWORD locY, SDWORD forPlayer, SDWORD sender, const char *beaconMsg);
