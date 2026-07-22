6457b2cbcd1691b386a496b3266a007e4f5ae5a4
aco4 <anthonythegu@gmail.com>
2026-07-22 07:07:09 -0400

    Allow clicks to pass through the console history overlay

--- src/display.cpp
+++ src/display.cpp
@@ -710,7 +710,7 @@ void processMouseClickInput()
 		return;
 	}
 
-	if (mouseReleased(MOUSE_LMB) && !OverRadar && dragBox3D.status != DRAG_RELEASED && !ignoreOrder && !mouseOverConsole && !bDisplayMultiJoiningStatus)
+	if (mouseReleased(MOUSE_LMB) && !OverRadar && dragBox3D.status != DRAG_RELEASED && !ignoreOrder && !bDisplayMultiJoiningStatus)
 	{
 		if (bRightClickOrders)
 		{
