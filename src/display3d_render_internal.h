ff251ba63f2e98c5337a50c046ca4afe7024116a
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2026-08-14 21:30:07 +0300

    ssao: Fill SSAO UV clamps from the sampled pipeline surface
    Add `fillSurfaceUvScaleClamp` so used/alloc comes from the catalog extent of
    the texture being sampled. Generate, fog, and blur depth use `ScenePrepassDepth`;
    downsample uses `SSAORaw` instead of a hand-rolled scene/divisor size.
    
    Signed-off-by: Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>

--- src/display3d_render_internal.h
+++ src/display3d_render_internal.h
@@ -1,14 +1,10 @@
 #pragma once
 
+#include "lib/ivis_opengl/gfx_api.h"
+
 #include <glm/fwd.hpp>
 #include <glm/vec4.hpp>
 #include <cstdint>
-
-namespace gfx_api
-{
-struct abstract_texture;
-struct buffer;
-}
 
 void display3d_renderSurroundings(const glm::mat4& projectionMatrix, const glm::mat4& skyboxViewMatrix);
 void display3d_doConstructionLines(const glm::mat4& viewMatrix);
@@ -25,5 +21,7 @@ void display3d_processDestinationTarget();
 gfx_api::buffer* display3d_getScreenTriangleVBO();
 /// Scale/clamp UVs from `sourceTex` onto a used sub-rect of size usedW x usedH.
 void display3d_fillUvScaleClamp(uint32_t usedW, uint32_t usedH, gfx_api::abstract_texture* sourceTex, glm::vec4& uvScaleClamp);
+/// Scale/clamp UVs from pipeline surface `id` onto that surface's used (dyn-res) extent.
+void display3d_fillSurfaceUvScaleClamp(gfx_api::PipelineSurfaceId id, glm::vec4& uvScaleClamp);
 /// Scale/clamp UVs from a scene-sized source texture onto the rendered scene sub-rect.
 void display3d_fillSceneUvScaleClamp(gfx_api::abstract_texture* sourceTex, glm::vec4& uvScaleClamp);
